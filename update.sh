18f4e315bcfd441bb56e25e6c51906617a17daea
Jean-Baptiste Giraudeau <jb@giraudeau.info>
2019-01-16 09:43:34 +0100

    more robust update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # make sure the hackage index is recent.
 cabal new-update
-$(find $NIX_TOOLS -type f -name "hackage-to-nix") .
+$(find -L $NIX_TOOLS -type f -name "hackage-to-nix") .
