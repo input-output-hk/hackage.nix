3159379905a8b78a512791cbf86179ac8f63126d
IOHK <devops+nix-tools@iohk.io>
2019-05-17 01:09:39 +0000

    Automatic update for Fri May 17 01:09:39 UTC 2019

---
+++ hackage/primitive-stablename-0.1-r0-dc9aeb27861a17726faa3a9eff1cbba64d2b558ab09799524063b4e25607f1dc.nix
@@ -0,0 +1,20 @@
+{ system, compiler, flags, pkgs, hsPkgs, pkgconfPkgs, ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "2.2";
+      identifier = { name = "primitive-stablename"; version = "0.1"; };
+      license = "BSD-3-Clause";
+      copyright = "© 2019 chessai";
+      maintainer = "chessai <chessai1996@gmail.com>";
+      author = "chessai";
+      homepage = "https://github.com/chessai/primitive-stablename";
+      url = "";
+      synopsis = "primitive operations on StableNames";
+      description = "Primitive operations on @'Data.Primitive.StableName'@s. This differs from the module\n'System.Mem.StableName', in that it works in 'Control.Monad.ST.ST', 'IO', and any monad\ntransformer stack built on top of either. It is recommended that users\nread the documentation of 'System.Mem.StableName' to learn more about\nthe properties of @'Data.Primitive.StableName'@s.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = { depends = [ (hsPkgs.base) (hsPkgs.primitive) ]; };
+      };
+    }
---
+++ hackage/simplistic-generics-0.1.0.0-r0-8c4d6b1749c2d1e2f6d400d92ebed63d11974569f43d7ada684914e9b1821181.nix
@@ -0,0 +1,22 @@
+{ system, compiler, flags, pkgs, hsPkgs, pkgconfPkgs, ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "simplistic-generics"; version = "0.1.0.0"; };
+      license = "BSD-3-Clause";
+      copyright = "";
+      maintainer = "trupill@gmail.com";
+      author = "Alejandro Serrano";
+      homepage = "";
+      url = "";
+      synopsis = "Generic programming without too many type classes";
+      description = "This library provides a representation build on top of\n`GHC.Generics`, which can be used to describe generic\noperations on a single function, instead of having\neach case defined in an instance of a type class.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [ (hsPkgs.base) (hsPkgs.kind-apply) (hsPkgs.comonad) ];
+        };
+      };
+    }
