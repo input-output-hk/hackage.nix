ea26e9a159960126634defe3f8ecf0161c4e260a
IOHK <devops+nix-tools@iohk.io>
2019-08-03 01:07:40 +0000

    Automatic update for Sat Aug  3 01:07:39 UTC 2019

---
+++ hackage/postgresql-lo-stream-0.1.1.1-r0-f12366dc7dc79936145696bd7fb6e0ba7defe634a5d3c54bd21a017216840a2b.nix
@@ -0,0 +1,30 @@
+{ system, compiler, flags, pkgs, hsPkgs, pkgconfPkgs, ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "postgresql-lo-stream"; version = "0.1.1.1"; };
+      license = "BSD-3-Clause";
+      copyright = "2019 Obsidian Systems LLC";
+      maintainer = "maintainer@obsidian.systems";
+      author = "Obsidian Systems LLC";
+      homepage = "https://github.com/obsidiansystems/posgresql-lo-stream";
+      url = "";
+      synopsis = "Utilities for streaming PostgreSQL LargeObjects";
+      description = "Functions for streaming large objects to and from PostgreSQL";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs.base)
+          (hsPkgs.bytestring)
+          (hsPkgs.io-streams)
+          (hsPkgs.lifted-base)
+          (hsPkgs.monad-loops)
+          (hsPkgs.mtl)
+          (hsPkgs.postgresql-simple)
+          ];
+        };
+      };
+    }
