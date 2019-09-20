c7d40a8f19ad55c3ed29253739a4117e4cea7152
IOHK <devops+nix-tools@iohk.io>
2019-09-20 01:05:51 +0000

    Automatic update for Fri Sep 20 01:05:51 UTC 2019

---
+++ hackage/aeson-schemas-1.0.0-r0-44206234736db100b3247bfaba35adf9f8aa796de4c2b011a2d1b22094c528c6.nix
@@ -0,0 +1,89 @@
+let
+  buildDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (build dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  sysDepError = pkg:
+    builtins.throw ''
+      The Nixpkgs package set does not contain the package: ${pkg} (system dependency).
+      
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      '';
+  pkgConfDepError = pkg:
+    builtins.throw ''
+      The pkg-conf packages does not contain the package: ${pkg} (pkg-conf dependency).
+      
+      You may need to augment the pkg-conf package mapping in haskell.nix so that it can be found.
+      '';
+  exeDepError = pkg:
+    builtins.throw ''
+      The local executable components do not include the component: ${pkg} (executable dependency).
+      '';
+  legacyExeDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (executable dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  buildToolDepError = pkg:
+    builtins.throw ''
+      Neither the Haskell package set or the Nixpkgs package set contain the package: ${pkg} (build tool dependency).
+      
+      If this is a system dependency:
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      
+      If this is a Haskell dependency:
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+in { system, compiler, flags, pkgs, hsPkgs, pkgconfPkgs, ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "aeson-schemas"; version = "1.0.0"; };
+      license = "BSD-3-Clause";
+      copyright = "";
+      maintainer = "Brandon Chinn <brandon@leapyear.io>";
+      author = "Brandon Chinn <brandon@leapyear.io>";
+      homepage = "https://github.com/LeapYear/aeson-schemas#readme";
+      url = "";
+      synopsis = "Easily consume JSON data on-demand with type-safety";
+      description = "Parse JSON data easily and safely without defining new data types. Useful\nfor deeply nested JSON data, which is difficult to parse using the default\nFromJSON instances.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."aeson" or (buildDepError "aeson"))
+          (hsPkgs."base" or (buildDepError "base"))
+          (hsPkgs."bytestring" or (buildDepError "bytestring"))
+          (hsPkgs."first-class-families" or (buildDepError "first-class-families"))
+          (hsPkgs."megaparsec" or (buildDepError "megaparsec"))
+          (hsPkgs."template-haskell" or (buildDepError "template-haskell"))
+          (hsPkgs."text" or (buildDepError "text"))
+          (hsPkgs."unordered-containers" or (buildDepError "unordered-containers"))
+          ];
+        };
+      tests = {
+        "aeson-schemas-test" = {
+          depends = [
+            (hsPkgs."aeson" or (buildDepError "aeson"))
+            (hsPkgs."aeson-schemas" or (buildDepError "aeson-schemas"))
+            (hsPkgs."base" or (buildDepError "base"))
+            (hsPkgs."bytestring" or (buildDepError "bytestring"))
+            (hsPkgs."first-class-families" or (buildDepError "first-class-families"))
+            (hsPkgs."megaparsec" or (buildDepError "megaparsec"))
+            (hsPkgs."raw-strings-qq" or (buildDepError "raw-strings-qq"))
+            (hsPkgs."tasty" or (buildDepError "tasty"))
+            (hsPkgs."tasty-golden" or (buildDepError "tasty-golden"))
+            (hsPkgs."template-haskell" or (buildDepError "template-haskell"))
+            (hsPkgs."text" or (buildDepError "text"))
+            (hsPkgs."th-test-utils" or (buildDepError "th-test-utils"))
+            (hsPkgs."unordered-containers" or (buildDepError "unordered-containers"))
+            ];
+          };
+        };
+      };
+    }
---
+++ hackage/purebred-email-0.3.0.0-r0-5aa964cf7c4d17bb66b152ac80ca65571c249dea601249312b632d3abb873e01.nix
@@ -0,0 +1,107 @@
+let
+  buildDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (build dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  sysDepError = pkg:
+    builtins.throw ''
+      The Nixpkgs package set does not contain the package: ${pkg} (system dependency).
+      
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      '';
+  pkgConfDepError = pkg:
+    builtins.throw ''
+      The pkg-conf packages does not contain the package: ${pkg} (pkg-conf dependency).
+      
+      You may need to augment the pkg-conf package mapping in haskell.nix so that it can be found.
+      '';
+  exeDepError = pkg:
+    builtins.throw ''
+      The local executable components do not include the component: ${pkg} (executable dependency).
+      '';
+  legacyExeDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (executable dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  buildToolDepError = pkg:
+    builtins.throw ''
+      Neither the Haskell package set or the Nixpkgs package set contain the package: ${pkg} (build tool dependency).
+      
+      If this is a system dependency:
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      
+      If this is a Haskell dependency:
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+in { system, compiler, flags, pkgs, hsPkgs, pkgconfPkgs, ... }:
+  {
+    flags = { demos = false; };
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "purebred-email"; version = "0.3.0.0"; };
+      license = "AGPL-3.0-only";
+      copyright = "Copyright 2017-2019  Fraser Tweedale";
+      maintainer = "frase@frase.id.au";
+      author = "Fraser Tweedale";
+      homepage = "https://github.com/purebred-mua/purebred-email";
+      url = "";
+      synopsis = "types and parser for email messages (including MIME)";
+      description = "The purebred email library.  RFC 5322, MIME, etc.\n\nThings that are currently implemented:\n\n- RFC 5322 message parsing and serialisation\n- MIME multipart parsing (RFC 2046) and serialisation\n- MIME message header extensions for non-ASCII text (RFC 2047)\n- MIME parameter value and encoded word extensions (RFC 2231)\n- Content-Disposition header field (RFC 2183)\n- Message encapsulation (forward/bounce)\n\nThings that are not yet implemented / need improvement:\n\n- Improve handling of dates\n- Probably lots of other things";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (buildDepError "base"))
+          (hsPkgs."attoparsec" or (buildDepError "attoparsec"))
+          (hsPkgs."base64-bytestring" or (buildDepError "base64-bytestring"))
+          (hsPkgs."bytestring" or (buildDepError "bytestring"))
+          (hsPkgs."case-insensitive" or (buildDepError "case-insensitive"))
+          (hsPkgs."concise" or (buildDepError "concise"))
+          (hsPkgs."deepseq" or (buildDepError "deepseq"))
+          (hsPkgs."lens" or (buildDepError "lens"))
+          (hsPkgs."semigroupoids" or (buildDepError "semigroupoids"))
+          (hsPkgs."semigroups" or (buildDepError "semigroups"))
+          (hsPkgs."stringsearch" or (buildDepError "stringsearch"))
+          (hsPkgs."text" or (buildDepError "text"))
+          (hsPkgs."time" or (buildDepError "time"))
+          ];
+        };
+      exes = {
+        "purebred-email-parse" = {
+          depends = [
+            (hsPkgs."base" or (buildDepError "base"))
+            (hsPkgs."attoparsec" or (buildDepError "attoparsec"))
+            (hsPkgs."bytestring" or (buildDepError "bytestring"))
+            (hsPkgs."semigroups" or (buildDepError "semigroups"))
+            (hsPkgs."purebred-email" or (buildDepError "purebred-email"))
+            ];
+          };
+        };
+      tests = {
+        "tests" = {
+          depends = [
+            (hsPkgs."base" or (buildDepError "base"))
+            (hsPkgs."bytestring" or (buildDepError "bytestring"))
+            (hsPkgs."lens" or (buildDepError "lens"))
+            (hsPkgs."text" or (buildDepError "text"))
+            (hsPkgs."purebred-email" or (buildDepError "purebred-email"))
+            (hsPkgs."tasty" or (buildDepError "tasty"))
+            (hsPkgs."tasty-quickcheck" or (buildDepError "tasty-quickcheck"))
+            (hsPkgs."tasty-hunit" or (buildDepError "tasty-hunit"))
+            (hsPkgs."tasty-golden" or (buildDepError "tasty-golden"))
+            (hsPkgs."attoparsec" or (buildDepError "attoparsec"))
+            (hsPkgs."QuickCheck" or (buildDepError "QuickCheck"))
+            (hsPkgs."quickcheck-instances" or (buildDepError "quickcheck-instances"))
+            (hsPkgs."case-insensitive" or (buildDepError "case-insensitive"))
+            (hsPkgs."semigroups" or (buildDepError "semigroups"))
+            (hsPkgs."time" or (buildDepError "time"))
+            ];
+          };
+        };
+      };
+    }
