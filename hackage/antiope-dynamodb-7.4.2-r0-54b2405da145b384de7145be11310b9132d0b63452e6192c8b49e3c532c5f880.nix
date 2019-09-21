a3a334663474b49309971af87b67ac0c629acd79
IOHK <devops+nix-tools@iohk.io>
2019-09-21 01:06:09 +0000

    Automatic update for Sat Sep 21 01:06:09 UTC 2019

---
+++ hackage/antiope-dynamodb-7.4.2-r0-54b2405da145b384de7145be11310b9132d0b63452e6192c8b49e3c532c5f880.nix
@@ -0,0 +1,93 @@
+let
+  buildDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (build dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  sysDepError = pkg:
+    builtins.throw ''
+      The Nixpkgs package set does not contain the package: ${pkg} (system dependency).
+      
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      '';
+  pkgConfDepError = pkg:
+    builtins.throw ''
+      The pkg-conf packages does not contain the package: ${pkg} (pkg-conf dependency).
+      
+      You may need to augment the pkg-conf package mapping in haskell.nix so that it can be found.
+      '';
+  exeDepError = pkg:
+    builtins.throw ''
+      The local executable components do not include the component: ${pkg} (executable dependency).
+      '';
+  legacyExeDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (executable dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  buildToolDepError = pkg:
+    builtins.throw ''
+      Neither the Haskell package set or the Nixpkgs package set contain the package: ${pkg} (build tool dependency).
+      
+      If this is a system dependency:
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      
+      If this is a Haskell dependency:
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+in { system, compiler, flags, pkgs, hsPkgs, pkgconfPkgs, ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "2.4";
+      identifier = { name = "antiope-dynamodb"; version = "7.4.2"; };
+      license = "MIT";
+      copyright = "Arbor Networks";
+      maintainer = "mayhem@arbor.net";
+      author = "Arbor Networks";
+      homepage = "https://github.com/arbor/antiope#readme";
+      url = "";
+      synopsis = "Please see the README on Github at <https://github.com/arbor/antiope#readme>";
+      description = "Please see the README on Github at <https://github.com/arbor/antiope#readme>.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (buildDepError "base"))
+          (hsPkgs."aeson" or (buildDepError "aeson"))
+          (hsPkgs."amazonka" or (buildDepError "amazonka"))
+          (hsPkgs."amazonka-core" or (buildDepError "amazonka-core"))
+          (hsPkgs."amazonka-dynamodb" or (buildDepError "amazonka-dynamodb"))
+          (hsPkgs."antiope-core" or (buildDepError "antiope-core"))
+          (hsPkgs."generic-lens" or (buildDepError "generic-lens"))
+          (hsPkgs."lens" or (buildDepError "lens"))
+          (hsPkgs."text" or (buildDepError "text"))
+          (hsPkgs."unliftio-core" or (buildDepError "unliftio-core"))
+          (hsPkgs."unordered-containers" or (buildDepError "unordered-containers"))
+          ];
+        };
+      tests = {
+        "antiope-dynamodb-test" = {
+          depends = [
+            (hsPkgs."base" or (buildDepError "base"))
+            (hsPkgs."amazonka" or (buildDepError "amazonka"))
+            (hsPkgs."amazonka-core" or (buildDepError "amazonka-core"))
+            (hsPkgs."amazonka-dynamodb" or (buildDepError "amazonka-dynamodb"))
+            (hsPkgs."antiope-core" or (buildDepError "antiope-core"))
+            (hsPkgs."antiope-dynamodb" or (buildDepError "antiope-dynamodb"))
+            (hsPkgs."generic-lens" or (buildDepError "generic-lens"))
+            (hsPkgs."lens" or (buildDepError "lens"))
+            (hsPkgs."text" or (buildDepError "text"))
+            (hsPkgs."unliftio-core" or (buildDepError "unliftio-core"))
+            (hsPkgs."unordered-containers" or (buildDepError "unordered-containers"))
+            ];
+          build-tools = [
+            (hsPkgs.buildPackages.hspec-discover or (pkgs.buildPackages.hspec-discover or (buildToolDepError "hspec-discover")))
+            ];
+          };
+        };
+      };
+    }
---
+++ hackage/fcm-client-0.2.0.0-r0-6b67f0d23a09504f763ef547306b015b93af376e922583c7e3a4a7f494f85539.nix
@@ -0,0 +1,116 @@
+let
+  buildDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (build dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  sysDepError = pkg:
+    builtins.throw ''
+      The Nixpkgs package set does not contain the package: ${pkg} (system dependency).
+      
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      '';
+  pkgConfDepError = pkg:
+    builtins.throw ''
+      The pkg-conf packages does not contain the package: ${pkg} (pkg-conf dependency).
+      
+      You may need to augment the pkg-conf package mapping in haskell.nix so that it can be found.
+      '';
+  exeDepError = pkg:
+    builtins.throw ''
+      The local executable components do not include the component: ${pkg} (executable dependency).
+      '';
+  legacyExeDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (executable dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  buildToolDepError = pkg:
+    builtins.throw ''
+      Neither the Haskell package set or the Nixpkgs package set contain the package: ${pkg} (build tool dependency).
+      
+      If this is a system dependency:
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      
+      If this is a Haskell dependency:
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+in { system, compiler, flags, pkgs, hsPkgs, pkgconfPkgs, ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "2.4";
+      identifier = { name = "fcm-client"; version = "0.2.0.0"; };
+      license = "BSD-3-Clause";
+      copyright = "2016-2018 Andrey Kartashov, 2018-2019 Holmusk";
+      maintainer = "Holmusk <tech@holmusk.com>";
+      author = "Andrey Kartashov, Holmusk";
+      homepage = "https://github.com/holmusk/fcm-client#readme";
+      url = "";
+      synopsis = "Admin API for Firebase Cloud Messaging";
+      description = "This package provides data type model and functions to call FCM endpoints.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (buildDepError "base"))
+          (hsPkgs."aeson" or (buildDepError "aeson"))
+          (hsPkgs."aeson-casing" or (buildDepError "aeson-casing"))
+          (hsPkgs."bytestring" or (buildDepError "bytestring"))
+          (hsPkgs."containers" or (buildDepError "containers"))
+          (hsPkgs."data-default-class" or (buildDepError "data-default-class"))
+          (hsPkgs."http-client" or (buildDepError "http-client"))
+          (hsPkgs."http-conduit" or (buildDepError "http-conduit"))
+          (hsPkgs."http-types" or (buildDepError "http-types"))
+          (hsPkgs."lens" or (buildDepError "lens"))
+          (hsPkgs."scientific" or (buildDepError "scientific"))
+          (hsPkgs."text" or (buildDepError "text"))
+          (hsPkgs."time" or (buildDepError "time"))
+          ];
+        };
+      exes = {
+        "fcm-client" = {
+          depends = [
+            (hsPkgs."base" or (buildDepError "base"))
+            (hsPkgs."aeson" or (buildDepError "aeson"))
+            (hsPkgs."async" or (buildDepError "async"))
+            (hsPkgs."data-default-class" or (buildDepError "data-default-class"))
+            (hsPkgs."bytestring" or (buildDepError "bytestring"))
+            (hsPkgs."conduit" or (buildDepError "conduit"))
+            (hsPkgs."conduit-extra" or (buildDepError "conduit-extra"))
+            (hsPkgs."fcm-client" or (buildDepError "fcm-client"))
+            (hsPkgs."http-client" or (buildDepError "http-client"))
+            (hsPkgs."http-types" or (buildDepError "http-types"))
+            (hsPkgs."lens" or (buildDepError "lens"))
+            (hsPkgs."resourcet" or (buildDepError "resourcet"))
+            (hsPkgs."retry" or (buildDepError "retry"))
+            (hsPkgs."optparse-applicative" or (buildDepError "optparse-applicative"))
+            (hsPkgs."stm-conduit" or (buildDepError "stm-conduit"))
+            (hsPkgs."text" or (buildDepError "text"))
+            (hsPkgs."utf8-string" or (buildDepError "utf8-string"))
+            ];
+          };
+        };
+      tests = {
+        "test" = {
+          depends = [
+            (hsPkgs."base" or (buildDepError "base"))
+            (hsPkgs."aeson" or (buildDepError "aeson"))
+            (hsPkgs."containers" or (buildDepError "containers"))
+            (hsPkgs."data-default-class" or (buildDepError "data-default-class"))
+            (hsPkgs."fcm-client" or (buildDepError "fcm-client"))
+            (hsPkgs."HUnit" or (buildDepError "HUnit"))
+            (hsPkgs."lens" or (buildDepError "lens"))
+            (hsPkgs."QuickCheck" or (buildDepError "QuickCheck"))
+            (hsPkgs."test-framework" or (buildDepError "test-framework"))
+            (hsPkgs."test-framework-hunit" or (buildDepError "test-framework-hunit"))
+            (hsPkgs."test-framework-quickcheck2" or (buildDepError "test-framework-quickcheck2"))
+            (hsPkgs."text" or (buildDepError "text"))
+            ];
+          };
+        };
+      };
+    }
