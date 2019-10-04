42a4f3b83ca8e69e83879ebe022b66f30d2b3ba2
IOHK <devops+nix-tools@iohk.io>
2019-10-04 01:06:34 +0000

    Automatic update for Fri Oct  4 01:06:33 UTC 2019

---
+++ hackage/hhp-0.0.2-r0-b0bc081521b26952b400365a8ef50882127ea87ce51094ad7d0beb23e57959d1.nix
@@ -0,0 +1,124 @@
+let
+  buildDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (build dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  sysDepError = pkg:
+    builtins.throw ''
+      The Nixpkgs package set does not contain the package: ${pkg} (system dependency).
+      
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      '';
+  pkgConfDepError = pkg:
+    builtins.throw ''
+      The pkg-conf packages does not contain the package: ${pkg} (pkg-conf dependency).
+      
+      You may need to augment the pkg-conf package mapping in haskell.nix so that it can be found.
+      '';
+  exeDepError = pkg:
+    builtins.throw ''
+      The local executable components do not include the component: ${pkg} (executable dependency).
+      '';
+  legacyExeDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (executable dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  buildToolDepError = pkg:
+    builtins.throw ''
+      Neither the Haskell package set or the Nixpkgs package set contain the package: ${pkg} (build tool dependency).
+      
+      If this is a system dependency:
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      
+      If this is a Haskell dependency:
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+in { system, compiler, flags, pkgs, hsPkgs, pkgconfPkgs, ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "hhp"; version = "0.0.2"; };
+      license = "BSD-3-Clause";
+      copyright = "";
+      maintainer = "Kazu Yamamoto <kazu@iij.ad.jp>";
+      author = "Kazu Yamamoto <kazu@iij.ad.jp>";
+      homepage = "https://github.com/kazu-yamamoto/hhp";
+      url = "";
+      synopsis = "Happy Haskell Programming";
+      description = "The hhp command is a backend command to enrich\nHaskell programming on editors.\nThe hhpc/hhpi commands are based on HHP library\nwhich is a wrapper of GHC API and Cabal API.\nThis package includes the hhpc command,\nthe hhpi command,\nthe HHP library, and Emacs front-end.\nFor more information, please see its home page.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (buildDepError "base"))
+          (hsPkgs."Cabal" or (buildDepError "Cabal"))
+          (hsPkgs."containers" or (buildDepError "containers"))
+          (hsPkgs."deepseq" or (buildDepError "deepseq"))
+          (hsPkgs."directory" or (buildDepError "directory"))
+          (hsPkgs."filepath" or (buildDepError "filepath"))
+          (hsPkgs."ghc" or (buildDepError "ghc"))
+          (hsPkgs."hlint" or (buildDepError "hlint"))
+          (hsPkgs."process" or (buildDepError "process"))
+          (hsPkgs."syb" or (buildDepError "syb"))
+          (hsPkgs."ghc-boot" or (buildDepError "ghc-boot"))
+          ];
+        buildable = true;
+        };
+      exes = {
+        "hhpc" = {
+          depends = [
+            (hsPkgs."base" or (buildDepError "base"))
+            (hsPkgs."directory" or (buildDepError "directory"))
+            (hsPkgs."filepath" or (buildDepError "filepath"))
+            (hsPkgs."ghc" or (buildDepError "ghc"))
+            (hsPkgs."hhp" or (buildDepError "hhp"))
+            ];
+          buildable = true;
+          };
+        "hhpi" = {
+          depends = [
+            (hsPkgs."base" or (buildDepError "base"))
+            (hsPkgs."containers" or (buildDepError "containers"))
+            (hsPkgs."directory" or (buildDepError "directory"))
+            (hsPkgs."filepath" or (buildDepError "filepath"))
+            (hsPkgs."ghc" or (buildDepError "ghc"))
+            (hsPkgs."hhp" or (buildDepError "hhp"))
+            ];
+          buildable = true;
+          };
+        };
+      tests = {
+        "doctest" = {
+          depends = [
+            (hsPkgs."base" or (buildDepError "base"))
+            (hsPkgs."doctest" or (buildDepError "doctest"))
+            (hsPkgs."hlint" or (buildDepError "hlint"))
+            ];
+          buildable = true;
+          };
+        "spec" = {
+          depends = [
+            (hsPkgs."base" or (buildDepError "base"))
+            (hsPkgs."Cabal" or (buildDepError "Cabal"))
+            (hsPkgs."containers" or (buildDepError "containers"))
+            (hsPkgs."deepseq" or (buildDepError "deepseq"))
+            (hsPkgs."directory" or (buildDepError "directory"))
+            (hsPkgs."filepath" or (buildDepError "filepath"))
+            (hsPkgs."ghc" or (buildDepError "ghc"))
+            (hsPkgs."hspec" or (buildDepError "hspec"))
+            (hsPkgs."process" or (buildDepError "process"))
+            (hsPkgs."syb" or (buildDepError "syb"))
+            (hsPkgs."hlint" or (buildDepError "hlint"))
+            (hsPkgs."ghc-boot" or (buildDepError "ghc-boot"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
---
+++ hackage/plot-0.2.3.10-r0-3625b87437435d338c24fc31241bbae13997db993a142b0fc8388ecfa392e50f.nix
@@ -0,0 +1,71 @@
+let
+  buildDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (build dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  sysDepError = pkg:
+    builtins.throw ''
+      The Nixpkgs package set does not contain the package: ${pkg} (system dependency).
+      
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      '';
+  pkgConfDepError = pkg:
+    builtins.throw ''
+      The pkg-conf packages does not contain the package: ${pkg} (pkg-conf dependency).
+      
+      You may need to augment the pkg-conf package mapping in haskell.nix so that it can be found.
+      '';
+  exeDepError = pkg:
+    builtins.throw ''
+      The local executable components do not include the component: ${pkg} (executable dependency).
+      '';
+  legacyExeDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (executable dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  buildToolDepError = pkg:
+    builtins.throw ''
+      Neither the Haskell package set or the Nixpkgs package set contain the package: ${pkg} (build tool dependency).
+      
+      If this is a system dependency:
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      
+      If this is a Haskell dependency:
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+in { system, compiler, flags, pkgs, hsPkgs, pkgconfPkgs, ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.8";
+      identifier = { name = "plot"; version = "0.2.3.10"; };
+      license = "BSD-3-Clause";
+      copyright = "(c) A.V.H. McPhail 2010, 2012-2017, 2019";
+      maintainer = "haskell.vivian.mcphail <at> gmail <dot> com";
+      author = "Vivian McPhail";
+      homepage = "http://github.com/amcphail/plot";
+      url = "";
+      synopsis = "A plotting library, exportable as eps/pdf/svg/png or renderable with gtk";
+      description = "A package for creating plots, built on top of the Cairo rendering engine.\n\nAn ambitious attempt to replace gnuplot.\n\nMonadic actions are used to configure a figure, which is a (rxc) array of subplots.\nEach plot displays a graph with optional heading, labels, legend, and annotations.\nThe annotations themselves may be used to draw diagrams.\n\nA figure is preprocessed in preparation for rendering by the Cairo renderer.\nThe Cairo library can be used to output the figure to PS, PDF, SVG, and PNG file formats,\nor to display the figure in a GTK Drawable context.  (see package 'plot-gtk').\n\nThe preprocessed figure can be embedded as an arbitrary Cairo render, including in a diagram\ncreated with the diagram package.  Conversely, arbitrary Cairo renders can be embedded in\nthe data region of a 'Figure'.\n\nThe data series are type \"Data.Packed.Vector\" from hmatrix, which, when hmatrix\nis compiled with '-fvector', is a synonym for \"Data.Vector.Storable\" from the\nvector package and are thus compatible with packages such as statistics.\n\nThe example in Graphics.Rendering.Plot can be viewed at\n<http://code.haskell.org/plot/examples/perturbed-sine.png>\n\n";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (buildDepError "base"))
+          (hsPkgs."mtl" or (buildDepError "mtl"))
+          (hsPkgs."array" or (buildDepError "array"))
+          (hsPkgs."transformers" or (buildDepError "transformers"))
+          (hsPkgs."pango" or (buildDepError "pango"))
+          (hsPkgs."cairo" or (buildDepError "cairo"))
+          (hsPkgs."colour" or (buildDepError "colour"))
+          (hsPkgs."hmatrix" or (buildDepError "hmatrix"))
+          ];
+        buildable = true;
+        };
+      };
+    }
