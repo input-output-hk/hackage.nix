13f7dbe3f330e72c9af48845bb680d50fbd7e055
IOHK <devops+nix-tools@iohk.io>
2019-10-19 01:07:40 +0000

    Automatic update for Sat Oct 19 01:07:40 UTC 2019

---
+++ hackage/json-tools-0.5.1-r0-3cc94994c44abfe0d75c29b8a9717ba955e1736250863779da7cd9f16a6df3af.nix
@@ -0,0 +1,171 @@
+let
+  buildDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (build dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  sysDepError = pkg:
+    builtins.throw ''
+      The Nixpkgs package set does not contain the package: ${pkg} (system dependency).
+      
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      '';
+  pkgConfDepError = pkg:
+    builtins.throw ''
+      The pkg-conf packages does not contain the package: ${pkg} (pkg-conf dependency).
+      
+      You may need to augment the pkg-conf package mapping in haskell.nix so that it can be found.
+      '';
+  exeDepError = pkg:
+    builtins.throw ''
+      The local executable components do not include the component: ${pkg} (executable dependency).
+      '';
+  legacyExeDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (executable dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  buildToolDepError = pkg:
+    builtins.throw ''
+      Neither the Haskell package set or the Nixpkgs package set contain the package: ${pkg} (build tool dependency).
+      
+      If this is a system dependency:
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      
+      If this is a Haskell dependency:
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+in { system, compiler, flags, pkgs, hsPkgs, pkgconfPkgs, ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.6";
+      identifier = { name = "json-tools"; version = "0.5.1"; };
+      license = "BSD-3-Clause";
+      copyright = "(c) Nicolas Pouillard";
+      maintainer = "Nicolas Pouillard <nicolas.pouillard@gmail.com>";
+      author = "Nicolas Pouillard";
+      homepage = "";
+      url = "";
+      synopsis = "A collection of JSON tools";
+      description = "A collection of JSON tools";
+      buildType = "Simple";
+      };
+    components = {
+      exes = {
+        "json-concat" = {
+          depends = [
+            (hsPkgs."base" or (buildDepError "base"))
+            (hsPkgs."aeson" or (buildDepError "aeson"))
+            (hsPkgs."bytestring" or (buildDepError "bytestring"))
+            ];
+          buildable = true;
+          };
+        "json-deep-select-key" = {
+          depends = [
+            (hsPkgs."base" or (buildDepError "base"))
+            (hsPkgs."aeson" or (buildDepError "aeson"))
+            (hsPkgs."bytestring" or (buildDepError "bytestring"))
+            (hsPkgs."containers" or (buildDepError "containers"))
+            ];
+          buildable = true;
+          };
+        "json-select" = {
+          depends = [
+            (hsPkgs."base" or (buildDepError "base"))
+            (hsPkgs."aeson" or (buildDepError "aeson"))
+            (hsPkgs."bytestring" or (buildDepError "bytestring"))
+            (hsPkgs."containers" or (buildDepError "containers"))
+            ];
+          buildable = true;
+          };
+        "json-iter" = {
+          depends = [
+            (hsPkgs."base" or (buildDepError "base"))
+            (hsPkgs."aeson" or (buildDepError "aeson"))
+            (hsPkgs."bytestring" or (buildDepError "bytestring"))
+            (hsPkgs."process" or (buildDepError "process"))
+            ];
+          buildable = true;
+          };
+        "json-lines" = {
+          depends = [
+            (hsPkgs."base" or (buildDepError "base"))
+            (hsPkgs."aeson" or (buildDepError "aeson"))
+            (hsPkgs."bytestring" or (buildDepError "bytestring"))
+            ];
+          buildable = true;
+          };
+        "json-strings" = {
+          depends = [
+            (hsPkgs."base" or (buildDepError "base"))
+            (hsPkgs."aeson" or (buildDepError "aeson"))
+            (hsPkgs."bytestring" or (buildDepError "bytestring"))
+            ];
+          buildable = true;
+          };
+        "json-unlines" = {
+          depends = [
+            (hsPkgs."base" or (buildDepError "base"))
+            (hsPkgs."bytestring" or (buildDepError "bytestring"))
+            ];
+          buildable = true;
+          };
+        "json-wrap" = {
+          depends = [
+            (hsPkgs."base" or (buildDepError "base"))
+            (hsPkgs."aeson" or (buildDepError "aeson"))
+            (hsPkgs."bytestring" or (buildDepError "bytestring"))
+            (hsPkgs."vector" or (buildDepError "vector"))
+            ];
+          buildable = true;
+          };
+        "json-xargs" = {
+          depends = [
+            (hsPkgs."base" or (buildDepError "base"))
+            (hsPkgs."aeson" or (buildDepError "aeson"))
+            (hsPkgs."bytestring" or (buildDepError "bytestring"))
+            ];
+          buildable = true;
+          };
+        "tar2json" = {
+          depends = [
+            (hsPkgs."base" or (buildDepError "base"))
+            (hsPkgs."bytestring" or (buildDepError "bytestring"))
+            (hsPkgs."aeson" or (buildDepError "aeson"))
+            (hsPkgs."unordered-containers" or (buildDepError "unordered-containers"))
+            (hsPkgs."text" or (buildDepError "text"))
+            (hsPkgs."tar" or (buildDepError "tar"))
+            (hsPkgs."string-conversions" or (buildDepError "string-conversions"))
+            ];
+          buildable = true;
+          };
+        "json-quote" = {
+          depends = [
+            (hsPkgs."base" or (buildDepError "base"))
+            (hsPkgs."aeson" or (buildDepError "aeson"))
+            (hsPkgs."text" or (buildDepError "text"))
+            ];
+          buildable = true;
+          };
+        "hjq" = {
+          depends = [
+            (hsPkgs."base" or (buildDepError "base"))
+            (hsPkgs."aeson" or (buildDepError "aeson"))
+            (hsPkgs."text" or (buildDepError "text"))
+            (hsPkgs."attoparsec" or (buildDepError "attoparsec"))
+            (hsPkgs."attoparsec-expr" or (buildDepError "attoparsec-expr"))
+            (hsPkgs."unordered-containers" or (buildDepError "unordered-containers"))
+            (hsPkgs."vector" or (buildDepError "vector"))
+            (hsPkgs."bytestring" or (buildDepError "bytestring"))
+            (hsPkgs."process" or (buildDepError "process"))
+            (hsPkgs."scientific" or (buildDepError "scientific"))
+            (hsPkgs."string-conversions" or (buildDepError "string-conversions"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
---
+++ hackage/net-mqtt-0.6.0.1-r0-ff2e679b8ec2699699ca803204fc4fb3c082c6590f4f13646c3082799dbc9408.nix
@@ -0,0 +1,158 @@
+let
+  buildDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (build dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  sysDepError = pkg:
+    builtins.throw ''
+      The Nixpkgs package set does not contain the package: ${pkg} (system dependency).
+      
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      '';
+  pkgConfDepError = pkg:
+    builtins.throw ''
+      The pkg-conf packages does not contain the package: ${pkg} (pkg-conf dependency).
+      
+      You may need to augment the pkg-conf package mapping in haskell.nix so that it can be found.
+      '';
+  exeDepError = pkg:
+    builtins.throw ''
+      The local executable components do not include the component: ${pkg} (executable dependency).
+      '';
+  legacyExeDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (executable dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  buildToolDepError = pkg:
+    builtins.throw ''
+      Neither the Haskell package set or the Nixpkgs package set contain the package: ${pkg} (build tool dependency).
+      
+      If this is a system dependency:
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      
+      If this is a Haskell dependency:
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+in { system, compiler, flags, pkgs, hsPkgs, pkgconfPkgs, ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.12";
+      identifier = { name = "net-mqtt"; version = "0.6.0.1"; };
+      license = "BSD-3-Clause";
+      copyright = "BSD3";
+      maintainer = "dustin@spy.net";
+      author = "Dustin Sallings";
+      homepage = "https://github.com/dustin/mqtt-hs#readme";
+      url = "";
+      synopsis = "An MQTT Protocol Implementation.";
+      description = "Please see the README on GitHub at <https://github.com/dustin/mqtt-hs#readme>";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."QuickCheck" or (buildDepError "QuickCheck"))
+          (hsPkgs."async" or (buildDepError "async"))
+          (hsPkgs."attoparsec" or (buildDepError "attoparsec"))
+          (hsPkgs."attoparsec-binary" or (buildDepError "attoparsec-binary"))
+          (hsPkgs."base" or (buildDepError "base"))
+          (hsPkgs."binary" or (buildDepError "binary"))
+          (hsPkgs."bytestring" or (buildDepError "bytestring"))
+          (hsPkgs."conduit" or (buildDepError "conduit"))
+          (hsPkgs."conduit-extra" or (buildDepError "conduit-extra"))
+          (hsPkgs."containers" or (buildDepError "containers"))
+          (hsPkgs."deepseq" or (buildDepError "deepseq"))
+          (hsPkgs."network-conduit-tls" or (buildDepError "network-conduit-tls"))
+          (hsPkgs."network-uri" or (buildDepError "network-uri"))
+          (hsPkgs."stm" or (buildDepError "stm"))
+          (hsPkgs."text" or (buildDepError "text"))
+          (hsPkgs."websockets" or (buildDepError "websockets"))
+          (hsPkgs."wuss" or (buildDepError "wuss"))
+          ];
+        buildable = true;
+        };
+      exes = {
+        "mqtt-example" = {
+          depends = [
+            (hsPkgs."QuickCheck" or (buildDepError "QuickCheck"))
+            (hsPkgs."async" or (buildDepError "async"))
+            (hsPkgs."attoparsec" or (buildDepError "attoparsec"))
+            (hsPkgs."attoparsec-binary" or (buildDepError "attoparsec-binary"))
+            (hsPkgs."base" or (buildDepError "base"))
+            (hsPkgs."binary" or (buildDepError "binary"))
+            (hsPkgs."bytestring" or (buildDepError "bytestring"))
+            (hsPkgs."conduit" or (buildDepError "conduit"))
+            (hsPkgs."conduit-extra" or (buildDepError "conduit-extra"))
+            (hsPkgs."containers" or (buildDepError "containers"))
+            (hsPkgs."deepseq" or (buildDepError "deepseq"))
+            (hsPkgs."net-mqtt" or (buildDepError "net-mqtt"))
+            (hsPkgs."network-conduit-tls" or (buildDepError "network-conduit-tls"))
+            (hsPkgs."network-uri" or (buildDepError "network-uri"))
+            (hsPkgs."stm" or (buildDepError "stm"))
+            (hsPkgs."text" or (buildDepError "text"))
+            (hsPkgs."websockets" or (buildDepError "websockets"))
+            (hsPkgs."wuss" or (buildDepError "wuss"))
+            ];
+          buildable = true;
+          };
+        "mqtt-watch" = {
+          depends = [
+            (hsPkgs."QuickCheck" or (buildDepError "QuickCheck"))
+            (hsPkgs."async" or (buildDepError "async"))
+            (hsPkgs."attoparsec" or (buildDepError "attoparsec"))
+            (hsPkgs."attoparsec-binary" or (buildDepError "attoparsec-binary"))
+            (hsPkgs."base" or (buildDepError "base"))
+            (hsPkgs."binary" or (buildDepError "binary"))
+            (hsPkgs."bytestring" or (buildDepError "bytestring"))
+            (hsPkgs."conduit" or (buildDepError "conduit"))
+            (hsPkgs."conduit-extra" or (buildDepError "conduit-extra"))
+            (hsPkgs."containers" or (buildDepError "containers"))
+            (hsPkgs."deepseq" or (buildDepError "deepseq"))
+            (hsPkgs."net-mqtt" or (buildDepError "net-mqtt"))
+            (hsPkgs."network-conduit-tls" or (buildDepError "network-conduit-tls"))
+            (hsPkgs."network-uri" or (buildDepError "network-uri"))
+            (hsPkgs."optparse-applicative" or (buildDepError "optparse-applicative"))
+            (hsPkgs."stm" or (buildDepError "stm"))
+            (hsPkgs."text" or (buildDepError "text"))
+            (hsPkgs."websockets" or (buildDepError "websockets"))
+            (hsPkgs."wuss" or (buildDepError "wuss"))
+            ];
+          buildable = true;
+          };
+        };
+      tests = {
+        "mqtt-test" = {
+          depends = [
+            (hsPkgs."HUnit" or (buildDepError "HUnit"))
+            (hsPkgs."QuickCheck" or (buildDepError "QuickCheck"))
+            (hsPkgs."async" or (buildDepError "async"))
+            (hsPkgs."attoparsec" or (buildDepError "attoparsec"))
+            (hsPkgs."attoparsec-binary" or (buildDepError "attoparsec-binary"))
+            (hsPkgs."base" or (buildDepError "base"))
+            (hsPkgs."binary" or (buildDepError "binary"))
+            (hsPkgs."bytestring" or (buildDepError "bytestring"))
+            (hsPkgs."conduit" or (buildDepError "conduit"))
+            (hsPkgs."conduit-extra" or (buildDepError "conduit-extra"))
+            (hsPkgs."containers" or (buildDepError "containers"))
+            (hsPkgs."deepseq" or (buildDepError "deepseq"))
+            (hsPkgs."net-mqtt" or (buildDepError "net-mqtt"))
+            (hsPkgs."network-conduit-tls" or (buildDepError "network-conduit-tls"))
+            (hsPkgs."network-uri" or (buildDepError "network-uri"))
+            (hsPkgs."stm" or (buildDepError "stm"))
+            (hsPkgs."tasty" or (buildDepError "tasty"))
+            (hsPkgs."tasty-hunit" or (buildDepError "tasty-hunit"))
+            (hsPkgs."tasty-quickcheck" or (buildDepError "tasty-quickcheck"))
+            (hsPkgs."text" or (buildDepError "text"))
+            (hsPkgs."websockets" or (buildDepError "websockets"))
+            (hsPkgs."wuss" or (buildDepError "wuss"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
