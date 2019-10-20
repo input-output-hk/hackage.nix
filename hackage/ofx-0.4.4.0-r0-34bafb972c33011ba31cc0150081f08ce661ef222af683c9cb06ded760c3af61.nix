ecfa4dd9dbc579996131d3593ce5c9f595550e9f
IOHK <devops+nix-tools@iohk.io>
2019-10-20 01:06:33 +0000

    Automatic update for Sun Oct 20 01:06:32 UTC 2019

---
+++ hackage/ofx-0.4.4.0-r0-34bafb972c33011ba31cc0150081f08ce661ef222af683c9cb06ded760c3af61.nix
@@ -0,0 +1,87 @@
+let
+  buildDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (build dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  sysDepError = pkg:
+    builtins.throw ''
+      The Nixpkgs package set does not contain the package: ${pkg} (system dependency).
+      
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      '';
+  pkgConfDepError = pkg:
+    builtins.throw ''
+      The pkg-conf packages does not contain the package: ${pkg} (pkg-conf dependency).
+      
+      You may need to augment the pkg-conf package mapping in haskell.nix so that it can be found.
+      '';
+  exeDepError = pkg:
+    builtins.throw ''
+      The local executable components do not include the component: ${pkg} (executable dependency).
+      '';
+  legacyExeDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (executable dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  buildToolDepError = pkg:
+    builtins.throw ''
+      Neither the Haskell package set or the Nixpkgs package set contain the package: ${pkg} (build tool dependency).
+      
+      If this is a system dependency:
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      
+      If this is a Haskell dependency:
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+in { system, compiler, flags, pkgs, hsPkgs, pkgconfPkgs, ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "ofx"; version = "0.4.4.0"; };
+      license = "BSD-3-Clause";
+      copyright = "Copyright (c) 2016-2019 Omari Norman";
+      maintainer = "omari@smileystation.com";
+      author = "Omari Norman";
+      homepage = "http://www.github.com/massysett/ofx";
+      url = "";
+      synopsis = "Parser for OFX data";
+      description = "A parser for Open Financial Exchange (OFX) financial data. This\nhandles OFX version 1.03, which is documented at http://www.ofx.net\nThis also handles QFX, which is OFX plus some minor additions made\nby Intuit, the publishers of Quicken.\n\nThe parser will place all the data into a tree, which you can query\nfor whatever data you may need, although you will need to be\nfamiliar with the OFX spec to do this. There is also a Transaction\ntype which you can easily parse from the tree; this will contain\nmost, perhaps all, of the data you will be interested in if your OFX\nfile is from a bank or credit card account.\n\nAll the OFX and QFX files I have seen use the format from the 1.0\nseries of OFX, which is primarily SGML based. OFX version 2 is XML\nbased.  This parser likely will not work on those files.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (buildDepError "base"))
+          (hsPkgs."parsec" or (buildDepError "parsec"))
+          (hsPkgs."pretty" or (buildDepError "pretty"))
+          (hsPkgs."time" or (buildDepError "time"))
+          ];
+        buildable = true;
+        };
+      exes = {
+        "renderOfx" = {
+          depends = [
+            (hsPkgs."base" or (buildDepError "base"))
+            (hsPkgs."parsec" or (buildDepError "parsec"))
+            (hsPkgs."pretty" or (buildDepError "pretty"))
+            (hsPkgs."time" or (buildDepError "time"))
+            ];
+          buildable = true;
+          };
+        "renderTransactions" = {
+          depends = [
+            (hsPkgs."base" or (buildDepError "base"))
+            (hsPkgs."parsec" or (buildDepError "parsec"))
+            (hsPkgs."pretty" or (buildDepError "pretty"))
+            (hsPkgs."time" or (buildDepError "time"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
