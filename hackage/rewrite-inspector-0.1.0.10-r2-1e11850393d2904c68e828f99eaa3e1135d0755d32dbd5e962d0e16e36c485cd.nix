b67921897f7f393c8657d9ce7b2c79240668db55
IOHK <devops+nix-tools@iohk.io>
2019-10-26 01:08:56 +0000

    Automatic update for Sat Oct 26 01:08:55 UTC 2019

---
+++ hackage/rewrite-inspector-0.1.0.10-r2-1e11850393d2904c68e828f99eaa3e1135d0755d32dbd5e962d0e16e36c485cd.nix
@@ -0,0 +1,94 @@
+let
+  buildDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (build dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  sysDepError = pkg:
+    builtins.throw ''
+      The Nixpkgs package set does not contain the package: ${pkg} (system dependency).
+      
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      '';
+  pkgConfDepError = pkg:
+    builtins.throw ''
+      The pkg-conf packages does not contain the package: ${pkg} (pkg-conf dependency).
+      
+      You may need to augment the pkg-conf package mapping in haskell.nix so that it can be found.
+      '';
+  exeDepError = pkg:
+    builtins.throw ''
+      The local executable components do not include the component: ${pkg} (executable dependency).
+      '';
+  legacyExeDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (executable dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  buildToolDepError = pkg:
+    builtins.throw ''
+      Neither the Haskell package set or the Nixpkgs package set contain the package: ${pkg} (build tool dependency).
+      
+      If this is a system dependency:
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      
+      If this is a Haskell dependency:
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+in { system, compiler, flags, pkgs, hsPkgs, pkgconfPkgs, ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "rewrite-inspector"; version = "0.1.0.10"; };
+      license = "BSD-3-Clause";
+      copyright = "";
+      maintainer = "Orestis Melkonian <melkon.or@gmail.com>";
+      author = "Orestis Melkonian";
+      homepage = "http://github.com/omelkonian/rewrite-inspector/";
+      url = "";
+      synopsis = "Inspection of rewriting steps";
+      description = "A terminal UI for inspecting steps taken by a rewriting process.\nUseful for the optimization phase of a compiler,\nor even evaluators of small languages.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (buildDepError "base"))
+          (hsPkgs."binary" or (buildDepError "binary"))
+          (hsPkgs."hashable" or (buildDepError "hashable"))
+          (hsPkgs."containers" or (buildDepError "containers"))
+          (hsPkgs."brick" or (buildDepError "brick"))
+          (hsPkgs."vty" or (buildDepError "vty"))
+          (hsPkgs."prettyprinter" or (buildDepError "prettyprinter"))
+          (hsPkgs."text" or (buildDepError "text"))
+          (hsPkgs."microlens" or (buildDepError "microlens"))
+          (hsPkgs."microlens-th" or (buildDepError "microlens-th"))
+          (hsPkgs."data-default" or (buildDepError "data-default"))
+          ];
+        buildable = true;
+        };
+      exes = {
+        "expr-example" = {
+          depends = [
+            (hsPkgs."base" or (buildDepError "base"))
+            (hsPkgs."prettyprinter" or (buildDepError "prettyprinter"))
+            (hsPkgs."rewrite-inspector" or (buildDepError "rewrite-inspector"))
+            ];
+          libs = [ (pkgs."pthread" or (sysDepError "pthread")) ];
+          buildable = true;
+          };
+        "str-example" = {
+          depends = [
+            (hsPkgs."base" or (buildDepError "base"))
+            (hsPkgs."prettyprinter" or (buildDepError "prettyprinter"))
+            (hsPkgs."rewrite-inspector" or (buildDepError "rewrite-inspector"))
+            ];
+          libs = [ (pkgs."pthread" or (sysDepError "pthread")) ];
+          buildable = true;
+          };
+        };
+      };
+    }
