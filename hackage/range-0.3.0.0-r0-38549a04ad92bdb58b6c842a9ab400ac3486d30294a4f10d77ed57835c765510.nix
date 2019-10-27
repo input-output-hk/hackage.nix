74ff2583b3516aaece343cf1957aec916ebd9cba
IOHK <devops+nix-tools@iohk.io>
2019-10-27 01:37:48 +0000

    Automatic update for Sun Oct 27 01:37:47 UTC 2019

---
+++ hackage/range-0.3.0.0-r0-38549a04ad92bdb58b6c842a9ab400ac3486d30294a4f10d77ed57835c765510.nix
@@ -0,0 +1,79 @@
+let
+  buildDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (build dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  sysDepError = pkg:
+    builtins.throw ''
+      The Nixpkgs package set does not contain the package: ${pkg} (system dependency).
+      
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      '';
+  pkgConfDepError = pkg:
+    builtins.throw ''
+      The pkg-conf packages does not contain the package: ${pkg} (pkg-conf dependency).
+      
+      You may need to augment the pkg-conf package mapping in haskell.nix so that it can be found.
+      '';
+  exeDepError = pkg:
+    builtins.throw ''
+      The local executable components do not include the component: ${pkg} (executable dependency).
+      '';
+  legacyExeDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (executable dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  buildToolDepError = pkg:
+    builtins.throw ''
+      Neither the Haskell package set or the Nixpkgs package set contain the package: ${pkg} (build tool dependency).
+      
+      If this is a system dependency:
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      
+      If this is a Haskell dependency:
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+in { system, compiler, flags, pkgs, hsPkgs, pkgconfPkgs, ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.8";
+      identifier = { name = "range"; version = "0.3.0.0"; };
+      license = "MIT";
+      copyright = "";
+      maintainer = "robertmassaioli@gmail.com";
+      author = "Robert Massaioli";
+      homepage = "https://bitbucket.org/robertmassaioli/range";
+      url = "";
+      synopsis = "An efficient and versatile range library.";
+      description = "The range library alows the use of performant and versatile ranges in your code.\nIt supports bounded and unbounded ranges, ranges in a nested manner (like library\nversions), an efficient algebra of range computation and even a simplified interface\nfor ranges for the common cases. This library is far more efficient than using the\ndefault Data.List functions to approximate range behaviour. Performance is the major\nvalue offering of this library.\nIf this is your first time using this library it is highly recommended that you start\nwith \"Data.Range\"; it contains the basics of this library that meet most use\ncases.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (buildDepError "base"))
+          (hsPkgs."parsec" or (buildDepError "parsec"))
+          ] ++ [ (hsPkgs."free" or (buildDepError "free")) ];
+        buildable = true;
+        };
+      tests = {
+        "test-range" = {
+          depends = [
+            (hsPkgs."base" or (buildDepError "base"))
+            (hsPkgs."Cabal" or (buildDepError "Cabal"))
+            (hsPkgs."QuickCheck" or (buildDepError "QuickCheck"))
+            (hsPkgs."test-framework-quickcheck2" or (buildDepError "test-framework-quickcheck2"))
+            (hsPkgs."test-framework" or (buildDepError "test-framework"))
+            (hsPkgs."random" or (buildDepError "random"))
+            (hsPkgs."range" or (buildDepError "range"))
+            ] ++ [ (hsPkgs."free" or (buildDepError "free")) ];
+          buildable = true;
+          };
+        };
+      };
+    }
