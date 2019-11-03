9871e9c445362c3fd75fd5e44b241fba3e52508c
IOHK <devops+nix-tools@iohk.io>
2019-11-03 01:07:28 +0000

    Automatic update for Sun Nov  3 01:07:27 UTC 2019

---
+++ hackage/haskell-names-0.9.7-r0-dbf379505431afe2c3a4eef855340d862dc80e3cfb3ee7f65d833ff492109800.nix
@@ -0,0 +1,92 @@
+let
+  buildDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (build dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  sysDepError = pkg:
+    builtins.throw ''
+      The Nixpkgs package set does not contain the package: ${pkg} (system dependency).
+      
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      '';
+  pkgConfDepError = pkg:
+    builtins.throw ''
+      The pkg-conf packages does not contain the package: ${pkg} (pkg-conf dependency).
+      
+      You may need to augment the pkg-conf package mapping in haskell.nix so that it can be found.
+      '';
+  exeDepError = pkg:
+    builtins.throw ''
+      The local executable components do not include the component: ${pkg} (executable dependency).
+      '';
+  legacyExeDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (executable dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  buildToolDepError = pkg:
+    builtins.throw ''
+      Neither the Haskell package set or the Nixpkgs package set contain the package: ${pkg} (build tool dependency).
+      
+      If this is a system dependency:
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      
+      If this is a Haskell dependency:
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+in { system, compiler, flags, pkgs, hsPkgs, pkgconfPkgs, ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "haskell-names"; version = "0.9.7"; };
+      license = "BSD-3-Clause";
+      copyright = "";
+      maintainer = "Philipp Schuster";
+      author = "Philipp Schuster, Roman Cheplyaka, Lennart Augustsson";
+      homepage = "http://documentup.com/haskell-suite/haskell-names";
+      url = "";
+      synopsis = "Name resolution library for Haskell";
+      description = "This package takes modules parsed with `haskell-src-exts`, resolves used names and annotates the parsed module with scoping information.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (buildDepError "base"))
+          (hsPkgs."haskell-src-exts" or (buildDepError "haskell-src-exts"))
+          (hsPkgs."mtl" or (buildDepError "mtl"))
+          (hsPkgs."transformers" or (buildDepError "transformers"))
+          (hsPkgs."filepath" or (buildDepError "filepath"))
+          (hsPkgs."containers" or (buildDepError "containers"))
+          (hsPkgs."uniplate" or (buildDepError "uniplate"))
+          (hsPkgs."aeson" or (buildDepError "aeson"))
+          (hsPkgs."bytestring" or (buildDepError "bytestring"))
+          (hsPkgs."data-lens-light" or (buildDepError "data-lens-light"))
+          (hsPkgs."traverse-with-class" or (buildDepError "traverse-with-class"))
+          ] ++ (pkgs.lib).optional (compiler.isGhc && (compiler.version).le "7.8") (hsPkgs."tagged" or (buildDepError "tagged"));
+        buildable = true;
+        };
+      tests = {
+        "test" = {
+          depends = [
+            (hsPkgs."base" or (buildDepError "base"))
+            (hsPkgs."haskell-names" or (buildDepError "haskell-names"))
+            (hsPkgs."haskell-src-exts" or (buildDepError "haskell-src-exts"))
+            (hsPkgs."mtl" or (buildDepError "mtl"))
+            (hsPkgs."filepath" or (buildDepError "filepath"))
+            (hsPkgs."containers" or (buildDepError "containers"))
+            (hsPkgs."tasty" or (buildDepError "tasty"))
+            (hsPkgs."tasty-golden" or (buildDepError "tasty-golden"))
+            (hsPkgs."filemanip" or (buildDepError "filemanip"))
+            (hsPkgs."pretty-show" or (buildDepError "pretty-show"))
+            (hsPkgs."traverse-with-class" or (buildDepError "traverse-with-class"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
---
+++ hackage/hoogle-5.0.17.12-r0-1a2d21cc7e08ac412f99427458c93341d5eaf291dd5dfb64ccdbf1b2db0266b1.nix
@@ -0,0 +1,113 @@
+let
+  buildDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (build dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  sysDepError = pkg:
+    builtins.throw ''
+      The Nixpkgs package set does not contain the package: ${pkg} (system dependency).
+      
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      '';
+  pkgConfDepError = pkg:
+    builtins.throw ''
+      The pkg-conf packages does not contain the package: ${pkg} (pkg-conf dependency).
+      
+      You may need to augment the pkg-conf package mapping in haskell.nix so that it can be found.
+      '';
+  exeDepError = pkg:
+    builtins.throw ''
+      The local executable components do not include the component: ${pkg} (executable dependency).
+      '';
+  legacyExeDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (executable dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  buildToolDepError = pkg:
+    builtins.throw ''
+      Neither the Haskell package set or the Nixpkgs package set contain the package: ${pkg} (build tool dependency).
+      
+      If this is a system dependency:
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      
+      If this is a Haskell dependency:
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+in { system, compiler, flags, pkgs, hsPkgs, pkgconfPkgs, ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.18";
+      identifier = { name = "hoogle"; version = "5.0.17.12"; };
+      license = "BSD-3-Clause";
+      copyright = "Neil Mitchell 2004-2019";
+      maintainer = "Neil Mitchell <ndmitchell@gmail.com>";
+      author = "Neil Mitchell <ndmitchell@gmail.com>";
+      homepage = "https://hoogle.haskell.org/";
+      url = "";
+      synopsis = "Haskell API Search";
+      description = "Hoogle is a Haskell API search engine, which allows you to\nsearch many standard Haskell libraries by either function name,\nor by approximate type signature.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."QuickCheck" or (buildDepError "QuickCheck"))
+          (hsPkgs."aeson" or (buildDepError "aeson"))
+          (hsPkgs."base" or (buildDepError "base"))
+          (hsPkgs."blaze-html" or (buildDepError "blaze-html"))
+          (hsPkgs."blaze-markup" or (buildDepError "blaze-markup"))
+          (hsPkgs."binary" or (buildDepError "binary"))
+          (hsPkgs."bytestring" or (buildDepError "bytestring"))
+          (hsPkgs."cmdargs" or (buildDepError "cmdargs"))
+          (hsPkgs."conduit" or (buildDepError "conduit"))
+          (hsPkgs."conduit-extra" or (buildDepError "conduit-extra"))
+          (hsPkgs."connection" or (buildDepError "connection"))
+          (hsPkgs."containers" or (buildDepError "containers"))
+          (hsPkgs."deepseq" or (buildDepError "deepseq"))
+          (hsPkgs."directory" or (buildDepError "directory"))
+          (hsPkgs."extra" or (buildDepError "extra"))
+          (hsPkgs."filepath" or (buildDepError "filepath"))
+          (hsPkgs."foundation" or (buildDepError "foundation"))
+          (hsPkgs."old-locale" or (buildDepError "old-locale"))
+          (hsPkgs."hashable" or (buildDepError "hashable"))
+          (hsPkgs."haskell-src-exts" or (buildDepError "haskell-src-exts"))
+          (hsPkgs."http-conduit" or (buildDepError "http-conduit"))
+          (hsPkgs."http-types" or (buildDepError "http-types"))
+          (hsPkgs."js-flot" or (buildDepError "js-flot"))
+          (hsPkgs."js-jquery" or (buildDepError "js-jquery"))
+          (hsPkgs."mmap" or (buildDepError "mmap"))
+          (hsPkgs."process-extras" or (buildDepError "process-extras"))
+          (hsPkgs."resourcet" or (buildDepError "resourcet"))
+          (hsPkgs."storable-tuple" or (buildDepError "storable-tuple"))
+          (hsPkgs."tar" or (buildDepError "tar"))
+          (hsPkgs."template-haskell" or (buildDepError "template-haskell"))
+          (hsPkgs."text" or (buildDepError "text"))
+          (hsPkgs."time" or (buildDepError "time"))
+          (hsPkgs."transformers" or (buildDepError "transformers"))
+          (hsPkgs."uniplate" or (buildDepError "uniplate"))
+          (hsPkgs."utf8-string" or (buildDepError "utf8-string"))
+          (hsPkgs."vector" or (buildDepError "vector"))
+          (hsPkgs."wai" or (buildDepError "wai"))
+          (hsPkgs."wai-logger" or (buildDepError "wai-logger"))
+          (hsPkgs."warp" or (buildDepError "warp"))
+          (hsPkgs."warp-tls" or (buildDepError "warp-tls"))
+          (hsPkgs."zlib" or (buildDepError "zlib"))
+          ] ++ (pkgs.lib).optional (compiler.isGhc && (compiler.version).lt "8.0") (hsPkgs."semigroups" or (buildDepError "semigroups"));
+        buildable = true;
+        };
+      exes = {
+        "hoogle" = {
+          depends = [
+            (hsPkgs."base" or (buildDepError "base"))
+            (hsPkgs."hoogle" or (buildDepError "hoogle"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
