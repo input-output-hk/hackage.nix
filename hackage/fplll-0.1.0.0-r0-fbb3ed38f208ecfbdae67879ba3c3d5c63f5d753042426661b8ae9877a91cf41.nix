85f8a7db8b1a0901dd41026fbc959b71d2645b15
IOHK <devops+nix-tools@iohk.io>
2019-12-06 01:12:56 +0000

    Automatic update for Fri Dec  6 01:12:56 UTC 2019

---
+++ hackage/fplll-0.1.0.0-r0-fbb3ed38f208ecfbdae67879ba3c3d5c63f5d753042426661b8ae9877a91cf41.nix
@@ -0,0 +1,77 @@
+let
+  buildDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (build dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  sysDepError = pkg:
+    builtins.throw ''
+      The Nixpkgs package set does not contain the package: ${pkg} (system dependency).
+      
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      '';
+  pkgConfDepError = pkg:
+    builtins.throw ''
+      The pkg-conf packages does not contain the package: ${pkg} (pkg-conf dependency).
+      
+      You may need to augment the pkg-conf package mapping in haskell.nix so that it can be found.
+      '';
+  exeDepError = pkg:
+    builtins.throw ''
+      The local executable components do not include the component: ${pkg} (executable dependency).
+      '';
+  legacyExeDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (executable dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  buildToolDepError = pkg:
+    builtins.throw ''
+      Neither the Haskell package set or the Nixpkgs package set contain the package: ${pkg} (build tool dependency).
+      
+      If this is a system dependency:
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      
+      If this is a Haskell dependency:
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+in { system, compiler, flags, pkgs, hsPkgs, pkgconfPkgs, ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "2.2";
+      identifier = { name = "fplll"; version = "0.1.0.0"; };
+      license = "LGPL-2.1-or-later";
+      copyright = "";
+      maintainer = "ldr709@gmail.com";
+      author = "Lance Roy";
+      homepage = "https://github.com/ldr709/haskell-fplll";
+      url = "";
+      synopsis = "Haskell bindings to <https://fplll.github.io/fplll/ fplll>";
+      description = "";
+      buildType = "Configure";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (buildDepError "base"))
+          (hsPkgs."hgmp" or (buildDepError "hgmp"))
+          (hsPkgs."semibounded-lattices" or (buildDepError "semibounded-lattices"))
+          (hsPkgs."lattices" or (buildDepError "lattices"))
+          ];
+        pkgconfig = [ (pkgconfPkgs."fplll" or (pkgConfDepError "fplll")) ];
+        buildable = true;
+        };
+      tests = {
+        "test" = {
+          depends = [
+            (hsPkgs."base" or (buildDepError "base"))
+            (hsPkgs."fplll" or (buildDepError "fplll"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
---
+++ hackage/random-fu-0.2.7.3-r0-931b5c35d5180aecbfe33b381fdf29a31b73e46908ffc27360bbe96f12fa1ee8.nix
@@ -0,0 +1,77 @@
+let
+  buildDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (build dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  sysDepError = pkg:
+    builtins.throw ''
+      The Nixpkgs package set does not contain the package: ${pkg} (system dependency).
+      
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      '';
+  pkgConfDepError = pkg:
+    builtins.throw ''
+      The pkg-conf packages does not contain the package: ${pkg} (pkg-conf dependency).
+      
+      You may need to augment the pkg-conf package mapping in haskell.nix so that it can be found.
+      '';
+  exeDepError = pkg:
+    builtins.throw ''
+      The local executable components do not include the component: ${pkg} (executable dependency).
+      '';
+  legacyExeDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (executable dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  buildToolDepError = pkg:
+    builtins.throw ''
+      Neither the Haskell package set or the Nixpkgs package set contain the package: ${pkg} (build tool dependency).
+      
+      If this is a system dependency:
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      
+      If this is a Haskell dependency:
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+in { system, compiler, flags, pkgs, hsPkgs, pkgconfPkgs, ... }:
+  {
+    flags = { base4_2 = true; mtl2 = true; };
+    package = {
+      specVersion = "1.6";
+      identifier = { name = "random-fu"; version = "0.2.7.3"; };
+      license = "LicenseRef-PublicDomain";
+      copyright = "";
+      maintainer = "Dominic Steinitz <dominic@steinitz.org>";
+      author = "James Cook <mokus@deepbondi.net>";
+      homepage = "https://github.com/mokus0/random-fu";
+      url = "";
+      synopsis = "Random number generation";
+      description = "Random number generation based on modeling random\nvariables in two complementary ways: first, by the\nparameters of standard mathematical distributions and,\nsecond, by an abstract type ('RVar') which can be\ncomposed and manipulated monadically and sampled in\neither monadic or \\\"pure\\\" styles.\n\nThe primary purpose of this library is to support\ndefining and sampling a wide variety of high quality\nrandom variables.  Quality is prioritized over speed,\nbut performance is an important goal too.\n\nIn my testing, I have found it capable of speed\ncomparable to other Haskell libraries, but still\na fair bit slower than straight C implementations of\nthe same algorithms.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = ([
+          (hsPkgs."math-functions" or (buildDepError "math-functions"))
+          (hsPkgs."monad-loops" or (buildDepError "monad-loops"))
+          (hsPkgs."random-shuffle" or (buildDepError "random-shuffle"))
+          (hsPkgs."random-source" or (buildDepError "random-source"))
+          (hsPkgs."rvar" or (buildDepError "rvar"))
+          (hsPkgs."syb" or (buildDepError "syb"))
+          (hsPkgs."template-haskell" or (buildDepError "template-haskell"))
+          (hsPkgs."transformers" or (buildDepError "transformers"))
+          (hsPkgs."vector" or (buildDepError "vector"))
+          (hsPkgs."erf" or (buildDepError "erf"))
+          ] ++ [ (hsPkgs."base" or (buildDepError "base")) ]) ++ [
+          (hsPkgs."mtl" or (buildDepError "mtl"))
+          ];
+        buildable = if compiler.isGhc && (compiler.version).eq "7.2.1"
+          then false
+          else true;
+        };
+      };
+    }
