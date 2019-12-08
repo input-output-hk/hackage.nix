f37c62265e40705c991962182b8d22b6c9adc8e5
IOHK <devops+nix-tools@iohk.io>
2019-12-08 01:08:56 +0000

    Automatic update for Sun Dec  8 01:08:56 UTC 2019

---
+++ hackage/edit-distance-linear-0.2.0.1-r0-6b559d984a32aca47d44f2df8076239e479d542afb7ad16d13fe081bfa34499f.nix
@@ -0,0 +1,105 @@
+let
+  buildDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (build dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  sysDepError = pkg:
+    builtins.throw ''
+      The Nixpkgs package set does not contain the package: ${pkg} (system dependency).
+      
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      '';
+  pkgConfDepError = pkg:
+    builtins.throw ''
+      The pkg-conf packages does not contain the package: ${pkg} (pkg-conf dependency).
+      
+      You may need to augment the pkg-conf package mapping in haskell.nix so that it can be found.
+      '';
+  exeDepError = pkg:
+    builtins.throw ''
+      The local executable components do not include the component: ${pkg} (executable dependency).
+      '';
+  legacyExeDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (executable dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  buildToolDepError = pkg:
+    builtins.throw ''
+      Neither the Haskell package set or the Nixpkgs package set contain the package: ${pkg} (build tool dependency).
+      
+      If this is a system dependency:
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      
+      If this is a Haskell dependency:
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+in { system, compiler, flags, pkgs, hsPkgs, pkgconfPkgs, ... }:
+  {
+    flags = { llvm = true; with-executable = false; };
+    package = {
+      specVersion = "1.12";
+      identifier = { name = "edit-distance-linear"; version = "0.2.0.1"; };
+      license = "BSD-3-Clause";
+      copyright = "2019 Georg Rudoy";
+      maintainer = "0xd34df00d@gmail.com";
+      author = "Georg Rudoy";
+      homepage = "https://github.com/0xd34df00d/edit-distance-linear#readme";
+      url = "";
+      synopsis = "Efficient implementation Levenshtein distance in linear memory.";
+      description = "Please see the README on GitHub at <https://github.com/0xd34df00d/edit-distance-linear#readme>";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."array" or (buildDepError "array"))
+          (hsPkgs."base" or (buildDepError "base"))
+          (hsPkgs."bytestring" or (buildDepError "bytestring"))
+          ];
+        buildable = true;
+        };
+      exes = {
+        "edit-distance-linear-exe" = {
+          depends = [
+            (hsPkgs."array" or (buildDepError "array"))
+            (hsPkgs."base" or (buildDepError "base"))
+            (hsPkgs."bytestring" or (buildDepError "bytestring"))
+            (hsPkgs."edit-distance-linear" or (buildDepError "edit-distance-linear"))
+            ];
+          buildable = if !flags.with-executable then false else true;
+          };
+        };
+      tests = {
+        "edit-distance-linear-test" = {
+          depends = [
+            (hsPkgs."QuickCheck" or (buildDepError "QuickCheck"))
+            (hsPkgs."array" or (buildDepError "array"))
+            (hsPkgs."base" or (buildDepError "base"))
+            (hsPkgs."bytestring" or (buildDepError "bytestring"))
+            (hsPkgs."edit-distance" or (buildDepError "edit-distance"))
+            (hsPkgs."edit-distance-linear" or (buildDepError "edit-distance-linear"))
+            (hsPkgs."hspec" or (buildDepError "hspec"))
+            ];
+          buildable = true;
+          };
+        };
+      benchmarks = {
+        "edit-distance-linear-bench" = {
+          depends = [
+            (hsPkgs."array" or (buildDepError "array"))
+            (hsPkgs."base" or (buildDepError "base"))
+            (hsPkgs."bytestring" or (buildDepError "bytestring"))
+            (hsPkgs."criterion" or (buildDepError "criterion"))
+            (hsPkgs."edit-distance-linear" or (buildDepError "edit-distance-linear"))
+            (hsPkgs."text" or (buildDepError "text"))
+            (hsPkgs."text-metrics" or (buildDepError "text-metrics"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
---
+++ hackage/tptp-0.1.1.0-r0-afd67690903b82b7426b5dc53361acc61014be92c8cad63a63c1600ab6313eeb.nix
@@ -0,0 +1,104 @@
+let
+  buildDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (build dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  sysDepError = pkg:
+    builtins.throw ''
+      The Nixpkgs package set does not contain the package: ${pkg} (system dependency).
+      
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      '';
+  pkgConfDepError = pkg:
+    builtins.throw ''
+      The pkg-conf packages does not contain the package: ${pkg} (pkg-conf dependency).
+      
+      You may need to augment the pkg-conf package mapping in haskell.nix so that it can be found.
+      '';
+  exeDepError = pkg:
+    builtins.throw ''
+      The local executable components do not include the component: ${pkg} (executable dependency).
+      '';
+  legacyExeDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (executable dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  buildToolDepError = pkg:
+    builtins.throw ''
+      Neither the Haskell package set or the Nixpkgs package set contain the package: ${pkg} (build tool dependency).
+      
+      If this is a system dependency:
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      
+      If this is a Haskell dependency:
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+in { system, compiler, flags, pkgs, hsPkgs, pkgconfPkgs, ... }:
+  {
+    flags = { werror = false; };
+    package = {
+      specVersion = "2.4";
+      identifier = { name = "tptp"; version = "0.1.1.0"; };
+      license = "GPL-3.0-only";
+      copyright = "";
+      maintainer = "evgeny.kotelnikov@gmail.com";
+      author = "Evgenii Kotelnikov";
+      homepage = "https://github.com/aztek/tptp";
+      url = "";
+      synopsis = "A parser and a pretty printer for the TPTP language";
+      description = "<http://www.tptp.org TPTP> (Thousands of Problems for Theorem Provers)\nis the standard language of problems, proofs, and models, used by automated\ntheorem provers.\n\nThis library provides definitions of data types, a\n<https://hackage.haskell.org/package/prettyprinter pretty printer> and an\n<https://hackage.haskell.org/package/attoparsec attoparsec> parser for the\nCNF, FOF, TFF0 and TFF1 subsets of the TPTP language.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (buildDepError "base"))
+          (hsPkgs."text" or (buildDepError "text"))
+          (hsPkgs."attoparsec" or (buildDepError "attoparsec"))
+          (hsPkgs."scientific" or (buildDepError "scientific"))
+          (hsPkgs."prettyprinter" or (buildDepError "prettyprinter"))
+          ] ++ (pkgs.lib).optional (compiler.isGhc && (compiler.version).lt "8") (hsPkgs."semigroups" or (buildDepError "semigroups"));
+        buildable = true;
+        };
+      tests = {
+        "quickcheck-spec" = {
+          depends = ([
+            (hsPkgs."base" or (buildDepError "base"))
+            (hsPkgs."text" or (buildDepError "text"))
+            (hsPkgs."attoparsec" or (buildDepError "attoparsec"))
+            (hsPkgs."scientific" or (buildDepError "scientific"))
+            (hsPkgs."prettyprinter" or (buildDepError "prettyprinter"))
+            (hsPkgs."generic-random" or (buildDepError "generic-random"))
+            (hsPkgs."QuickCheck" or (buildDepError "QuickCheck"))
+            (hsPkgs."tptp" or (buildDepError "tptp"))
+            ] ++ (pkgs.lib).optional (compiler.isGhc && (compiler.version).lt "8") (hsPkgs."semigroups" or (buildDepError "semigroups"))) ++ (pkgs.lib).optional (compiler.isGhc && (compiler.version).lt "8.2") (hsPkgs."bifunctors" or (buildDepError "bifunctors"));
+          buildable = true;
+          };
+        "unit-tests" = {
+          depends = [
+            (hsPkgs."base" or (buildDepError "base"))
+            (hsPkgs."text" or (buildDepError "text"))
+            (hsPkgs."Cabal" or (buildDepError "Cabal"))
+            (hsPkgs."extra" or (buildDepError "extra"))
+            (hsPkgs."directory" or (buildDepError "directory"))
+            (hsPkgs."tptp" or (buildDepError "tptp"))
+            ];
+          buildable = if compiler.isGhc && (compiler.version).ge "8.0.0" && (compiler.isGhc && (compiler.version).lt "8.1.0")
+            then false
+            else true;
+          };
+        "doctests" = {
+          depends = [
+            (hsPkgs."base" or (buildDepError "base"))
+            (hsPkgs."QuickCheck" or (buildDepError "QuickCheck"))
+            (hsPkgs."doctest" or (buildDepError "doctest"))
+            ];
+          buildable = false;
+          };
+        };
+      };
+    }
