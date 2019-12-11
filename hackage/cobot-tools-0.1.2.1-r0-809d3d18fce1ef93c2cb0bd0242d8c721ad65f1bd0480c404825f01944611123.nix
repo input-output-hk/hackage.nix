b37df5cb0a39a51b336faf2344678dd11f419b0d
IOHK <devops+nix-tools@iohk.io>
2019-12-11 01:11:52 +0000

    Automatic update for Wed Dec 11 01:11:51 UTC 2019

---
+++ hackage/cobot-tools-0.1.2.1-r0-809d3d18fce1ef93c2cb0bd0242d8c721ad65f1bd0480c404825f01944611123.nix
@@ -0,0 +1,100 @@
+let
+  buildDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (build dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  sysDepError = pkg:
+    builtins.throw ''
+      The Nixpkgs package set does not contain the package: ${pkg} (system dependency).
+      
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      '';
+  pkgConfDepError = pkg:
+    builtins.throw ''
+      The pkg-conf packages does not contain the package: ${pkg} (pkg-conf dependency).
+      
+      You may need to augment the pkg-conf package mapping in haskell.nix so that it can be found.
+      '';
+  exeDepError = pkg:
+    builtins.throw ''
+      The local executable components do not include the component: ${pkg} (executable dependency).
+      '';
+  legacyExeDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (executable dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  buildToolDepError = pkg:
+    builtins.throw ''
+      Neither the Haskell package set or the Nixpkgs package set contain the package: ${pkg} (build tool dependency).
+      
+      If this is a system dependency:
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      
+      If this is a Haskell dependency:
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+in { system, compiler, flags, pkgs, hsPkgs, pkgconfPkgs, ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.12";
+      identifier = { name = "cobot-tools"; version = "0.1.2.1"; };
+      license = "BSD-3-Clause";
+      copyright = "2018-2019, Less Wrong Bio";
+      maintainer = "pavel@yakovlev.me";
+      author = "Pavel Yakovlev, Bogdan Neterebskii, Alexander Sadovnikov";
+      homepage = "https://github.com/less-wrong/cobot-tools#readme";
+      url = "";
+      synopsis = "Biological data file formats and IO";
+      description = "Please see the README on GitHub at <https://github.com/less-wrong/cobot-tools#readme>";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."array" or (buildDepError "array"))
+          (hsPkgs."base" or (buildDepError "base"))
+          (hsPkgs."bytestring" or (buildDepError "bytestring"))
+          (hsPkgs."cobot" or (buildDepError "cobot"))
+          (hsPkgs."containers" or (buildDepError "containers"))
+          (hsPkgs."data-default" or (buildDepError "data-default"))
+          (hsPkgs."data-msgpack" or (buildDepError "data-msgpack"))
+          (hsPkgs."deepseq" or (buildDepError "deepseq"))
+          (hsPkgs."lens" or (buildDepError "lens"))
+          (hsPkgs."mtl" or (buildDepError "mtl"))
+          (hsPkgs."regex-tdfa" or (buildDepError "regex-tdfa"))
+          (hsPkgs."text" or (buildDepError "text"))
+          ];
+        libs = [ (pkgs."RNA" or (sysDepError "RNA")) ];
+        buildable = true;
+        };
+      tests = {
+        "cobot-tools-test" = {
+          depends = [
+            (hsPkgs."QuickCheck" or (buildDepError "QuickCheck"))
+            (hsPkgs."array" or (buildDepError "array"))
+            (hsPkgs."base" or (buildDepError "base"))
+            (hsPkgs."bytestring" or (buildDepError "bytestring"))
+            (hsPkgs."cobot" or (buildDepError "cobot"))
+            (hsPkgs."cobot-tools" or (buildDepError "cobot-tools"))
+            (hsPkgs."containers" or (buildDepError "containers"))
+            (hsPkgs."data-default" or (buildDepError "data-default"))
+            (hsPkgs."data-msgpack" or (buildDepError "data-msgpack"))
+            (hsPkgs."deepseq" or (buildDepError "deepseq"))
+            (hsPkgs."hspec" or (buildDepError "hspec"))
+            (hsPkgs."lens" or (buildDepError "lens"))
+            (hsPkgs."mtl" or (buildDepError "mtl"))
+            (hsPkgs."neat-interpolation" or (buildDepError "neat-interpolation"))
+            (hsPkgs."random" or (buildDepError "random"))
+            (hsPkgs."regex-tdfa" or (buildDepError "regex-tdfa"))
+            (hsPkgs."text" or (buildDepError "text"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
---
+++ hackage/diagrams-svg-1.4.3-r0-d926916f0807b04193c16636b3fe92a43748d62d13fa92420b65ec841a8c6ace.nix
@@ -0,0 +1,81 @@
+let
+  buildDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (build dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  sysDepError = pkg:
+    builtins.throw ''
+      The Nixpkgs package set does not contain the package: ${pkg} (system dependency).
+      
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      '';
+  pkgConfDepError = pkg:
+    builtins.throw ''
+      The pkg-conf packages does not contain the package: ${pkg} (pkg-conf dependency).
+      
+      You may need to augment the pkg-conf package mapping in haskell.nix so that it can be found.
+      '';
+  exeDepError = pkg:
+    builtins.throw ''
+      The local executable components do not include the component: ${pkg} (executable dependency).
+      '';
+  legacyExeDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (executable dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  buildToolDepError = pkg:
+    builtins.throw ''
+      Neither the Haskell package set or the Nixpkgs package set contain the package: ${pkg} (build tool dependency).
+      
+      If this is a system dependency:
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      
+      If this is a Haskell dependency:
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+in { system, compiler, flags, pkgs, hsPkgs, pkgconfPkgs, ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "diagrams-svg"; version = "1.4.3"; };
+      license = "BSD-3-Clause";
+      copyright = "";
+      maintainer = "diagrams-discuss@googlegroups.com";
+      author = "Felipe Lessa, Deepak Jois";
+      homepage = "http://projects.haskell.org/diagrams/";
+      url = "";
+      synopsis = "SVG backend for diagrams drawing EDSL.";
+      description = "This package provides a modular backend for rendering\ndiagrams created with the diagrams EDSL to SVG\nfiles.  It uses @lucid-svg@ to be a native\nHaskell backend, making it suitable for use on\nany platform.\n\nThe package provides the following modules:\n\n* \"Diagrams.Backend.SVG.CmdLine\" - if you're\njust getting started with diagrams, begin here.\n\n* \"Diagrams.Backend.SVG\" - look at this next.\nThe general API for the SVG backend.\n\nAdditional documentation can be found in the\nREADME file distributed with the source tarball or\nviewable on GitHub:\n<https://github.com/diagrams/diagrams-svg/blob/master/README.md>.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (buildDepError "base"))
+          (hsPkgs."filepath" or (buildDepError "filepath"))
+          (hsPkgs."mtl" or (buildDepError "mtl"))
+          (hsPkgs."bytestring" or (buildDepError "bytestring"))
+          (hsPkgs."base64-bytestring" or (buildDepError "base64-bytestring"))
+          (hsPkgs."colour" or (buildDepError "colour"))
+          (hsPkgs."diagrams-core" or (buildDepError "diagrams-core"))
+          (hsPkgs."diagrams-lib" or (buildDepError "diagrams-lib"))
+          (hsPkgs."monoid-extras" or (buildDepError "monoid-extras"))
+          (hsPkgs."svg-builder" or (buildDepError "svg-builder"))
+          (hsPkgs."text" or (buildDepError "text"))
+          (hsPkgs."JuicyPixels" or (buildDepError "JuicyPixels"))
+          (hsPkgs."split" or (buildDepError "split"))
+          (hsPkgs."containers" or (buildDepError "containers"))
+          (hsPkgs."lens" or (buildDepError "lens"))
+          (hsPkgs."hashable" or (buildDepError "hashable"))
+          (hsPkgs."optparse-applicative" or (buildDepError "optparse-applicative"))
+          (hsPkgs."semigroups" or (buildDepError "semigroups"))
+          ] ++ (pkgs.lib).optional (compiler.isGhc && (compiler.version).lt "7.6") (hsPkgs."ghc-prim" or (buildDepError "ghc-prim"));
+        buildable = true;
+        };
+      };
+    }
