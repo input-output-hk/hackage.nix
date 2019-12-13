fba0f3f24d3943cf97b9efa2bdabf57b5ccee89f
IOHK <devops+nix-tools@iohk.io>
2019-12-13 01:09:04 +0000

    Automatic update for Fri Dec 13 01:09:04 UTC 2019

---
+++ hackage/pandoc-crossref-0.3.6.0-r0-33aa5b35e50227d563c8fcf10e2d937a508074cd96869071a444272e016472a2.nix
@@ -0,0 +1,155 @@
+let
+  buildDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (build dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  sysDepError = pkg:
+    builtins.throw ''
+      The Nixpkgs package set does not contain the package: ${pkg} (system dependency).
+      
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      '';
+  pkgConfDepError = pkg:
+    builtins.throw ''
+      The pkg-conf packages does not contain the package: ${pkg} (pkg-conf dependency).
+      
+      You may need to augment the pkg-conf package mapping in haskell.nix so that it can be found.
+      '';
+  exeDepError = pkg:
+    builtins.throw ''
+      The local executable components do not include the component: ${pkg} (executable dependency).
+      '';
+  legacyExeDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (executable dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  buildToolDepError = pkg:
+    builtins.throw ''
+      Neither the Haskell package set or the Nixpkgs package set contain the package: ${pkg} (build tool dependency).
+      
+      If this is a system dependency:
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      
+      If this is a Haskell dependency:
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+in { system, compiler, flags, pkgs, hsPkgs, pkgconfPkgs, ... }:
+  {
+    flags = { enable_flaky_tests = false; };
+    package = {
+      specVersion = "1.12";
+      identifier = { name = "pandoc-crossref"; version = "0.3.6.0"; };
+      license = "GPL-2.0-only";
+      copyright = "";
+      maintainer = "root@livid.pp.ru";
+      author = "Nikolay Yakimov";
+      homepage = "https://github.com/lierdakil/pandoc-crossref#readme";
+      url = "";
+      synopsis = "Pandoc filter for cross-references";
+      description = "pandoc-crossref is a pandoc filter for numbering figures, equations, tables and cross-references to them.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (buildDepError "base"))
+          (hsPkgs."containers" or (buildDepError "containers"))
+          (hsPkgs."data-accessor" or (buildDepError "data-accessor"))
+          (hsPkgs."data-accessor-template" or (buildDepError "data-accessor-template"))
+          (hsPkgs."data-accessor-transformers" or (buildDepError "data-accessor-transformers"))
+          (hsPkgs."data-default" or (buildDepError "data-default"))
+          (hsPkgs."directory" or (buildDepError "directory"))
+          (hsPkgs."filepath" or (buildDepError "filepath"))
+          (hsPkgs."mtl" or (buildDepError "mtl"))
+          (hsPkgs."pandoc" or (buildDepError "pandoc"))
+          (hsPkgs."pandoc-types" or (buildDepError "pandoc-types"))
+          (hsPkgs."roman-numerals" or (buildDepError "roman-numerals"))
+          (hsPkgs."syb" or (buildDepError "syb"))
+          (hsPkgs."template-haskell" or (buildDepError "template-haskell"))
+          (hsPkgs."text" or (buildDepError "text"))
+          (hsPkgs."utility-ht" or (buildDepError "utility-ht"))
+          ];
+        buildable = true;
+        };
+      exes = {
+        "pandoc-crossref" = {
+          depends = [
+            (hsPkgs."base" or (buildDepError "base"))
+            (hsPkgs."containers" or (buildDepError "containers"))
+            (hsPkgs."data-accessor" or (buildDepError "data-accessor"))
+            (hsPkgs."data-accessor-template" or (buildDepError "data-accessor-template"))
+            (hsPkgs."data-accessor-transformers" or (buildDepError "data-accessor-transformers"))
+            (hsPkgs."data-default" or (buildDepError "data-default"))
+            (hsPkgs."deepseq" or (buildDepError "deepseq"))
+            (hsPkgs."directory" or (buildDepError "directory"))
+            (hsPkgs."filepath" or (buildDepError "filepath"))
+            (hsPkgs."gitrev" or (buildDepError "gitrev"))
+            (hsPkgs."mtl" or (buildDepError "mtl"))
+            (hsPkgs."open-browser" or (buildDepError "open-browser"))
+            (hsPkgs."optparse-applicative" or (buildDepError "optparse-applicative"))
+            (hsPkgs."pandoc" or (buildDepError "pandoc"))
+            (hsPkgs."pandoc-crossref" or (buildDepError "pandoc-crossref"))
+            (hsPkgs."pandoc-types" or (buildDepError "pandoc-types"))
+            (hsPkgs."roman-numerals" or (buildDepError "roman-numerals"))
+            (hsPkgs."syb" or (buildDepError "syb"))
+            (hsPkgs."template-haskell" or (buildDepError "template-haskell"))
+            (hsPkgs."temporary" or (buildDepError "temporary"))
+            (hsPkgs."text" or (buildDepError "text"))
+            (hsPkgs."utility-ht" or (buildDepError "utility-ht"))
+            ];
+          buildable = true;
+          };
+        };
+      tests = {
+        "test-integrative" = {
+          depends = [
+            (hsPkgs."base" or (buildDepError "base"))
+            (hsPkgs."containers" or (buildDepError "containers"))
+            (hsPkgs."data-accessor" or (buildDepError "data-accessor"))
+            (hsPkgs."data-accessor-template" or (buildDepError "data-accessor-template"))
+            (hsPkgs."data-accessor-transformers" or (buildDepError "data-accessor-transformers"))
+            (hsPkgs."data-default" or (buildDepError "data-default"))
+            (hsPkgs."directory" or (buildDepError "directory"))
+            (hsPkgs."filepath" or (buildDepError "filepath"))
+            (hsPkgs."hspec" or (buildDepError "hspec"))
+            (hsPkgs."mtl" or (buildDepError "mtl"))
+            (hsPkgs."pandoc" or (buildDepError "pandoc"))
+            (hsPkgs."pandoc-crossref" or (buildDepError "pandoc-crossref"))
+            (hsPkgs."pandoc-types" or (buildDepError "pandoc-types"))
+            (hsPkgs."roman-numerals" or (buildDepError "roman-numerals"))
+            (hsPkgs."syb" or (buildDepError "syb"))
+            (hsPkgs."template-haskell" or (buildDepError "template-haskell"))
+            (hsPkgs."text" or (buildDepError "text"))
+            (hsPkgs."utility-ht" or (buildDepError "utility-ht"))
+            ];
+          buildable = true;
+          };
+        "test-pandoc-crossref" = {
+          depends = [
+            (hsPkgs."base" or (buildDepError "base"))
+            (hsPkgs."containers" or (buildDepError "containers"))
+            (hsPkgs."data-accessor" or (buildDepError "data-accessor"))
+            (hsPkgs."data-accessor-template" or (buildDepError "data-accessor-template"))
+            (hsPkgs."data-accessor-transformers" or (buildDepError "data-accessor-transformers"))
+            (hsPkgs."data-default" or (buildDepError "data-default"))
+            (hsPkgs."directory" or (buildDepError "directory"))
+            (hsPkgs."filepath" or (buildDepError "filepath"))
+            (hsPkgs."hspec" or (buildDepError "hspec"))
+            (hsPkgs."mtl" or (buildDepError "mtl"))
+            (hsPkgs."pandoc" or (buildDepError "pandoc"))
+            (hsPkgs."pandoc-types" or (buildDepError "pandoc-types"))
+            (hsPkgs."roman-numerals" or (buildDepError "roman-numerals"))
+            (hsPkgs."syb" or (buildDepError "syb"))
+            (hsPkgs."template-haskell" or (buildDepError "template-haskell"))
+            (hsPkgs."text" or (buildDepError "text"))
+            (hsPkgs."utility-ht" or (buildDepError "utility-ht"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
---
+++ hackage/tasty-rerun-1.1.15-r0-5907483d0a475dfbdccce293ec75fb668c4d2e0f6d015398729d4f894febb432.nix
@@ -0,0 +1,72 @@
+let
+  buildDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (build dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  sysDepError = pkg:
+    builtins.throw ''
+      The Nixpkgs package set does not contain the package: ${pkg} (system dependency).
+      
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      '';
+  pkgConfDepError = pkg:
+    builtins.throw ''
+      The pkg-conf packages does not contain the package: ${pkg} (pkg-conf dependency).
+      
+      You may need to augment the pkg-conf package mapping in haskell.nix so that it can be found.
+      '';
+  exeDepError = pkg:
+    builtins.throw ''
+      The local executable components do not include the component: ${pkg} (executable dependency).
+      '';
+  legacyExeDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (executable dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  buildToolDepError = pkg:
+    builtins.throw ''
+      Neither the Haskell package set or the Nixpkgs package set contain the package: ${pkg} (build tool dependency).
+      
+      If this is a system dependency:
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      
+      If this is a Haskell dependency:
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+in { system, compiler, flags, pkgs, hsPkgs, pkgconfPkgs, ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "tasty-rerun"; version = "1.1.15"; };
+      license = "BSD-3-Clause";
+      copyright = "Oliver Charles (c) 2014,\nAndrew Lelechenko (c) 2019";
+      maintainer = "ollie@ocharles.org.uk";
+      author = "Oliver Charles";
+      homepage = "http://github.com/ocharles/tasty-rerun";
+      url = "";
+      synopsis = "Rerun only tests which failed in a previous test run";
+      description = "This ingredient adds the ability to run tests by first filtering the test tree\nbased on the result of a previous test run. For example, you can use this to\nrun only those tests that failed in the last run, or to run only tests that\nhave been added since tests were last ran.\n\nThis ingredient is specifically an ingredient *transformer* - given a list of\n'Tasty.Ingredient's, 'rerunningTests' adds the ability for all of these\ningredients to run against a filtered test tree. This transformer can be\napplied as follows:\n\n> import Test.Tasty\n> import Test.Tasty.Runners\n> import Test.Tasty.Ingredients.Rerun\n>\n> main :: IO ()\n> main =\n>   defaultMainWithIngredients\n>     [ rerunningTests [ listingTests, consoleTestReporter ] ]\n>     tests\n>\n> tests :: TestTree\n> tests = undefined\n\nThis ingredient adds three command line parameters:\n\n[@--rerun-update@] If specified the results of this test run will be saved to\nthe log file at @--rerun-log-file@. If the ingredient does not execute tests\n(for example, @--list-tests@ is used) then the log file will not be\nupdated. This option is not enabled by default.  This option does not require\na value.\n\n[@--rerun-log-file@] The path to the log file to read previous test\ninformation from, and where to write new information to (if @--rerun-update@\nis specified). This option defaults to @.tasty-rerun-log@.\n\n[@--rerun-filter@] Which filters to apply to the 'Tasty.TestTree' based on\nprevious test runs. The value of this option is a comma separated list of the\nfollowing options:\n\n* @failures@: Only run tests that failed on the previous run.\n\n* @exceptions@: Only run tests that threw an exception on the previous run.\n\n* @new@: Only run tests that are new since the previous test run.\n\n* @successful@: Only run tests that were successful in the previous run.\n\nMultiple options can be combined and will be taken under disjunction - so\n@--rerun-filter=failures,exceptions@ will run only tests that failed *or*\nthrew an exception on the last run.\n\nDefaults to all filters, which means all tests will be ran.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (buildDepError "base"))
+          (hsPkgs."containers" or (buildDepError "containers"))
+          (hsPkgs."mtl" or (buildDepError "mtl"))
+          (hsPkgs."optparse-applicative" or (buildDepError "optparse-applicative"))
+          (hsPkgs."split" or (buildDepError "split"))
+          (hsPkgs."stm" or (buildDepError "stm"))
+          (hsPkgs."tagged" or (buildDepError "tagged"))
+          (hsPkgs."tasty" or (buildDepError "tasty"))
+          (hsPkgs."transformers" or (buildDepError "transformers"))
+          ];
+        buildable = true;
+        };
+      };
+    }
