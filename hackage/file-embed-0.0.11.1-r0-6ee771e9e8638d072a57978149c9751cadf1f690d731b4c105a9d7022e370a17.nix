ed290bdb9a9af9b3ffb893518f02616548702beb
IOHK <devops+nix-tools@iohk.io>
2019-12-20 01:06:51 +0000

    Automatic update for Fri Dec 20 01:06:51 UTC 2019

---
+++ hackage/file-embed-0.0.11.1-r0-6ee771e9e8638d072a57978149c9751cadf1f690d731b4c105a9d7022e370a17.nix
@@ -0,0 +1,78 @@
+let
+  buildDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (build dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  sysDepError = pkg:
+    builtins.throw ''
+      The Nixpkgs package set does not contain the package: ${pkg} (system dependency).
+      
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      '';
+  pkgConfDepError = pkg:
+    builtins.throw ''
+      The pkg-conf packages does not contain the package: ${pkg} (pkg-conf dependency).
+      
+      You may need to augment the pkg-conf package mapping in haskell.nix so that it can be found.
+      '';
+  exeDepError = pkg:
+    builtins.throw ''
+      The local executable components do not include the component: ${pkg} (executable dependency).
+      '';
+  legacyExeDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (executable dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  buildToolDepError = pkg:
+    builtins.throw ''
+      Neither the Haskell package set or the Nixpkgs package set contain the package: ${pkg} (build tool dependency).
+      
+      If this is a system dependency:
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      
+      If this is a Haskell dependency:
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+in { system, compiler, flags, pkgs, hsPkgs, pkgconfPkgs, ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.8";
+      identifier = { name = "file-embed"; version = "0.0.11.1"; };
+      license = "BSD-3-Clause";
+      copyright = "";
+      maintainer = "Michael Snoyman <michael@snoyman.com>";
+      author = "Michael Snoyman <michael@snoyman.com>";
+      homepage = "https://github.com/snoyberg/file-embed";
+      url = "";
+      synopsis = "Use Template Haskell to embed file contents directly.";
+      description = "Use Template Haskell to read a file or all the files in a\ndirectory, and turn them into (path, bytestring) pairs\nembedded in your Haskell code.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (buildDepError "base"))
+          (hsPkgs."bytestring" or (buildDepError "bytestring"))
+          (hsPkgs."directory" or (buildDepError "directory"))
+          (hsPkgs."template-haskell" or (buildDepError "template-haskell"))
+          (hsPkgs."filepath" or (buildDepError "filepath"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "test" = {
+          depends = [
+            (hsPkgs."base" or (buildDepError "base"))
+            (hsPkgs."file-embed" or (buildDepError "file-embed"))
+            (hsPkgs."filepath" or (buildDepError "filepath"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
---
+++ hackage/tcod-haskell-0.3.0.0-r0-88fc46549fff78a66a1f70bd6ed6b613ec3f221ebf0851d61dd33e762f294ac3.nix
@@ -0,0 +1,98 @@
+let
+  buildDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (build dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  sysDepError = pkg:
+    builtins.throw ''
+      The Nixpkgs package set does not contain the package: ${pkg} (system dependency).
+      
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      '';
+  pkgConfDepError = pkg:
+    builtins.throw ''
+      The pkg-conf packages does not contain the package: ${pkg} (pkg-conf dependency).
+      
+      You may need to augment the pkg-conf package mapping in haskell.nix so that it can be found.
+      '';
+  exeDepError = pkg:
+    builtins.throw ''
+      The local executable components do not include the component: ${pkg} (executable dependency).
+      '';
+  legacyExeDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (executable dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  buildToolDepError = pkg:
+    builtins.throw ''
+      Neither the Haskell package set or the Nixpkgs package set contain the package: ${pkg} (build tool dependency).
+      
+      If this is a system dependency:
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      
+      If this is a Haskell dependency:
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+in { system, compiler, flags, pkgs, hsPkgs, pkgconfPkgs, ... }:
+  {
+    flags = { examples = false; use-pkg-config = true; };
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "tcod-haskell"; version = "0.3.0.0"; };
+      license = "BSD-3-Clause";
+      copyright = "2017 Anton Gushcha";
+      maintainer = "ncrashed@gmail.com";
+      author = "Anton Gushcha";
+      homepage = "https://github.com/ncrashed/tcod-haskell#readme";
+      url = "";
+      synopsis = "Bindings to libtcod roguelike engine";
+      description = "Haskell bindings for popular library for making roguelike games";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (buildDepError "base"))
+          (hsPkgs."bytestring" or (buildDepError "bytestring"))
+          (hsPkgs."containers" or (buildDepError "containers"))
+          (hsPkgs."inline-c" or (buildDepError "inline-c"))
+          (hsPkgs."repa" or (buildDepError "repa"))
+          (hsPkgs."sdl2" or (buildDepError "sdl2"))
+          (hsPkgs."vector" or (buildDepError "vector"))
+          ];
+        libs = (pkgs.lib).optional (!flags.use-pkg-config) (pkgs."tcod" or (sysDepError "tcod"));
+        pkgconfig = (pkgs.lib).optional (flags.use-pkg-config) (pkgconfPkgs."libtcod" or (pkgConfDepError "libtcod"));
+        buildable = true;
+        };
+      exes = {
+        "tcod-haskell-sample01" = {
+          depends = [
+            (hsPkgs."base" or (buildDepError "base"))
+            (hsPkgs."tcod-haskell" or (buildDepError "tcod-haskell"))
+            ];
+          libs = [ (pkgs."tcod" or (sysDepError "tcod")) ];
+          buildable = if flags.examples then true else false;
+          };
+        "tcod-haskell-sample02" = {
+          depends = [
+            (hsPkgs."base" or (buildDepError "base"))
+            (hsPkgs."tcod-haskell" or (buildDepError "tcod-haskell"))
+            ];
+          libs = [ (pkgs."tcod" or (sysDepError "tcod")) ];
+          buildable = if flags.examples then true else false;
+          };
+        "tcod-haskell-sample03" = {
+          depends = [
+            (hsPkgs."base" or (buildDepError "base"))
+            (hsPkgs."tcod-haskell" or (buildDepError "tcod-haskell"))
+            ];
+          libs = [ (pkgs."tcod" or (sysDepError "tcod")) ];
+          buildable = if flags.examples then true else false;
+          };
+        };
+      };
+    }
