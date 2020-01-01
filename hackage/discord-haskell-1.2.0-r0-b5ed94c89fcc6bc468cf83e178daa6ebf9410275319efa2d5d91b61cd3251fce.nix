789f1d57b5f9ffeda1a2529672f6779e55323abc
IOHK <devops+nix-tools@iohk.io>
2020-01-01 02:23:58 +0000

    Automatic update for Wed Jan  1 02:23:58 UTC 2020

---
+++ hackage/discord-haskell-1.2.0-r0-b5ed94c89fcc6bc468cf83e178daa6ebf9410275319efa2d5d91b61cd3251fce.nix
@@ -0,0 +1,93 @@
+let
+  buildDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (build dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  sysDepError = pkg:
+    builtins.throw ''
+      The Nixpkgs package set does not contain the package: ${pkg} (system dependency).
+      
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      '';
+  pkgConfDepError = pkg:
+    builtins.throw ''
+      The pkg-conf packages does not contain the package: ${pkg} (pkg-conf dependency).
+      
+      You may need to augment the pkg-conf package mapping in haskell.nix so that it can be found.
+      '';
+  exeDepError = pkg:
+    builtins.throw ''
+      The local executable components do not include the component: ${pkg} (executable dependency).
+      '';
+  legacyExeDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (executable dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  buildToolDepError = pkg:
+    builtins.throw ''
+      Neither the Haskell package set or the Nixpkgs package set contain the package: ${pkg} (build tool dependency).
+      
+      If this is a system dependency:
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      
+      If this is a Haskell dependency:
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+in { system, compiler, flags, pkgs, hsPkgs, pkgconfPkgs, ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "2.0";
+      identifier = { name = "discord-haskell"; version = "1.2.0"; };
+      license = "MIT";
+      copyright = "2019 Karl";
+      maintainer = "ksfish5@gmail.com";
+      author = "Karl";
+      homepage = "https://github.com/aquarial/discord-haskell";
+      url = "";
+      synopsis = "Write bots for Discord in Haskell";
+      description = "Functions and data types to write discord bots.\nOfficial discord docs <https://discordapp.com/developers/docs/reference>.\n\nSee the project readme for quickstart notes\n<https://github.com/aquarial/discord-haskell#discord-haskell->";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (buildDepError "base"))
+          (hsPkgs."aeson" or (buildDepError "aeson"))
+          (hsPkgs."async" or (buildDepError "async"))
+          (hsPkgs."bytestring" or (buildDepError "bytestring"))
+          (hsPkgs."base64-bytestring" or (buildDepError "base64-bytestring"))
+          (hsPkgs."containers" or (buildDepError "containers"))
+          (hsPkgs."data-default" or (buildDepError "data-default"))
+          (hsPkgs."emoji" or (buildDepError "emoji"))
+          (hsPkgs."http-client" or (buildDepError "http-client"))
+          (hsPkgs."iso8601-time" or (buildDepError "iso8601-time"))
+          (hsPkgs."MonadRandom" or (buildDepError "MonadRandom"))
+          (hsPkgs."req" or (buildDepError "req"))
+          (hsPkgs."JuicyPixels" or (buildDepError "JuicyPixels"))
+          (hsPkgs."safe-exceptions" or (buildDepError "safe-exceptions"))
+          (hsPkgs."text" or (buildDepError "text"))
+          (hsPkgs."time" or (buildDepError "time"))
+          (hsPkgs."unordered-containers" or (buildDepError "unordered-containers"))
+          (hsPkgs."vector" or (buildDepError "vector"))
+          (hsPkgs."websockets" or (buildDepError "websockets"))
+          (hsPkgs."wuss" or (buildDepError "wuss"))
+          ];
+        buildable = true;
+        };
+      exes = {
+        "ping-pong" = {
+          depends = [
+            (hsPkgs."base" or (buildDepError "base"))
+            (hsPkgs."text" or (buildDepError "text"))
+            (hsPkgs."discord-haskell" or (buildDepError "discord-haskell"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
---
+++ hackage/network-transport-tcp-0.7.0-r0-b526d26714bd7f43222e552fdd35f4f9fa7e7753c460ee41ea9a94495b90f9b5.nix
@@ -0,0 +1,105 @@
+let
+  buildDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (build dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  sysDepError = pkg:
+    builtins.throw ''
+      The Nixpkgs package set does not contain the package: ${pkg} (system dependency).
+      
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      '';
+  pkgConfDepError = pkg:
+    builtins.throw ''
+      The pkg-conf packages does not contain the package: ${pkg} (pkg-conf dependency).
+      
+      You may need to augment the pkg-conf package mapping in haskell.nix so that it can be found.
+      '';
+  exeDepError = pkg:
+    builtins.throw ''
+      The local executable components do not include the component: ${pkg} (executable dependency).
+      '';
+  legacyExeDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (executable dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  buildToolDepError = pkg:
+    builtins.throw ''
+      Neither the Haskell package set or the Nixpkgs package set contain the package: ${pkg} (build tool dependency).
+      
+      If this is a system dependency:
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      
+      If this is a Haskell dependency:
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+in { system, compiler, flags, pkgs, hsPkgs, pkgconfPkgs, ... }:
+  {
+    flags = { use-mock-network = false; };
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "network-transport-tcp"; version = "0.7.0"; };
+      license = "BSD-3-Clause";
+      copyright = "Well-Typed LLP, Tweag I/O Limited";
+      maintainer = "Facundo Domínguez <facundo.dominguez@tweag.io>";
+      author = "Duncan Coutts, Nicolas Wu, Edsko de Vries";
+      homepage = "http://haskell-distributed.github.com";
+      url = "";
+      synopsis = "TCP instantiation of Network.Transport";
+      description = "TCP instantiation of Network.Transport";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (buildDepError "base"))
+          (hsPkgs."async" or (buildDepError "async"))
+          (hsPkgs."network-transport" or (buildDepError "network-transport"))
+          (hsPkgs."data-accessor" or (buildDepError "data-accessor"))
+          (hsPkgs."containers" or (buildDepError "containers"))
+          (hsPkgs."bytestring" or (buildDepError "bytestring"))
+          (hsPkgs."network" or (buildDepError "network"))
+          (hsPkgs."uuid" or (buildDepError "uuid"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "TestTCP" = {
+          depends = [
+            (hsPkgs."base" or (buildDepError "base"))
+            (hsPkgs."bytestring" or (buildDepError "bytestring"))
+            (hsPkgs."network-transport-tests" or (buildDepError "network-transport-tests"))
+            (hsPkgs."network" or (buildDepError "network"))
+            (hsPkgs."network-transport" or (buildDepError "network-transport"))
+            (hsPkgs."network-transport-tcp" or (buildDepError "network-transport-tcp"))
+            ];
+          buildable = true;
+          };
+        "TestQC" = {
+          depends = (pkgs.lib).optionals (flags.use-mock-network) [
+            (hsPkgs."base" or (buildDepError "base"))
+            (hsPkgs."test-framework" or (buildDepError "test-framework"))
+            (hsPkgs."test-framework-quickcheck2" or (buildDepError "test-framework-quickcheck2"))
+            (hsPkgs."test-framework-hunit" or (buildDepError "test-framework-hunit"))
+            (hsPkgs."QuickCheck" or (buildDepError "QuickCheck"))
+            (hsPkgs."HUnit" or (buildDepError "HUnit"))
+            (hsPkgs."network-transport" or (buildDepError "network-transport"))
+            (hsPkgs."network-transport-tcp" or (buildDepError "network-transport-tcp"))
+            (hsPkgs."containers" or (buildDepError "containers"))
+            (hsPkgs."bytestring" or (buildDepError "bytestring"))
+            (hsPkgs."pretty" or (buildDepError "pretty"))
+            (hsPkgs."data-accessor" or (buildDepError "data-accessor"))
+            (hsPkgs."data-accessor-transformers" or (buildDepError "data-accessor-transformers"))
+            (hsPkgs."mtl" or (buildDepError "mtl"))
+            (hsPkgs."transformers" or (buildDepError "transformers"))
+            (hsPkgs."lockfree-queue" or (buildDepError "lockfree-queue"))
+            ];
+          buildable = if flags.use-mock-network then true else false;
+          };
+        };
+      };
+    }
