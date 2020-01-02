16d2c16d59c8610c67702246c661c35010bc4b82
IOHK <devops+nix-tools@iohk.io>
2020-01-02 01:26:01 +0000

    Automatic update for Thu Jan  2 01:26:01 UTC 2020

---
+++ hackage/bitwise-enum-0.1.0.1-r0-6148fe28c27f17ac5fa2b2fed4cb1290a41cddfe095c7f424519781140df46d1.nix
@@ -0,0 +1,100 @@
+let
+  buildDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (build dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  sysDepError = pkg:
+    builtins.throw ''
+      The Nixpkgs package set does not contain the package: ${pkg} (system dependency).
+      
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      '';
+  pkgConfDepError = pkg:
+    builtins.throw ''
+      The pkg-conf packages does not contain the package: ${pkg} (pkg-conf dependency).
+      
+      You may need to augment the pkg-conf package mapping in haskell.nix so that it can be found.
+      '';
+  exeDepError = pkg:
+    builtins.throw ''
+      The local executable components do not include the component: ${pkg} (executable dependency).
+      '';
+  legacyExeDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (executable dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  buildToolDepError = pkg:
+    builtins.throw ''
+      Neither the Haskell package set or the Nixpkgs package set contain the package: ${pkg} (build tool dependency).
+      
+      If this is a system dependency:
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      
+      If this is a Haskell dependency:
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+in { system, compiler, flags, pkgs, hsPkgs, pkgconfPkgs, ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.12";
+      identifier = { name = "bitwise-enum"; version = "0.1.0.1"; };
+      license = "BSD-3-Clause";
+      copyright = "";
+      maintainer = "Joshua Booth <joshua.n.booth@gmail.com>";
+      author = "Joshua Booth <joshua.n.booth@gmail.com>";
+      homepage = "https://github.com/jnbooth/bitwise-enum#readme";
+      url = "";
+      synopsis = "Bitwise operations on bounded enumerations";
+      description = "Bitwise operations on bounded enumerations.\n\n[\"Data.Enum.Set\"] Constant-time sets using bit flags.\n\n[\"Data.Enum.Memo\"] Constant-time lookup memoization for functions on enumerated types.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."aeson" or (buildDepError "aeson"))
+          (hsPkgs."array" or (buildDepError "array"))
+          (hsPkgs."base" or (buildDepError "base"))
+          (hsPkgs."deepseq" or (buildDepError "deepseq"))
+          (hsPkgs."mono-traversable" or (buildDepError "mono-traversable"))
+          (hsPkgs."vector" or (buildDepError "vector"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "enumset-test-suite" = {
+          depends = [
+            (hsPkgs."QuickCheck" or (buildDepError "QuickCheck"))
+            (hsPkgs."aeson" or (buildDepError "aeson"))
+            (hsPkgs."base" or (buildDepError "base"))
+            (hsPkgs."bitwise-enum" or (buildDepError "bitwise-enum"))
+            (hsPkgs."deepseq" or (buildDepError "deepseq"))
+            (hsPkgs."mono-traversable" or (buildDepError "mono-traversable"))
+            (hsPkgs."test-framework" or (buildDepError "test-framework"))
+            (hsPkgs."test-framework-quickcheck2" or (buildDepError "test-framework-quickcheck2"))
+            (hsPkgs."vector" or (buildDepError "vector"))
+            ];
+          buildable = true;
+          };
+        };
+      benchmarks = {
+        "enumset-benchmarks" = {
+          depends = [
+            (hsPkgs."aeson" or (buildDepError "aeson"))
+            (hsPkgs."base" or (buildDepError "base"))
+            (hsPkgs."bitwise-enum" or (buildDepError "bitwise-enum"))
+            (hsPkgs."deepseq" or (buildDepError "deepseq"))
+            (hsPkgs."gauge" or (buildDepError "gauge"))
+            (hsPkgs."mono-traversable" or (buildDepError "mono-traversable"))
+            (hsPkgs."vector" or (buildDepError "vector"))
+            (hsPkgs."wide-word" or (buildDepError "wide-word"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
---
+++ hackage/exp-extended-0.2-r0-3462dc900cc91b863af6b183f3cd7df722eaace300da21f2d97d95654cd1a18d.nix
@@ -0,0 +1,62 @@
+let
+  buildDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (build dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  sysDepError = pkg:
+    builtins.throw ''
+      The Nixpkgs package set does not contain the package: ${pkg} (system dependency).
+      
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      '';
+  pkgConfDepError = pkg:
+    builtins.throw ''
+      The pkg-conf packages does not contain the package: ${pkg} (pkg-conf dependency).
+      
+      You may need to augment the pkg-conf package mapping in haskell.nix so that it can be found.
+      '';
+  exeDepError = pkg:
+    builtins.throw ''
+      The local executable components do not include the component: ${pkg} (executable dependency).
+      '';
+  legacyExeDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (executable dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  buildToolDepError = pkg:
+    builtins.throw ''
+      Neither the Haskell package set or the Nixpkgs package set contain the package: ${pkg} (build tool dependency).
+      
+      If this is a system dependency:
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      
+      If this is a Haskell dependency:
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+in { system, compiler, flags, pkgs, hsPkgs, pkgconfPkgs, ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "exp-extended"; version = "0.2"; };
+      license = "BSD-3-Clause";
+      copyright = "2015,2016,2019,2020 Claude Heiland-Allen";
+      maintainer = "claude@mathr.co.uk";
+      author = "Claude Heiland-Allen";
+      homepage = "https://code.mathr.co.uk/exp-extended";
+      url = "";
+      synopsis = "floating point with extended exponent range";
+      description = "A small library to extend floating point types with a larger\nexponent, so that you can represent really huge or really tiny\nnumbers without overflow to infinity or underflow to zero.\n\n> > unExpExtended . log . exp .          expExtended' \$ 1000\n> 1000.0\n> >                 log . exp                         \$ 1000\n> Infinity\n> > unExpExtended . log . exp . negate . expExtended' \$ 1000\n> -1000.0\n> >                 log . exp . negate                \$ 1000\n> -Infinity\n\nVersion 0.2 has lighter dependencies and fixes an overflow bug in expm1.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [ (hsPkgs."base" or (buildDepError "base")) ];
+        buildable = true;
+        };
+      };
+    }
