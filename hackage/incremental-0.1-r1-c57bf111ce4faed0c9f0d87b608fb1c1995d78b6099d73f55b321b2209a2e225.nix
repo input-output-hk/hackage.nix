fe2e6bb5d1ba7d11c98b07a9fbdf6828b71090bf
IOHK <devops+nix-tools@iohk.io>
2020-01-12 01:09:22 +0000

    Automatic update for Sun Jan 12 01:09:21 UTC 2020

---
+++ hackage/incremental-0.1-r1-c57bf111ce4faed0c9f0d87b608fb1c1995d78b6099d73f55b321b2209a2e225.nix
@@ -0,0 +1,79 @@
+let
+  buildDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (build dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  sysDepError = pkg:
+    builtins.throw ''
+      The Nixpkgs package set does not contain the package: ${pkg} (system dependency).
+      
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      '';
+  pkgConfDepError = pkg:
+    builtins.throw ''
+      The pkg-conf packages does not contain the package: ${pkg} (pkg-conf dependency).
+      
+      You may need to augment the pkg-conf package mapping in haskell.nix so that it can be found.
+      '';
+  exeDepError = pkg:
+    builtins.throw ''
+      The local executable components do not include the component: ${pkg} (executable dependency).
+      '';
+  legacyExeDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (executable dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  buildToolDepError = pkg:
+    builtins.throw ''
+      Neither the Haskell package set or the Nixpkgs package set contain the package: ${pkg} (build tool dependency).
+      
+      If this is a system dependency:
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      
+      If this is a Haskell dependency:
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+in { system, compiler, flags, pkgs, hsPkgs, pkgconfPkgs, ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "incremental"; version = "0.1"; };
+      license = "BSD-3-Clause";
+      copyright = "2017 Fumiaki Kinoshita";
+      maintainer = "fumiexcel@gmail.com";
+      author = "Fumiaki Kinoshita";
+      homepage = "https://github.com/fumieval/incremental#readme";
+      url = "";
+      synopsis = "incremental update library";
+      description = "Generic interface for incremental updates";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (buildDepError "base"))
+          (hsPkgs."containers" or (buildDepError "containers"))
+          (hsPkgs."semigroups" or (buildDepError "semigroups"))
+          (hsPkgs."deepseq" or (buildDepError "deepseq"))
+          (hsPkgs."extensible" or (buildDepError "extensible"))
+          (hsPkgs."aeson" or (buildDepError "aeson"))
+          (hsPkgs."text" or (buildDepError "text"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "incremental-test" = {
+          depends = [
+            (hsPkgs."base" or (buildDepError "base"))
+            (hsPkgs."incremental" or (buildDepError "incremental"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
