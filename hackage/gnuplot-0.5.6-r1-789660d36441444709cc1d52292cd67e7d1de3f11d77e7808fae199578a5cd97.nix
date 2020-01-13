ff9f61885f6cf9c1a9aec92736fbbff92ba08842
IOHK <devops+nix-tools@iohk.io>
2020-01-13 01:09:35 +0000

    Automatic update for Mon Jan 13 01:09:35 UTC 2020

---
+++ hackage/gnuplot-0.5.6-r1-789660d36441444709cc1d52292cd67e7d1de3f11d77e7808fae199578a5cd97.nix
@@ -0,0 +1,93 @@
+let
+  buildDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (build dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  sysDepError = pkg:
+    builtins.throw ''
+      The Nixpkgs package set does not contain the package: ${pkg} (system dependency).
+      
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      '';
+  pkgConfDepError = pkg:
+    builtins.throw ''
+      The pkg-conf packages does not contain the package: ${pkg} (pkg-conf dependency).
+      
+      You may need to augment the pkg-conf package mapping in haskell.nix so that it can be found.
+      '';
+  exeDepError = pkg:
+    builtins.throw ''
+      The local executable components do not include the component: ${pkg} (executable dependency).
+      '';
+  legacyExeDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (executable dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  buildToolDepError = pkg:
+    builtins.throw ''
+      Neither the Haskell package set or the Nixpkgs package set contain the package: ${pkg} (build tool dependency).
+      
+      If this is a system dependency:
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      
+      If this is a Haskell dependency:
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+in { system, compiler, flags, pkgs, hsPkgs, pkgconfPkgs, ... }:
+  {
+    flags = {
+      buildexamples = false;
+      executepipe = false;
+      executeshell = false;
+      };
+    package = {
+      specVersion = "1.8";
+      identifier = { name = "gnuplot"; version = "0.5.6"; };
+      license = "BSD-3-Clause";
+      copyright = "";
+      maintainer = "Henning Thielemann <haskell@henning-thielemann.de>";
+      author = "Henning Thielemann <haskell@henning-thielemann.de>";
+      homepage = "http://www.haskell.org/haskellwiki/Gnuplot";
+      url = "http://code.haskell.org/gnuplot/";
+      synopsis = "2D and 3D plots using gnuplot";
+      description = "This is a wrapper to gnuplot\nwhich lets you create 2D and 3D plots.\n\nStart a simple session with @make ghci@.\nThis loads the module \"Graphics.Gnuplot.Simple\"\nwhich is ready for use in GHCi.\nIt does not address all fancy gnuplot features\nin order to stay simple.\nFor more sophisticated plots,\nespecially batch generated graphics,\nI recommend \"Graphics.Gnuplot.Advanced\".\nThis module contains also an overview\nof the hierarchy of objects.\n\nExamples for using this interface can be found in the \"Demo\" module.\nIn order to compile this and install an example data file,\nuse the Cabal flag @buildExamples@:\n\n> \$ cabal install -fbuildExamples gnuplot\n\nWith the Cabal flags executePipe and executeShell\nyou can switch to more convenient\nbut probably less portable ways\nof feeding gnuplot with a script.\n\nAlternative packages: @plot@, @HPlot@, @Chart@, @textPlot@, @easyplot@";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."filepath" or (buildDepError "filepath"))
+          (hsPkgs."temporary" or (buildDepError "temporary"))
+          (hsPkgs."array" or (buildDepError "array"))
+          (hsPkgs."containers" or (buildDepError "containers"))
+          (hsPkgs."utility-ht" or (buildDepError "utility-ht"))
+          (hsPkgs."data-accessor-transformers" or (buildDepError "data-accessor-transformers"))
+          (hsPkgs."data-accessor" or (buildDepError "data-accessor"))
+          (hsPkgs."transformers" or (buildDepError "transformers"))
+          (hsPkgs."semigroups" or (buildDepError "semigroups"))
+          (hsPkgs."deepseq" or (buildDepError "deepseq"))
+          (hsPkgs."process" or (buildDepError "process"))
+          (hsPkgs."time" or (buildDepError "time"))
+          (hsPkgs."base" or (buildDepError "base"))
+          ];
+        buildable = true;
+        };
+      exes = {
+        "gnuplot-demo" = {
+          depends = (pkgs.lib).optionals (flags.buildexamples) [
+            (hsPkgs."gnuplot" or (buildDepError "gnuplot"))
+            (hsPkgs."time" or (buildDepError "time"))
+            (hsPkgs."array" or (buildDepError "array"))
+            (hsPkgs."filepath" or (buildDepError "filepath"))
+            (hsPkgs."utility-ht" or (buildDepError "utility-ht"))
+            (hsPkgs."base" or (buildDepError "base"))
+            ];
+          buildable = if flags.buildexamples then true else false;
+          };
+        };
+      };
+    }
---
+++ hackage/hsinstall-2.6-r0-c9be8d032714202d032edc6a2ccfad49228d2da652dd32235bc336383fbc4363.nix
@@ -0,0 +1,84 @@
+let
+  buildDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (build dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  sysDepError = pkg:
+    builtins.throw ''
+      The Nixpkgs package set does not contain the package: ${pkg} (system dependency).
+      
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      '';
+  pkgConfDepError = pkg:
+    builtins.throw ''
+      The pkg-conf packages does not contain the package: ${pkg} (pkg-conf dependency).
+      
+      You may need to augment the pkg-conf package mapping in haskell.nix so that it can be found.
+      '';
+  exeDepError = pkg:
+    builtins.throw ''
+      The local executable components do not include the component: ${pkg} (executable dependency).
+      '';
+  legacyExeDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (executable dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  buildToolDepError = pkg:
+    builtins.throw ''
+      Neither the Haskell package set or the Nixpkgs package set contain the package: ${pkg} (build tool dependency).
+      
+      If this is a system dependency:
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      
+      If this is a Haskell dependency:
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+in { system, compiler, flags, pkgs, hsPkgs, pkgconfPkgs, ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.12";
+      identifier = { name = "hsinstall"; version = "2.6"; };
+      license = "ISC";
+      copyright = "2016-2020 Dino Morelli";
+      maintainer = "Dino Morelli <dino@ui3.info>";
+      author = "Dino Morelli";
+      homepage = "https://github.com/dino-/hsinstall#readme";
+      url = "";
+      synopsis = "Install Haskell software";
+      description = "This is a tool for deploying software projects into directory structures suitable for installation on a system. It builds upon the `stack install` command and adds more features. It's also a tool for easier AppImage creation.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (buildDepError "base"))
+          (hsPkgs."directory" or (buildDepError "directory"))
+          (hsPkgs."filepath" or (buildDepError "filepath"))
+          ];
+        buildable = true;
+        };
+      exes = {
+        "hsinstall" = {
+          depends = [
+            (hsPkgs."Cabal" or (buildDepError "Cabal"))
+            (hsPkgs."ansi-wl-pprint" or (buildDepError "ansi-wl-pprint"))
+            (hsPkgs."base" or (buildDepError "base"))
+            (hsPkgs."directory" or (buildDepError "directory"))
+            (hsPkgs."filepath" or (buildDepError "filepath"))
+            (hsPkgs."heredoc" or (buildDepError "heredoc"))
+            (hsPkgs."hsinstall" or (buildDepError "hsinstall"))
+            (hsPkgs."optparse-applicative" or (buildDepError "optparse-applicative"))
+            (hsPkgs."process" or (buildDepError "process"))
+            (hsPkgs."safe-exceptions" or (buildDepError "safe-exceptions"))
+            (hsPkgs."transformers" or (buildDepError "transformers"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
