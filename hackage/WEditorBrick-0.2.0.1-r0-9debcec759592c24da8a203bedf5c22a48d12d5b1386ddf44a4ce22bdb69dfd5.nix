e13aa5882c8b2ef3f1f21eae36c6212c312048a9
IOHK <devops+nix-tools@iohk.io>
2020-04-21 01:15:22 +0000

    Automatic update for Tue Apr 21 01:15:21 UTC 2020

---
+++ hackage/WEditorBrick-0.2.0.1-r0-9debcec759592c24da8a203bedf5c22a48d12d5b1386ddf44a4ce22bdb69dfd5.nix
@@ -0,0 +1,80 @@
+let
+  buildDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (build dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  sysDepError = pkg:
+    builtins.throw ''
+      The Nixpkgs package set does not contain the package: ${pkg} (system dependency).
+      
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      '';
+  pkgConfDepError = pkg:
+    builtins.throw ''
+      The pkg-conf packages does not contain the package: ${pkg} (pkg-conf dependency).
+      
+      You may need to augment the pkg-conf package mapping in haskell.nix so that it can be found.
+      '';
+  exeDepError = pkg:
+    builtins.throw ''
+      The local executable components do not include the component: ${pkg} (executable dependency).
+      '';
+  legacyExeDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (executable dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  buildToolDepError = pkg:
+    builtins.throw ''
+      Neither the Haskell package set or the Nixpkgs package set contain the package: ${pkg} (build tool dependency).
+      
+      If this is a system dependency:
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      
+      If this is a Haskell dependency:
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+in { system, compiler, flags, pkgs, hsPkgs, pkgconfPkgs, ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "2.0";
+      identifier = { name = "WEditorBrick"; version = "0.2.0.1"; };
+      license = "Apache-2.0";
+      copyright = "(c) Kevin P. Barry 2020";
+      maintainer = "Kevin P. Barry <ta0kira@gmail.com>";
+      author = "Kevin P. Barry";
+      homepage = "https://github.com/ta0kira/wrapping-editor";
+      url = "";
+      synopsis = "Text-editor widget with dynamic line-wrapping for use with Brick.";
+      description = "This package provides a text-editor widget for\n@<http://hackage.haskell.org/package/brick brick>@  that supports line\nwrapping with dynamic resizing. The editor functionality is extensible (e.g.,\ncustom wrapping and hyphenation) via the\n@<http://hackage.haskell.org/package/WEditor WEditor>@ package.\n\nAlso see @<http://hackage.haskell.org/package/WEditorHyphen WEditorHyphen>@\nfor  language-specific hyphenation rules.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (buildDepError "base"))
+          (hsPkgs."brick" or (buildDepError "brick"))
+          (hsPkgs."vty" or (buildDepError "vty"))
+          (hsPkgs."microlens" or (buildDepError "microlens"))
+          (hsPkgs."WEditor" or (buildDepError "WEditor"))
+          ];
+        buildable = true;
+        };
+      exes = {
+        "brick-example" = {
+          depends = [
+            (hsPkgs."base" or (buildDepError "base"))
+            (hsPkgs."brick" or (buildDepError "brick"))
+            (hsPkgs."vty" or (buildDepError "vty"))
+            (hsPkgs."WEditorBrick" or (buildDepError "WEditorBrick"))
+            (hsPkgs."WEditor" or (buildDepError "WEditor"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
---
+++ hackage/bitwise-1.0.0.1-r3-b5c988b1ceba6c8a7b1fa4b2aa8c69d8fbdc96145b8f22ad8e2c529d5688a66b.nix
@@ -0,0 +1,88 @@
+let
+  buildDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (build dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  sysDepError = pkg:
+    builtins.throw ''
+      The Nixpkgs package set does not contain the package: ${pkg} (system dependency).
+      
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      '';
+  pkgConfDepError = pkg:
+    builtins.throw ''
+      The pkg-conf packages does not contain the package: ${pkg} (pkg-conf dependency).
+      
+      You may need to augment the pkg-conf package mapping in haskell.nix so that it can be found.
+      '';
+  exeDepError = pkg:
+    builtins.throw ''
+      The local executable components do not include the component: ${pkg} (executable dependency).
+      '';
+  legacyExeDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (executable dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  buildToolDepError = pkg:
+    builtins.throw ''
+      Neither the Haskell package set or the Nixpkgs package set contain the package: ${pkg} (build tool dependency).
+      
+      If this is a system dependency:
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      
+      If this is a Haskell dependency:
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+in { system, compiler, flags, pkgs, hsPkgs, pkgconfPkgs, ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "bitwise"; version = "1.0.0.1"; };
+      license = "BSD-3-Clause";
+      copyright = "(c) 2012,2016,2018 Claude Heiland-Allen";
+      maintainer = "claude@mathr.co.uk";
+      author = "Claude Heiland-Allen";
+      homepage = "https://code.mathr.co.uk/bitwise";
+      url = "";
+      synopsis = "fast multi-dimensional unboxed bit packed Bool arrays";
+      description = "Unboxed multidimensional bit packed Bool arrays with fast aggregate\noperations based on lifting Bool operations to bitwise operations.\n\nThere are many other bit packed structures out there, but none met\nall of these requirements:\n\n(1) unboxed bit packed Bool array,\n\n(2) multi-dimensional indexing,\n\n(3) fast (de)serialization, or interoperable with foreign code,\n\n(4) fast aggregate operations (fold, map, zip).\n\nQuick tour of the bitwise library:\n\n[Data.Bits.Bitwise] Lift boolean operations on 'Bool' to bitwise\noperations on 'Data.Bits.Bits'.\n\n[Data.Array.BitArray] Immutable bit arrays.\n\n[Data.Array.BitArray.ST] Mutable bit arrays in 'Control.Monad.ST.ST'.\n\n[Data.Array.BitArray.IO] Mutable bit arrays in 'IO'.\n\n[Data.Array.BitArray.ByteString] (De)serialization.\n\n[Codec.Image.PBM] Portable bitmap monochrome 2D image format.\n\nVery rough performance benchmarks:\n\n* immutable random access single bit reads:\n@BitArray ix@ is about 40% slower than @UArray ix Bool@,\n\n* 'Control.Monad.ST.ST' mutable random access single bit reads:\n@STBitArray s ix@ is about the same as @STUArray s ix Bool@,\n\n* immutable map @Bool -> Bool@:\n@BitArray ix@ is about 85x faster than @UArray ix Bool@,\n\n* immutable zipWith @Bool -> Bool -> Bool@:\n@BitArray ix@ is about 1300x faster than @UArray ix Bool@.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (buildDepError "base"))
+          (hsPkgs."bytestring" or (buildDepError "bytestring"))
+          (hsPkgs."array" or (buildDepError "array"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "bitwise-testsuite" = {
+          depends = [
+            (hsPkgs."bitwise" or (buildDepError "bitwise"))
+            (hsPkgs."base" or (buildDepError "base"))
+            (hsPkgs."QuickCheck" or (buildDepError "QuickCheck"))
+            ];
+          buildable = true;
+          };
+        };
+      benchmarks = {
+        "bitwise-benchmark" = {
+          depends = [
+            (hsPkgs."bitwise" or (buildDepError "bitwise"))
+            (hsPkgs."base" or (buildDepError "base"))
+            (hsPkgs."array" or (buildDepError "array"))
+            (hsPkgs."bytestring" or (buildDepError "bytestring"))
+            (hsPkgs."criterion" or (buildDepError "criterion"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
