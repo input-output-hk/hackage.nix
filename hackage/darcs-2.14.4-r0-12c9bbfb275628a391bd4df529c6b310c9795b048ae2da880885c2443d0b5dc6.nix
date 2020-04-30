0cebc32af06dc90ed201c81513d2924de57219ec
IOHK <devops+nix-tools@iohk.io>
2020-04-30 01:15:56 +0000

    Automatic update for Thu Apr 30 01:15:55 UTC 2020

---
+++ hackage/darcs-2.14.4-r0-12c9bbfb275628a391bd4df529c6b310c9795b048ae2da880885c2443d0b5dc6.nix
@@ -0,0 +1,160 @@
+let
+  buildDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (build dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  sysDepError = pkg:
+    builtins.throw ''
+      The Nixpkgs package set does not contain the package: ${pkg} (system dependency).
+      
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      '';
+  pkgConfDepError = pkg:
+    builtins.throw ''
+      The pkg-conf packages does not contain the package: ${pkg} (pkg-conf dependency).
+      
+      You may need to augment the pkg-conf package mapping in haskell.nix so that it can be found.
+      '';
+  exeDepError = pkg:
+    builtins.throw ''
+      The local executable components do not include the component: ${pkg} (executable dependency).
+      '';
+  legacyExeDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (executable dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  buildToolDepError = pkg:
+    builtins.throw ''
+      Neither the Haskell package set or the Nixpkgs package set contain the package: ${pkg} (build tool dependency).
+      
+      If this is a system dependency:
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      
+      If this is a Haskell dependency:
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+in { system, compiler, flags, pkgs, hsPkgs, pkgconfPkgs, ... }:
+  {
+    flags = {
+      curl = true;
+      pkgconfig = false;
+      static = false;
+      terminfo = true;
+      threaded = true;
+      executable = true;
+      rts = false;
+      warn-as-error = false;
+      };
+    package = {
+      specVersion = "1.24";
+      identifier = { name = "darcs"; version = "2.14.4"; };
+      license = "GPL-2.0-only";
+      copyright = "";
+      maintainer = "<darcs-devel@darcs.net>";
+      author = "David Roundy <droundy@darcs.net>, <darcs-devel@darcs.net>";
+      homepage = "http://darcs.net/";
+      url = "";
+      synopsis = "a distributed, interactive, smart revision control system";
+      description = "Darcs is a free, open source revision control\nsystem. It is:\n\n* Distributed: Every user has access to the full\ncommand set, removing boundaries between server and\nclient or committer and non-committers.\n\n* Interactive: Darcs is easy to learn and efficient to\nuse because it asks you questions in response to\nsimple commands, giving you choices in your work\nflow. You can choose to record one change in a file,\nwhile ignoring another. As you update from upstream,\nyou can review each patch name, even the full \"diff\"\nfor interesting patches.\n\n* Smart: Originally developed by physicist David\nRoundy, darcs is based on a unique algebra of\npatches.\n\nThis smartness lets you respond to changing demands\nin ways that would otherwise not be possible. Learn\nmore about spontaneous branches with darcs.\n\n* Please note that hackage does not correctly display\nthe license. It is meant to be \"GPL-2.0-or-later\".";
+      buildType = "Custom";
+      setup-depends = [
+        (hsPkgs.buildPackages.base or (pkgs.buildPackages.base or (buildToolDepError "base")))
+        (hsPkgs.buildPackages.Cabal or (pkgs.buildPackages.Cabal or (buildToolDepError "Cabal")))
+        (hsPkgs.buildPackages.process or (pkgs.buildPackages.process or (buildToolDepError "process")))
+        (hsPkgs.buildPackages.filepath or (pkgs.buildPackages.filepath or (buildToolDepError "filepath")))
+        (hsPkgs.buildPackages.directory or (pkgs.buildPackages.directory or (buildToolDepError "directory")))
+        ];
+      };
+    components = {
+      "library" = {
+        depends = ([
+          (hsPkgs."base" or (buildDepError "base"))
+          (hsPkgs."stm" or (buildDepError "stm"))
+          (hsPkgs."binary" or (buildDepError "binary"))
+          (hsPkgs."containers" or (buildDepError "containers"))
+          (hsPkgs."regex-compat-tdfa" or (buildDepError "regex-compat-tdfa"))
+          (hsPkgs."regex-applicative" or (buildDepError "regex-applicative"))
+          (hsPkgs."mtl" or (buildDepError "mtl"))
+          (hsPkgs."transformers" or (buildDepError "transformers"))
+          (hsPkgs."parsec" or (buildDepError "parsec"))
+          (hsPkgs."fgl" or (buildDepError "fgl"))
+          (hsPkgs."graphviz" or (buildDepError "graphviz"))
+          (hsPkgs."html" or (buildDepError "html"))
+          (hsPkgs."filepath" or (buildDepError "filepath"))
+          (hsPkgs."haskeline" or (buildDepError "haskeline"))
+          (hsPkgs."cryptohash" or (buildDepError "cryptohash"))
+          (hsPkgs."base16-bytestring" or (buildDepError "base16-bytestring"))
+          (hsPkgs."utf8-string" or (buildDepError "utf8-string"))
+          (hsPkgs."vector" or (buildDepError "vector"))
+          (hsPkgs."tar" or (buildDepError "tar"))
+          (hsPkgs."data-ordlist" or (buildDepError "data-ordlist"))
+          (hsPkgs."attoparsec" or (buildDepError "attoparsec"))
+          (hsPkgs."zip-archive" or (buildDepError "zip-archive"))
+          (hsPkgs."async" or (buildDepError "async"))
+          (hsPkgs."sandi" or (buildDepError "sandi"))
+          (hsPkgs."unix-compat" or (buildDepError "unix-compat"))
+          (hsPkgs."bytestring" or (buildDepError "bytestring"))
+          (hsPkgs."old-time" or (buildDepError "old-time"))
+          (hsPkgs."time" or (buildDepError "time"))
+          (hsPkgs."text" or (buildDepError "text"))
+          (hsPkgs."directory" or (buildDepError "directory"))
+          (hsPkgs."process" or (buildDepError "process"))
+          (hsPkgs."array" or (buildDepError "array"))
+          (hsPkgs."random" or (buildDepError "random"))
+          (hsPkgs."hashable" or (buildDepError "hashable"))
+          (hsPkgs."mmap" or (buildDepError "mmap"))
+          (hsPkgs."zlib" or (buildDepError "zlib"))
+          (hsPkgs."network-uri" or (buildDepError "network-uri"))
+          (hsPkgs."network" or (buildDepError "network"))
+          (hsPkgs."HTTP" or (buildDepError "HTTP"))
+          ] ++ (if system.isWindows
+          then [ (hsPkgs."Win32" or (buildDepError "Win32")) ]
+          else [
+            (hsPkgs."unix" or (buildDepError "unix"))
+            ])) ++ (pkgs.lib).optional (flags.terminfo && !system.isWindows) (hsPkgs."terminfo" or (buildDepError "terminfo"));
+        libs = (pkgs.lib).optionals (flags.curl) ((pkgs.lib).optional (!flags.pkgconfig) (pkgs."curl" or (sysDepError "curl")));
+        pkgconfig = (pkgs.lib).optionals (flags.curl) ((pkgs.lib).optional (flags.pkgconfig) (pkgconfPkgs."libcurl" or (pkgConfDepError "libcurl")));
+        buildable = true;
+        };
+      exes = {
+        "darcs" = {
+          depends = [
+            (hsPkgs."darcs" or (buildDepError "darcs"))
+            (hsPkgs."base" or (buildDepError "base"))
+            ];
+          buildable = if !flags.executable then false else true;
+          };
+        };
+      tests = {
+        "darcs-test" = {
+          depends = ([
+            (hsPkgs."darcs" or (buildDepError "darcs"))
+            (hsPkgs."base" or (buildDepError "base"))
+            (hsPkgs."array" or (buildDepError "array"))
+            (hsPkgs."bytestring" or (buildDepError "bytestring"))
+            (hsPkgs."cmdargs" or (buildDepError "cmdargs"))
+            (hsPkgs."containers" or (buildDepError "containers"))
+            (hsPkgs."filepath" or (buildDepError "filepath"))
+            (hsPkgs."mtl" or (buildDepError "mtl"))
+            (hsPkgs."transformers" or (buildDepError "transformers"))
+            (hsPkgs."shelly" or (buildDepError "shelly"))
+            (hsPkgs."split" or (buildDepError "split"))
+            (hsPkgs."text" or (buildDepError "text"))
+            (hsPkgs."directory" or (buildDepError "directory"))
+            (hsPkgs."FindBin" or (buildDepError "FindBin"))
+            (hsPkgs."QuickCheck" or (buildDepError "QuickCheck"))
+            (hsPkgs."HUnit" or (buildDepError "HUnit"))
+            (hsPkgs."test-framework" or (buildDepError "test-framework"))
+            (hsPkgs."test-framework-hunit" or (buildDepError "test-framework-hunit"))
+            (hsPkgs."test-framework-quickcheck2" or (buildDepError "test-framework-quickcheck2"))
+            (hsPkgs."zip-archive" or (buildDepError "zip-archive"))
+            ] ++ (pkgs.lib).optional (system.isWindows) (hsPkgs."Win32" or (buildDepError "Win32"))) ++ (pkgs.lib).optional (system.isWindows) (hsPkgs."shelly" or (buildDepError "shelly"));
+          buildable = true;
+          };
+        };
+      };
+    }
---
+++ hackage/dialogflow-fulfillment-0.1.1.0-r0-fdd3a92af29d0755d10d0bf77937ecd1c1a2bba4f2eb3ff102bfa6997fbfac44.nix
@@ -0,0 +1,88 @@
+let
+  buildDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (build dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  sysDepError = pkg:
+    builtins.throw ''
+      The Nixpkgs package set does not contain the package: ${pkg} (system dependency).
+      
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      '';
+  pkgConfDepError = pkg:
+    builtins.throw ''
+      The pkg-conf packages does not contain the package: ${pkg} (pkg-conf dependency).
+      
+      You may need to augment the pkg-conf package mapping in haskell.nix so that it can be found.
+      '';
+  exeDepError = pkg:
+    builtins.throw ''
+      The local executable components do not include the component: ${pkg} (executable dependency).
+      '';
+  legacyExeDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (executable dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  buildToolDepError = pkg:
+    builtins.throw ''
+      Neither the Haskell package set or the Nixpkgs package set contain the package: ${pkg} (build tool dependency).
+      
+      If this is a system dependency:
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      
+      If this is a Haskell dependency:
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+in { system, compiler, flags, pkgs, hsPkgs, pkgconfPkgs, ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "2.4";
+      identifier = { name = "dialogflow-fulfillment"; version = "0.1.1.0"; };
+      license = "BSD-3-Clause";
+      copyright = "(c) 2019 Mauricio Fierro";
+      maintainer = "mauriciofierrom@gmail.com";
+      author = "Mauricio Fierro";
+      homepage = "https://github.com/mauriciofierrom/dialogflow-fulfillment";
+      url = "";
+      synopsis = "A Dialogflow Fulfillment library for Haskell.";
+      description = "A library to create responses for Google's Dialogflow fulfillment webhook.\n\nDialogflow is an end-to-end, build-once deploy-everywhere development suite\nfor creating conversational interfaces for websites, mobile applications,\npopular messaging platforms, and IoT devices. Find more at the Google Cloud\n<https://cloud.google.com/dialogflow/ site for the project>.\n\nCheck how fulfillment works in the Dialogflow\n<https://cloud.google.com/dialogflow/docs/fulfillment-how documentation>.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."aeson" or (buildDepError "aeson"))
+          (hsPkgs."base" or (buildDepError "base"))
+          (hsPkgs."bytestring" or (buildDepError "bytestring"))
+          (hsPkgs."containers" or (buildDepError "containers"))
+          (hsPkgs."text" or (buildDepError "text"))
+          (hsPkgs."unordered-containers" or (buildDepError "unordered-containers"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "dialogflow-fulfillment-test" = {
+          depends = [
+            (hsPkgs."aeson" or (buildDepError "aeson"))
+            (hsPkgs."aeson-pretty" or (buildDepError "aeson-pretty"))
+            (hsPkgs."base" or (buildDepError "base"))
+            (hsPkgs."bytestring" or (buildDepError "bytestring"))
+            (hsPkgs."containers" or (buildDepError "containers"))
+            (hsPkgs."dialogflow-fulfillment" or (buildDepError "dialogflow-fulfillment"))
+            (hsPkgs."directory" or (buildDepError "directory"))
+            (hsPkgs."hspec" or (buildDepError "hspec"))
+            (hsPkgs."hspec-discover" or (buildDepError "hspec-discover"))
+            ];
+          build-tools = [
+            (hsPkgs.buildPackages.hspec-discover or (pkgs.buildPackages.hspec-discover or (buildToolDepError "hspec-discover")))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
