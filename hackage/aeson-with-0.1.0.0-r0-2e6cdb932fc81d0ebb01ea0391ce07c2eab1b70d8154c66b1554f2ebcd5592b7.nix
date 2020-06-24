7f1d3a0849ddef5a9c43f330330c24df026d0472
IOHK <devops+nix-tools@iohk.io>
2020-06-24 01:16:17 +0000

    Automatic update for Wed Jun 24 01:16:16 UTC 2020

---
+++ hackage/aeson-with-0.1.0.0-r0-2e6cdb932fc81d0ebb01ea0391ce07c2eab1b70d8154c66b1554f2ebcd5592b7.nix
@@ -0,0 +1,41 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.12";
+      identifier = { name = "aeson-with"; version = "0.1.0.0"; };
+      license = "BSD-3-Clause";
+      copyright = "2020 Daniel Firth";
+      maintainer = "dan.firth@homotopic.tech";
+      author = "Daniel Firth";
+      homepage = "";
+      url = "";
+      synopsis = "withXField combinators for aeson";
+      description = "Silly little withXField combinators for adding fields to an existing JSON value.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."aeson" or (errorHandler.buildDepError "aeson"))
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."hashmap" or (errorHandler.buildDepError "hashmap"))
+          (hsPkgs."lens" or (errorHandler.buildDepError "lens"))
+          (hsPkgs."lens-aeson" or (errorHandler.buildDepError "lens-aeson"))
+          (hsPkgs."scientific" or (errorHandler.buildDepError "scientific"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."unordered-containers" or (errorHandler.buildDepError "unordered-containers"))
+          (hsPkgs."vector" or (errorHandler.buildDepError "vector"))
+          ];
+        buildable = true;
+        };
+      };
+    }
---
+++ hackage/parsek-1.0.2.0-r0-7149843e7677d527a3a3e40097fb13db5bfdf0e0c2fa61613fc34c08a67b9223.nix
@@ -0,0 +1,31 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.12";
+      identifier = { name = "parsek"; version = "1.0.2.0"; };
+      license = "GPL-3.0-only";
+      copyright = "";
+      maintainer = "jeanphilippe.bernardy@gmail.com";
+      author = "Koen Claessen, Jean-Philippe Bernardy";
+      homepage = "";
+      url = "";
+      synopsis = "Parallel Parsing Processes";
+      description = "Koen Claessen's PPP, Modified.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [ (hsPkgs."base" or (errorHandler.buildDepError "base")) ];
+        buildable = true;
+        };
+      };
+    }
