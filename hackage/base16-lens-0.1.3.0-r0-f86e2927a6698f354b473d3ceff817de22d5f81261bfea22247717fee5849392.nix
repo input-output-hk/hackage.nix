d56b5d5492ad00ce97c135c8c514e72f10b9a556
IOHK <devops+nix-tools@iohk.io>
2020-07-26 01:16:35 +0000

    Automatic update for Sun Jul 26 01:16:34 UTC 2020

---
+++ hackage/base16-lens-0.1.3.0-r0-f86e2927a6698f354b473d3ceff817de22d5f81261bfea22247717fee5849392.nix
@@ -0,0 +1,54 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "2.0";
+      identifier = { name = "base16-lens"; version = "0.1.3.0"; };
+      license = "BSD-3-Clause";
+      copyright = "(c) 2020 Emily Pillmore";
+      maintainer = "emilypi@cohomolo.gy";
+      author = "Emily Pillmore";
+      homepage = "https://github.com/emilypi/base16-lens";
+      url = "";
+      synopsis = "Optics for the Base16 library";
+      description = "Prisms and pattern synonyms for the Base16 library";
+      buildType = "Custom";
+      setup-depends = [
+        (hsPkgs.buildPackages.base or (pkgs.buildPackages.base or (errorHandler.buildToolDepError "base")))
+        (hsPkgs.buildPackages.Cabal or (pkgs.buildPackages.Cabal or (errorHandler.buildToolDepError "Cabal")))
+        (hsPkgs.buildPackages.cabal-doctest or (pkgs.buildPackages.cabal-doctest or (errorHandler.buildToolDepError "cabal-doctest")))
+        ];
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."base16" or (errorHandler.buildDepError "base16"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."lens" or (errorHandler.buildDepError "lens"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."text-short" or (errorHandler.buildDepError "text-short"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "doctests" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."base16-lens" or (errorHandler.buildDepError "base16-lens"))
+            (hsPkgs."doctest" or (errorHandler.buildDepError "doctest"))
+            (hsPkgs."lens" or (errorHandler.buildDepError "lens"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
---
+++ hackage/hs-gchart-0.4.2-r0-b549f9fd19e79eb164b222e171f640790dbf7e2b0f6ab8e908b81424ac7b28a6.nix
@@ -0,0 +1,34 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.18";
+      identifier = { name = "hs-gchart"; version = "0.4.2"; };
+      license = "BSD-3-Clause";
+      copyright = "(c) 2010-2020 Deepak Jois <deepak.jois@gmail.com>";
+      maintainer = "Tom Ellis";
+      author = "Deepak Jois";
+      homepage = "http://github.com/deepakjois/hs-gchart";
+      url = "";
+      synopsis = "Haskell wrapper for the Google Chart API";
+      description = "This module is a wrapper around the Google Chart API. It exposes a rich\nset of Haskell data types to specify your chart data, which can then be\nconverted into a URL that generates the PNG image of the chart.\nThis package is very old and not really supported.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+          ];
+        buildable = true;
+        };
+      };
+    }
