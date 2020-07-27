209075b68d8c9026375705a7f710f31af6976987
IOHK <devops+nix-tools@iohk.io>
2020-07-27 01:16:03 +0000

    Automatic update for Mon Jul 27 01:16:02 UTC 2020

---
+++ hackage/fakedata-parser-0.1.0.0-r0-6de870a2c21624aa4516f7ee28fc0076b3b19f3c32d23c969f8f0d7de47c606a.nix
@@ -0,0 +1,47 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.12";
+      identifier = { name = "fakedata-parser"; version = "0.1.0.0"; };
+      license = "BSD-3-Clause";
+      copyright = "Sibi Prabakaran";
+      maintainer = "sibi@psibi.in";
+      author = "Sibi Prabakaran";
+      homepage = "https://github.com/psibi/fakedata-parser#readme";
+      url = "";
+      synopsis = "";
+      description = "Please see the README on GitHub at <https://github.com/psibi/fakedata-parser#readme>";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."attoparsec" or (errorHandler.buildDepError "attoparsec"))
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "fakedata-parser-test" = {
+          depends = [
+            (hsPkgs."attoparsec" or (errorHandler.buildDepError "attoparsec"))
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."fakedata-parser" or (errorHandler.buildDepError "fakedata-parser"))
+            (hsPkgs."hspec" or (errorHandler.buildDepError "hspec"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
---
+++ hackage/packdeps-0.5.0.0-r0-cd236477412385ea25db0938648c2e3dcc9d73651db8435eff6f81626616994c.nix
@@ -0,0 +1,56 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "packdeps"; version = "0.5.0.0"; };
+      license = "BSD-3-Clause";
+      copyright = "";
+      maintainer = "Michael Snoyman <michael@snoyman.com>";
+      author = "Michael Snoyman <michael@snoyman.com>";
+      homepage = "http://packdeps.haskellers.com/";
+      url = "";
+      synopsis = "Check your cabal packages for lagging dependencies.";
+      description = "This provides a library and command line tool for checking if the upper bounds in your package's dependency list excludes the newest package available. The code was originally available only as a web interface at <http://packdeps.haskellers.com/>, but is now available for standalone use as well.\n\nThe command line tool has an incredibly simple interface: simply pass it a list of cabal files, and it will tell you what dependencies- if any- are restricted.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."tar" or (errorHandler.buildDepError "tar"))
+          (hsPkgs."split" or (errorHandler.buildDepError "split"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."Cabal" or (errorHandler.buildDepError "Cabal"))
+          (hsPkgs."time" or (errorHandler.buildDepError "time"))
+          (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+          (hsPkgs."directory" or (errorHandler.buildDepError "directory"))
+          (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
+          ];
+        buildable = true;
+        };
+      exes = {
+        "packdeps" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."Cabal" or (errorHandler.buildDepError "Cabal"))
+            (hsPkgs."optparse-applicative" or (errorHandler.buildDepError "optparse-applicative"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."semigroups" or (errorHandler.buildDepError "semigroups"))
+            (hsPkgs."process" or (errorHandler.buildDepError "process"))
+            (hsPkgs."packdeps" or (errorHandler.buildDepError "packdeps"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
