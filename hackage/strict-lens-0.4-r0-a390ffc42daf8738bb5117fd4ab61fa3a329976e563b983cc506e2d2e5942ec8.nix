1809278eef448d165ac0dfa8a960bdb485661e4b
IOHK <devops+nix-tools@iohk.io>
2020-08-07 01:15:44 +0000

    Automatic update for Fri Aug  7 01:15:43 UTC 2020

---
+++ hackage/strict-lens-0.4-r0-a390ffc42daf8738bb5117fd4ab61fa3a329976e563b983cc506e2d2e5942ec8.nix
@@ -0,0 +1,35 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "strict-lens"; version = "0.4"; };
+      license = "BSD-3-Clause";
+      copyright = "(c) 2006-2008 by Roman Leshchinskiy\n(c) 2013-2014 by Simon Meier";
+      maintainer = "Oleg Grenrus <oleg.grenrus@iki.fi>";
+      author = "Roman Leshchinskiy <rl@cse.unsw.edu.au>,\nSimon Meier <iridcode@gmail.com>";
+      homepage = "https://github.com/haskell-strict/strict";
+      url = "";
+      synopsis = "Lenses for types in strict package";
+      description = "Lenses for types in strict package.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."lens" or (errorHandler.buildDepError "lens"))
+          (hsPkgs."strict" or (errorHandler.buildDepError "strict"))
+          ];
+        buildable = true;
+        };
+      };
+    }
