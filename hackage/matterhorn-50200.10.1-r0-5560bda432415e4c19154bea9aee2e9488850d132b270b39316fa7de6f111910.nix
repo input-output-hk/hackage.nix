9094618a7b904881fe723e63a5e417d7b165c208
IOHK <devops+nix-tools@iohk.io>
2020-08-20 01:16:59 +0000

    Automatic update for Thu Aug 20 01:16:58 UTC 2020

---
+++ hackage/matterhorn-50200.10.1-r0-5560bda432415e4c19154bea9aee2e9488850d132b270b39316fa7de6f111910.nix
@@ -0,0 +1,124 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.18";
+      identifier = { name = "matterhorn"; version = "50200.10.1"; };
+      license = "BSD-3-Clause";
+      copyright = "©2016-2019 docs/AUTHORS.txt";
+      maintainer = "matterhorn@galois.com";
+      author = "matterhorn@galois.com";
+      homepage = "";
+      url = "";
+      synopsis = "Terminal client for the Mattermost chat system";
+      description = "This is a terminal client for the Mattermost chat\nsystem. Please see the README for a list of\nfeatures and information on getting started.";
+      buildType = "Simple";
+      };
+    components = {
+      exes = {
+        "matterhorn" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."mattermost-api" or (errorHandler.buildDepError "mattermost-api"))
+            (hsPkgs."base-compat" or (errorHandler.buildDepError "base-compat"))
+            (hsPkgs."unordered-containers" or (errorHandler.buildDepError "unordered-containers"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."split" or (errorHandler.buildDepError "split"))
+            (hsPkgs."data-clist" or (errorHandler.buildDepError "data-clist"))
+            (hsPkgs."semigroups" or (errorHandler.buildDepError "semigroups"))
+            (hsPkgs."connection" or (errorHandler.buildDepError "connection"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."stm" or (errorHandler.buildDepError "stm"))
+            (hsPkgs."config-ini" or (errorHandler.buildDepError "config-ini"))
+            (hsPkgs."process" or (errorHandler.buildDepError "process"))
+            (hsPkgs."microlens-platform" or (errorHandler.buildDepError "microlens-platform"))
+            (hsPkgs."brick" or (errorHandler.buildDepError "brick"))
+            (hsPkgs."brick-skylighting" or (errorHandler.buildDepError "brick-skylighting"))
+            (hsPkgs."vty" or (errorHandler.buildDepError "vty"))
+            (hsPkgs."word-wrap" or (errorHandler.buildDepError "word-wrap"))
+            (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+            (hsPkgs."text-zipper" or (errorHandler.buildDepError "text-zipper"))
+            (hsPkgs."time" or (errorHandler.buildDepError "time"))
+            (hsPkgs."xdg-basedir" or (errorHandler.buildDepError "xdg-basedir"))
+            (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
+            (hsPkgs."directory" or (errorHandler.buildDepError "directory"))
+            (hsPkgs."vector" or (errorHandler.buildDepError "vector"))
+            (hsPkgs."strict" or (errorHandler.buildDepError "strict"))
+            (hsPkgs."hashable" or (errorHandler.buildDepError "hashable"))
+            (hsPkgs."cheapskate" or (errorHandler.buildDepError "cheapskate"))
+            (hsPkgs."utf8-string" or (errorHandler.buildDepError "utf8-string"))
+            (hsPkgs."temporary" or (errorHandler.buildDepError "temporary"))
+            (hsPkgs."gitrev" or (errorHandler.buildDepError "gitrev"))
+            (hsPkgs."Hclip" or (errorHandler.buildDepError "Hclip"))
+            (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+            (hsPkgs."aspell-pipe" or (errorHandler.buildDepError "aspell-pipe"))
+            (hsPkgs."stm-delay" or (errorHandler.buildDepError "stm-delay"))
+            (hsPkgs."unix" or (errorHandler.buildDepError "unix"))
+            (hsPkgs."skylighting-core" or (errorHandler.buildDepError "skylighting-core"))
+            (hsPkgs."timezone-olson" or (errorHandler.buildDepError "timezone-olson"))
+            (hsPkgs."timezone-series" or (errorHandler.buildDepError "timezone-series"))
+            (hsPkgs."aeson" or (errorHandler.buildDepError "aeson"))
+            (hsPkgs."async" or (errorHandler.buildDepError "async"))
+            (hsPkgs."uuid" or (errorHandler.buildDepError "uuid"))
+            (hsPkgs."random" or (errorHandler.buildDepError "random"))
+            (hsPkgs."network-uri" or (errorHandler.buildDepError "network-uri"))
+            ];
+          buildable = true;
+          };
+        };
+      tests = {
+        "test_messages" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."base-compat" or (errorHandler.buildDepError "base-compat"))
+            (hsPkgs."brick" or (errorHandler.buildDepError "brick"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."cheapskate" or (errorHandler.buildDepError "cheapskate"))
+            (hsPkgs."checkers" or (errorHandler.buildDepError "checkers"))
+            (hsPkgs."config-ini" or (errorHandler.buildDepError "config-ini"))
+            (hsPkgs."connection" or (errorHandler.buildDepError "connection"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."directory" or (errorHandler.buildDepError "directory"))
+            (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
+            (hsPkgs."hashable" or (errorHandler.buildDepError "hashable"))
+            (hsPkgs."Hclip" or (errorHandler.buildDepError "Hclip"))
+            (hsPkgs."mattermost-api" or (errorHandler.buildDepError "mattermost-api"))
+            (hsPkgs."mattermost-api-qc" or (errorHandler.buildDepError "mattermost-api-qc"))
+            (hsPkgs."microlens-platform" or (errorHandler.buildDepError "microlens-platform"))
+            (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+            (hsPkgs."process" or (errorHandler.buildDepError "process"))
+            (hsPkgs."quickcheck-text" or (errorHandler.buildDepError "quickcheck-text"))
+            (hsPkgs."stm" or (errorHandler.buildDepError "stm"))
+            (hsPkgs."strict" or (errorHandler.buildDepError "strict"))
+            (hsPkgs."string-conversions" or (errorHandler.buildDepError "string-conversions"))
+            (hsPkgs."tasty" or (errorHandler.buildDepError "tasty"))
+            (hsPkgs."tasty-hunit" or (errorHandler.buildDepError "tasty-hunit"))
+            (hsPkgs."tasty-quickcheck" or (errorHandler.buildDepError "tasty-quickcheck"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."text-zipper" or (errorHandler.buildDepError "text-zipper"))
+            (hsPkgs."time" or (errorHandler.buildDepError "time"))
+            (hsPkgs."timezone-olson" or (errorHandler.buildDepError "timezone-olson"))
+            (hsPkgs."timezone-series" or (errorHandler.buildDepError "timezone-series"))
+            (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+            (hsPkgs."Unique" or (errorHandler.buildDepError "Unique"))
+            (hsPkgs."unordered-containers" or (errorHandler.buildDepError "unordered-containers"))
+            (hsPkgs."vector" or (errorHandler.buildDepError "vector"))
+            (hsPkgs."vty" or (errorHandler.buildDepError "vty"))
+            (hsPkgs."xdg-basedir" or (errorHandler.buildDepError "xdg-basedir"))
+            (hsPkgs."semigroups" or (errorHandler.buildDepError "semigroups"))
+            (hsPkgs."uuid" or (errorHandler.buildDepError "uuid"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
---
+++ hackage/mime-mail-ses-0.4.2-r0-4bdb852ad3cc94c8713b16cbf011dc9fae8e792c17ccb0e1d6e0886224e1caa1.nix
@@ -0,0 +1,77 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "mime-mail-ses"; version = "0.4.2"; };
+      license = "MIT";
+      copyright = "";
+      maintainer = "michael@snoyman.com";
+      author = "Michael Snoyman";
+      homepage = "http://github.com/snoyberg/mime-mail";
+      url = "";
+      synopsis = "Send mime-mail messages via Amazon SES";
+      description = "Hackage documentation generation is not reliable. For up to date documentation, please see: <http://www.stackage.org/package/mime-mail-ses>.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."base16-bytestring" or (errorHandler.buildDepError "base16-bytestring"))
+          (hsPkgs."base64-bytestring" or (errorHandler.buildDepError "base64-bytestring"))
+          (hsPkgs."byteable" or (errorHandler.buildDepError "byteable"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."case-insensitive" or (errorHandler.buildDepError "case-insensitive"))
+          (hsPkgs."conduit" or (errorHandler.buildDepError "conduit"))
+          (hsPkgs."cryptohash" or (errorHandler.buildDepError "cryptohash"))
+          (hsPkgs."http-client" or (errorHandler.buildDepError "http-client"))
+          (hsPkgs."http-client-tls" or (errorHandler.buildDepError "http-client-tls"))
+          (hsPkgs."http-conduit" or (errorHandler.buildDepError "http-conduit"))
+          (hsPkgs."http-types" or (errorHandler.buildDepError "http-types"))
+          (hsPkgs."mime-mail" or (errorHandler.buildDepError "mime-mail"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."time" or (errorHandler.buildDepError "time"))
+          (hsPkgs."xml-conduit" or (errorHandler.buildDepError "xml-conduit"))
+          (hsPkgs."xml-types" or (errorHandler.buildDepError "xml-types"))
+          ];
+        buildable = true;
+        };
+      exes = {
+        "send-aws" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."http-client" or (errorHandler.buildDepError "http-client"))
+            (hsPkgs."http-client-tls" or (errorHandler.buildDepError "http-client-tls"))
+            (hsPkgs."mime-mail" or (errorHandler.buildDepError "mime-mail"))
+            (hsPkgs."mime-mail-ses" or (errorHandler.buildDepError "mime-mail-ses"))
+            (hsPkgs."optparse-applicative" or (errorHandler.buildDepError "optparse-applicative"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            ];
+          buildable = true;
+          };
+        };
+      tests = {
+        "by-the-book" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."case-insensitive" or (errorHandler.buildDepError "case-insensitive"))
+            (hsPkgs."mime-mail-ses" or (errorHandler.buildDepError "mime-mail-ses"))
+            (hsPkgs."tasty" or (errorHandler.buildDepError "tasty"))
+            (hsPkgs."tasty-hunit" or (errorHandler.buildDepError "tasty-hunit"))
+            (hsPkgs."time" or (errorHandler.buildDepError "time"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
