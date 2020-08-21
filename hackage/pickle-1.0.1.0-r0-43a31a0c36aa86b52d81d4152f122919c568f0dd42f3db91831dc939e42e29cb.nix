efffd563209c8697a16aab150ae83cb4ab17b3e5
IOHK <devops+nix-tools@iohk.io>
2020-08-21 01:11:35 +0000

    Automatic update for Fri Aug 21 01:11:34 UTC 2020

---
+++ hackage/pickle-1.0.1.0-r0-43a31a0c36aa86b52d81d4152f122919c568f0dd42f3db91831dc939e42e29cb.nix
@@ -0,0 +1,37 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "2.4";
+      identifier = { name = "pickle"; version = "1.0.1.0"; };
+      license = "MIT";
+      copyright = "Copyright 2019 Anduril Industries";
+      maintainer = "bob.blackmon@ymail.com";
+      author = "Bob Blackmon";
+      homepage = "";
+      url = "";
+      synopsis = "Instant StatsD in Haskell";
+      description = "Instant StatsD in Haskell. Easy to implement in a program you've already written without any refactoring to pass any sockets/configuration around.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+          (hsPkgs."network" or (errorHandler.buildDepError "network"))
+          (hsPkgs."stm" or (errorHandler.buildDepError "stm"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          ];
+        buildable = true;
+        };
+      };
+    }
