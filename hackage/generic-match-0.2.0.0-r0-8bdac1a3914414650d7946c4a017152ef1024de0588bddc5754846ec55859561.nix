3a73f909de8800e973cdf17751f89de0cc2fd8c3
IOHK <devops+nix-tools@iohk.io>
2020-08-25 01:11:53 +0000

    Automatic update for Tue Aug 25 01:11:52 UTC 2020

---
+++ hackage/generic-match-0.2.0.0-r0-8bdac1a3914414650d7946c4a017152ef1024de0588bddc5754846ec55859561.nix
@@ -0,0 +1,31 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "2.4";
+      identifier = { name = "generic-match"; version = "0.2.0.0"; };
+      license = "MIT";
+      copyright = "2020 Samuel Schlesinger";
+      maintainer = "sgschlesinger@gmail.com";
+      author = "Samuel Schlesinger";
+      homepage = "";
+      url = "";
+      synopsis = "First class pattern matching";
+      description = "First class pattern matching.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [ (hsPkgs."base" or (errorHandler.buildDepError "base")) ];
+        buildable = true;
+        };
+      };
+    }
---
+++ hackage/persistent-redis-2.5.2.5-r0-57a0f2dee4b108494aa86f98081fcb13a6eba7d897813480780db79979375bde.nix
@@ -0,0 +1,70 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "persistent-redis"; version = "2.5.2.5"; };
+      license = "BSD-3-Clause";
+      copyright = "";
+      maintainer = "Pavel Ryzhov <paul@paulrz.cz>";
+      author = "Pavel Ryzhov <paul@paulrz.cz>";
+      homepage = "";
+      url = "";
+      synopsis = "Backend for persistent library using Redis.";
+      description = "Based on the Redis package.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."persistent" or (errorHandler.buildDepError "persistent"))
+          (hsPkgs."aeson" or (errorHandler.buildDepError "aeson"))
+          (hsPkgs."binary" or (errorHandler.buildDepError "binary"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."hedis" or (errorHandler.buildDepError "hedis"))
+          (hsPkgs."http-api-data" or (errorHandler.buildDepError "http-api-data"))
+          (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+          (hsPkgs."path-pieces" or (errorHandler.buildDepError "path-pieces"))
+          (hsPkgs."scientific" or (errorHandler.buildDepError "scientific"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."time" or (errorHandler.buildDepError "time"))
+          (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+          (hsPkgs."utf8-string" or (errorHandler.buildDepError "utf8-string"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "basic" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."persistent" or (errorHandler.buildDepError "persistent"))
+            (hsPkgs."persistent-redis" or (errorHandler.buildDepError "persistent-redis"))
+            (hsPkgs."persistent-template" or (errorHandler.buildDepError "persistent-template"))
+            (hsPkgs."aeson" or (errorHandler.buildDepError "aeson"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."binary" or (errorHandler.buildDepError "binary"))
+            (hsPkgs."hedis" or (errorHandler.buildDepError "hedis"))
+            (hsPkgs."http-api-data" or (errorHandler.buildDepError "http-api-data"))
+            (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+            (hsPkgs."path-pieces" or (errorHandler.buildDepError "path-pieces"))
+            (hsPkgs."scientific" or (errorHandler.buildDepError "scientific"))
+            (hsPkgs."template-haskell" or (errorHandler.buildDepError "template-haskell"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."time" or (errorHandler.buildDepError "time"))
+            (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+            (hsPkgs."utf8-string" or (errorHandler.buildDepError "utf8-string"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
