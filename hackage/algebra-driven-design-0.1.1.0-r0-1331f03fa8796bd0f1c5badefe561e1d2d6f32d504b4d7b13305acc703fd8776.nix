680de648771b03911dfd650d569b96960b6f2239
IOHK <devops+nix-tools@iohk.io>
2020-09-01 01:12:47 +0000

    Automatic update for Tue Sep  1 01:12:46 UTC 2020

---
+++ hackage/algebra-driven-design-0.1.1.0-r0-1331f03fa8796bd0f1c5badefe561e1d2d6f32d504b4d7b13305acc703fd8776.nix
@@ -0,0 +1,46 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.12";
+      identifier = { name = "algebra-driven-design"; version = "0.1.1.0"; };
+      license = "BSD-3-Clause";
+      copyright = "2020 Sandy Maguire";
+      maintainer = "sandy@sandymaguire.me";
+      author = "Sandy Maguire";
+      homepage = "https://github.com/isovector/algebra-driven-design#readme";
+      url = "";
+      synopsis = "Companion library for the book Algebra-Driven Design by Sandy Maguire";
+      description = "Please see the README on GitHub at <https://github.com/isovector/algebra-driven-design#readme>";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."JuicyPixels" or (errorHandler.buildDepError "JuicyPixels"))
+          (hsPkgs."QuickCheck" or (errorHandler.buildDepError "QuickCheck"))
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+          (hsPkgs."dlist" or (errorHandler.buildDepError "dlist"))
+          (hsPkgs."file-embed" or (errorHandler.buildDepError "file-embed"))
+          (hsPkgs."generic-data" or (errorHandler.buildDepError "generic-data"))
+          (hsPkgs."hashable" or (errorHandler.buildDepError "hashable"))
+          (hsPkgs."monoid-subclasses" or (errorHandler.buildDepError "monoid-subclasses"))
+          (hsPkgs."monoidal-containers" or (errorHandler.buildDepError "monoidal-containers"))
+          (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+          (hsPkgs."multiset" or (errorHandler.buildDepError "multiset"))
+          (hsPkgs."quickspec" or (errorHandler.buildDepError "quickspec"))
+          ];
+        buildable = true;
+        };
+      };
+    }
---
+++ hackage/conversions-0.0.3-r0-4adcd605bc4ee3b351359852bebfb4e7032966d93bf5200c56884c0103b7a6d0.nix
@@ -0,0 +1,60 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = { development = false; };
+    package = {
+      specVersion = "1.12";
+      identifier = { name = "conversions"; version = "0.0.3"; };
+      license = "MIT";
+      copyright = "2020 Strainge Inc.";
+      maintainer = "Markus Schirp mbj@schirp-ds.com, Allan Lukwago <epicallan.al@gmail.com>";
+      author = "";
+      homepage = "https://github.com/mbj/conversions#readme";
+      url = "";
+      synopsis = "Injective explicit total and partial conversions";
+      description = "";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."control-bool" or (errorHandler.buildDepError "control-bool"))
+          (hsPkgs."devtools" or (errorHandler.buildDepError "devtools"))
+          (hsPkgs."exceptions" or (errorHandler.buildDepError "exceptions"))
+          (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+          (hsPkgs."source-constraints" or (errorHandler.buildDepError "source-constraints"))
+          (hsPkgs."template-haskell" or (errorHandler.buildDepError "template-haskell"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."unliftio-core" or (errorHandler.buildDepError "unliftio-core"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "test" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."control-bool" or (errorHandler.buildDepError "control-bool"))
+            (hsPkgs."conversions" or (errorHandler.buildDepError "conversions"))
+            (hsPkgs."devtools" or (errorHandler.buildDepError "devtools"))
+            (hsPkgs."exceptions" or (errorHandler.buildDepError "exceptions"))
+            (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+            (hsPkgs."source-constraints" or (errorHandler.buildDepError "source-constraints"))
+            (hsPkgs."template-haskell" or (errorHandler.buildDepError "template-haskell"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."unliftio-core" or (errorHandler.buildDepError "unliftio-core"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
