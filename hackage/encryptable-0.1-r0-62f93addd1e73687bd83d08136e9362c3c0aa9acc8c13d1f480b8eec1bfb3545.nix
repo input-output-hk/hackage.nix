4264ec7f790d806d222ffbabed0dd8f1c198bbd0
IOHK <devops+nix-tools@iohk.io>
2020-09-04 01:11:12 +0000

    Automatic update for Fri Sep  4 01:11:11 UTC 2020

---
+++ hackage/encryptable-0.1-r0-62f93addd1e73687bd83d08136e9362c3c0aa9acc8c13d1f480b8eec1bfb3545.nix
@@ -0,0 +1,64 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.12";
+      identifier = { name = "encryptable"; version = "0.1"; };
+      license = "BSD-3-Clause";
+      copyright = "2020 Coingaming <hello@coingaming.io>";
+      maintainer = "Ilja Tkachuk <tkachuk.labs@gmail.com>";
+      author = "Ilja Tkachuk <tkachuk.labs@gmail.com>";
+      homepage = "https://github.com/coingaming/encryptable#readme";
+      url = "";
+      synopsis = "Typed encryption with persistent support";
+      description = "You can find documentation at <http://hackage.haskell.org/package/encryptable/docs/Data-Encryptable.html hackage>";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."QuickCheck" or (errorHandler.buildDepError "QuickCheck"))
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."cryptonite" or (errorHandler.buildDepError "cryptonite"))
+          (hsPkgs."esqueleto" or (errorHandler.buildDepError "esqueleto"))
+          (hsPkgs."generic-arbitrary" or (errorHandler.buildDepError "generic-arbitrary"))
+          (hsPkgs."hspec" or (errorHandler.buildDepError "hspec"))
+          (hsPkgs."persistent" or (errorHandler.buildDepError "persistent"))
+          (hsPkgs."persistent-template" or (errorHandler.buildDepError "persistent-template"))
+          (hsPkgs."quickcheck-instances" or (errorHandler.buildDepError "quickcheck-instances"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."universum" or (errorHandler.buildDepError "universum"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "encryptable-test" = {
+          depends = [
+            (hsPkgs."QuickCheck" or (errorHandler.buildDepError "QuickCheck"))
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."cryptonite" or (errorHandler.buildDepError "cryptonite"))
+            (hsPkgs."encryptable" or (errorHandler.buildDepError "encryptable"))
+            (hsPkgs."esqueleto" or (errorHandler.buildDepError "esqueleto"))
+            (hsPkgs."generic-arbitrary" or (errorHandler.buildDepError "generic-arbitrary"))
+            (hsPkgs."hspec" or (errorHandler.buildDepError "hspec"))
+            (hsPkgs."persistent" or (errorHandler.buildDepError "persistent"))
+            (hsPkgs."persistent-template" or (errorHandler.buildDepError "persistent-template"))
+            (hsPkgs."quickcheck-instances" or (errorHandler.buildDepError "quickcheck-instances"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."universum" or (errorHandler.buildDepError "universum"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
