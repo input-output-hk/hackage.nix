124c0d5dd8e095bc30054a9f5f7c5ef58ed6a722
IOHK <devops+nix-tools@iohk.io>
2020-09-11 01:11:36 +0000

    Automatic update for Fri Sep 11 01:11:36 UTC 2020

---
+++ hackage/capnp-0.6.0.0-r1-3ca2980f0d80d97418767d8df8c9d0af39dfebf62e1813b903e40f65b3391e95.nix
@@ -0,0 +1,121 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "2.2";
+      identifier = { name = "capnp"; version = "0.6.0.0"; };
+      license = "MIT";
+      copyright = "2016-2020 haskell-capnp contributors (see CONTRIBUTORS file).";
+      maintainer = "ian@zenhack.net";
+      author = "Ian Denhardt";
+      homepage = "https://github.com/zenhack/haskell-capnp";
+      url = "";
+      synopsis = "Cap'n Proto for Haskell";
+      description = "A native Haskell implementation of the Cap'N Proto cerialization format and\nRPC protocol.\n\nThe library implements serialization and level 1 RPC.\n\nThe \"Capnp.Tutorial\" module is the best place to start reading; the\nreference documentation can seem bewildering without that context.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."bytes" or (errorHandler.buildDepError "bytes"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+          (hsPkgs."data-default" or (errorHandler.buildDepError "data-default"))
+          (hsPkgs."exceptions" or (errorHandler.buildDepError "exceptions"))
+          (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+          (hsPkgs."primitive" or (errorHandler.buildDepError "primitive"))
+          (hsPkgs."reinterpret-cast" or (errorHandler.buildDepError "reinterpret-cast"))
+          (hsPkgs."safe-exceptions" or (errorHandler.buildDepError "safe-exceptions"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+          (hsPkgs."vector" or (errorHandler.buildDepError "vector"))
+          (hsPkgs."monad-stm" or (errorHandler.buildDepError "monad-stm"))
+          (hsPkgs."cpu" or (errorHandler.buildDepError "cpu"))
+          (hsPkgs."hashable" or (errorHandler.buildDepError "hashable"))
+          (hsPkgs."data-default-instances-vector" or (errorHandler.buildDepError "data-default-instances-vector"))
+          (hsPkgs."stm" or (errorHandler.buildDepError "stm"))
+          (hsPkgs."stm-containers" or (errorHandler.buildDepError "stm-containers"))
+          (hsPkgs."list-t" or (errorHandler.buildDepError "list-t"))
+          (hsPkgs."focus" or (errorHandler.buildDepError "focus"))
+          (hsPkgs."async" or (errorHandler.buildDepError "async"))
+          (hsPkgs."network-simple" or (errorHandler.buildDepError "network-simple"))
+          (hsPkgs."network" or (errorHandler.buildDepError "network"))
+          (hsPkgs."supervisors" or (errorHandler.buildDepError "supervisors"))
+          (hsPkgs."pretty-show" or (errorHandler.buildDepError "pretty-show"))
+          ];
+        buildable = true;
+        };
+      exes = {
+        "capnpc-haskell" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."bytes" or (errorHandler.buildDepError "bytes"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."data-default" or (errorHandler.buildDepError "data-default"))
+            (hsPkgs."exceptions" or (errorHandler.buildDepError "exceptions"))
+            (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+            (hsPkgs."primitive" or (errorHandler.buildDepError "primitive"))
+            (hsPkgs."reinterpret-cast" or (errorHandler.buildDepError "reinterpret-cast"))
+            (hsPkgs."safe-exceptions" or (errorHandler.buildDepError "safe-exceptions"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+            (hsPkgs."vector" or (errorHandler.buildDepError "vector"))
+            (hsPkgs."monad-stm" or (errorHandler.buildDepError "monad-stm"))
+            (hsPkgs."capnp" or (errorHandler.buildDepError "capnp"))
+            (hsPkgs."wl-pprint-text" or (errorHandler.buildDepError "wl-pprint-text"))
+            (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
+            (hsPkgs."directory" or (errorHandler.buildDepError "directory"))
+            ];
+          buildable = true;
+          };
+        };
+      tests = {
+        "tests" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."bytes" or (errorHandler.buildDepError "bytes"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."data-default" or (errorHandler.buildDepError "data-default"))
+            (hsPkgs."exceptions" or (errorHandler.buildDepError "exceptions"))
+            (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+            (hsPkgs."primitive" or (errorHandler.buildDepError "primitive"))
+            (hsPkgs."reinterpret-cast" or (errorHandler.buildDepError "reinterpret-cast"))
+            (hsPkgs."safe-exceptions" or (errorHandler.buildDepError "safe-exceptions"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+            (hsPkgs."vector" or (errorHandler.buildDepError "vector"))
+            (hsPkgs."monad-stm" or (errorHandler.buildDepError "monad-stm"))
+            (hsPkgs."capnp" or (errorHandler.buildDepError "capnp"))
+            (hsPkgs."network" or (errorHandler.buildDepError "network"))
+            (hsPkgs."network-simple" or (errorHandler.buildDepError "network-simple"))
+            (hsPkgs."stm" or (errorHandler.buildDepError "stm"))
+            (hsPkgs."async" or (errorHandler.buildDepError "async"))
+            (hsPkgs."process" or (errorHandler.buildDepError "process"))
+            (hsPkgs."process-extras" or (errorHandler.buildDepError "process-extras"))
+            (hsPkgs."QuickCheck" or (errorHandler.buildDepError "QuickCheck"))
+            (hsPkgs."quickcheck-io" or (errorHandler.buildDepError "quickcheck-io"))
+            (hsPkgs."quickcheck-instances" or (errorHandler.buildDepError "quickcheck-instances"))
+            (hsPkgs."hspec" or (errorHandler.buildDepError "hspec"))
+            (hsPkgs."directory" or (errorHandler.buildDepError "directory"))
+            (hsPkgs."resourcet" or (errorHandler.buildDepError "resourcet"))
+            (hsPkgs."heredoc" or (errorHandler.buildDepError "heredoc"))
+            (hsPkgs."deepseq" or (errorHandler.buildDepError "deepseq"))
+            (hsPkgs."pretty-show" or (errorHandler.buildDepError "pretty-show"))
+            (hsPkgs."supervisors" or (errorHandler.buildDepError "supervisors"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
---
+++ hackage/fast-builder-0.1.2.1-r0-6e3148d9ac21e5ec900fa59d1d65322e6b4c0491f19b53bdbe093186a519eb9f.nix
@@ -0,0 +1,89 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "fast-builder"; version = "0.1.2.1"; };
+      license = "LicenseRef-PublicDomain";
+      copyright = "";
+      maintainer = "tak@anoak.io";
+      author = "Takano Akio";
+      homepage = "http://github.com/takano-akio/fast-builder";
+      url = "";
+      synopsis = "Fast ByteString Builder";
+      description = "An efficient implementation of ByteString builder. It should be faster than\nthe standard implementation in most cases.\n\nIn many benchmarks, the performance improvement is 2x-10x.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."ghc-prim" or (errorHandler.buildDepError "ghc-prim"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "prop" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."fast-builder" or (errorHandler.buildDepError "fast-builder"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."QuickCheck" or (errorHandler.buildDepError "QuickCheck"))
+            (hsPkgs."stm" or (errorHandler.buildDepError "stm"))
+            (hsPkgs."process" or (errorHandler.buildDepError "process"))
+            ];
+          buildable = true;
+          };
+        };
+      benchmarks = {
+        "aeson" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."fast-builder" or (errorHandler.buildDepError "fast-builder"))
+            (hsPkgs."aeson" or (errorHandler.buildDepError "aeson"))
+            (hsPkgs."criterion" or (errorHandler.buildDepError "criterion"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."scientific" or (errorHandler.buildDepError "scientific"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."vector" or (errorHandler.buildDepError "vector"))
+            (hsPkgs."deepseq" or (errorHandler.buildDepError "deepseq"))
+            (hsPkgs."ghc-prim" or (errorHandler.buildDepError "ghc-prim"))
+            (hsPkgs."template-haskell" or (errorHandler.buildDepError "template-haskell"))
+            (hsPkgs."unordered-containers" or (errorHandler.buildDepError "unordered-containers"))
+            ];
+          buildable = true;
+          };
+        "vector" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."fast-builder" or (errorHandler.buildDepError "fast-builder"))
+            (hsPkgs."criterion" or (errorHandler.buildDepError "criterion"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."vector" or (errorHandler.buildDepError "vector"))
+            (hsPkgs."deepseq" or (errorHandler.buildDepError "deepseq"))
+            ];
+          buildable = true;
+          };
+        "map" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."fast-builder" or (errorHandler.buildDepError "fast-builder"))
+            (hsPkgs."criterion" or (errorHandler.buildDepError "criterion"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."deepseq" or (errorHandler.buildDepError "deepseq"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
