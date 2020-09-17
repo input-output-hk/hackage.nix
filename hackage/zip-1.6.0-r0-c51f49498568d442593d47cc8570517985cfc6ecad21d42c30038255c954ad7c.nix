d2c691b28f33df440fd12004b83958a240ca7786
IOHK <devops+nix-tools@iohk.io>
2020-09-17 01:12:17 +0000

    Automatic update for Thu Sep 17 01:12:16 UTC 2020

---
+++ hackage/zip-1.6.0-r0-c51f49498568d442593d47cc8570517985cfc6ecad21d42c30038255c954ad7c.nix
@@ -0,0 +1,83 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = { dev = false; disable-bzip2 = false; disable-zstd = false; };
+    package = {
+      specVersion = "1.18";
+      identifier = { name = "zip"; version = "1.6.0"; };
+      license = "BSD-3-Clause";
+      copyright = "";
+      maintainer = "Mark Karpov <markkarpov92@gmail.com>";
+      author = "Mark Karpov <markkarpov92@gmail.com>";
+      homepage = "https://github.com/mrkkrp/zip";
+      url = "";
+      synopsis = "Operations on zip archives";
+      description = "Operations on zip archives.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = ([
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."case-insensitive" or (errorHandler.buildDepError "case-insensitive"))
+          (hsPkgs."cereal" or (errorHandler.buildDepError "cereal"))
+          (hsPkgs."conduit" or (errorHandler.buildDepError "conduit"))
+          (hsPkgs."conduit-extra" or (errorHandler.buildDepError "conduit-extra"))
+          (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+          (hsPkgs."digest" or (errorHandler.buildDepError "digest"))
+          (hsPkgs."directory" or (errorHandler.buildDepError "directory"))
+          (hsPkgs."dlist" or (errorHandler.buildDepError "dlist"))
+          (hsPkgs."exceptions" or (errorHandler.buildDepError "exceptions"))
+          (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
+          (hsPkgs."monad-control" or (errorHandler.buildDepError "monad-control"))
+          (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+          (hsPkgs."resourcet" or (errorHandler.buildDepError "resourcet"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."time" or (errorHandler.buildDepError "time"))
+          (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+          (hsPkgs."transformers-base" or (errorHandler.buildDepError "transformers-base"))
+          ] ++ (pkgs.lib).optional (!flags.disable-bzip2) (hsPkgs."bzlib-conduit" or (errorHandler.buildDepError "bzlib-conduit"))) ++ (pkgs.lib).optional (!flags.disable-zstd) (hsPkgs."conduit-zstd" or (errorHandler.buildDepError "conduit-zstd"));
+        buildable = true;
+        };
+      exes = {
+        "haskell-zip-app" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
+            (hsPkgs."zip" or (errorHandler.buildDepError "zip"))
+            ];
+          buildable = true;
+          };
+        };
+      tests = {
+        "tests" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."QuickCheck" or (errorHandler.buildDepError "QuickCheck"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."conduit" or (errorHandler.buildDepError "conduit"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."directory" or (errorHandler.buildDepError "directory"))
+            (hsPkgs."dlist" or (errorHandler.buildDepError "dlist"))
+            (hsPkgs."exceptions" or (errorHandler.buildDepError "exceptions"))
+            (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
+            (hsPkgs."hspec" or (errorHandler.buildDepError "hspec"))
+            (hsPkgs."temporary" or (errorHandler.buildDepError "temporary"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."time" or (errorHandler.buildDepError "time"))
+            (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+            (hsPkgs."zip" or (errorHandler.buildDepError "zip"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
