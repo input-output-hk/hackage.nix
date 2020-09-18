47ca9e1e8cf04967f2da6b2dc4f67903fe624b23
IOHK <devops+nix-tools@iohk.io>
2020-09-18 01:11:58 +0000

    Automatic update for Fri Sep 18 01:11:57 UTC 2020

---
+++ hackage/uniqueness-periods-vector-filters-0.1.0.0-r0-fbd5adb9280958b844a379bf5883b7bc7181250147131649b86fa1a7b3a9cc31.nix
@@ -0,0 +1,38 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.10";
+      identifier = {
+        name = "uniqueness-periods-vector-filters";
+        version = "0.1.0.0";
+        };
+      license = "MIT";
+      copyright = "Oleksandr Zhabenko";
+      maintainer = "olexandr543@yahoo.com";
+      author = "OleksandrZhabenko";
+      homepage = "https://hackage.haskell.org/package/uniqueness-periods-vector-filters";
+      url = "";
+      synopsis = "A library allows to change the structure of the 'RealFrac' function output.";
+      description = "A library allows to change the structure of the 'RealFrac' function output. At the moment only the equal intervals are supported. ";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."mmsyn2" or (errorHandler.buildDepError "mmsyn2"))
+          (hsPkgs."vector" or (errorHandler.buildDepError "vector"))
+          ];
+        buildable = true;
+        };
+      };
+    }
