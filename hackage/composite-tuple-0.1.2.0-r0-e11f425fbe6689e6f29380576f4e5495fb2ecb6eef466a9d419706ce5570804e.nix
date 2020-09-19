76d112c0e1e9aa592fa756d9d0639b341ca59f3f
IOHK <devops+nix-tools@iohk.io>
2020-09-19 01:11:48 +0000

    Automatic update for Sat Sep 19 01:11:47 UTC 2020

---
+++ hackage/composite-tuple-0.1.2.0-r0-e11f425fbe6689e6f29380576f4e5495fb2ecb6eef466a9d419706ce5570804e.nix
@@ -0,0 +1,34 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.12";
+      identifier = { name = "composite-tuple"; version = "0.1.2.0"; };
+      license = "MIT";
+      copyright = "2020 Daniel Firth";
+      maintainer = "dan.firth@homotopic.tech";
+      author = "Daniel Firth";
+      homepage = "";
+      url = "";
+      synopsis = "Tuple functions for composite records.";
+      description = "Tuple functions for composite records.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."composite-base" or (errorHandler.buildDepError "composite-base"))
+          ];
+        buildable = true;
+        };
+      };
+    }
---
+++ hackage/pgf2-1.2.1-r0-4ba5b3f4f0a68bedba671b593b1ae078d4137df46107e3568d4aafb48c0a28a2.nix
@@ -0,0 +1,42 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "pgf2"; version = "1.2.1"; };
+      license = "LGPL-3.0-only";
+      copyright = "";
+      maintainer = "kr.angelov@gmail.com";
+      author = "Krasimir Angelov";
+      homepage = "https://www.grammaticalframework.org";
+      url = "";
+      synopsis = "Bindings to the C version of the PGF runtime";
+      description = "GF, Grammatical Framework, is a programming language for multilingual grammar applications.\nGF grammars are compiled into Portable Grammar Format (PGF) which can be used with the PGF runtime, written in C.\nThis package provides Haskell bindings to that runtime.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+          (hsPkgs."pretty" or (errorHandler.buildDepError "pretty"))
+          ];
+        libs = [
+          (pkgs."pgf" or (errorHandler.sysDepError "pgf"))
+          (pkgs."gu" or (errorHandler.sysDepError "gu"))
+          ];
+        build-tools = [
+          (hsPkgs.buildPackages.hsc2hs or (pkgs.buildPackages.hsc2hs or (errorHandler.buildToolDepError "hsc2hs")))
+          ];
+        buildable = true;
+        };
+      };
+    }
