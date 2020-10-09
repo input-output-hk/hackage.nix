9d285f2e91105e47222ea49baa5892ec3134fc65
IOHK <devops+nix-tools@iohk.io>
2020-10-09 01:14:13 +0000

    Automatic update for Fri Oct  9 01:14:12 UTC 2020

---
+++ hackage/uniqueness-periods-vector-properties-0.4.0.1-r0-3da9a434410f5302868539bd536488635832059b325180c8f044c826bc4b08c8.nix
@@ -0,0 +1,40 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.10";
+      identifier = {
+        name = "uniqueness-periods-vector-properties";
+        version = "0.4.0.1";
+        };
+      license = "MIT";
+      copyright = "Oleksandr Zhabenko";
+      maintainer = "olexandr543@yahoo.com";
+      author = "OleksandrZhabenko";
+      homepage = "https://hackage.haskell.org/package/uniqueness-periods-vector-properties";
+      url = "";
+      synopsis = "Metrices for the maximum element for the uniqueness-periods-vector packages family.";
+      description = "Metrices for the maximum element for the uniqueness-periods-vector packages family. Generalization of the DobutokO.Poetry.Norms and DobutokO.Poetry.Norms.Extended modules from dobutokO-poetry package.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."vector" or (errorHandler.buildDepError "vector"))
+          (hsPkgs."uniqueness-periods-vector" or (errorHandler.buildDepError "uniqueness-periods-vector"))
+          (hsPkgs."mmsyn7s" or (errorHandler.buildDepError "mmsyn7s"))
+          (hsPkgs."phonetic-languages-rhythmicity" or (errorHandler.buildDepError "phonetic-languages-rhythmicity"))
+          ];
+        buildable = true;
+        };
+      };
+    }
