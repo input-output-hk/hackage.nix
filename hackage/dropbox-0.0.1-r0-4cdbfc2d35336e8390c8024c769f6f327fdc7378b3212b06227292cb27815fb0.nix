7ef3080c28065ab22ed01b70e28a0b461f498dd3
IOHK <devops+nix-tools@iohk.io>
2020-10-24 01:13:10 +0000

    Automatic update for Sat Oct 24 01:13:09 UTC 2020

---
+++ hackage/dropbox-0.0.1-r0-4cdbfc2d35336e8390c8024c769f6f327fdc7378b3212b06227292cb27815fb0.nix
@@ -0,0 +1,81 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.12";
+      identifier = { name = "dropbox"; version = "0.0.1"; };
+      license = "MIT";
+      copyright = "2020 Riskbook";
+      maintainer = "support@riskbook.com";
+      author = "Riskbook";
+      homepage = "https://github.com/riskbook/dropbox#readme";
+      url = "";
+      synopsis = "Dropbox API client";
+      description = "A dropbox API client generated through servant generic.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."aeson" or (errorHandler.buildDepError "aeson"))
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."http-api-data" or (errorHandler.buildDepError "http-api-data"))
+          (hsPkgs."http-client-tls" or (errorHandler.buildDepError "http-client-tls"))
+          (hsPkgs."servant" or (errorHandler.buildDepError "servant"))
+          (hsPkgs."servant-auth" or (errorHandler.buildDepError "servant-auth"))
+          (hsPkgs."servant-auth-client" or (errorHandler.buildDepError "servant-auth-client"))
+          (hsPkgs."servant-client" or (errorHandler.buildDepError "servant-client"))
+          (hsPkgs."servant-client-core" or (errorHandler.buildDepError "servant-client-core"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          ];
+        buildable = true;
+        };
+      exes = {
+        "exe" = {
+          depends = [
+            (hsPkgs."aeson" or (errorHandler.buildDepError "aeson"))
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."dropbox" or (errorHandler.buildDepError "dropbox"))
+            (hsPkgs."http-api-data" or (errorHandler.buildDepError "http-api-data"))
+            (hsPkgs."http-client-tls" or (errorHandler.buildDepError "http-client-tls"))
+            (hsPkgs."servant" or (errorHandler.buildDepError "servant"))
+            (hsPkgs."servant-auth" or (errorHandler.buildDepError "servant-auth"))
+            (hsPkgs."servant-auth-client" or (errorHandler.buildDepError "servant-auth-client"))
+            (hsPkgs."servant-client" or (errorHandler.buildDepError "servant-client"))
+            (hsPkgs."servant-client-core" or (errorHandler.buildDepError "servant-client-core"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            ];
+          buildable = true;
+          };
+        };
+      tests = {
+        "unit" = {
+          depends = [
+            (hsPkgs."aeson" or (errorHandler.buildDepError "aeson"))
+            (hsPkgs."aeson-qq" or (errorHandler.buildDepError "aeson-qq"))
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."dropbox" or (errorHandler.buildDepError "dropbox"))
+            (hsPkgs."hspec" or (errorHandler.buildDepError "hspec"))
+            (hsPkgs."hspec-core" or (errorHandler.buildDepError "hspec-core"))
+            (hsPkgs."http-api-data" or (errorHandler.buildDepError "http-api-data"))
+            (hsPkgs."http-client-tls" or (errorHandler.buildDepError "http-client-tls"))
+            (hsPkgs."servant" or (errorHandler.buildDepError "servant"))
+            (hsPkgs."servant-auth" or (errorHandler.buildDepError "servant-auth"))
+            (hsPkgs."servant-auth-client" or (errorHandler.buildDepError "servant-auth-client"))
+            (hsPkgs."servant-client" or (errorHandler.buildDepError "servant-client"))
+            (hsPkgs."servant-client-core" or (errorHandler.buildDepError "servant-client-core"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
