1d338fef552e92360dc0d74b6a0a3746d63435f9
IOHK <devops+nix-tools@iohk.io>
2020-10-25 01:14:08 +0000

    Automatic update for Sun Oct 25 01:14:07 UTC 2020

---
+++ hackage/happstack-hsp-7.3.7.5-r0-b899cfbfb0000f928775d450db671367972f4e6e068ae86707978fba61eaa19b.nix
@@ -0,0 +1,42 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "happstack-hsp"; version = "7.3.7.5"; };
+      license = "BSD-3-Clause";
+      copyright = "2011-2015 Jeremy Shaw";
+      maintainer = "Happstack team <happs@googlegroups.com>";
+      author = "Jeremy Shaw";
+      homepage = "http://www.happstack.com/";
+      url = "";
+      synopsis = "Support for using HSP templates in Happstack";
+      description = "Happstack is a web application framework. HSP is an XML templating solution. This package makes it easy to use HSP templates with Happstack.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."happstack-server" or (errorHandler.buildDepError "happstack-server"))
+          (hsPkgs."harp" or (errorHandler.buildDepError "harp"))
+          (hsPkgs."hsp" or (errorHandler.buildDepError "hsp"))
+          (hsPkgs."hsx2hs" or (errorHandler.buildDepError "hsx2hs"))
+          (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+          (hsPkgs."utf8-string" or (errorHandler.buildDepError "utf8-string"))
+          (hsPkgs."syb" or (errorHandler.buildDepError "syb"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          ];
+        buildable = true;
+        };
+      };
+    }
---
+++ hackage/morley-1.8.0-r0-bc863a01fa8ab19e7744210ba3474829a96e700cbfb6c28e5930b37a3aae2404.nix
@@ -0,0 +1,95 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "2.2";
+      identifier = { name = "morley"; version = "1.8.0"; };
+      license = "MIT";
+      copyright = "2018 camlCase, 2019-2020 Tocqueville Group";
+      maintainer = "Serokell <hi@serokell.io>";
+      author = "camlCase, Serokell, Tocqueville Group";
+      homepage = "https://gitlab.com/morley-framework/morley";
+      url = "";
+      synopsis = "Developer tools for the Michelson Language";
+      description = "A library to make writing smart contracts in Michelson — the smart contract language of the Tezos blockchain — pleasant and effective.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."aeson" or (errorHandler.buildDepError "aeson"))
+          (hsPkgs."aeson-casing" or (errorHandler.buildDepError "aeson-casing"))
+          (hsPkgs."aeson-pretty" or (errorHandler.buildDepError "aeson-pretty"))
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."base58-bytestring" or (errorHandler.buildDepError "base58-bytestring"))
+          (hsPkgs."binary" or (errorHandler.buildDepError "binary"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."constraints" or (errorHandler.buildDepError "constraints"))
+          (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+          (hsPkgs."cryptonite" or (errorHandler.buildDepError "cryptonite"))
+          (hsPkgs."data-default" or (errorHandler.buildDepError "data-default"))
+          (hsPkgs."first-class-families" or (errorHandler.buildDepError "first-class-families"))
+          (hsPkgs."fmt" or (errorHandler.buildDepError "fmt"))
+          (hsPkgs."generic-deriving" or (errorHandler.buildDepError "generic-deriving"))
+          (hsPkgs."gitrev" or (errorHandler.buildDepError "gitrev"))
+          (hsPkgs."hex-text" or (errorHandler.buildDepError "hex-text"))
+          (hsPkgs."interpolate" or (errorHandler.buildDepError "interpolate"))
+          (hsPkgs."lens" or (errorHandler.buildDepError "lens"))
+          (hsPkgs."megaparsec" or (errorHandler.buildDepError "megaparsec"))
+          (hsPkgs."memory" or (errorHandler.buildDepError "memory"))
+          (hsPkgs."morley-prelude" or (errorHandler.buildDepError "morley-prelude"))
+          (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+          (hsPkgs."named" or (errorHandler.buildDepError "named"))
+          (hsPkgs."optparse-applicative" or (errorHandler.buildDepError "optparse-applicative"))
+          (hsPkgs."parser-combinators" or (errorHandler.buildDepError "parser-combinators"))
+          (hsPkgs."semigroups" or (errorHandler.buildDepError "semigroups"))
+          (hsPkgs."show-type" or (errorHandler.buildDepError "show-type"))
+          (hsPkgs."singletons" or (errorHandler.buildDepError "singletons"))
+          (hsPkgs."syb" or (errorHandler.buildDepError "syb"))
+          (hsPkgs."template-haskell" or (errorHandler.buildDepError "template-haskell"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."th-lift" or (errorHandler.buildDepError "th-lift"))
+          (hsPkgs."th-lift-instances" or (errorHandler.buildDepError "th-lift-instances"))
+          (hsPkgs."time" or (errorHandler.buildDepError "time"))
+          (hsPkgs."timerep" or (errorHandler.buildDepError "timerep"))
+          (hsPkgs."transformers-compat" or (errorHandler.buildDepError "transformers-compat"))
+          (hsPkgs."unordered-containers" or (errorHandler.buildDepError "unordered-containers"))
+          (hsPkgs."vector" or (errorHandler.buildDepError "vector"))
+          (hsPkgs."vinyl" or (errorHandler.buildDepError "vinyl"))
+          (hsPkgs."with-utf8" or (errorHandler.buildDepError "with-utf8"))
+          (hsPkgs."wl-pprint-text" or (errorHandler.buildDepError "wl-pprint-text"))
+          ];
+        buildable = true;
+        };
+      exes = {
+        "morley" = {
+          depends = [
+            (hsPkgs."aeson" or (errorHandler.buildDepError "aeson"))
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."data-default" or (errorHandler.buildDepError "data-default"))
+            (hsPkgs."fmt" or (errorHandler.buildDepError "fmt"))
+            (hsPkgs."haskeline" or (errorHandler.buildDepError "haskeline"))
+            (hsPkgs."megaparsec" or (errorHandler.buildDepError "megaparsec"))
+            (hsPkgs."morley" or (errorHandler.buildDepError "morley"))
+            (hsPkgs."morley-prelude" or (errorHandler.buildDepError "morley-prelude"))
+            (hsPkgs."named" or (errorHandler.buildDepError "named"))
+            (hsPkgs."optparse-applicative" or (errorHandler.buildDepError "optparse-applicative"))
+            (hsPkgs."pretty-simple" or (errorHandler.buildDepError "pretty-simple"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."vinyl" or (errorHandler.buildDepError "vinyl"))
+            (hsPkgs."with-utf8" or (errorHandler.buildDepError "with-utf8"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
