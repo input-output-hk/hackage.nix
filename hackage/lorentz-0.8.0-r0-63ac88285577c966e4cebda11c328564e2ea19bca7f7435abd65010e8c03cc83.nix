e774ac543e342ab1042bf4d6af4e56b3dfbd1909
IOHK <devops+nix-tools@iohk.io>
2020-12-03 01:13:45 +0000

    Automatic update for Thu Dec  3 01:13:44 UTC 2020

---
+++ hackage/lorentz-0.8.0-r0-63ac88285577c966e4cebda11c328564e2ea19bca7f7435abd65010e8c03cc83.nix
@@ -0,0 +1,55 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "2.0";
+      identifier = { name = "lorentz"; version = "0.8.0"; };
+      license = "MIT";
+      copyright = "2019-2020 Tocqueville Group";
+      maintainer = "Serokell <hi@serokell.io>";
+      author = "camlCase, Serokell, Tocqueville Group";
+      homepage = "https://gitlab.com/morley-framework/morley";
+      url = "";
+      synopsis = "EDSL for the Michelson Language";
+      description = "Lorentz is a powerful meta-programming tool which allows one to write Michelson contracts directly in Haskell. It has the same instructions as Michelson, but operates on Haskell values and allows one to use Haskell features.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."aeson-pretty" or (errorHandler.buildDepError "aeson-pretty"))
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."bimap" or (errorHandler.buildDepError "bimap"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."constraints" or (errorHandler.buildDepError "constraints"))
+          (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+          (hsPkgs."data-default" or (errorHandler.buildDepError "data-default"))
+          (hsPkgs."first-class-families" or (errorHandler.buildDepError "first-class-families"))
+          (hsPkgs."fmt" or (errorHandler.buildDepError "fmt"))
+          (hsPkgs."interpolate" or (errorHandler.buildDepError "interpolate"))
+          (hsPkgs."lens" or (errorHandler.buildDepError "lens"))
+          (hsPkgs."morley" or (errorHandler.buildDepError "morley"))
+          (hsPkgs."morley-prelude" or (errorHandler.buildDepError "morley-prelude"))
+          (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+          (hsPkgs."named" or (errorHandler.buildDepError "named"))
+          (hsPkgs."optparse-applicative" or (errorHandler.buildDepError "optparse-applicative"))
+          (hsPkgs."singletons" or (errorHandler.buildDepError "singletons"))
+          (hsPkgs."template-haskell" or (errorHandler.buildDepError "template-haskell"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."text-manipulate" or (errorHandler.buildDepError "text-manipulate"))
+          (hsPkgs."unordered-containers" or (errorHandler.buildDepError "unordered-containers"))
+          (hsPkgs."vinyl" or (errorHandler.buildDepError "vinyl"))
+          (hsPkgs."with-utf8" or (errorHandler.buildDepError "with-utf8"))
+          ];
+        buildable = true;
+        };
+      };
+    }
---
+++ hackage/mail-pool-2.2.0-r0-220f3a54b5df7dddf229b0430519709a39481c7599f90808e7cf397538682b10.nix
@@ -0,0 +1,60 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.12";
+      identifier = { name = "mail-pool"; version = "2.2.0"; };
+      license = "MIT";
+      copyright = "2019 Jappie Klooster";
+      maintainer = "jappieklooster@hotmail.com";
+      author = "Jappie Klooster";
+      homepage = "https://github.com/jappeace/email-pool#readme";
+      url = "";
+      synopsis = "Preconfigured email connection pool on top of smtp.";
+      description = "Email helper functions with some sane defaults such as a resource pool and cli support";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."HaskellNet" or (errorHandler.buildDepError "HaskellNet"))
+          (hsPkgs."HaskellNet-SSL" or (errorHandler.buildDepError "HaskellNet-SSL"))
+          (hsPkgs."aeson" or (errorHandler.buildDepError "aeson"))
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."microlens" or (errorHandler.buildDepError "microlens"))
+          (hsPkgs."mime-mail" or (errorHandler.buildDepError "mime-mail"))
+          (hsPkgs."network" or (errorHandler.buildDepError "network"))
+          (hsPkgs."optparse-applicative" or (errorHandler.buildDepError "optparse-applicative"))
+          (hsPkgs."resource-pool" or (errorHandler.buildDepError "resource-pool"))
+          (hsPkgs."time" or (errorHandler.buildDepError "time"))
+          ];
+        buildable = true;
+        };
+      exes = {
+        "exe" = {
+          depends = [
+            (hsPkgs."HaskellNet" or (errorHandler.buildDepError "HaskellNet"))
+            (hsPkgs."HaskellNet-SSL" or (errorHandler.buildDepError "HaskellNet-SSL"))
+            (hsPkgs."aeson" or (errorHandler.buildDepError "aeson"))
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."mail-pool" or (errorHandler.buildDepError "mail-pool"))
+            (hsPkgs."microlens" or (errorHandler.buildDepError "microlens"))
+            (hsPkgs."mime-mail" or (errorHandler.buildDepError "mime-mail"))
+            (hsPkgs."network" or (errorHandler.buildDepError "network"))
+            (hsPkgs."optparse-applicative" or (errorHandler.buildDepError "optparse-applicative"))
+            (hsPkgs."resource-pool" or (errorHandler.buildDepError "resource-pool"))
+            (hsPkgs."time" or (errorHandler.buildDepError "time"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
