f101f4436c9e2ccafa20388ccc2038de2135ab0c
IOHK <devops+nix-tools@iohk.io>
2020-12-11 01:12:28 +0000

    Automatic update for Fri Dec 11 01:12:27 UTC 2020

---
+++ hackage/anagrep-0.1.0.0-r0-dcb7d88d83f16226c6f8068b0ce781464dcc5d364bd70fba179c94d157fa3ea9.nix
@@ -0,0 +1,75 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = { ghc = true; };
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "anagrep"; version = "0.1.0.0"; };
+      license = "BSD-3-Clause";
+      copyright = "2020, Dylan Simon";
+      maintainer = "dylan@dylex.net";
+      author = "Dylan Simon";
+      homepage = "";
+      url = "";
+      synopsis = "Find strings with permutations (anagrams) that match a regular expression";
+      description = "Given a regular expression, determine if it matches any permutation of a given string.  For example, @\"lt[aeiou]*\"@ would match all strings with one \\'l\\', one \\'t\\', and vowels (like \\\"elate\\\", \\\"tail\\\", \\\"tl\\\", etc.).\nRegular expression parsing is based on <//hackage.haskell.org/package/regex-tdfa regex-tdfa> and generally follows those semantics, but not all regular expression features are supported.  For example, repeat modifiers cannot be applied to groups (such as \"(abc)*\").\nThe goal is for matching to be fairly efficient in most cases, given that this problem is NP-complete.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+          (hsPkgs."vector" or (errorHandler.buildDepError "vector"))
+          (hsPkgs."regex-tdfa" or (errorHandler.buildDepError "regex-tdfa"))
+          (hsPkgs."case-insensitive" or (errorHandler.buildDepError "case-insensitive"))
+          (hsPkgs."deepseq" or (errorHandler.buildDepError "deepseq"))
+          ] ++ (pkgs.lib).optionals (flags.ghc) [
+          (hsPkgs."ghc-prim" or (errorHandler.buildDepError "ghc-prim"))
+          (hsPkgs."integer-gmp" or (errorHandler.buildDepError "integer-gmp"))
+          ];
+        buildable = true;
+        };
+      exes = {
+        "anagrep" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."anagrep" or (errorHandler.buildDepError "anagrep"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."case-insensitive" or (errorHandler.buildDepError "case-insensitive"))
+            ];
+          buildable = true;
+          };
+        };
+      tests = {
+        "test" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."anagrep" or (errorHandler.buildDepError "anagrep"))
+            (hsPkgs."case-insensitive" or (errorHandler.buildDepError "case-insensitive"))
+            (hsPkgs."hspec" or (errorHandler.buildDepError "hspec"))
+            (hsPkgs."QuickCheck" or (errorHandler.buildDepError "QuickCheck"))
+            ];
+          buildable = true;
+          };
+        };
+      benchmarks = {
+        "bench" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."anagrep" or (errorHandler.buildDepError "anagrep"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."criterion" or (errorHandler.buildDepError "criterion"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
---
+++ hackage/call-alloy-0.2.1.1-r0-3cb20e8f2a354e147d5aa36b66bc41e8b1c86b457b466d0c9865a7197c42277f.nix
@@ -0,0 +1,69 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.12";
+      identifier = { name = "call-alloy"; version = "0.2.1.1"; };
+      license = "MIT";
+      copyright = "2019-2020 Marcellus Siegburg";
+      maintainer = "marcellus.siegburg@uni-due.de";
+      author = "Marcellus Siegburg";
+      homepage = "https://github.com/marcellussiegburg/call-alloy#readme";
+      url = "";
+      synopsis = "A simple library to call Alloy given a specification";
+      description = "Please see the README on GitHub at <https://github.com/marcellussiegburg/call-alloy#readme>";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+          (hsPkgs."directory" or (errorHandler.buildDepError "directory"))
+          (hsPkgs."file-embed" or (errorHandler.buildDepError "file-embed"))
+          (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
+          (hsPkgs."hashable" or (errorHandler.buildDepError "hashable"))
+          (hsPkgs."lens" or (errorHandler.buildDepError "lens"))
+          (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+          (hsPkgs."process" or (errorHandler.buildDepError "process"))
+          (hsPkgs."split" or (errorHandler.buildDepError "split"))
+          (hsPkgs."trifecta" or (errorHandler.buildDepError "trifecta"))
+          ] ++ (if system.isWindows
+          then [ (hsPkgs."Win32" or (errorHandler.buildDepError "Win32")) ]
+          else [ (hsPkgs."unix" or (errorHandler.buildDepError "unix")) ]);
+        buildable = true;
+        };
+      tests = {
+        "call-alloy-test" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."call-alloy" or (errorHandler.buildDepError "call-alloy"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."directory" or (errorHandler.buildDepError "directory"))
+            (hsPkgs."file-embed" or (errorHandler.buildDepError "file-embed"))
+            (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
+            (hsPkgs."hashable" or (errorHandler.buildDepError "hashable"))
+            (hsPkgs."hspec" or (errorHandler.buildDepError "hspec"))
+            (hsPkgs."lens" or (errorHandler.buildDepError "lens"))
+            (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+            (hsPkgs."process" or (errorHandler.buildDepError "process"))
+            (hsPkgs."split" or (errorHandler.buildDepError "split"))
+            (hsPkgs."trifecta" or (errorHandler.buildDepError "trifecta"))
+            ] ++ (if system.isWindows
+            then [ (hsPkgs."Win32" or (errorHandler.buildDepError "Win32")) ]
+            else [ (hsPkgs."unix" or (errorHandler.buildDepError "unix")) ]);
+          buildable = true;
+          };
+        };
+      };
+    }
