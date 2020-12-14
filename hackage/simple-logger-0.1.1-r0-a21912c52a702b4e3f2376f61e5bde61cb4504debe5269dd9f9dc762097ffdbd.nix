4c52bafb84411e13adee6aa764cbf24dba69dd91
IOHK <devops+nix-tools@iohk.io>
2020-12-14 01:12:07 +0000

    Automatic update for Mon Dec 14 01:12:06 UTC 2020

---
+++ hackage/simple-logger-0.1.1-r0-a21912c52a702b4e3f2376f61e5bde61cb4504debe5269dd9f9dc762097ffdbd.nix
@@ -0,0 +1,37 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.12";
+      identifier = { name = "simple-logger"; version = "0.1.1"; };
+      license = "MIT";
+      copyright = "";
+      maintainer = "Alexander Thiemann <mail@athiemann.net>";
+      author = "";
+      homepage = "https://github.com/agrafix/simple-logger#readme";
+      url = "";
+      synopsis = "A very simple but efficient logging framework";
+      description = "A very simple but efficient logging framework";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."fast-logger" or (errorHandler.buildDepError "fast-logger"))
+          (hsPkgs."monad-logger" or (errorHandler.buildDepError "monad-logger"))
+          (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          ];
+        buildable = true;
+        };
+      };
+    }
