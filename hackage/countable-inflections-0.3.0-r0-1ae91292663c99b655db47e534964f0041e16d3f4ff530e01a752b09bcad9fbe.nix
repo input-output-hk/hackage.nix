5da9c85d741580f69fc46a4728af013eea9fb2d9
IOHK <devops+nix-tools@iohk.io>
2020-12-23 01:10:15 +0000

    Automatic update for Wed Dec 23 01:10:14 UTC 2020

---
+++ hackage/countable-inflections-0.3.0-r0-1ae91292663c99b655db47e534964f0041e16d3f4ff530e01a752b09bcad9fbe.nix
@@ -0,0 +1,50 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = { dev = false; };
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "countable-inflections"; version = "0.3.0"; };
+      license = "MIT";
+      copyright = "2016 Brady Ouren";
+      maintainer = "Brady Ouren <brady.ouren@gmail.com>";
+      author = "Brady Ouren <brady.ouren@gmail.com>";
+      homepage = "https://github.com/tippenein/countable-inflections";
+      url = "";
+      synopsis = "Countable Text Inflections";
+      description = "Provides methods for singularizing and pluralizing text.\nThe library is based on Rails' inflections.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."exceptions" or (errorHandler.buildDepError "exceptions"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."regex-pcre-builtin" or (errorHandler.buildDepError "regex-pcre-builtin"))
+          (hsPkgs."pcre-utils" or (errorHandler.buildDepError "pcre-utils"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "test" = {
+          depends = [
+            (hsPkgs."countable-inflections" or (errorHandler.buildDepError "countable-inflections"))
+            (hsPkgs."QuickCheck" or (errorHandler.buildDepError "QuickCheck"))
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."hspec" or (errorHandler.buildDepError "hspec"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
---
+++ hackage/yesod-form-multi-1.7.0-r0-f07f1bfbde8b3cd8bf489f70f9ab06378f94425f83b2c304d2851d85c3a2cf0f.nix
@@ -0,0 +1,39 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = { network-uri = true; };
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "yesod-form-multi"; version = "1.7.0"; };
+      license = "MIT";
+      copyright = "";
+      maintainer = "James Burton <jamesejburton@gmail.com>";
+      author = "James Burton <jamesejburton@gmail.com>";
+      homepage = "http://www.yesodweb.com/";
+      url = "";
+      synopsis = "Multi-input form handling for Yesod Web Framework";
+      description = "API docs and the README are available at <http://www.stackage.org/package/yesod-form-multi>.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+          (hsPkgs."shakespeare" or (errorHandler.buildDepError "shakespeare"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+          (hsPkgs."yesod-core" or (errorHandler.buildDepError "yesod-core"))
+          (hsPkgs."yesod-form" or (errorHandler.buildDepError "yesod-form"))
+          ] ++ (pkgs.lib).optional (flags.network-uri) (hsPkgs."network-uri" or (errorHandler.buildDepError "network-uri"));
+        buildable = true;
+        };
+      };
+    }
