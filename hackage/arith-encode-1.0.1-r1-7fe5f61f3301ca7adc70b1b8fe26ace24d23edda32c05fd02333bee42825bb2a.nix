5f329610754786f35663addab31b1c4fb0c1def6
IOHK <devops+nix-tools@iohk.io>
2020-12-31 01:13:21 +0000

    Automatic update for Thu Dec 31 01:13:20 UTC 2020

---
+++ hackage/arith-encode-1.0.1-r1-7fe5f61f3301ca7adc70b1b8fe26ace24d23edda32c05fd02333bee42825bb2a.nix
@@ -0,0 +1,54 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.16";
+      identifier = { name = "arith-encode"; version = "1.0.1"; };
+      license = "BSD-3-Clause";
+      copyright = "Copyright (c) 2014 Eric McCorkle.  All rights reserved.";
+      maintainer = "Eric McCorkle <emc2@metricspace.net>, Andrew Lelechenko <andrew.lelechenko@gmail.com>";
+      author = "Eric McCorkle";
+      homepage = "https://github.com/emc2/arith-encode";
+      url = "";
+      synopsis = "A practical arithmetic encoding (aka Godel numbering) library.";
+      description = "A library providing tools and various schemes for encoding arbitrary datatypes\nas natural numbers.  The underlying theory is that of isomorphisms with the natural\nnumbers (known as Godel numbering).  The library provides functionality for defining\nmultiple such encodings for a given datatype, as well as a collection of stock\nencodings and combinators which can be used to build more complex encodings.\n\nThis has various uses, among them binary serialization/deserialization and\nenumeration testing.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."arithmoi" or (errorHandler.buildDepError "arithmoi"))
+          (hsPkgs."array" or (errorHandler.buildDepError "array"))
+          (hsPkgs."binary" or (errorHandler.buildDepError "binary"))
+          (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+          (hsPkgs."hashable" or (errorHandler.buildDepError "hashable"))
+          (hsPkgs."integer-logarithms" or (errorHandler.buildDepError "integer-logarithms"))
+          (hsPkgs."unordered-containers" or (errorHandler.buildDepError "unordered-containers"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "UnitTest" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."arith-encode" or (errorHandler.buildDepError "arith-encode"))
+            (hsPkgs."binary" or (errorHandler.buildDepError "binary"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."hashable" or (errorHandler.buildDepError "hashable"))
+            (hsPkgs."HUnit-Plus" or (errorHandler.buildDepError "HUnit-Plus"))
+            (hsPkgs."unordered-containers" or (errorHandler.buildDepError "unordered-containers"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
---
+++ hackage/net-spider-pangraph-0.2.0.3-r0-bec583f86af88ffae571b79cf2d26fd890ec7b1cdfa444a612b18113b4cdfb69.nix
@@ -0,0 +1,60 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "net-spider-pangraph"; version = "0.2.0.3"; };
+      license = "BSD-3-Clause";
+      copyright = "";
+      maintainer = "Toshio Ito <debug.ito@gmail.com>";
+      author = "Toshio Ito <debug.ito@gmail.com>";
+      homepage = "https://github.com/debug-ito/net-spider";
+      url = "";
+      synopsis = "Conversion between net-spider and pangraph";
+      description = "Conversion between <https://hackage.haskell.org/package/net-spider net-spider> and <https://hackage.haskell.org/package/pangraph pangraph>. See README.md for detail.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."net-spider" or (errorHandler.buildDepError "net-spider"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."pangraph" or (errorHandler.buildDepError "pangraph"))
+          (hsPkgs."time" or (errorHandler.buildDepError "time"))
+          (hsPkgs."greskell" or (errorHandler.buildDepError "greskell"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "spec" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."net-spider-pangraph" or (errorHandler.buildDepError "net-spider-pangraph"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."pangraph" or (errorHandler.buildDepError "pangraph"))
+            (hsPkgs."net-spider" or (errorHandler.buildDepError "net-spider"))
+            (hsPkgs."hspec" or (errorHandler.buildDepError "hspec"))
+            ];
+          buildable = true;
+          };
+        "doctest" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."doctest" or (errorHandler.buildDepError "doctest"))
+            (hsPkgs."doctest-discover" or (errorHandler.buildDepError "doctest-discover"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
