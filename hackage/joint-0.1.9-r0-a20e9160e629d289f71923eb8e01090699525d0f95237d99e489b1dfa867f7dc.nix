36a716bc8d2efc8e494efb6b39d345eea2596897
IOHK <devops+nix-tools@iohk.io>
2021-01-02 01:09:53 +0000

    Automatic update for Sat Jan  2 01:09:52 UTC 2021

---
+++ hackage/joint-0.1.9-r0-a20e9160e629d289f71923eb8e01090699525d0f95237d99e489b1dfa867f7dc.nix
@@ -0,0 +1,37 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "joint"; version = "0.1.9"; };
+      license = "BSD-3-Clause";
+      copyright = "Copyright (c) 2018 Murat Kasimov";
+      maintainer = "Murat Kasimov <iokasimov.m@gmail.com>";
+      author = "Murat Kasimov";
+      homepage = "https://github.com/iokasimov/joint";
+      url = "";
+      synopsis = "Trying to compose non-composable";
+      description = "";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+          (hsPkgs."comonad" or (errorHandler.buildDepError "comonad"))
+          (hsPkgs."adjunctions" or (errorHandler.buildDepError "adjunctions"))
+          (hsPkgs."distributive" or (errorHandler.buildDepError "distributive"))
+          ];
+        buildable = true;
+        };
+      };
+    }
---
+++ hackage/symantic-parser-0.0.0.20210101-r0-714950f5a6f0bd9532e060e2d2b7d0a620595bdc3ac8c0c2b3e403f339e1ec20.nix
@@ -0,0 +1,66 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = { dump-core = false; dump-splices = false; };
+    package = {
+      specVersion = "2.2";
+      identifier = { name = "symantic-parser"; version = "0.0.0.20210101"; };
+      license = "GPL-3.0-or-later";
+      copyright = "Julien Moutinho <julm+symantic-parser@sourcephile.fr>";
+      maintainer = "Julien Moutinho <julm+symantic-parser@sourcephile.fr>";
+      author = "Julien Moutinho <julm+symantic-parser@sourcephile.fr>";
+      homepage = "";
+      url = "";
+      synopsis = "Parser combinators statically optimized and staged via typed meta-programming";
+      description = "This is a work-in-progress experimental library to generate parsers,\nleveraging Tagless-Final interpreters and Typed Template Haskell staging.\n\nThis is an alternative but less powerful/reviewed\nimplementation of [ParsleyHaskell](https://github.com/J-mie6/ParsleyHaskell).\nSee the paper by Jamie Willis, Nicolas Wu, and Matthew\nPickering, admirably well presented at ICFP-2020: [Staged\nSelective Parser\nCombinators](https://icfp20.sigplan.org/details/icfp-2020-papers/20/Staged-Selective-Parser-Combinators).";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."array" or (errorHandler.buildDepError "array"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+          (hsPkgs."ghc-prim" or (errorHandler.buildDepError "ghc-prim"))
+          (hsPkgs."hashable" or (errorHandler.buildDepError "hashable"))
+          (hsPkgs."template-haskell" or (errorHandler.buildDepError "template-haskell"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+          (hsPkgs."unordered-containers" or (errorHandler.buildDepError "unordered-containers"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "symantic-parser-test" = {
+          depends = [
+            (hsPkgs."symantic-parser" or (errorHandler.buildDepError "symantic-parser"))
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."deepseq" or (errorHandler.buildDepError "deepseq"))
+            (hsPkgs."directory" or (errorHandler.buildDepError "directory"))
+            (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
+            (hsPkgs."hashable" or (errorHandler.buildDepError "hashable"))
+            (hsPkgs."process" or (errorHandler.buildDepError "process"))
+            (hsPkgs."strict" or (errorHandler.buildDepError "strict"))
+            (hsPkgs."tasty" or (errorHandler.buildDepError "tasty"))
+            (hsPkgs."tasty-golden" or (errorHandler.buildDepError "tasty-golden"))
+            (hsPkgs."template-haskell" or (errorHandler.buildDepError "template-haskell"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+            (hsPkgs."unix" or (errorHandler.buildDepError "unix"))
+            (hsPkgs."unordered-containers" or (errorHandler.buildDepError "unordered-containers"))
+            ] ++ (pkgs.lib).optional (flags.dump-core) (hsPkgs."dump-core" or (errorHandler.buildDepError "dump-core"));
+          buildable = true;
+          };
+        };
+      };
+    }
