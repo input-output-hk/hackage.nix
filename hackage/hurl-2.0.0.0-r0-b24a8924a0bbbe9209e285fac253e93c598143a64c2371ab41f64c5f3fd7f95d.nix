d0b246eb72d47aba016f6f5ef3d0fb38b0df83b0
IOHK <devops+nix-tools@iohk.io>
2021-01-08 01:14:35 +0000

    Automatic update for Fri Jan  8 01:14:34 UTC 2021

---
+++ hackage/hurl-2.0.0.0-r0-b24a8924a0bbbe9209e285fac253e93c598143a64c2371ab41f64c5f3fd7f95d.nix
@@ -0,0 +1,75 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {
+      http = true;
+      gemini = true;
+      file = true;
+      data = true;
+      freedesktop = true;
+      rewriters = true;
+      };
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "hurl"; version = "2.0.0.0"; };
+      license = "GPL-3.0-only";
+      copyright = "";
+      maintainer = "adrian@openwork.nz";
+      author = "Adrian Cochrane";
+      homepage = "https://git.adrian.geek.nz/hurl.git/";
+      url = "";
+      synopsis = "Haskell URL resolver";
+      description = "Retrieves resources for a URI, whether they be HTTP(S), gemini:, file:, or data:.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = (((([
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."network-uri" or (errorHandler.buildDepError "network-uri"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."async" or (errorHandler.buildDepError "async"))
+          (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
+          (hsPkgs."directory" or (errorHandler.buildDepError "directory"))
+          (hsPkgs."time" or (errorHandler.buildDepError "time"))
+          ] ++ (pkgs.lib).optionals (flags.http) [
+          (hsPkgs."http-client" or (errorHandler.buildDepError "http-client"))
+          (hsPkgs."http-types" or (errorHandler.buildDepError "http-types"))
+          (hsPkgs."http-client-openssl" or (errorHandler.buildDepError "http-client-openssl"))
+          (hsPkgs."HsOpenSSL" or (errorHandler.buildDepError "HsOpenSSL"))
+          ]) ++ (pkgs.lib).optionals (flags.gemini) [
+          (hsPkgs."HsOpenSSL" or (errorHandler.buildDepError "HsOpenSSL"))
+          (hsPkgs."openssl-streams" or (errorHandler.buildDepError "openssl-streams"))
+          (hsPkgs."io-streams" or (errorHandler.buildDepError "io-streams"))
+          ]) ++ (pkgs.lib).optional (flags.data) (hsPkgs."base64-bytestring" or (errorHandler.buildDepError "base64-bytestring"))) ++ (pkgs.lib).optionals (flags.freedesktop) [
+          (hsPkgs."process" or (errorHandler.buildDepError "process"))
+          (hsPkgs."xml-conduit" or (errorHandler.buildDepError "xml-conduit"))
+          (hsPkgs."zlib" or (errorHandler.buildDepError "zlib"))
+          (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+          ]) ++ (pkgs.lib).optionals (flags.rewriters) [
+          (hsPkgs."regex" or (errorHandler.buildDepError "regex"))
+          (hsPkgs."regex-tdfa" or (errorHandler.buildDepError "regex-tdfa"))
+          ];
+        buildable = true;
+        };
+      exes = {
+        "hurl" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."hurl" or (errorHandler.buildDepError "hurl"))
+            (hsPkgs."network-uri" or (errorHandler.buildDepError "network-uri"))
+            (hsPkgs."directory" or (errorHandler.buildDepError "directory"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
