6732b499bfb997f6282d66de26792279392e0951
IOHK <devops+nix-tools@iohk.io>
2021-01-15 01:12:53 +0000

    Automatic update for Fri Jan 15 01:12:52 UTC 2021

---
+++ hackage/arch-web-0.1.0-r0-22f28275dbe03287571b31f06fa06f396f99de8b323c5c10665c4f4599bdc6b3.nix
@@ -0,0 +1,69 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "2.2";
+      identifier = { name = "arch-web"; version = "0.1.0"; };
+      license = "MIT";
+      copyright = "Copyright (c) berberman 2021";
+      maintainer = "berberman <berberman@yandex.com>";
+      author = "berberman";
+      homepage = "https://github.com/berberman/arch-web";
+      url = "";
+      synopsis = "Arch Linux official and AUR web interface binding";
+      description = "Arch Linux official and AUR web interface binding.\nSee README for details.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."aeson" or (errorHandler.buildDepError "aeson"))
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."deriving-aeson" or (errorHandler.buildDepError "deriving-aeson"))
+          (hsPkgs."exceptions" or (errorHandler.buildDepError "exceptions"))
+          (hsPkgs."http-client" or (errorHandler.buildDepError "http-client"))
+          (hsPkgs."http-client-tls" or (errorHandler.buildDepError "http-client-tls"))
+          (hsPkgs."http-types" or (errorHandler.buildDepError "http-types"))
+          (hsPkgs."lens" or (errorHandler.buildDepError "lens"))
+          (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+          (hsPkgs."servant" or (errorHandler.buildDepError "servant"))
+          (hsPkgs."servant-client" or (errorHandler.buildDepError "servant-client"))
+          (hsPkgs."servant-client-core" or (errorHandler.buildDepError "servant-client-core"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."time" or (errorHandler.buildDepError "time"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "arch-web-test" = {
+          depends = [
+            (hsPkgs."aeson" or (errorHandler.buildDepError "aeson"))
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."deriving-aeson" or (errorHandler.buildDepError "deriving-aeson"))
+            (hsPkgs."exceptions" or (errorHandler.buildDepError "exceptions"))
+            (hsPkgs."http-client" or (errorHandler.buildDepError "http-client"))
+            (hsPkgs."http-client-tls" or (errorHandler.buildDepError "http-client-tls"))
+            (hsPkgs."http-types" or (errorHandler.buildDepError "http-types"))
+            (hsPkgs."lens" or (errorHandler.buildDepError "lens"))
+            (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+            (hsPkgs."servant" or (errorHandler.buildDepError "servant"))
+            (hsPkgs."servant-client" or (errorHandler.buildDepError "servant-client"))
+            (hsPkgs."servant-client-core" or (errorHandler.buildDepError "servant-client-core"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."time" or (errorHandler.buildDepError "time"))
+            (hsPkgs."arch-web" or (errorHandler.buildDepError "arch-web"))
+            (hsPkgs."HUnit" or (errorHandler.buildDepError "HUnit"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
---
+++ hackage/termonad-4.1.1.0-r0-583674f8a0b540675aa10fdfa8ce41e99a6a643e32c174e33dfa0047b71ab6df.nix
@@ -0,0 +1,143 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = { buildexamples = false; };
+    package = {
+      specVersion = "1.12";
+      identifier = { name = "termonad"; version = "4.1.1.0"; };
+      license = "BSD-3-Clause";
+      copyright = "2017 Dennis Gosnell";
+      maintainer = "cdep.illabout@gmail.com";
+      author = "Dennis Gosnell";
+      homepage = "https://github.com/cdepillabout/termonad";
+      url = "";
+      synopsis = "Terminal emulator configurable in Haskell";
+      description = "Termonad is a terminal emulator configurable in Haskell.  It is extremely\ncustomizable and provides hooks to modify the default behavior.  It can be\nthought of as the \\\"XMonad\\\" of terminal emulators.  Termonad was featured on\nan <https://www.youtube.com/watch?v=TLNr_gBv5HY episode> of\n<https://www.youtube.com/channel/UCVls1GmFKf6WlTraIb_IaJg DistroTube>.  This\nvideo gives a short overview of Termonad.\n\nPlease see <https://github.com/cdepillabout/termonad#readme README.md> for\nmore information.";
+      buildType = "Custom";
+      setup-depends = [
+        (hsPkgs.buildPackages.base or (pkgs.buildPackages.base or (errorHandler.buildToolDepError "base")))
+        (hsPkgs.buildPackages.Cabal or (pkgs.buildPackages.Cabal or (errorHandler.buildToolDepError "Cabal")))
+        (hsPkgs.buildPackages.cabal-doctest or (pkgs.buildPackages.cabal-doctest or (errorHandler.buildToolDepError "cabal-doctest")))
+        ];
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."adjunctions" or (errorHandler.buildDepError "adjunctions"))
+          (hsPkgs."classy-prelude" or (errorHandler.buildDepError "classy-prelude"))
+          (hsPkgs."colour" or (errorHandler.buildDepError "colour"))
+          (hsPkgs."constraints" or (errorHandler.buildDepError "constraints"))
+          (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+          (hsPkgs."data-default" or (errorHandler.buildDepError "data-default"))
+          (hsPkgs."directory" or (errorHandler.buildDepError "directory"))
+          (hsPkgs."distributive" or (errorHandler.buildDepError "distributive"))
+          (hsPkgs."dyre" or (errorHandler.buildDepError "dyre"))
+          (hsPkgs."file-embed" or (errorHandler.buildDepError "file-embed"))
+          (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
+          (hsPkgs."focuslist" or (errorHandler.buildDepError "focuslist"))
+          (hsPkgs."gi-gdk" or (errorHandler.buildDepError "gi-gdk"))
+          (hsPkgs."gi-gio" or (errorHandler.buildDepError "gi-gio"))
+          (hsPkgs."gi-glib" or (errorHandler.buildDepError "gi-glib"))
+          (hsPkgs."gi-gtk" or (errorHandler.buildDepError "gi-gtk"))
+          (hsPkgs."gi-pango" or (errorHandler.buildDepError "gi-pango"))
+          (hsPkgs."gi-vte" or (errorHandler.buildDepError "gi-vte"))
+          (hsPkgs."haskell-gi-base" or (errorHandler.buildDepError "haskell-gi-base"))
+          (hsPkgs."inline-c" or (errorHandler.buildDepError "inline-c"))
+          (hsPkgs."lens" or (errorHandler.buildDepError "lens"))
+          (hsPkgs."mono-traversable" or (errorHandler.buildDepError "mono-traversable"))
+          (hsPkgs."pretty-simple" or (errorHandler.buildDepError "pretty-simple"))
+          (hsPkgs."QuickCheck" or (errorHandler.buildDepError "QuickCheck"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+          (hsPkgs."yaml" or (errorHandler.buildDepError "yaml"))
+          (hsPkgs."xml-conduit" or (errorHandler.buildDepError "xml-conduit"))
+          (hsPkgs."xml-html-qq" or (errorHandler.buildDepError "xml-html-qq"))
+          ];
+        pkgconfig = [
+          (pkgconfPkgs."gtk+-3.0" or (errorHandler.pkgConfDepError "gtk+-3.0"))
+          (pkgconfPkgs."libpcre2-8" or (errorHandler.pkgConfDepError "libpcre2-8"))
+          (pkgconfPkgs."vte-2.91" or (errorHandler.pkgConfDepError "vte-2.91"))
+          ];
+        buildable = true;
+        };
+      exes = {
+        "termonad" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."termonad" or (errorHandler.buildDepError "termonad"))
+            ];
+          buildable = true;
+          };
+        "termonad-readme" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."markdown-unlit" or (errorHandler.buildDepError "markdown-unlit"))
+            (hsPkgs."termonad" or (errorHandler.buildDepError "termonad"))
+            (hsPkgs."colour" or (errorHandler.buildDepError "colour"))
+            ];
+          buildable = if flags.buildexamples then true else false;
+          };
+        "termonad-example-colour-extension" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."termonad" or (errorHandler.buildDepError "termonad"))
+            (hsPkgs."colour" or (errorHandler.buildDepError "colour"))
+            ];
+          buildable = if flags.buildexamples then true else false;
+          };
+        "termonad-example-colour-extension-gruvbox" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."termonad" or (errorHandler.buildDepError "termonad"))
+            (hsPkgs."colour" or (errorHandler.buildDepError "colour"))
+            ];
+          buildable = if flags.buildexamples then true else false;
+          };
+        "termonad-example-colour-extension-solarized" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."termonad" or (errorHandler.buildDepError "termonad"))
+            (hsPkgs."colour" or (errorHandler.buildDepError "colour"))
+            ];
+          buildable = if flags.buildexamples then true else false;
+          };
+        };
+      tests = {
+        "doctests" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."doctest" or (errorHandler.buildDepError "doctest"))
+            (hsPkgs."QuickCheck" or (errorHandler.buildDepError "QuickCheck"))
+            (hsPkgs."template-haskell" or (errorHandler.buildDepError "template-haskell"))
+            (hsPkgs."termonad" or (errorHandler.buildDepError "termonad"))
+            ];
+          buildable = if compiler.isGhc && (compiler.version).le "8.10.2"
+            then true
+            else false;
+          };
+        "termonad-test" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."genvalidity-containers" or (errorHandler.buildDepError "genvalidity-containers"))
+            (hsPkgs."genvalidity-hspec" or (errorHandler.buildDepError "genvalidity-hspec"))
+            (hsPkgs."hedgehog" or (errorHandler.buildDepError "hedgehog"))
+            (hsPkgs."lens" or (errorHandler.buildDepError "lens"))
+            (hsPkgs."QuickCheck" or (errorHandler.buildDepError "QuickCheck"))
+            (hsPkgs."termonad" or (errorHandler.buildDepError "termonad"))
+            (hsPkgs."tasty" or (errorHandler.buildDepError "tasty"))
+            (hsPkgs."tasty-hedgehog" or (errorHandler.buildDepError "tasty-hedgehog"))
+            (hsPkgs."tasty-hspec" or (errorHandler.buildDepError "tasty-hspec"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
