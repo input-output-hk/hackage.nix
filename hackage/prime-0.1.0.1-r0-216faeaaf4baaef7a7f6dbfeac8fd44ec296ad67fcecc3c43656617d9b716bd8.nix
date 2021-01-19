972cf011638e3bb4509bdb1edd731aab18a31ec8
IOHK <devops+nix-tools@iohk.io>
2021-01-19 01:15:41 +0000

    Automatic update for Tue Jan 19 01:15:40 UTC 2021

---
+++ hackage/prime-0.1.0.1-r0-216faeaaf4baaef7a7f6dbfeac8fd44ec296ad67fcecc3c43656617d9b716bd8.nix
@@ -0,0 +1,41 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.12";
+      identifier = { name = "prime"; version = "0.1.0.1"; };
+      license = "GPL-3.0-only";
+      copyright = "(C) 2021 Jonathan Lamothe";
+      maintainer = "jonathan@jlamothe.net";
+      author = "Jonathan Lamothe";
+      homepage = "https://github.com/jlamothe/prime#readme";
+      url = "";
+      synopsis = "prime number tools";
+      description = "Please see the README on GitHub at <https://github.com/jlamothe/prime#readme>";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [ (hsPkgs."base" or (errorHandler.buildDepError "base")) ];
+        buildable = true;
+        };
+      tests = {
+        "prime-test" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."hspec" or (errorHandler.buildDepError "hspec"))
+            (hsPkgs."prime" or (errorHandler.buildDepError "prime"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
