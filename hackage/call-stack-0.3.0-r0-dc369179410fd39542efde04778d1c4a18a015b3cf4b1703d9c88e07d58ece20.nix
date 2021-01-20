b0cc6eb3bc592341d6f6d485a336e3d5b2ad930a
IOHK <devops+nix-tools@iohk.io>
2021-01-20 01:14:56 +0000

    Automatic update for Wed Jan 20 01:14:55 UTC 2021

---
+++ hackage/call-stack-0.3.0-r0-dc369179410fd39542efde04778d1c4a18a015b3cf4b1703d9c88e07d58ece20.nix
@@ -0,0 +1,41 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.12";
+      identifier = { name = "call-stack"; version = "0.3.0"; };
+      license = "MIT";
+      copyright = "";
+      maintainer = "Simon Hengel <sol@typeful.net>";
+      author = "";
+      homepage = "https://github.com/sol/call-stack#readme";
+      url = "";
+      synopsis = "Use GHC call-stacks in a backward compatible way";
+      description = "";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [ (hsPkgs."base" or (errorHandler.buildDepError "base")) ];
+        buildable = true;
+        };
+      tests = {
+        "spec" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."call-stack" or (errorHandler.buildDepError "call-stack"))
+            (hsPkgs."nanospec" or (errorHandler.buildDepError "nanospec"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
---
+++ hackage/tasty-sugar-1.0.1.1-r0-6fd61ae814831919a6a845848e5ba2b765093687d31d6099523b2dabd7e7a121.nix
@@ -0,0 +1,91 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "2.0";
+      identifier = { name = "tasty-sugar"; version = "1.0.1.1"; };
+      license = "ISC";
+      copyright = "Kevin Quick, 2019-2021";
+      maintainer = "kquick@galois.com";
+      author = "Kevin Quick";
+      homepage = "https://github.com/kquick/tasty-sugar";
+      url = "";
+      synopsis = "Tests defined by Search Using Golden Answer References";
+      description = "\nA tasty testing framework that builds individual test configurations\nfrom a set of input files and expected results (golden) files along\nwith associated files, where multiple expected results and\nassociated files for each input file can be parameterized by\nfilename.\n\nAdditionally a tasty ingredient is supplied that can be used to show\nthe search process and resulting test configurations without\nactually running the tests.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."directory" or (errorHandler.buildDepError "directory"))
+          (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
+          (hsPkgs."filemanip" or (errorHandler.buildDepError "filemanip"))
+          (hsPkgs."logict" or (errorHandler.buildDepError "logict"))
+          (hsPkgs."optparse-applicative" or (errorHandler.buildDepError "optparse-applicative"))
+          (hsPkgs."prettyprinter" or (errorHandler.buildDepError "prettyprinter"))
+          (hsPkgs."tagged" or (errorHandler.buildDepError "tagged"))
+          (hsPkgs."tasty" or (errorHandler.buildDepError "tasty"))
+          (hsPkgs."tasty-sugar-internal" or (errorHandler.buildDepError "tasty-sugar-internal"))
+          ];
+        buildable = true;
+        };
+      sublibs = {
+        "tasty-sugar-internal" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
+            (hsPkgs."filemanip" or (errorHandler.buildDepError "filemanip"))
+            (hsPkgs."logict" or (errorHandler.buildDepError "logict"))
+            (hsPkgs."prettyprinter" or (errorHandler.buildDepError "prettyprinter"))
+            ];
+          buildable = true;
+          };
+        };
+      tests = {
+        "test-sugar" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
+            (hsPkgs."hedgehog" or (errorHandler.buildDepError "hedgehog"))
+            (hsPkgs."logict" or (errorHandler.buildDepError "logict"))
+            (hsPkgs."pretty-show" or (errorHandler.buildDepError "pretty-show"))
+            (hsPkgs."prettyprinter" or (errorHandler.buildDepError "prettyprinter"))
+            (hsPkgs."raw-strings-qq" or (errorHandler.buildDepError "raw-strings-qq"))
+            (hsPkgs."tasty" or (errorHandler.buildDepError "tasty"))
+            (hsPkgs."tasty-hedgehog" or (errorHandler.buildDepError "tasty-hedgehog"))
+            (hsPkgs."tasty-hunit" or (errorHandler.buildDepError "tasty-hunit"))
+            (hsPkgs."tasty-sugar" or (errorHandler.buildDepError "tasty-sugar"))
+            ];
+          buildable = true;
+          };
+        "test-passthru-ascii" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."tasty" or (errorHandler.buildDepError "tasty"))
+            (hsPkgs."tasty-hunit" or (errorHandler.buildDepError "tasty-hunit"))
+            (hsPkgs."tasty-sugar" or (errorHandler.buildDepError "tasty-sugar"))
+            ];
+          buildable = true;
+          };
+        "test-params" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."pretty-show" or (errorHandler.buildDepError "pretty-show"))
+            (hsPkgs."tasty" or (errorHandler.buildDepError "tasty"))
+            (hsPkgs."tasty-hunit" or (errorHandler.buildDepError "tasty-hunit"))
+            (hsPkgs."tasty-sugar" or (errorHandler.buildDepError "tasty-sugar"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
