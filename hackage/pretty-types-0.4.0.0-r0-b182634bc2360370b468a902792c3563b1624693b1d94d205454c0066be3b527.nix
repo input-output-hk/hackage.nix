4978ee340490b58c5c1c6069d62b4af890de6d5c
IOHK <devops+nix-tools@iohk.io>
2021-01-22 01:16:22 +0000

    Automatic update for Fri Jan 22 01:16:21 UTC 2021

---
+++ hackage/pretty-types-0.4.0.0-r0-b182634bc2360370b468a902792c3563b1624693b1d94d205454c0066be3b527.nix
@@ -0,0 +1,46 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.22";
+      identifier = { name = "pretty-types"; version = "0.4.0.0"; };
+      license = "BSD-3-Clause";
+      copyright = "2016-2021 Sven Heyll";
+      maintainer = "sven.heyll@gmail.com";
+      author = "Sven Heyll";
+      homepage = "https://github.com/sheyll/pretty-types#readme";
+      url = "";
+      synopsis = "A small pretty printing DSL for complex types.";
+      description = "Please see README.md";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+          (hsPkgs."tagged" or (errorHandler.buildDepError "tagged"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "spec" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."hspec" or (errorHandler.buildDepError "hspec"))
+            (hsPkgs."pretty-types" or (errorHandler.buildDepError "pretty-types"))
+            (hsPkgs."tagged" or (errorHandler.buildDepError "tagged"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
