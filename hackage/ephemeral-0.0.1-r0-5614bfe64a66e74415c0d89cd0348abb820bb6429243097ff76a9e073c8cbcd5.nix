f38f159df7d2b3a716c344edff4c9f630e070453
IOHK <devops+nix-tools@iohk.io>
2021-01-24 01:11:02 +0000

    Automatic update for Sun Jan 24 01:11:01 UTC 2021

---
+++ hackage/ephemeral-0.0.1-r0-5614bfe64a66e74415c0d89cd0348abb820bb6429243097ff76a9e073c8cbcd5.nix
@@ -0,0 +1,65 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "2.4";
+      identifier = { name = "ephemeral"; version = "0.0.1"; };
+      license = "BSD-3-Clause";
+      copyright = "Tony Day (c) AfterTimes";
+      maintainer = "tonyday567@gmail.com";
+      author = "Tony Day";
+      homepage = "https://github.com/tonyday567/ephemeral#readme";
+      url = "";
+      synopsis = "See readme.md";
+      description = "See readme.md for description.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."attoparsec" or (errorHandler.buildDepError "attoparsec"))
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."box" or (errorHandler.buildDepError "box"))
+          (hsPkgs."box-csv" or (errorHandler.buildDepError "box-csv"))
+          (hsPkgs."chart-svg" or (errorHandler.buildDepError "chart-svg"))
+          (hsPkgs."concurrency" or (errorHandler.buildDepError "concurrency"))
+          (hsPkgs."lens" or (errorHandler.buildDepError "lens"))
+          (hsPkgs."lucid" or (errorHandler.buildDepError "lucid"))
+          (hsPkgs."moo" or (errorHandler.buildDepError "moo"))
+          (hsPkgs."mealy" or (errorHandler.buildDepError "mealy"))
+          (hsPkgs."mwc-probability" or (errorHandler.buildDepError "mwc-probability"))
+          (hsPkgs."numhask" or (errorHandler.buildDepError "numhask"))
+          (hsPkgs."numhask-array" or (errorHandler.buildDepError "numhask-array"))
+          (hsPkgs."numhask-space" or (errorHandler.buildDepError "numhask-space"))
+          (hsPkgs."primitive" or (errorHandler.buildDepError "primitive"))
+          (hsPkgs."profunctors" or (errorHandler.buildDepError "profunctors"))
+          (hsPkgs."random" or (errorHandler.buildDepError "random"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."time" or (errorHandler.buildDepError "time"))
+          (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+          (hsPkgs."unordered-containers" or (errorHandler.buildDepError "unordered-containers"))
+          (hsPkgs."microlens" or (errorHandler.buildDepError "microlens"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "test" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."doctest" or (errorHandler.buildDepError "doctest"))
+            (hsPkgs."numhask" or (errorHandler.buildDepError "numhask"))
+            (hsPkgs."ephemeral" or (errorHandler.buildDepError "ephemeral"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
---
+++ hackage/net-mqtt-0.7.1.0-r0-cc7f6600700b3f19eb4b85363bb858e2a1dd4cfbdbe6a170b0c63323715e8114.nix
@@ -0,0 +1,127 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.12";
+      identifier = { name = "net-mqtt"; version = "0.7.1.0"; };
+      license = "BSD-3-Clause";
+      copyright = "BSD3";
+      maintainer = "dustin@spy.net";
+      author = "Dustin Sallings";
+      homepage = "https://github.com/dustin/mqtt-hs#readme";
+      url = "";
+      synopsis = "An MQTT Protocol Implementation.";
+      description = "Please see the README on GitHub at <https://github.com/dustin/mqtt-hs#readme>";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."QuickCheck" or (errorHandler.buildDepError "QuickCheck"))
+          (hsPkgs."async" or (errorHandler.buildDepError "async"))
+          (hsPkgs."attoparsec" or (errorHandler.buildDepError "attoparsec"))
+          (hsPkgs."attoparsec-binary" or (errorHandler.buildDepError "attoparsec-binary"))
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."binary" or (errorHandler.buildDepError "binary"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."conduit" or (errorHandler.buildDepError "conduit"))
+          (hsPkgs."conduit-extra" or (errorHandler.buildDepError "conduit-extra"))
+          (hsPkgs."connection" or (errorHandler.buildDepError "connection"))
+          (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+          (hsPkgs."deepseq" or (errorHandler.buildDepError "deepseq"))
+          (hsPkgs."network-conduit-tls" or (errorHandler.buildDepError "network-conduit-tls"))
+          (hsPkgs."network-uri" or (errorHandler.buildDepError "network-uri"))
+          (hsPkgs."stm" or (errorHandler.buildDepError "stm"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."websockets" or (errorHandler.buildDepError "websockets"))
+          ];
+        buildable = true;
+        };
+      exes = {
+        "mqtt-example" = {
+          depends = [
+            (hsPkgs."QuickCheck" or (errorHandler.buildDepError "QuickCheck"))
+            (hsPkgs."async" or (errorHandler.buildDepError "async"))
+            (hsPkgs."attoparsec" or (errorHandler.buildDepError "attoparsec"))
+            (hsPkgs."attoparsec-binary" or (errorHandler.buildDepError "attoparsec-binary"))
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."binary" or (errorHandler.buildDepError "binary"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."conduit" or (errorHandler.buildDepError "conduit"))
+            (hsPkgs."conduit-extra" or (errorHandler.buildDepError "conduit-extra"))
+            (hsPkgs."connection" or (errorHandler.buildDepError "connection"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."deepseq" or (errorHandler.buildDepError "deepseq"))
+            (hsPkgs."net-mqtt" or (errorHandler.buildDepError "net-mqtt"))
+            (hsPkgs."network-conduit-tls" or (errorHandler.buildDepError "network-conduit-tls"))
+            (hsPkgs."network-uri" or (errorHandler.buildDepError "network-uri"))
+            (hsPkgs."stm" or (errorHandler.buildDepError "stm"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."websockets" or (errorHandler.buildDepError "websockets"))
+            ];
+          buildable = true;
+          };
+        "mqtt-watch" = {
+          depends = [
+            (hsPkgs."QuickCheck" or (errorHandler.buildDepError "QuickCheck"))
+            (hsPkgs."async" or (errorHandler.buildDepError "async"))
+            (hsPkgs."attoparsec" or (errorHandler.buildDepError "attoparsec"))
+            (hsPkgs."attoparsec-binary" or (errorHandler.buildDepError "attoparsec-binary"))
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."binary" or (errorHandler.buildDepError "binary"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."conduit" or (errorHandler.buildDepError "conduit"))
+            (hsPkgs."conduit-extra" or (errorHandler.buildDepError "conduit-extra"))
+            (hsPkgs."connection" or (errorHandler.buildDepError "connection"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."deepseq" or (errorHandler.buildDepError "deepseq"))
+            (hsPkgs."net-mqtt" or (errorHandler.buildDepError "net-mqtt"))
+            (hsPkgs."network-conduit-tls" or (errorHandler.buildDepError "network-conduit-tls"))
+            (hsPkgs."network-uri" or (errorHandler.buildDepError "network-uri"))
+            (hsPkgs."optparse-applicative" or (errorHandler.buildDepError "optparse-applicative"))
+            (hsPkgs."stm" or (errorHandler.buildDepError "stm"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."websockets" or (errorHandler.buildDepError "websockets"))
+            ];
+          buildable = true;
+          };
+        };
+      tests = {
+        "mqtt-test" = {
+          depends = [
+            (hsPkgs."HUnit" or (errorHandler.buildDepError "HUnit"))
+            (hsPkgs."QuickCheck" or (errorHandler.buildDepError "QuickCheck"))
+            (hsPkgs."async" or (errorHandler.buildDepError "async"))
+            (hsPkgs."attoparsec" or (errorHandler.buildDepError "attoparsec"))
+            (hsPkgs."attoparsec-binary" or (errorHandler.buildDepError "attoparsec-binary"))
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."binary" or (errorHandler.buildDepError "binary"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."conduit" or (errorHandler.buildDepError "conduit"))
+            (hsPkgs."conduit-extra" or (errorHandler.buildDepError "conduit-extra"))
+            (hsPkgs."connection" or (errorHandler.buildDepError "connection"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."deepseq" or (errorHandler.buildDepError "deepseq"))
+            (hsPkgs."net-mqtt" or (errorHandler.buildDepError "net-mqtt"))
+            (hsPkgs."network-conduit-tls" or (errorHandler.buildDepError "network-conduit-tls"))
+            (hsPkgs."network-uri" or (errorHandler.buildDepError "network-uri"))
+            (hsPkgs."stm" or (errorHandler.buildDepError "stm"))
+            (hsPkgs."tasty" or (errorHandler.buildDepError "tasty"))
+            (hsPkgs."tasty-hunit" or (errorHandler.buildDepError "tasty-hunit"))
+            (hsPkgs."tasty-quickcheck" or (errorHandler.buildDepError "tasty-quickcheck"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."websockets" or (errorHandler.buildDepError "websockets"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
