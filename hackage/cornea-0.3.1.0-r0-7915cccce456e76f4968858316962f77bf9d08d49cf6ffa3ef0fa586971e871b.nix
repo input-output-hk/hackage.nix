b8a19a2cde905598ef65e9456a85131d3c7a7e20
IOHK <devops+nix-tools@iohk.io>
2021-01-27 01:13:25 +0000

    Automatic update for Wed Jan 27 01:13:24 UTC 2021

---
+++ hackage/cornea-0.3.1.0-r0-7915cccce456e76f4968858316962f77bf9d08d49cf6ffa3ef0fa586971e871b.nix
@@ -0,0 +1,61 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.12";
+      identifier = { name = "cornea"; version = "0.3.1.0"; };
+      license = "LicenseRef-OtherLicense";
+      copyright = "2019 Torsten Schmits";
+      maintainer = "tek@tryp.io";
+      author = "Torsten Schmits";
+      homepage = "https://github.com/tek/cornea#readme";
+      url = "";
+      synopsis = "classy optical monadic state";
+      description = "Please see the README on GitHub at <https://github.com/tek/cornea>";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base-noprelude" or (errorHandler.buildDepError "base-noprelude"))
+          (hsPkgs."either" or (errorHandler.buildDepError "either"))
+          (hsPkgs."lens" or (errorHandler.buildDepError "lens"))
+          (hsPkgs."lifted-base" or (errorHandler.buildDepError "lifted-base"))
+          (hsPkgs."monad-control" or (errorHandler.buildDepError "monad-control"))
+          (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+          (hsPkgs."relude" or (errorHandler.buildDepError "relude"))
+          (hsPkgs."template-haskell" or (errorHandler.buildDepError "template-haskell"))
+          (hsPkgs."th-abstraction" or (errorHandler.buildDepError "th-abstraction"))
+          (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "cornea-unit" = {
+          depends = [
+            (hsPkgs."HTF" or (errorHandler.buildDepError "HTF"))
+            (hsPkgs."base-noprelude" or (errorHandler.buildDepError "base-noprelude"))
+            (hsPkgs."cornea" or (errorHandler.buildDepError "cornea"))
+            (hsPkgs."either" or (errorHandler.buildDepError "either"))
+            (hsPkgs."lens" or (errorHandler.buildDepError "lens"))
+            (hsPkgs."lifted-base" or (errorHandler.buildDepError "lifted-base"))
+            (hsPkgs."monad-control" or (errorHandler.buildDepError "monad-control"))
+            (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+            (hsPkgs."relude" or (errorHandler.buildDepError "relude"))
+            (hsPkgs."template-haskell" or (errorHandler.buildDepError "template-haskell"))
+            (hsPkgs."th-abstraction" or (errorHandler.buildDepError "th-abstraction"))
+            (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
---
+++ hackage/drifter-0.3.0-r0-e5da0350beebddfe5dcded03e274a006c70bcf974be2e8d27de0d89436ec84e2.nix
@@ -0,0 +1,49 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = { lib-werror = false; };
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "drifter"; version = "0.3.0"; };
+      license = "MIT";
+      copyright = "";
+      maintainer = "Michael Xavier <michael@michaelxavier.net>";
+      author = "AndrewRademacher";
+      homepage = "https://github.com/AndrewRademacher/drifter";
+      url = "";
+      synopsis = "Simple schema management for arbitrary databases.";
+      description = "Simple support for migrating database schemas, which allows\nhaskell functions to be run as a part of the migration. Note\nthat this library isn't meant to be used directly, but rather\nas a base library for a specific driver. For an example of a\nfunctioning driver, you should check out <https://hackage.haskell.org/package/drifter-postgresql drifter-postgresql>.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."fgl" or (errorHandler.buildDepError "fgl"))
+          (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "test" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."drifter" or (errorHandler.buildDepError "drifter"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."tasty" or (errorHandler.buildDepError "tasty"))
+            (hsPkgs."tasty-quickcheck" or (errorHandler.buildDepError "tasty-quickcheck"))
+            (hsPkgs."tasty-hunit" or (errorHandler.buildDepError "tasty-hunit"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
