b0f6ebce466ac2103793f1ddaa4cf70c5616e4dd
IOHK <devops+nix-tools@iohk.io>
2021-02-04 01:11:55 +0000

    Automatic update for Thu Feb  4 01:11:54 UTC 2021

---
+++ hackage/fswatcher-0.3.0-r0-8285649abdba28a09a802bd3d48c76d44a50d6526271f98f595418407cf88e4c.nix
@@ -0,0 +1,42 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "fswatcher"; version = "0.3.0"; };
+      license = "BSD-3-Clause";
+      copyright = "";
+      maintainer = "erlend@hamberg.no";
+      author = "Erlend Hamberg";
+      homepage = "http://www.github.com/ehamberg/fswatcher/";
+      url = "";
+      synopsis = "Watch a file/directory and run a command when it's modified";
+      description = "A simple program that watches a file or a directory and\nruns a given command whenever the file or a file within the\ndirectory is changed.";
+      buildType = "Simple";
+      };
+    components = {
+      exes = {
+        "fswatcher" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."unix" or (errorHandler.buildDepError "unix"))
+            (hsPkgs."process" or (errorHandler.buildDepError "process"))
+            (hsPkgs."fsnotify" or (errorHandler.buildDepError "fsnotify"))
+            (hsPkgs."system-filepath" or (errorHandler.buildDepError "system-filepath"))
+            (hsPkgs."directory" or (errorHandler.buildDepError "directory"))
+            (hsPkgs."optparse-applicative" or (errorHandler.buildDepError "optparse-applicative"))
+            (hsPkgs."regex-pcre-builtin" or (errorHandler.buildDepError "regex-pcre-builtin"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
