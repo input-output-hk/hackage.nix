316f0f86bcd64983f783b635adbcdda8520fbb0a
IOHK <devops+nix-tools@iohk.io>
2021-02-17 01:13:18 +0000

    Automatic update for Wed Feb 17 01:13:17 UTC 2021

---
+++ hackage/cobot-io-0.1.3.16-r0-8582f8aeee1021ccd55159c2ae9e9d50fc5fca20c50b1c4e727053fb7bfc82b1.nix
@@ -0,0 +1,76 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.12";
+      identifier = { name = "cobot-io"; version = "0.1.3.16"; };
+      license = "BSD-3-Clause";
+      copyright = "2020, Biocad";
+      maintainer = "pavel@yakovlev.me";
+      author = "Pavel Yakovlev, Bogdan Neterebskii, Alexander Sadovnikov";
+      homepage = "https://github.com/biocad/cobot-io#readme";
+      url = "";
+      synopsis = "Biological data file formats and IO";
+      description = "Please see the README on GitHub at <https://github.com/biocad/cobot-io#readme>";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."array" or (errorHandler.buildDepError "array"))
+          (hsPkgs."attoparsec" or (errorHandler.buildDepError "attoparsec"))
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."binary" or (errorHandler.buildDepError "binary"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+          (hsPkgs."data-msgpack" or (errorHandler.buildDepError "data-msgpack"))
+          (hsPkgs."deepseq" or (errorHandler.buildDepError "deepseq"))
+          (hsPkgs."http-conduit" or (errorHandler.buildDepError "http-conduit"))
+          (hsPkgs."hyraxAbif" or (errorHandler.buildDepError "hyraxAbif"))
+          (hsPkgs."lens" or (errorHandler.buildDepError "lens"))
+          (hsPkgs."linear" or (errorHandler.buildDepError "linear"))
+          (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+          (hsPkgs."split" or (errorHandler.buildDepError "split"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."vector" or (errorHandler.buildDepError "vector"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "cobot-io-test" = {
+          depends = [
+            (hsPkgs."QuickCheck" or (errorHandler.buildDepError "QuickCheck"))
+            (hsPkgs."array" or (errorHandler.buildDepError "array"))
+            (hsPkgs."attoparsec" or (errorHandler.buildDepError "attoparsec"))
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."binary" or (errorHandler.buildDepError "binary"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."cobot-io" or (errorHandler.buildDepError "cobot-io"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."data-msgpack" or (errorHandler.buildDepError "data-msgpack"))
+            (hsPkgs."deepseq" or (errorHandler.buildDepError "deepseq"))
+            (hsPkgs."directory" or (errorHandler.buildDepError "directory"))
+            (hsPkgs."hspec" or (errorHandler.buildDepError "hspec"))
+            (hsPkgs."http-conduit" or (errorHandler.buildDepError "http-conduit"))
+            (hsPkgs."hyraxAbif" or (errorHandler.buildDepError "hyraxAbif"))
+            (hsPkgs."lens" or (errorHandler.buildDepError "lens"))
+            (hsPkgs."linear" or (errorHandler.buildDepError "linear"))
+            (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+            (hsPkgs."neat-interpolation" or (errorHandler.buildDepError "neat-interpolation"))
+            (hsPkgs."split" or (errorHandler.buildDepError "split"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."vector" or (errorHandler.buildDepError "vector"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
