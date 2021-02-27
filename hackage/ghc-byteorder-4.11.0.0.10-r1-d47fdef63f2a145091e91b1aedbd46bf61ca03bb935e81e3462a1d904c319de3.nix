59bda9dfcbc6419ec17c7f303daea146017ceea3
IOHK <devops+nix-tools@iohk.io>
2021-02-27 01:11:20 +0000

    Automatic update for Sat Feb 27 01:11:19 UTC 2021

---
+++ hackage/ghc-byteorder-4.11.0.0.10-r1-d47fdef63f2a145091e91b1aedbd46bf61ca03bb935e81e3462a1d904c319de3.nix
@@ -0,0 +1,40 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "2.2";
+      identifier = { name = "ghc-byteorder"; version = "4.11.0.0.10"; };
+      license = "BSD-3-Clause";
+      copyright = "";
+      maintainer = "Herbert Valerio Riedel <hvr@gnu.org>";
+      author = "";
+      homepage = "";
+      url = "";
+      synopsis = "\"GHC.ByteOrder\" API Compatibility Layer";
+      description = "This package transparently supplies the \"GHC.ByteOrder\" API as provided in @base-4.11.0.0@ for legacy GHC releases which didn't provide it yet (i.e. GHC 7.0 through GHC 8.2).\n\nThis release reexports [GHC.ByteOrder from base](https://hackage.haskell.org/package/base-4.11.1.0/docs/GHC-ByteOrder.html) via Cabal's @reexported-modules@ mechanism.  For GHC 8.2 and earlier a different release of @ghc-byteorder@ (with the same minor version) is eligible by the Cabal solver with a backported \"GHC.ByteOrder\" module.\n\nIn order to use this compatibility layer, simply declare a dependency on @ghc-byteorder@ in your @.cabal@ package description like so\n\n> build-depends: ghc-byteorder ^>= 4.11.0.0\n\nAnd your code will be able to access this respective API version of the module via the usual\n\n> import GHC.ByteOrder\n\nmechanism.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [ (hsPkgs."base" or (errorHandler.buildDepError "base")) ];
+        buildable = true;
+        };
+      tests = {
+        "tests" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."ghc-byteorder" or (errorHandler.buildDepError "ghc-byteorder"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
---
+++ hackage/linux-capabilities-0.1.0.0-r0-2bfaaa5577e99d217c10efb2dbf63f35a7321826c8c18e32e07cfd4df8179c69.nix
@@ -0,0 +1,31 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "2.4";
+      identifier = { name = "linux-capabilities"; version = "0.1.0.0"; };
+      license = "Apache-2.0";
+      copyright = "2021 Red Hat";
+      maintainer = "Tristan de Cacqueray <tdecacqu@redhat.com>";
+      author = "Tristan de Cacqueray";
+      homepage = "https://github.com/softwarefactory-project/linux-capabilities-haskell#readme";
+      url = "";
+      synopsis = "Linux capabilities Haskell data type";
+      description = "linux-capabilities provides a data type and documentation for the Linux capabilities(7).\n";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [ (hsPkgs."base" or (errorHandler.buildDepError "base")) ];
+        buildable = true;
+        };
+      };
+    }
