9f61382fad98e24fe8678db2ffb0c51a863a6023
IOHK <devops+nix-tools@iohk.io>
2021-03-05 01:12:09 +0000

    Automatic update for Fri Mar  5 01:12:08 UTC 2021

---
+++ hackage/harg-0.5.0.0-r0-7e61638b8efeb6d500b72b29c6b191d14e3fc6eae843d6bc566469400b65f3d4.nix
@@ -0,0 +1,63 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = { builddocstest = false; };
+    package = {
+      specVersion = "2.4";
+      identifier = { name = "harg"; version = "0.5.0.0"; };
+      license = "BSD-3-Clause";
+      copyright = "Copyright (c) 2020 Alex Peitsinis";
+      maintainer = "alexpeitsinis@gmail.com";
+      author = "Alex Peitsinis";
+      homepage = "https://github.com/alexpeits/harg";
+      url = "";
+      synopsis = "Haskell program configuration using higher kinded data";
+      description = "Please see the README on GitHub at <https://github.com/alexpeits/harg#readme>";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."aeson" or (errorHandler.buildDepError "aeson"))
+          (hsPkgs."barbies" or (errorHandler.buildDepError "barbies"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."directory" or (errorHandler.buildDepError "directory"))
+          (hsPkgs."higgledy" or (errorHandler.buildDepError "higgledy"))
+          (hsPkgs."optparse-applicative" or (errorHandler.buildDepError "optparse-applicative"))
+          (hsPkgs."split" or (errorHandler.buildDepError "split"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."yaml" or (errorHandler.buildDepError "yaml"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "harg-test" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."harg" or (errorHandler.buildDepError "harg"))
+            ];
+          buildable = true;
+          };
+        "docs-test" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."aeson" or (errorHandler.buildDepError "aeson"))
+            (hsPkgs."barbies" or (errorHandler.buildDepError "barbies"))
+            (hsPkgs."higgledy" or (errorHandler.buildDepError "higgledy"))
+            (hsPkgs."markdown-unlit" or (errorHandler.buildDepError "markdown-unlit"))
+            (hsPkgs."optparse-applicative" or (errorHandler.buildDepError "optparse-applicative"))
+            (hsPkgs."harg" or (errorHandler.buildDepError "harg"))
+            ];
+          buildable = if flags.builddocstest then true else false;
+          };
+        };
+      };
+    }
