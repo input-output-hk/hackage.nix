84306f01ebd1c7610072ccd62d81143a9a075520
IOHK <devops+nix-tools@iohk.io>
2021-03-13 01:11:08 +0000

    Automatic update for Sat Mar 13 01:11:07 UTC 2021

---
+++ hackage/influxdb-1.9.1.1-r0-1930c0cd04b9cbde4567de0c7163e12212c56470a64848d775f20e8a7701784c.nix
@@ -0,0 +1,111 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = { examples = false; };
+    package = {
+      specVersion = "1.24";
+      identifier = { name = "influxdb"; version = "1.9.1.1"; };
+      license = "BSD-3-Clause";
+      copyright = "Copyright (C) 2014-2021 Mitsutoshi Aoe";
+      maintainer = "Mitsutoshi Aoe <me@maoe.name>";
+      author = "Mitsutoshi Aoe";
+      homepage = "https://github.com/maoe/influxdb-haskell";
+      url = "";
+      synopsis = "InfluxDB client library for Haskell";
+      description = "@influxdb@ is an InfluxDB client library for Haskell.\n\nSee \"Database.InfluxDB\" for a quick start guide.";
+      buildType = "Custom";
+      setup-depends = [
+        (hsPkgs.buildPackages.base or (pkgs.buildPackages.base or (errorHandler.setupDepError "base")))
+        (hsPkgs.buildPackages.Cabal or (pkgs.buildPackages.Cabal or (errorHandler.setupDepError "Cabal")))
+        (hsPkgs.buildPackages.cabal-doctest or (pkgs.buildPackages.cabal-doctest or (errorHandler.setupDepError "cabal-doctest")))
+        ];
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."aeson" or (errorHandler.buildDepError "aeson"))
+          (hsPkgs."attoparsec" or (errorHandler.buildDepError "attoparsec"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."clock" or (errorHandler.buildDepError "clock"))
+          (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+          (hsPkgs."foldl" or (errorHandler.buildDepError "foldl"))
+          (hsPkgs."http-client" or (errorHandler.buildDepError "http-client"))
+          (hsPkgs."http-types" or (errorHandler.buildDepError "http-types"))
+          (hsPkgs."lens" or (errorHandler.buildDepError "lens"))
+          (hsPkgs."network" or (errorHandler.buildDepError "network"))
+          (hsPkgs."optional-args" or (errorHandler.buildDepError "optional-args"))
+          (hsPkgs."scientific" or (errorHandler.buildDepError "scientific"))
+          (hsPkgs."tagged" or (errorHandler.buildDepError "tagged"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."time" or (errorHandler.buildDepError "time"))
+          (hsPkgs."unordered-containers" or (errorHandler.buildDepError "unordered-containers"))
+          (hsPkgs."vector" or (errorHandler.buildDepError "vector"))
+          ];
+        buildable = true;
+        };
+      exes = {
+        "influx-random-points" = {
+          depends = [
+            (hsPkgs."aeson" or (errorHandler.buildDepError "aeson"))
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."foldl" or (errorHandler.buildDepError "foldl"))
+            (hsPkgs."http-client" or (errorHandler.buildDepError "http-client"))
+            (hsPkgs."influxdb" or (errorHandler.buildDepError "influxdb"))
+            (hsPkgs."lens" or (errorHandler.buildDepError "lens"))
+            (hsPkgs."mwc-random" or (errorHandler.buildDepError "mwc-random"))
+            (hsPkgs."optional-args" or (errorHandler.buildDepError "optional-args"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."time" or (errorHandler.buildDepError "time"))
+            (hsPkgs."vector" or (errorHandler.buildDepError "vector"))
+            ];
+          buildable = if !flags.examples then false else true;
+          };
+        "influx-write-udp" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."influxdb" or (errorHandler.buildDepError "influxdb"))
+            (hsPkgs."lens" or (errorHandler.buildDepError "lens"))
+            (hsPkgs."network" or (errorHandler.buildDepError "network"))
+            (hsPkgs."time" or (errorHandler.buildDepError "time"))
+            ];
+          buildable = if !flags.examples then false else true;
+          };
+        };
+      tests = {
+        "doctests" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."doctest" or (errorHandler.buildDepError "doctest"))
+            (hsPkgs."influxdb" or (errorHandler.buildDepError "influxdb"))
+            (hsPkgs."template-haskell" or (errorHandler.buildDepError "template-haskell"))
+            ];
+          buildable = true;
+          };
+        "regressions" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."influxdb" or (errorHandler.buildDepError "influxdb"))
+            (hsPkgs."lens" or (errorHandler.buildDepError "lens"))
+            (hsPkgs."tasty" or (errorHandler.buildDepError "tasty"))
+            (hsPkgs."tasty-hunit" or (errorHandler.buildDepError "tasty-hunit"))
+            (hsPkgs."time" or (errorHandler.buildDepError "time"))
+            (hsPkgs."raw-strings-qq" or (errorHandler.buildDepError "raw-strings-qq"))
+            (hsPkgs."vector" or (errorHandler.buildDepError "vector"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
---
+++ hackage/pandoc-crossref-0.3.10.0-r0-30c3e0de53f30971b29f84e19929824fce5e82073fe8df0d19bc6ffa688ab436.nix
@@ -0,0 +1,124 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = { enable_flaky_tests = false; };
+    package = {
+      specVersion = "1.12";
+      identifier = { name = "pandoc-crossref"; version = "0.3.10.0"; };
+      license = "GPL-2.0-only";
+      copyright = "";
+      maintainer = "root@livid.pp.ru";
+      author = "Nikolay Yakimov";
+      homepage = "https://github.com/lierdakil/pandoc-crossref#readme";
+      url = "";
+      synopsis = "Pandoc filter for cross-references";
+      description = "pandoc-crossref is a pandoc filter for numbering figures, equations, tables and cross-references to them.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+          (hsPkgs."data-accessor" or (errorHandler.buildDepError "data-accessor"))
+          (hsPkgs."data-accessor-template" or (errorHandler.buildDepError "data-accessor-template"))
+          (hsPkgs."data-accessor-transformers" or (errorHandler.buildDepError "data-accessor-transformers"))
+          (hsPkgs."data-default" or (errorHandler.buildDepError "data-default"))
+          (hsPkgs."directory" or (errorHandler.buildDepError "directory"))
+          (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
+          (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+          (hsPkgs."pandoc" or (errorHandler.buildDepError "pandoc"))
+          (hsPkgs."pandoc-types" or (errorHandler.buildDepError "pandoc-types"))
+          (hsPkgs."roman-numerals" or (errorHandler.buildDepError "roman-numerals"))
+          (hsPkgs."syb" or (errorHandler.buildDepError "syb"))
+          (hsPkgs."template-haskell" or (errorHandler.buildDepError "template-haskell"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."utility-ht" or (errorHandler.buildDepError "utility-ht"))
+          ];
+        buildable = true;
+        };
+      exes = {
+        "pandoc-crossref" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."data-accessor" or (errorHandler.buildDepError "data-accessor"))
+            (hsPkgs."data-accessor-template" or (errorHandler.buildDepError "data-accessor-template"))
+            (hsPkgs."data-accessor-transformers" or (errorHandler.buildDepError "data-accessor-transformers"))
+            (hsPkgs."data-default" or (errorHandler.buildDepError "data-default"))
+            (hsPkgs."deepseq" or (errorHandler.buildDepError "deepseq"))
+            (hsPkgs."directory" or (errorHandler.buildDepError "directory"))
+            (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
+            (hsPkgs."gitrev" or (errorHandler.buildDepError "gitrev"))
+            (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+            (hsPkgs."open-browser" or (errorHandler.buildDepError "open-browser"))
+            (hsPkgs."optparse-applicative" or (errorHandler.buildDepError "optparse-applicative"))
+            (hsPkgs."pandoc" or (errorHandler.buildDepError "pandoc"))
+            (hsPkgs."pandoc-crossref" or (errorHandler.buildDepError "pandoc-crossref"))
+            (hsPkgs."pandoc-types" or (errorHandler.buildDepError "pandoc-types"))
+            (hsPkgs."roman-numerals" or (errorHandler.buildDepError "roman-numerals"))
+            (hsPkgs."syb" or (errorHandler.buildDepError "syb"))
+            (hsPkgs."template-haskell" or (errorHandler.buildDepError "template-haskell"))
+            (hsPkgs."temporary" or (errorHandler.buildDepError "temporary"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."utility-ht" or (errorHandler.buildDepError "utility-ht"))
+            ];
+          buildable = true;
+          };
+        };
+      tests = {
+        "test-integrative" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."data-accessor" or (errorHandler.buildDepError "data-accessor"))
+            (hsPkgs."data-accessor-template" or (errorHandler.buildDepError "data-accessor-template"))
+            (hsPkgs."data-accessor-transformers" or (errorHandler.buildDepError "data-accessor-transformers"))
+            (hsPkgs."data-default" or (errorHandler.buildDepError "data-default"))
+            (hsPkgs."directory" or (errorHandler.buildDepError "directory"))
+            (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
+            (hsPkgs."hspec" or (errorHandler.buildDepError "hspec"))
+            (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+            (hsPkgs."pandoc" or (errorHandler.buildDepError "pandoc"))
+            (hsPkgs."pandoc-crossref" or (errorHandler.buildDepError "pandoc-crossref"))
+            (hsPkgs."pandoc-types" or (errorHandler.buildDepError "pandoc-types"))
+            (hsPkgs."roman-numerals" or (errorHandler.buildDepError "roman-numerals"))
+            (hsPkgs."syb" or (errorHandler.buildDepError "syb"))
+            (hsPkgs."template-haskell" or (errorHandler.buildDepError "template-haskell"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."utility-ht" or (errorHandler.buildDepError "utility-ht"))
+            ];
+          buildable = true;
+          };
+        "test-pandoc-crossref" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."data-accessor" or (errorHandler.buildDepError "data-accessor"))
+            (hsPkgs."data-accessor-template" or (errorHandler.buildDepError "data-accessor-template"))
+            (hsPkgs."data-accessor-transformers" or (errorHandler.buildDepError "data-accessor-transformers"))
+            (hsPkgs."data-default" or (errorHandler.buildDepError "data-default"))
+            (hsPkgs."directory" or (errorHandler.buildDepError "directory"))
+            (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
+            (hsPkgs."hspec" or (errorHandler.buildDepError "hspec"))
+            (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+            (hsPkgs."pandoc" or (errorHandler.buildDepError "pandoc"))
+            (hsPkgs."pandoc-types" or (errorHandler.buildDepError "pandoc-types"))
+            (hsPkgs."roman-numerals" or (errorHandler.buildDepError "roman-numerals"))
+            (hsPkgs."syb" or (errorHandler.buildDepError "syb"))
+            (hsPkgs."template-haskell" or (errorHandler.buildDepError "template-haskell"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."utility-ht" or (errorHandler.buildDepError "utility-ht"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
