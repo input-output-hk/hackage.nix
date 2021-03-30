20acf5c02423f5fd4fb358540ee52e37b9918e5e
IOHK <devops+nix-tools@iohk.io>
2021-03-30 01:13:25 +0000

    Automatic update for Tue Mar 30 01:13:24 UTC 2021

---
+++ hackage/mmsyn2-0.3.2.0-r0-b4e11b49f99ece74edda5f4417b10b6245c869d130b208a3b5cd441c6a76169b.nix
@@ -0,0 +1,34 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "mmsyn2"; version = "0.3.2.0"; };
+      license = "MIT";
+      copyright = "Oleksandr Zhabenko";
+      maintainer = "olexandr543@yahoo.com";
+      author = "OleksandrZhabenko";
+      homepage = "https://oleksandrzhabenko.github.io/mmsyn2";
+      url = "";
+      synopsis = "The library that can be used for multiple (Ord a) => a -> b transformations";
+      description = "The library that can be used for optimization or another representation of multiple (Ord a) => a -> b transformations";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."vector" or (errorHandler.buildDepError "vector"))
+          ];
+        buildable = true;
+        };
+      };
+    }
---
+++ hackage/polysemy-conc-0.1.0.0-r0-6d7c109f47b1f4fb0d345f86f9985f8175ad5f2c88fb4c4ffdde3ad6f3b3b470.nix
@@ -0,0 +1,70 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "2.2";
+      identifier = { name = "polysemy-conc"; version = "0.1.0.0"; };
+      license = "BSD-2-Clause-Patent";
+      copyright = "2021 Torsten Schmits";
+      maintainer = "tek@tryp.io";
+      author = "Torsten Schmits";
+      homepage = "https://github.com/tek/polysemy-conc#readme";
+      url = "";
+      synopsis = "Polysemy Effects for Concurrency";
+      description = "See <https://hackage.haskell.org/package/polysemy-conc/docs/Polysemy-Conc.html>";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."async" or (errorHandler.buildDepError "async"))
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+          (hsPkgs."polysemy" or (errorHandler.buildDepError "polysemy"))
+          (hsPkgs."polysemy-time" or (errorHandler.buildDepError "polysemy-time"))
+          (hsPkgs."relude" or (errorHandler.buildDepError "relude"))
+          (hsPkgs."stm" or (errorHandler.buildDepError "stm"))
+          (hsPkgs."stm-chans" or (errorHandler.buildDepError "stm-chans"))
+          (hsPkgs."string-interpolate" or (errorHandler.buildDepError "string-interpolate"))
+          (hsPkgs."template-haskell" or (errorHandler.buildDepError "template-haskell"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."time" or (errorHandler.buildDepError "time"))
+          (hsPkgs."unix" or (errorHandler.buildDepError "unix"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "polysemy-conc-unit" = {
+          depends = [
+            (hsPkgs."async" or (errorHandler.buildDepError "async"))
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."hedgehog" or (errorHandler.buildDepError "hedgehog"))
+            (hsPkgs."polysemy" or (errorHandler.buildDepError "polysemy"))
+            (hsPkgs."polysemy-conc" or (errorHandler.buildDepError "polysemy-conc"))
+            (hsPkgs."polysemy-test" or (errorHandler.buildDepError "polysemy-test"))
+            (hsPkgs."polysemy-time" or (errorHandler.buildDepError "polysemy-time"))
+            (hsPkgs."relude" or (errorHandler.buildDepError "relude"))
+            (hsPkgs."stm" or (errorHandler.buildDepError "stm"))
+            (hsPkgs."stm-chans" or (errorHandler.buildDepError "stm-chans"))
+            (hsPkgs."string-interpolate" or (errorHandler.buildDepError "string-interpolate"))
+            (hsPkgs."tasty" or (errorHandler.buildDepError "tasty"))
+            (hsPkgs."tasty-hedgehog" or (errorHandler.buildDepError "tasty-hedgehog"))
+            (hsPkgs."template-haskell" or (errorHandler.buildDepError "template-haskell"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."time" or (errorHandler.buildDepError "time"))
+            (hsPkgs."unix" or (errorHandler.buildDepError "unix"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
