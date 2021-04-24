bb0307bf43a966724fea5a7074fad3f59a84aa5a
IOHK <devops+nix-tools@iohk.io>
2021-04-24 01:11:40 +0000

    Automatic update for Sat Apr 24 01:11:39 UTC 2021

---
+++ hackage/mysql-0.2.0.1-r0-e9b7151b9eed5d22e63dbf68c685709a072fc1b90b0c9c9b80cca121297bbc7f.nix
@@ -0,0 +1,50 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = { developer = false; };
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "mysql"; version = "0.2.0.1"; };
+      license = "BSD-3-Clause";
+      copyright = "Copyright 2011 MailRank, Inc.\nCopyright 2013 Bryan O'Sullivan <bos@serpentine.com>";
+      maintainer = "Paul Rouse <pyr@doynton.org>";
+      author = "Bryan O'Sullivan <bos@serpentine.com>";
+      homepage = "https://github.com/paul-rouse/mysql";
+      url = "";
+      synopsis = "A low-level MySQL client library.";
+      description = "A low-level client library for the MySQL database, implemented as\nbindings to the C @mysqlclient@ API.\n\n/Important licensing note/: This library is BSD-licensed under the\nterms of the MySQL FOSS License Exception\n<http://www.mysql.com/about/legal/licensing/foss-exception/>.\n\nSince this library links against the GPL-licensed @mysqlclient@\nlibrary, a non-open-source application that uses it /may/ be\nsubject to the terms of the GPL.";
+      buildType = "Custom";
+      setup-depends = [
+        (hsPkgs.buildPackages.base or (pkgs.buildPackages.base or (errorHandler.setupDepError "base")))
+        (hsPkgs.buildPackages.Cabal or (pkgs.buildPackages.Cabal or (errorHandler.setupDepError "Cabal")))
+        ];
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+          ] ++ (pkgs.lib).optional (!(compiler.isGhc && (compiler.version).ge "8.0")) (hsPkgs."semigroups" or (errorHandler.buildDepError "semigroups"));
+        buildable = true;
+        };
+      tests = {
+        "test" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."hspec" or (errorHandler.buildDepError "hspec"))
+            (hsPkgs."mysql" or (errorHandler.buildDepError "mysql"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
---
+++ hackage/speculate-0.4.6-r0-30c53ae9d54501be925d3faef234722e1a19ba72fdf0f692d6b2289f6472b355.nix
@@ -0,0 +1,113 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.18";
+      identifier = { name = "speculate"; version = "0.4.6"; };
+      license = "BSD-3-Clause";
+      copyright = "";
+      maintainer = "Rudy Matela <rudy@matela.com.br>";
+      author = "Rudy Matela,  Colin Runciman";
+      homepage = "https://github.com/rudymatela/speculate#readme";
+      url = "";
+      synopsis = "discovery of properties about Haskell functions";
+      description = "Speculate automatically discovers laws about Haskell functions.\nGive Speculate a bunch of Haskell functions and it will discover laws like:\n\n* equations, such as @ id x == x @;\n\n* inequalities, such as @ 0 <= x * x @;\n\n* conditional equations, such as @ x \\<= 0  ==\\>  x + abs x == 0 @.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."leancheck" or (errorHandler.buildDepError "leancheck"))
+          (hsPkgs."express" or (errorHandler.buildDepError "express"))
+          (hsPkgs."cmdargs" or (errorHandler.buildDepError "cmdargs"))
+          (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "creason" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."leancheck" or (errorHandler.buildDepError "leancheck"))
+            (hsPkgs."express" or (errorHandler.buildDepError "express"))
+            (hsPkgs."speculate" or (errorHandler.buildDepError "speculate"))
+            ];
+          buildable = true;
+          };
+        "engine" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."leancheck" or (errorHandler.buildDepError "leancheck"))
+            (hsPkgs."express" or (errorHandler.buildDepError "express"))
+            (hsPkgs."speculate" or (errorHandler.buildDepError "speculate"))
+            ];
+          buildable = true;
+          };
+        "eval" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."leancheck" or (errorHandler.buildDepError "leancheck"))
+            (hsPkgs."express" or (errorHandler.buildDepError "express"))
+            (hsPkgs."speculate" or (errorHandler.buildDepError "speculate"))
+            ];
+          buildable = true;
+          };
+        "expr" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."leancheck" or (errorHandler.buildDepError "leancheck"))
+            (hsPkgs."express" or (errorHandler.buildDepError "express"))
+            (hsPkgs."speculate" or (errorHandler.buildDepError "speculate"))
+            ];
+          buildable = true;
+          };
+        "order" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."leancheck" or (errorHandler.buildDepError "leancheck"))
+            (hsPkgs."express" or (errorHandler.buildDepError "express"))
+            (hsPkgs."speculate" or (errorHandler.buildDepError "speculate"))
+            ];
+          buildable = true;
+          };
+        "reason" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."leancheck" or (errorHandler.buildDepError "leancheck"))
+            (hsPkgs."express" or (errorHandler.buildDepError "express"))
+            (hsPkgs."speculate" or (errorHandler.buildDepError "speculate"))
+            ];
+          buildable = true;
+          };
+        "utils" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."leancheck" or (errorHandler.buildDepError "leancheck"))
+            (hsPkgs."express" or (errorHandler.buildDepError "express"))
+            (hsPkgs."speculate" or (errorHandler.buildDepError "speculate"))
+            ];
+          buildable = true;
+          };
+        };
+      benchmarks = {
+        "test-stats" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."leancheck" or (errorHandler.buildDepError "leancheck"))
+            (hsPkgs."express" or (errorHandler.buildDepError "express"))
+            (hsPkgs."speculate" or (errorHandler.buildDepError "speculate"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
