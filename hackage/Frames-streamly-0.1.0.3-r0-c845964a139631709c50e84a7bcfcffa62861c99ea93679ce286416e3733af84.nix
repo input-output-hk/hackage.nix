938921a8d973d0c37e3551a7bada05cbaeca527c
IOHK <devops+nix-tools@iohk.io>
2021-04-28 01:14:38 +0000

    Automatic update for Wed Apr 28 01:14:37 UTC 2021

---
+++ hackage/Frames-streamly-0.1.0.3-r0-c845964a139631709c50e84a7bcfcffa62861c99ea93679ce286416e3733af84.nix
@@ -0,0 +1,78 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "2.2";
+      identifier = { name = "Frames-streamly"; version = "0.1.0.3"; };
+      license = "BSD-3-Clause";
+      copyright = "2020 Adam Conner-Sax";
+      maintainer = "adam_conner_sax@yahoo.com";
+      author = "Adam Conner-Sax";
+      homepage = "https://github.com/adamConnerSax/Frames-streamly#readme";
+      url = "";
+      synopsis = "A streamly layer for Frames I/O";
+      description = "More information is available in the <https://github.com/adamConnerSax/Frames-streamly/blob/master/Readme.md readme>.'";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."Frames" or (errorHandler.buildDepError "Frames"))
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."exceptions" or (errorHandler.buildDepError "exceptions"))
+          (hsPkgs."primitive" or (errorHandler.buildDepError "primitive"))
+          (hsPkgs."relude" or (errorHandler.buildDepError "relude"))
+          (hsPkgs."streamly" or (errorHandler.buildDepError "streamly"))
+          (hsPkgs."strict" or (errorHandler.buildDepError "strict"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."vinyl" or (errorHandler.buildDepError "vinyl"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "Demo" = {
+          depends = [
+            (hsPkgs."Frames" or (errorHandler.buildDepError "Frames"))
+            (hsPkgs."Frames-streamly" or (errorHandler.buildDepError "Frames-streamly"))
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."streamly" or (errorHandler.buildDepError "streamly"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."vinyl" or (errorHandler.buildDepError "vinyl"))
+            ];
+          buildable = true;
+          };
+        "Strictness" = {
+          depends = [
+            (hsPkgs."Frames" or (errorHandler.buildDepError "Frames"))
+            (hsPkgs."Frames-streamly" or (errorHandler.buildDepError "Frames-streamly"))
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."cereal" or (errorHandler.buildDepError "cereal"))
+            (hsPkgs."binary" or (errorHandler.buildDepError "binary"))
+            (hsPkgs."fast-builder" or (errorHandler.buildDepError "fast-builder"))
+            (hsPkgs."bytestring-strict-builder" or (errorHandler.buildDepError "bytestring-strict-builder"))
+            (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+            (hsPkgs."foldl" or (errorHandler.buildDepError "foldl"))
+            (hsPkgs."primitive" or (errorHandler.buildDepError "primitive"))
+            (hsPkgs."relude" or (errorHandler.buildDepError "relude"))
+            (hsPkgs."streamly" or (errorHandler.buildDepError "streamly"))
+            (hsPkgs."streamly-bytestring" or (errorHandler.buildDepError "streamly-bytestring"))
+            (hsPkgs."strict" or (errorHandler.buildDepError "strict"))
+            (hsPkgs."clock" or (errorHandler.buildDepError "clock"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."vector" or (errorHandler.buildDepError "vector"))
+            (hsPkgs."vinyl" or (errorHandler.buildDepError "vinyl"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
---
+++ hackage/sarsi-0.0.5.2-r0-d26cacba766cc941971582a5ae683e27fa88cee66f498366440c0e04f3f9835f.nix
@@ -0,0 +1,124 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "sarsi"; version = "0.0.5.2"; };
+      license = "Apache-2.0";
+      copyright = "";
+      maintainer = "alois.cochard@gmail.com";
+      author = "Alois Cochard";
+      homepage = "http://github.com/aloiscochard/sarsi";
+      url = "";
+      synopsis = "A universal quickfix toolkit and his protocol.";
+      description = "Usage overview can be found in the <http://github.com/aloiscochard/sarsi#sarsi README>.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."ansi-terminal" or (errorHandler.buildDepError "ansi-terminal"))
+          (hsPkgs."async" or (errorHandler.buildDepError "async"))
+          (hsPkgs."attoparsec" or (errorHandler.buildDepError "attoparsec"))
+          (hsPkgs."binary" or (errorHandler.buildDepError "binary"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+          (hsPkgs."cryptonite" or (errorHandler.buildDepError "cryptonite"))
+          (hsPkgs."msgpack" or (errorHandler.buildDepError "msgpack"))
+          (hsPkgs."directory" or (errorHandler.buildDepError "directory"))
+          (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
+          (hsPkgs."fsnotify" or (errorHandler.buildDepError "fsnotify"))
+          (hsPkgs."machines" or (errorHandler.buildDepError "machines"))
+          (hsPkgs."machines-binary" or (errorHandler.buildDepError "machines-binary"))
+          (hsPkgs."machines-process" or (errorHandler.buildDepError "machines-process"))
+          (hsPkgs."machines-io" or (errorHandler.buildDepError "machines-io"))
+          (hsPkgs."network" or (errorHandler.buildDepError "network"))
+          (hsPkgs."process" or (errorHandler.buildDepError "process"))
+          (hsPkgs."stm" or (errorHandler.buildDepError "stm"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."vector" or (errorHandler.buildDepError "vector"))
+          ];
+        buildable = true;
+        };
+      exes = {
+        "sarsi" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."sarsi" or (errorHandler.buildDepError "sarsi"))
+            (hsPkgs."Cabal" or (errorHandler.buildDepError "Cabal"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            ];
+          buildable = true;
+          };
+        "sarsi-nvim" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."sarsi" or (errorHandler.buildDepError "sarsi"))
+            (hsPkgs."async" or (errorHandler.buildDepError "async"))
+            (hsPkgs."machines" or (errorHandler.buildDepError "machines"))
+            (hsPkgs."binary" or (errorHandler.buildDepError "binary"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."machines" or (errorHandler.buildDepError "machines"))
+            (hsPkgs."machines-binary" or (errorHandler.buildDepError "machines-binary"))
+            (hsPkgs."machines-io" or (errorHandler.buildDepError "machines-io"))
+            (hsPkgs."msgpack" or (errorHandler.buildDepError "msgpack"))
+            (hsPkgs."network" or (errorHandler.buildDepError "network"))
+            (hsPkgs."process" or (errorHandler.buildDepError "process"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."stm" or (errorHandler.buildDepError "stm"))
+            (hsPkgs."vector" or (errorHandler.buildDepError "vector"))
+            (hsPkgs."unordered-containers" or (errorHandler.buildDepError "unordered-containers"))
+            ];
+          buildable = true;
+          };
+        "sarsi-sbt" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."sarsi" or (errorHandler.buildDepError "sarsi"))
+            (hsPkgs."machines" or (errorHandler.buildDepError "machines"))
+            (hsPkgs."machines-io" or (errorHandler.buildDepError "machines-io"))
+            (hsPkgs."machines-process" or (errorHandler.buildDepError "machines-process"))
+            (hsPkgs."process" or (errorHandler.buildDepError "process"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            ];
+          buildable = true;
+          };
+        "sarsi-vi" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."sarsi" or (errorHandler.buildDepError "sarsi"))
+            (hsPkgs."directory" or (errorHandler.buildDepError "directory"))
+            (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
+            (hsPkgs."machines" or (errorHandler.buildDepError "machines"))
+            (hsPkgs."machines-io" or (errorHandler.buildDepError "machines-io"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."vector" or (errorHandler.buildDepError "vector"))
+            ];
+          buildable = true;
+          };
+        "srs" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."sarsi" or (errorHandler.buildDepError "sarsi"))
+            (hsPkgs."async" or (errorHandler.buildDepError "async"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."machines" or (errorHandler.buildDepError "machines"))
+            (hsPkgs."machines-io" or (errorHandler.buildDepError "machines-io"))
+            (hsPkgs."machines-process" or (errorHandler.buildDepError "machines-process"))
+            (hsPkgs."process" or (errorHandler.buildDepError "process"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
