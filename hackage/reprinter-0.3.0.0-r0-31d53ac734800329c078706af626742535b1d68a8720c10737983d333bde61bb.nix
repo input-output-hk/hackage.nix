ec2f5d2d2c42b24de55cdc6587f7f8ed0c3f05b6
IOHK <devops+nix-tools@iohk.io>
2021-04-30 01:16:01 +0000

    Automatic update for Fri Apr 30 01:16:00 UTC 2021

---
+++ hackage/reprinter-0.3.0.0-r0-31d53ac734800329c078706af626742535b1d68a8720c10737983d333bde61bb.nix
@@ -0,0 +1,54 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.12";
+      identifier = { name = "reprinter"; version = "0.3.0.0"; };
+      license = "Apache-2.0";
+      copyright = "";
+      maintainer = "d.a.orchard@kent.ac.uk";
+      author = "Dominic Orchard, Vilem-Benjamin Liepelt, Harry Clarke";
+      homepage = "https://github.com/camfort/reprinter#readme";
+      url = "";
+      synopsis = "Scrap Your Reprinter";
+      description = "A datatype generic algorithm for layout-preserving refactoring";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+          (hsPkgs."syb" or (errorHandler.buildDepError "syb"))
+          (hsPkgs."syz" or (errorHandler.buildDepError "syz"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "spec" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."hspec" or (errorHandler.buildDepError "hspec"))
+            (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+            (hsPkgs."reprinter" or (errorHandler.buildDepError "reprinter"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            ];
+          build-tools = [
+            (hsPkgs.buildPackages.hspec-discover.components.exes.hspec-discover or (pkgs.buildPackages.hspec-discover or (errorHandler.buildToolDepError "hspec-discover:hspec-discover")))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
