64cc6ef16f8d21ac7f4408839577e1ca0d22ba6d
IOHK <devops+nix-tools@iohk.io>
2021-05-03 01:12:45 +0000

    Automatic update for Mon May  3 01:12:44 UTC 2021

---
+++ hackage/css-selectors-0.4.0.0-r0-e1e3aaac7aea188cd7e4e6174857e38e0c2d6e5cc8186c127597bca54e85f43f.nix
@@ -0,0 +1,65 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "css-selectors"; version = "0.4.0.0"; };
+      license = "BSD-3-Clause";
+      copyright = "2019 Willem Van Onsem";
+      maintainer = "hapytexeu+gh@gmail.com";
+      author = "Willem Van Onsem";
+      homepage = "https://github.com/hapytex/css-selectors#readme";
+      url = "";
+      synopsis = "Parsing, rendering and manipulating css selectors in Haskell.";
+      description = "A library for parsing, manipulating, and rendering css selectors (not css files,\njust the selectors).\n\nIt has a /quasiquoter/ to enable Haskell to validate the css selector at compile\ntime.\n\nCurrently the css grammar is implemented /without/ the pseudo-classes,\npseudo-elements and negations. One can furthermore calculate the specificity of\na css-selector, and thus perform an analysis over what css-selector will take\nprecedence.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."aeson" or (errorHandler.buildDepError "aeson"))
+          (hsPkgs."array" or (errorHandler.buildDepError "array"))
+          (hsPkgs."binary" or (errorHandler.buildDepError "binary"))
+          (hsPkgs."blaze-markup" or (errorHandler.buildDepError "blaze-markup"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."data-default" or (errorHandler.buildDepError "data-default"))
+          (hsPkgs."Decimal" or (errorHandler.buildDepError "Decimal"))
+          (hsPkgs."hashable" or (errorHandler.buildDepError "hashable"))
+          (hsPkgs."QuickCheck" or (errorHandler.buildDepError "QuickCheck"))
+          (hsPkgs."shakespeare" or (errorHandler.buildDepError "shakespeare"))
+          (hsPkgs."template-haskell" or (errorHandler.buildDepError "template-haskell"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."zlib" or (errorHandler.buildDepError "zlib"))
+          ];
+        build-tools = [
+          (hsPkgs.buildPackages.alex.components.exes.alex or (pkgs.buildPackages.alex or (errorHandler.buildToolDepError "alex:alex")))
+          (hsPkgs.buildPackages.happy.components.exes.happy or (pkgs.buildPackages.happy or (errorHandler.buildToolDepError "happy:happy")))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "css-selectors-test" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."binary" or (errorHandler.buildDepError "binary"))
+            (hsPkgs."css-selectors" or (errorHandler.buildDepError "css-selectors"))
+            (hsPkgs."hashable" or (errorHandler.buildDepError "hashable"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."test-framework" or (errorHandler.buildDepError "test-framework"))
+            (hsPkgs."test-framework-quickcheck2" or (errorHandler.buildDepError "test-framework-quickcheck2"))
+            (hsPkgs."QuickCheck" or (errorHandler.buildDepError "QuickCheck"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
