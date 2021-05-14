b72c8c2e9f9f03395a8101fd1556fc5b47c6b58a
IOHK <devops+nix-tools@iohk.io>
2021-05-14 01:11:59 +0000

    Automatic update for Fri May 14 01:11:58 UTC 2021

---
+++ hackage/hierarchical-env-0.2.0.2-r0-52c926fec608b12472640ee66a3419abf40620acb5e3cb73e3b60fff1a3e4ac5.nix
@@ -0,0 +1,60 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "2.4";
+      identifier = { name = "hierarchical-env"; version = "0.2.0.2"; };
+      license = "BSD-3-Clause";
+      copyright = "";
+      maintainer = "autotaker@gmail.com";
+      author = "Taku Terao";
+      homepage = "https://github.com/autotaker/hierarchical-env";
+      url = "";
+      synopsis = "hierarchical environments for dependency injection";
+      description = "This library provides scalable dependency injection for RIO monad";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."method" or (errorHandler.buildDepError "method"))
+          (hsPkgs."microlens" or (errorHandler.buildDepError "microlens"))
+          (hsPkgs."microlens-mtl" or (errorHandler.buildDepError "microlens-mtl"))
+          (hsPkgs."microlens-th" or (errorHandler.buildDepError "microlens-th"))
+          (hsPkgs."rio" or (errorHandler.buildDepError "rio"))
+          (hsPkgs."template-haskell" or (errorHandler.buildDepError "template-haskell"))
+          (hsPkgs."th-abstraction" or (errorHandler.buildDepError "th-abstraction"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "hierarchical-env-test" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."method" or (errorHandler.buildDepError "method"))
+            (hsPkgs."microlens" or (errorHandler.buildDepError "microlens"))
+            (hsPkgs."microlens-mtl" or (errorHandler.buildDepError "microlens-mtl"))
+            (hsPkgs."microlens-th" or (errorHandler.buildDepError "microlens-th"))
+            (hsPkgs."rio" or (errorHandler.buildDepError "rio"))
+            (hsPkgs."template-haskell" or (errorHandler.buildDepError "template-haskell"))
+            (hsPkgs."th-abstraction" or (errorHandler.buildDepError "th-abstraction"))
+            (hsPkgs."hspec" or (errorHandler.buildDepError "hspec"))
+            (hsPkgs."hierarchical-env" or (errorHandler.buildDepError "hierarchical-env"))
+            ];
+          build-tools = [
+            (hsPkgs.buildPackages.hspec-discover.components.exes.hspec-discover or (pkgs.buildPackages.hspec-discover or (errorHandler.buildToolDepError "hspec-discover:hspec-discover")))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
---
+++ hackage/slugify-0.1.0.1-r0-0764c30a5d6d6342ca055a97f0a7b4279a26b42b9483b73420d1b364d3f8fb38.nix
@@ -0,0 +1,50 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "slugify"; version = "0.1.0.1"; };
+      license = "BSD-3-Clause";
+      copyright = "2020 Willem Van Onsem";
+      maintainer = "hapytexeu+gh@gmail.com";
+      author = "Willem Van Onsem";
+      homepage = "https://github.com/hapytex/slugify#readme";
+      url = "";
+      synopsis = "Convert text into slugs.";
+      description = "Convert a 'Text' object into a visually pleasant URL component.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."unicode-transforms" or (errorHandler.buildDepError "unicode-transforms"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "gulsify" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."slugify" or (errorHandler.buildDepError "slugify"))
+            (hsPkgs."hspec" or (errorHandler.buildDepError "hspec"))
+            (hsPkgs."QuickCheck" or (errorHandler.buildDepError "QuickCheck"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            ];
+          build-tools = [
+            (hsPkgs.buildPackages.hspec-discover.components.exes.hspec-discover or (pkgs.buildPackages.hspec-discover or (errorHandler.buildToolDepError "hspec-discover:hspec-discover")))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
