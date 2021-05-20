4dbf9b8e17e1db2fff8c5feecd2f92c22ab46627
IOHK <devops+nix-tools@iohk.io>
2021-05-20 01:12:20 +0000

    Automatic update for Thu May 20 01:12:19 UTC 2021

---
+++ hackage/hasklepias-0.4.2-r0-327920870a000a6787abebd35da2e65f325305cc08a73add84bed8db05e14445.nix
@@ -0,0 +1,81 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "2.2";
+      identifier = { name = "hasklepias"; version = "0.4.2"; };
+      license = "BSD-3-Clause";
+      copyright = "NoviSci, Inc";
+      maintainer = "bsaul@novisci.com";
+      author = "Bradley Saul";
+      homepage = "https://github.com/novisci/asclepias/#readme";
+      url = "";
+      synopsis = "Define features from events";
+      description = "Please see the README on GitHub at <https://github.com/novisci/asclepias#readme>";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."aeson" or (errorHandler.buildDepError "aeson"))
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+          (hsPkgs."flow" or (errorHandler.buildDepError "flow"))
+          (hsPkgs."interval-algebra" or (errorHandler.buildDepError "interval-algebra"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."time" or (errorHandler.buildDepError "time"))
+          (hsPkgs."unordered-containers" or (errorHandler.buildDepError "unordered-containers"))
+          (hsPkgs."safe" or (errorHandler.buildDepError "safe"))
+          (hsPkgs."vector" or (errorHandler.buildDepError "vector"))
+          (hsPkgs."QuickCheck" or (errorHandler.buildDepError "QuickCheck"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "hasklepias-test" = {
+          depends = [
+            (hsPkgs."aeson" or (errorHandler.buildDepError "aeson"))
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."flow" or (errorHandler.buildDepError "flow"))
+            (hsPkgs."hasklepias" or (errorHandler.buildDepError "hasklepias"))
+            (hsPkgs."hspec" or (errorHandler.buildDepError "hspec"))
+            (hsPkgs."interval-algebra" or (errorHandler.buildDepError "interval-algebra"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."time" or (errorHandler.buildDepError "time"))
+            (hsPkgs."unordered-containers" or (errorHandler.buildDepError "unordered-containers"))
+            (hsPkgs."vector" or (errorHandler.buildDepError "vector"))
+            (hsPkgs."QuickCheck" or (errorHandler.buildDepError "QuickCheck"))
+            ];
+          buildable = true;
+          };
+        "examples" = {
+          depends = [
+            (hsPkgs."hasklepias" or (errorHandler.buildDepError "hasklepias"))
+            (hsPkgs."hspec" or (errorHandler.buildDepError "hspec"))
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."flow" or (errorHandler.buildDepError "flow"))
+            (hsPkgs."interval-algebra" or (errorHandler.buildDepError "interval-algebra"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."time" or (errorHandler.buildDepError "time"))
+            (hsPkgs."aeson" or (errorHandler.buildDepError "aeson"))
+            (hsPkgs."unordered-containers" or (errorHandler.buildDepError "unordered-containers"))
+            (hsPkgs."vector" or (errorHandler.buildDepError "vector"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
---
+++ hackage/raaz-0.2.3-r0-5c24cbfc3f4112622eea0d6a3634444e9931d803091e3486526ad3de10805e4c.nix
@@ -0,0 +1,124 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {
+      opt-native = false;
+      vector128 = false;
+      vector256 = false;
+      vector512 = false;
+      avx2 = false;
+      linux-getrandom = false;
+      liquidhaskell = false;
+      };
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "raaz"; version = "0.2.3"; };
+      license = "BSD-3-Clause";
+      copyright = "";
+      maintainer = "ppk@cse.iitk.ac.in";
+      author = "Piyush P Kurur";
+      homepage = "http://github.com/raaz-crypto/raaz";
+      url = "";
+      synopsis = "The raaz cryptographic library.";
+      description = "Raaz is a cryptographic library for Haskell. The library\nis designed with a special emphasis on using the type system of\nHaskell to eliminate a large set of vulnerabilities like buffer\noverflows, timing attacks etc. It also strives to achieve this\nsafety with no compromise on performance.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."deepseq" or (errorHandler.buildDepError "deepseq"))
+          (hsPkgs."vector" or (errorHandler.buildDepError "vector"))
+          ] ++ (pkgs.lib).optional (compiler.isGhc && (compiler.version).lt "8") (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"));
+        libs = (pkgs.lib).optionals (system.isWindows) [
+          (pkgs."Advapi32" or (errorHandler.sysDepError "Advapi32"))
+          (pkgs."Kernel32" or (errorHandler.sysDepError "Kernel32"))
+          ];
+        build-tools = (pkgs.lib).optional (system.isWindows) (hsPkgs.buildPackages.hsc2hs.components.exes.hsc2hs or (pkgs.buildPackages.hsc2hs or (errorHandler.buildToolDepError "hsc2hs:hsc2hs")));
+        buildable = true;
+        };
+      exes = {
+        "raaz" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."optparse-applicative" or (errorHandler.buildDepError "optparse-applicative"))
+            (hsPkgs."raaz" or (errorHandler.buildDepError "raaz"))
+            ] ++ (pkgs.lib).optional (compiler.isGhc && (compiler.version).lt "8") (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"));
+          buildable = true;
+          };
+        };
+      tests = {
+        "spec" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."HUnit" or (errorHandler.buildDepError "HUnit"))
+            (hsPkgs."QuickCheck" or (errorHandler.buildDepError "QuickCheck"))
+            (hsPkgs."hspec" or (errorHandler.buildDepError "hspec"))
+            (hsPkgs."hspec-discover" or (errorHandler.buildDepError "hspec-discover"))
+            (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+            (hsPkgs."raaz" or (errorHandler.buildDepError "raaz"))
+            (hsPkgs."vector" or (errorHandler.buildDepError "vector"))
+            ];
+          build-tools = [
+            (hsPkgs.buildPackages.hspec-discover.components.exes.hspec-discover or (pkgs.buildPackages.hspec-discover or (errorHandler.buildToolDepError "hspec-discover:hspec-discover")))
+            ];
+          buildable = true;
+          };
+        "liquidspec" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            ] ++ (pkgs.lib).optionals (flags.liquidhaskell) [
+            (hsPkgs."liquidhaskell" or (errorHandler.buildDepError "liquidhaskell"))
+            (hsPkgs."directory" or (errorHandler.buildDepError "directory"))
+            (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
+            ];
+          buildable = if flags.liquidhaskell then true else false;
+          };
+        };
+      benchmarks = {
+        "blaze-vs-write" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."blaze-builder" or (errorHandler.buildDepError "blaze-builder"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."criterion" or (errorHandler.buildDepError "criterion"))
+            (hsPkgs."criterion-measurement" or (errorHandler.buildDepError "criterion-measurement"))
+            (hsPkgs."raaz" or (errorHandler.buildDepError "raaz"))
+            ];
+          buildable = true;
+          };
+        "bench-ciphers" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."blaze-builder" or (errorHandler.buildDepError "blaze-builder"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."criterion" or (errorHandler.buildDepError "criterion"))
+            (hsPkgs."criterion-measurement" or (errorHandler.buildDepError "criterion-measurement"))
+            (hsPkgs."raaz" or (errorHandler.buildDepError "raaz"))
+            ];
+          buildable = true;
+          };
+        "primitives" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."blaze-builder" or (errorHandler.buildDepError "blaze-builder"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."criterion" or (errorHandler.buildDepError "criterion"))
+            (hsPkgs."criterion-measurement" or (errorHandler.buildDepError "criterion-measurement"))
+            (hsPkgs."pretty" or (errorHandler.buildDepError "pretty"))
+            (hsPkgs."raaz" or (errorHandler.buildDepError "raaz"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
