527506ee5167e1d20c8bc69dda8c8719d1ce2caa
IOHK <devops+nix-tools@iohk.io>
2021-05-22 01:10:38 +0000

    Automatic update for Sat May 22 01:10:37 UTC 2021

---
+++ hackage/fft-0.1.8.7-r0-5ca9e32f8b06ef884d9e56b3090a5e6f339657d8e88b927aac25592cef160442.nix
@@ -0,0 +1,64 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = { splitbase = true; base4 = true; };
+    package = {
+      specVersion = "1.12";
+      identifier = { name = "fft"; version = "0.1.8.7"; };
+      license = "BSD-3-Clause";
+      copyright = "";
+      maintainer = "Jed Brown <jed@59A2.org>, Henning Thielemann <fft@henning-thielemann.de>";
+      author = "Jed Brown";
+      homepage = "";
+      url = "";
+      synopsis = "Bindings to the FFTW library.";
+      description = "Bindings to the FFTW library.\n\nProvides high performance discrete fourier transforms in\narbitrary dimensions.  Include transforms of complex data,\nreal data, and real to real transforms.\n";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = ([
+          (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+          (hsPkgs."carray" or (errorHandler.buildDepError "carray"))
+          (hsPkgs."storable-complex" or (errorHandler.buildDepError "storable-complex"))
+          (hsPkgs."ix-shapable" or (errorHandler.buildDepError "ix-shapable"))
+          ] ++ (if flags.splitbase
+          then [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."array" or (errorHandler.buildDepError "array"))
+            ]
+          else [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            ])) ++ (if flags.base4
+          then [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."syb" or (errorHandler.buildDepError "syb"))
+            ]
+          else [ (hsPkgs."base" or (errorHandler.buildDepError "base")) ]);
+        pkgconfig = [
+          (pkgconfPkgs."fftw3f" or (errorHandler.pkgConfDepError "fftw3f"))
+          (pkgconfPkgs."fftw3" or (errorHandler.pkgConfDepError "fftw3"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "test-fft" = {
+          depends = [
+            (hsPkgs."fft" or (errorHandler.buildDepError "fft"))
+            (hsPkgs."carray" or (errorHandler.buildDepError "carray"))
+            (hsPkgs."storable-complex" or (errorHandler.buildDepError "storable-complex"))
+            (hsPkgs."QuickCheck" or (errorHandler.buildDepError "QuickCheck"))
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
---
+++ hackage/haskell-stack-trace-plugin-0.1.2.0-r0-3950aa4a212d8df59cd66de641c218e5e5339fcac296e870eb206f5d0c3f9c6f.nix
@@ -0,0 +1,60 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = { dev = false; };
+    package = {
+      specVersion = "2.4";
+      identifier = {
+        name = "haskell-stack-trace-plugin";
+        version = "0.1.2.0";
+        };
+      license = "MIT";
+      copyright = "2018-2021 Shinya Yamaguchi";
+      maintainer = "a@wado.dev";
+      author = "Shinya Yamaguchi";
+      homepage = "https://github.com/waddlaw/haskell-stack-trace-plugin";
+      url = "";
+      synopsis = "haskell-stack-trace-plugin";
+      description = "This plugin allow implicitly add HasCallStack class to every top-level function for all module. Hence, we can to get completely continuous call stack.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."ghc" or (errorHandler.buildDepError "ghc"))
+          ];
+        buildable = true;
+        };
+      exes = {
+        "example" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."haskell-stack-trace-plugin" or (errorHandler.buildDepError "haskell-stack-trace-plugin"))
+            ];
+          buildable = true;
+          };
+        };
+      tests = {
+        "test" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."hspec" or (errorHandler.buildDepError "hspec"))
+            (hsPkgs."typed-process" or (errorHandler.buildDepError "typed-process"))
+            ];
+          build-tools = [
+            (hsPkgs.buildPackages.hspec-discover.components.exes.hspec-discover or (pkgs.buildPackages.hspec-discover or (errorHandler.buildToolDepError "hspec-discover:hspec-discover")))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
