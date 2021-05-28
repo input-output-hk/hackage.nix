17a92282d2f4be1817a4f366a44351f24e2cbf13
IOHK <devops+nix-tools@iohk.io>
2021-05-28 01:11:14 +0000

    Automatic update for Fri May 28 01:11:13 UTC 2021

---
+++ hackage/partial-semigroup-0.5.1.12-r0-5ef87f4c37f066014d0e8fb87dc9d64ec558eb6c598a1aa25657bd4a3e91ed9b.nix
@@ -0,0 +1,60 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = { enable-hedgehog = true; enable-doctest = true; };
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "partial-semigroup"; version = "0.5.1.12"; };
+      license = "Apache-2.0";
+      copyright = "";
+      maintainer = "Chris Martin <ch.martin@gmail.com>";
+      author = "Chris Martin <ch.martin@gmail.com>";
+      homepage = "https://github.com/chris-martin/partial-semigroup";
+      url = "";
+      synopsis = "A partial binary associative operator";
+      description = "A partial semigroup is like a semigroup, but the operator\nis partial. We represent this in Haskell as a total function\n@(<>?) :: a -> a -> Maybe a@.\n\nThe\n<https://hackage.haskell.org/package/partial-semigroup-hedgehog partial-semigroup-hedgehog>\ncompanion package provides support for checking the partial\nsemigroup associativity axiom using the\n<https://hackage.haskell.org/package/hedgehog hedgehog>\npackage.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [ (hsPkgs."base" or (errorHandler.buildDepError "base")) ];
+        buildable = true;
+        };
+      tests = {
+        "docs" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            ] ++ (pkgs.lib).optional (flags.enable-doctest) (hsPkgs."doctest" or (errorHandler.buildDepError "doctest"));
+          buildable = true;
+          };
+        "examples" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."partial-semigroup" or (errorHandler.buildDepError "partial-semigroup"))
+            ] ++ (pkgs.lib).optional (flags.enable-hedgehog) (hsPkgs."hedgehog" or (errorHandler.buildDepError "hedgehog"));
+          buildable = true;
+          };
+        "properties" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."partial-semigroup" or (errorHandler.buildDepError "partial-semigroup"))
+            ] ++ (pkgs.lib).optional (flags.enable-hedgehog) (hsPkgs."hedgehog" or (errorHandler.buildDepError "hedgehog"));
+          buildable = true;
+          };
+        "generics" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."partial-semigroup" or (errorHandler.buildDepError "partial-semigroup"))
+            ] ++ (pkgs.lib).optional (flags.enable-hedgehog) (hsPkgs."hedgehog" or (errorHandler.buildDepError "hedgehog"));
+          buildable = true;
+          };
+        };
+      };
+    }
---
+++ hackage/partial-semigroup-hedgehog-0.6.0.8-r0-7fc2ec8bf60c79190bfef0aca6a0af177b876270265ad7947dfcefc228718417.nix
@@ -0,0 +1,38 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.10";
+      identifier = {
+        name = "partial-semigroup-hedgehog";
+        version = "0.6.0.8";
+        };
+      license = "Apache-2.0";
+      copyright = "";
+      maintainer = "Chris Martin <ch.martin@gmail.com>";
+      author = "Chris Martin <ch.martin@gmail.com>";
+      homepage = "https://github.com/chris-martin/partial-semigroup";
+      url = "";
+      synopsis = "Property testing for partial semigroups using Hedgehog";
+      description = "Provides a\n<https://hackage.haskell.org/package/hedgehog hedgehog>\nproperty to check the associativity axiom for the\n@PartialSemigroup@ class defined in the\n<https://hackage.haskell.org/package/partial-semigroup partial-semigroup>\npackage.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."hedgehog" or (errorHandler.buildDepError "hedgehog"))
+          (hsPkgs."partial-semigroup" or (errorHandler.buildDepError "partial-semigroup"))
+          ];
+        buildable = true;
+        };
+      };
+    }
