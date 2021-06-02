27980e8ee6fe043253a40d19bac344659ec40d2b
IOHK <devops+nix-tools@iohk.io>
2021-06-02 01:11:46 +0000

    Automatic update for Wed Jun  2 01:11:45 UTC 2021

---
+++ hackage/fitspec-0.4.10-r0-07d4498f28085936e176ec27b495eef02776c5abc3f9aeef5e15dea36e2042a4.nix
@@ -0,0 +1,164 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.18";
+      identifier = { name = "fitspec"; version = "0.4.10"; };
+      license = "BSD-3-Clause";
+      copyright = "";
+      maintainer = "Rudy Matela <rudy@matela.com.br>";
+      author = "Rudy Matela,  Colin Runciman";
+      homepage = "https://github.com/rudymatela/fitspec#readme";
+      url = "";
+      synopsis = "refining property sets for testing Haskell programs";
+      description = "FitSpec provides automated assistance in the task of refining test properties\nfor Haskell functions.\n\nFitSpec tests mutant variations of functions under test against a given\nproperty set, recording any surviving mutants that pass all tests. FitSpec\nthen reports:\n\n* surviving mutants: indicating incompleteness of properties,\nprompting the user to amend a property or to add a new one;\n\n* conjectures: indicating redundancy in the property set,\nprompting the user to remove properties so to reduce the cost of testing.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."leancheck" or (errorHandler.buildDepError "leancheck"))
+          (hsPkgs."cmdargs" or (errorHandler.buildDepError "cmdargs"))
+          (hsPkgs."template-haskell" or (errorHandler.buildDepError "template-haskell"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "mutate" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."leancheck" or (errorHandler.buildDepError "leancheck"))
+            (hsPkgs."fitspec" or (errorHandler.buildDepError "fitspec"))
+            ];
+          buildable = true;
+          };
+        "showmutable" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."leancheck" or (errorHandler.buildDepError "leancheck"))
+            (hsPkgs."fitspec" or (errorHandler.buildDepError "fitspec"))
+            ];
+          buildable = true;
+          };
+        "derive" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."leancheck" or (errorHandler.buildDepError "leancheck"))
+            (hsPkgs."fitspec" or (errorHandler.buildDepError "fitspec"))
+            ];
+          buildable = true;
+          };
+        "utils" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."leancheck" or (errorHandler.buildDepError "leancheck"))
+            (hsPkgs."fitspec" or (errorHandler.buildDepError "fitspec"))
+            ];
+          buildable = true;
+          };
+        };
+      benchmarks = {
+        "avltrees" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."fitspec" or (errorHandler.buildDepError "fitspec"))
+            ];
+          buildable = true;
+          };
+        "bools" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."fitspec" or (errorHandler.buildDepError "fitspec"))
+            ];
+          buildable = true;
+          };
+        "digraphs" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."fitspec" or (errorHandler.buildDepError "fitspec"))
+            ];
+          buildable = true;
+          };
+        "heaps" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."fitspec" or (errorHandler.buildDepError "fitspec"))
+            ];
+          buildable = true;
+          };
+        "id" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."fitspec" or (errorHandler.buildDepError "fitspec"))
+            ];
+          buildable = true;
+          };
+        "list" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."fitspec" or (errorHandler.buildDepError "fitspec"))
+            ];
+          buildable = true;
+          };
+        "mergeheaps" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."fitspec" or (errorHandler.buildDepError "fitspec"))
+            ];
+          buildable = true;
+          };
+        "pretty" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."fitspec" or (errorHandler.buildDepError "fitspec"))
+            (hsPkgs."pretty" or (errorHandler.buildDepError "pretty"))
+            ];
+          buildable = true;
+          };
+        "sets" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."fitspec" or (errorHandler.buildDepError "fitspec"))
+            ];
+          buildable = true;
+          };
+        "setsofsets" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."fitspec" or (errorHandler.buildDepError "fitspec"))
+            ];
+          buildable = true;
+          };
+        "sieve" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."fitspec" or (errorHandler.buildDepError "fitspec"))
+            ];
+          buildable = true;
+          };
+        "sorting" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."fitspec" or (errorHandler.buildDepError "fitspec"))
+            ];
+          buildable = true;
+          };
+        "spring" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."fitspec" or (errorHandler.buildDepError "fitspec"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
---
+++ hackage/xmonad-0.15-r1-8da7cffd1d1f837f4559d9f69e86db58f022231b4996ef3fc894c28bb149107b.nix
@@ -0,0 +1,78 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = { testing = false; generatemanpage = false; };
+    package = {
+      specVersion = "1.8";
+      identifier = { name = "xmonad"; version = "0.15"; };
+      license = "BSD-3-Clause";
+      copyright = "";
+      maintainer = "xmonad@haskell.org";
+      author = "Spencer Janssen, Don Stewart, Adam Vogt, David Roundy, Jason Creighton,\nBrent Yorgey, Peter Jones, Peter Simons, Andrea Rossato, Devin Mullins,\nLukas Mai, Alec Berryman, Stefan O'Rear, Daniel Wagner, Peter J. Jones,\nDaniel Schoepe, Karsten Schoelzel, Neil Mitchell, Joachim Breitner,\nPeter De Wachter, Eric Mertens, Geoff Reedy, Michiel Derhaeg,\nPhilipp Balzarek, Valery V. Vorotyntsev, Alex Tarkovsky, Fabian Beuke,\nFelix Hirn, Michael Sloan, Tomas Janousek, Vanessa McHale, Nicolas Pouillard,\nAaron Denney, Austin Seipp, Benno Fünfstück, Brandon S Allbery, Chris Mears,\nChristian Thiemann, Clint Adams, Daniel Neri, David Lazar, Ferenc Wagner,\nFrancesco Ariis, Gábor Lipták, Ivan N. Veselov, Ivan Tarasov, Javran Cheng,\nJens Petersen, Joey Hess, Jonne Ransijn, Josh Holland, Khudyakov Alexey,\nKlaus Weidner, Michael G. Sloan, Mikkel Christiansen, Nicolas Dudebout,\nOndřej Súkup, Paul Hebble, Shachaf Ben-Kiki, Siim Põder, Tim McIver,\nTrevor Elliott, Wouter Swierstra, Conrad Irwin, Tim Thelion";
+      homepage = "http://xmonad.org";
+      url = "";
+      synopsis = "A tiling window manager";
+      description = "xmonad is a tiling window manager for X. Windows are arranged\nautomatically to tile the screen without gaps or overlap, maximising\nscreen use. All features of the window manager are accessible from the\nkeyboard: a mouse is strictly optional. xmonad is written and\nextensible in Haskell. Custom layout algorithms, and other extensions,\nmay be written by the user in config files. Layouts are applied\ndynamically, and different layouts may be used on each workspace.\nXinerama is fully supported, allowing windows to be tiled on several\nscreens.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."X11" or (errorHandler.buildDepError "X11"))
+          (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+          (hsPkgs."data-default" or (errorHandler.buildDepError "data-default"))
+          (hsPkgs."directory" or (errorHandler.buildDepError "directory"))
+          (hsPkgs."extensible-exceptions" or (errorHandler.buildDepError "extensible-exceptions"))
+          (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
+          (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+          (hsPkgs."process" or (errorHandler.buildDepError "process"))
+          (hsPkgs."setlocale" or (errorHandler.buildDepError "setlocale"))
+          (hsPkgs."unix" or (errorHandler.buildDepError "unix"))
+          (hsPkgs."utf8-string" or (errorHandler.buildDepError "utf8-string"))
+          ];
+        buildable = if flags.testing then false else true;
+        };
+      exes = {
+        "xmonad" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."X11" or (errorHandler.buildDepError "X11"))
+            (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+            (hsPkgs."unix" or (errorHandler.buildDepError "unix"))
+            (hsPkgs."xmonad" or (errorHandler.buildDepError "xmonad"))
+            ];
+          buildable = true;
+          };
+        "generatemanpage" = {
+          depends = (pkgs.lib).optionals (flags.generatemanpage) [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."pandoc" or (errorHandler.buildDepError "pandoc"))
+            (hsPkgs."regex-posix" or (errorHandler.buildDepError "regex-posix"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            ];
+          buildable = if flags.generatemanpage then true else false;
+          };
+        };
+      tests = {
+        "properties" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."QuickCheck" or (errorHandler.buildDepError "QuickCheck"))
+            (hsPkgs."X11" or (errorHandler.buildDepError "X11"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."extensible-exceptions" or (errorHandler.buildDepError "extensible-exceptions"))
+            (hsPkgs."xmonad" or (errorHandler.buildDepError "xmonad"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
