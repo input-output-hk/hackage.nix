85fb3b211e95b9c8028ff675df1f6dcb0fcc5474
IOHK <devops+nix-tools@iohk.io>
2021-06-10 01:13:53 +0000

    Automatic update for Thu Jun 10 01:13:51 UTC 2021

---
+++ hackage/literatex-0.1.0.2-r0-cd847f6a9aedc8980f9c4d8189e8c80a735d8f093e0805c5866f39c0f52328c5.nix
@@ -0,0 +1,80 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = { examples = false; write-hie = false; };
+    package = {
+      specVersion = "1.24";
+      identifier = { name = "literatex"; version = "0.1.0.2"; };
+      license = "MIT";
+      copyright = "Copyright (c) 2021 Travis Cardwell";
+      maintainer = "Travis Cardwell <travis.cardwell@extrema.is>";
+      author = "Travis Cardwell <travis.cardwell@extrema.is>";
+      homepage = "https://github.com/ExtremaIS/literatex-haskell#readme";
+      url = "";
+      synopsis = "transform literate source code to Markdown";
+      description = "This package provides a library as well as a command-line utility that\ntransforms literate source code to Markdown.  Please see the README on\nGitHub at <https://github.com/ExtremaIS/literatex-haskell#readme>.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."conduit" or (errorHandler.buildDepError "conduit"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."ttc" or (errorHandler.buildDepError "ttc"))
+          (hsPkgs."unliftio" or (errorHandler.buildDepError "unliftio"))
+          ];
+        buildable = true;
+        };
+      exes = {
+        "literatex" = {
+          depends = [
+            (hsPkgs."ansi-wl-pprint" or (errorHandler.buildDepError "ansi-wl-pprint"))
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."optparse-applicative" or (errorHandler.buildDepError "optparse-applicative"))
+            (hsPkgs."literatex" or (errorHandler.buildDepError "literatex"))
+            (hsPkgs."ttc" or (errorHandler.buildDepError "ttc"))
+            ];
+          buildable = true;
+          };
+        "example-highlevel" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."literatex" or (errorHandler.buildDepError "literatex"))
+            ];
+          buildable = if flags.examples then true else false;
+          };
+        "example-lowlevel" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."literatex" or (errorHandler.buildDepError "literatex"))
+            ];
+          buildable = if flags.examples then true else false;
+          };
+        };
+      tests = {
+        "literatex-test" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
+            (hsPkgs."literatex" or (errorHandler.buildDepError "literatex"))
+            (hsPkgs."tasty" or (errorHandler.buildDepError "tasty"))
+            (hsPkgs."tasty-hunit" or (errorHandler.buildDepError "tasty-hunit"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."ttc" or (errorHandler.buildDepError "ttc"))
+            (hsPkgs."unliftio" or (errorHandler.buildDepError "unliftio"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
---
+++ hackage/safe-decimal-0.2.1.0-r0-cba4e9873698fa6ad6f59163f84d02063848ae05324e310679f108e7f0554c03.nix
@@ -0,0 +1,60 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "safe-decimal"; version = "0.2.1.0"; };
+      license = "BSD-3-Clause";
+      copyright = "2018-2020 FP Complete, 2021 Alexey Kuleshevich";
+      maintainer = "alexey@kuleshevi.ch";
+      author = "Alexey Kuleshevich";
+      homepage = "https://github.com/fpco/safe-decimal#readme";
+      url = "";
+      synopsis = "Safe and very efficient arithmetic operations on fixed decimal point numbers";
+      description = "Please see the README on GitHub at <https://github.com/fpco/safe-decimal#readme>";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."deepseq" or (errorHandler.buildDepError "deepseq"))
+          (hsPkgs."exceptions" or (errorHandler.buildDepError "exceptions"))
+          (hsPkgs."scientific" or (errorHandler.buildDepError "scientific"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "doctests" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."doctest" or (errorHandler.buildDepError "doctest"))
+            (hsPkgs."safe-decimal" or (errorHandler.buildDepError "safe-decimal"))
+            ];
+          buildable = true;
+          };
+        "tests" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."deepseq" or (errorHandler.buildDepError "deepseq"))
+            (hsPkgs."safe-decimal" or (errorHandler.buildDepError "safe-decimal"))
+            (hsPkgs."scientific" or (errorHandler.buildDepError "scientific"))
+            (hsPkgs."QuickCheck" or (errorHandler.buildDepError "QuickCheck"))
+            (hsPkgs."hspec" or (errorHandler.buildDepError "hspec"))
+            ];
+          build-tools = [
+            (hsPkgs.buildPackages.hspec-discover.components.exes.hspec-discover or (pkgs.buildPackages.hspec-discover or (errorHandler.buildToolDepError "hspec-discover:hspec-discover")))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
