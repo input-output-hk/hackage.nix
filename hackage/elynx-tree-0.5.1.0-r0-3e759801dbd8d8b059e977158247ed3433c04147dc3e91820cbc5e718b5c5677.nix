891be71d0b1ddc5220bc3f5badb065936fe46de7
IOHK <devops+nix-tools@iohk.io>
2021-06-13 01:11:38 +0000

    Automatic update for Sun Jun 13 01:11:37 UTC 2021

---
+++ hackage/elynx-tree-0.5.1.0-r0-3e759801dbd8d8b059e977158247ed3433c04147dc3e91820cbc5e718b5c5677.nix
@@ -0,0 +1,75 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "2.2";
+      identifier = { name = "elynx-tree"; version = "0.5.1.0"; };
+      license = "GPL-3.0-or-later";
+      copyright = "Dominik Schrempf (2020)";
+      maintainer = "dominik.schrempf@gmail.com";
+      author = "Dominik Schrempf";
+      homepage = "https://github.com/dschrempf/elynx#readme";
+      url = "";
+      synopsis = "Handle phylogenetic trees";
+      description = "Examine, compare, and simulate phylogenetic trees in a reproducible way. Please see the README on GitHub at <https://github.com/dschrempf/elynx>.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."aeson" or (errorHandler.buildDepError "aeson"))
+          (hsPkgs."attoparsec" or (errorHandler.buildDepError "attoparsec"))
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."comonad" or (errorHandler.buildDepError "comonad"))
+          (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+          (hsPkgs."deepseq" or (errorHandler.buildDepError "deepseq"))
+          (hsPkgs."double-conversion" or (errorHandler.buildDepError "double-conversion"))
+          (hsPkgs."elynx-nexus" or (errorHandler.buildDepError "elynx-nexus"))
+          (hsPkgs."math-functions" or (errorHandler.buildDepError "math-functions"))
+          (hsPkgs."mwc-random" or (errorHandler.buildDepError "mwc-random"))
+          (hsPkgs."parallel" or (errorHandler.buildDepError "parallel"))
+          (hsPkgs."primitive" or (errorHandler.buildDepError "primitive"))
+          (hsPkgs."statistics" or (errorHandler.buildDepError "statistics"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "tree-test" = {
+          depends = [
+            (hsPkgs."QuickCheck" or (errorHandler.buildDepError "QuickCheck"))
+            (hsPkgs."attoparsec" or (errorHandler.buildDepError "attoparsec"))
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."elynx-tools" or (errorHandler.buildDepError "elynx-tools"))
+            (hsPkgs."elynx-tree" or (errorHandler.buildDepError "elynx-tree"))
+            (hsPkgs."hspec" or (errorHandler.buildDepError "hspec"))
+            ];
+          buildable = true;
+          };
+        };
+      benchmarks = {
+        "tree-bench" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."criterion" or (errorHandler.buildDepError "criterion"))
+            (hsPkgs."elynx-tools" or (errorHandler.buildDepError "elynx-tools"))
+            (hsPkgs."elynx-tree" or (errorHandler.buildDepError "elynx-tree"))
+            (hsPkgs."microlens" or (errorHandler.buildDepError "microlens"))
+            (hsPkgs."mwc-random" or (errorHandler.buildDepError "mwc-random"))
+            (hsPkgs."parallel" or (errorHandler.buildDepError "parallel"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
---
+++ hackage/google-maps-geocoding-0.7.0.0-r0-29e58db4bf083cacbaf1e4c7947af5c1cd48069d1da4f2d229031d4821533379.nix
@@ -0,0 +1,39 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "google-maps-geocoding"; version = "0.7.0.0"; };
+      license = "BSD-3-Clause";
+      copyright = "Mike Pilgrem";
+      maintainer = "public@pilgrem.com";
+      author = "Mike Pilgrem";
+      homepage = "https://github.com/mpilgrem/google-maps-geocoding#readme";
+      url = "";
+      synopsis = "Bindings to the Google Geocoding API (formerly Maps\nGeocoding API)";
+      description = "The\n<https://developers.google.com/maps/documentation/geocoding/intro Google Geocoding API>\nprovides a direct way to access geocoding and reverse\ngeocoding services via an HTTP request. This library\npackage provides bindings in Haskell to that API.\n\nNB: The use of the API's services is subject to the\n<https://cloud.google.com/maps-platform/terms/ Google Maps Platform Terms of Service>,\nwhich terms restrict the use of content. End Users’ use of\nGoogle Maps is subject to the then-current Google\nMaps/Google Earth Additional Terms of Service at\n<https://maps.google.com/help/terms_maps.html> and Google\nPrivacy Policy at\n<https://www.google.com/policies/privacy/>.\n\nThis package has no connection with Google Inc. or its\naffiliates.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."aeson" or (errorHandler.buildDepError "aeson"))
+          (hsPkgs."google-static-maps" or (errorHandler.buildDepError "google-static-maps"))
+          (hsPkgs."http-client" or (errorHandler.buildDepError "http-client"))
+          (hsPkgs."servant" or (errorHandler.buildDepError "servant"))
+          (hsPkgs."servant-client" or (errorHandler.buildDepError "servant-client"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          ];
+        buildable = true;
+        };
+      };
+    }
