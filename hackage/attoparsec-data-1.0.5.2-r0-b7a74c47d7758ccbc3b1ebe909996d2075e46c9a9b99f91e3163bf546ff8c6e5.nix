3ee8650d587fa5fe191c9caee072c9832c7dee7f
IOHK <devops+nix-tools@iohk.io>
2021-06-27 01:12:21 +0000

    Automatic update for Sun Jun 27 01:12:20 UTC 2021

---
+++ hackage/attoparsec-data-1.0.5.2-r0-b7a74c47d7758ccbc3b1ebe909996d2075e46c9a9b99f91e3163bf546ff8c6e5.nix
@@ -0,0 +1,40 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "attoparsec-data"; version = "1.0.5.2"; };
+      license = "MIT";
+      copyright = "(c) 2017, Nikita Volkov";
+      maintainer = "Nikita Volkov <nikita.y.volkov@mail.ru>";
+      author = "Nikita Volkov <nikita.y.volkov@mail.ru>";
+      homepage = "https://github.com/nikita-volkov/attoparsec-data";
+      url = "";
+      synopsis = "Parsers for the standard Haskell data types";
+      description = "";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."attoparsec" or (errorHandler.buildDepError "attoparsec"))
+          (hsPkgs."attoparsec-time" or (errorHandler.buildDepError "attoparsec-time"))
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."scientific" or (errorHandler.buildDepError "scientific"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."time" or (errorHandler.buildDepError "time"))
+          (hsPkgs."uuid" or (errorHandler.buildDepError "uuid"))
+          ];
+        buildable = true;
+        };
+      };
+    }
---
+++ hackage/jsaddle-hello-2.0.0.1-r0-3d9db4572706441357cda018e56492c1788ba209e5fda23b4c7e623849e8f505.nix
@@ -0,0 +1,75 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "3.0";
+      identifier = { name = "jsaddle-hello"; version = "2.0.0.1"; };
+      license = "MIT";
+      copyright = "(c) Hamish Mackenzie";
+      maintainer = "Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>";
+      author = "Hamish Mackenzie";
+      homepage = "https://github.com/ghcjs/jsaddle-hello";
+      url = "";
+      synopsis = "JSaddle Hello World, an example package";
+      description = "This is an implementation of the classic \"Hello World\" program using JSaddle,\nas an example of how to create a minimal JSaddle application.\nPlease submit any suggestions and improvements.";
+      buildType = "Custom";
+      setup-depends = [
+        (hsPkgs.buildPackages.base or (pkgs.buildPackages.base or (errorHandler.setupDepError "base")))
+        (hsPkgs.buildPackages.Cabal or (pkgs.buildPackages.Cabal or (errorHandler.setupDepError "Cabal")))
+        (hsPkgs.buildPackages.cabal-macosx or (pkgs.buildPackages.cabal-macosx or (errorHandler.setupDepError "cabal-macosx")))
+        ];
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."jsaddle" or (errorHandler.buildDepError "jsaddle"))
+          (hsPkgs."lens" or (errorHandler.buildDepError "lens"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          ];
+        buildable = true;
+        };
+      exes = {
+        "jsaddle-hello" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."jsaddle-hello" or (errorHandler.buildDepError "jsaddle-hello"))
+            (hsPkgs."jsaddle-warp" or (errorHandler.buildDepError "jsaddle-warp"))
+            (hsPkgs."lens" or (errorHandler.buildDepError "lens"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            ];
+          buildable = true;
+          };
+        "jsaddle-hello-webkitgtk" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."jsaddle-hello" or (errorHandler.buildDepError "jsaddle-hello"))
+            (hsPkgs."jsaddle-webkit2gtk" or (errorHandler.buildDepError "jsaddle-webkit2gtk"))
+            (hsPkgs."lens" or (errorHandler.buildDepError "lens"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            ];
+          buildable = if compiler.isGhcjs && true then false else true;
+          };
+        "jsaddle-hello-wkwebview" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."jsaddle-hello" or (errorHandler.buildDepError "jsaddle-hello"))
+            (hsPkgs."jsaddle-wkwebview" or (errorHandler.buildDepError "jsaddle-wkwebview"))
+            (hsPkgs."lens" or (errorHandler.buildDepError "lens"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            ];
+          buildable = if !system.isOsx || compiler.isGhcjs && true
+            then false
+            else true;
+          };
+        };
+      };
+    }
