e832c7df75012bffd478768c0a601237ce21ddbe
IOHK <devops+nix-tools@iohk.io>
2021-07-12 01:11:01 +0000

    Automatic update for Mon Jul 12 01:11:00 UTC 2021

---
+++ hackage/pagerduty-hs-0.1.0.1-r0-00913e4ede5bc6b572b58e9f5b2b605687ebaea4baf0dc36ce2a5a8a3a09d2e9.nix
@@ -0,0 +1,56 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.12";
+      identifier = { name = "pagerduty-hs"; version = "0.1.0.1"; };
+      license = "BSD-3-Clause";
+      copyright = "Copyright © 2021 Dustin Sallings";
+      maintainer = "dustin@spy.net";
+      author = "Dustin Sallings";
+      homepage = "https://github.com/dustin/pagerduty-hs#readme";
+      url = "";
+      synopsis = "An interface to the PagerDuty API.";
+      description = "Please see the README on GitHub at <https://github.com/dustin/pagerduty-hs#readme>";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."aeson" or (errorHandler.buildDepError "aeson"))
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."exceptions" or (errorHandler.buildDepError "exceptions"))
+          (hsPkgs."lens" or (errorHandler.buildDepError "lens"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."wreq" or (errorHandler.buildDepError "wreq"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "pagerduty-test" = {
+          depends = [
+            (hsPkgs."HUnit" or (errorHandler.buildDepError "HUnit"))
+            (hsPkgs."aeson" or (errorHandler.buildDepError "aeson"))
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."exceptions" or (errorHandler.buildDepError "exceptions"))
+            (hsPkgs."lens" or (errorHandler.buildDepError "lens"))
+            (hsPkgs."pagerduty-hs" or (errorHandler.buildDepError "pagerduty-hs"))
+            (hsPkgs."tasty" or (errorHandler.buildDepError "tasty"))
+            (hsPkgs."tasty-hunit" or (errorHandler.buildDepError "tasty-hunit"))
+            (hsPkgs."tasty-quickcheck" or (errorHandler.buildDepError "tasty-quickcheck"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."wreq" or (errorHandler.buildDepError "wreq"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
---
+++ hackage/symantic-parser-0.2.0.20210703-r0-9dcda801cbe413de18fe9e3c298b7829c5343ba3283d9242ecac156067a35987.nix
@@ -0,0 +1,119 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = { dump-core = false; disable-ormolu-check = false; };
+    package = {
+      specVersion = "3.0";
+      identifier = { name = "symantic-parser"; version = "0.2.0.20210703"; };
+      license = "AGPL-3.0-or-later";
+      copyright = "Julien Moutinho <julm+symantic-parser@sourcephile.fr>";
+      maintainer = "Julien Moutinho <julm+symantic-parser@sourcephile.fr>";
+      author = "Julien Moutinho <julm+symantic-parser@sourcephile.fr>";
+      homepage = "";
+      url = "";
+      synopsis = "Parser combinators statically optimized and staged via typed meta-programming";
+      description = "This is a work-in-progress experimental library to generate parsers,\nleveraging Tagless-Final interpreters and Typed Template Haskell staging.\n.\nThis is an alternative but less powerful/reviewed\nimplementation of [ParsleyHaskell](https://github.com/J-mie6/ParsleyHaskell).\nSee the paper by Jamie Willis, Nicolas Wu, and Matthew Pickering,\nadmirably well presented at ICFP-2020: [Staged Selective Parser Combinators](https://icfp20.sigplan.org/details/icfp-2020-papers/20/Staged-Selective-Parser-Combinators).";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."array" or (errorHandler.buildDepError "array"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+          (hsPkgs."deepseq" or (errorHandler.buildDepError "deepseq"))
+          (hsPkgs."ghc-prim" or (errorHandler.buildDepError "ghc-prim"))
+          (hsPkgs."hashable" or (errorHandler.buildDepError "hashable"))
+          (hsPkgs."pretty" or (errorHandler.buildDepError "pretty"))
+          (hsPkgs."symantic-base" or (errorHandler.buildDepError "symantic-base"))
+          (hsPkgs."template-haskell" or (errorHandler.buildDepError "template-haskell"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+          (hsPkgs."unordered-containers" or (errorHandler.buildDepError "unordered-containers"))
+          ];
+        buildable = true;
+        };
+      sublibs = {
+        "parsers" = {
+          depends = [
+            (hsPkgs."symantic-parser" or (errorHandler.buildDepError "symantic-parser"))
+            (hsPkgs."attoparsec" or (errorHandler.buildDepError "attoparsec"))
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."deepseq" or (errorHandler.buildDepError "deepseq"))
+            (hsPkgs."directory" or (errorHandler.buildDepError "directory"))
+            (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
+            (hsPkgs."ghc-prim" or (errorHandler.buildDepError "ghc-prim"))
+            (hsPkgs."hashable" or (errorHandler.buildDepError "hashable"))
+            (hsPkgs."megaparsec" or (errorHandler.buildDepError "megaparsec"))
+            (hsPkgs."process" or (errorHandler.buildDepError "process"))
+            (hsPkgs."strict" or (errorHandler.buildDepError "strict"))
+            (hsPkgs."symantic-base" or (errorHandler.buildDepError "symantic-base"))
+            (hsPkgs."tasty" or (errorHandler.buildDepError "tasty"))
+            (hsPkgs."tasty-golden" or (errorHandler.buildDepError "tasty-golden"))
+            (hsPkgs."template-haskell" or (errorHandler.buildDepError "template-haskell"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+            (hsPkgs."unix" or (errorHandler.buildDepError "unix"))
+            (hsPkgs."unordered-containers" or (errorHandler.buildDepError "unordered-containers"))
+            ];
+          buildable = true;
+          };
+        };
+      tests = {
+        "symantic-parser-test" = {
+          depends = [
+            (hsPkgs."symantic-parser" or (errorHandler.buildDepError "symantic-parser"))
+            (hsPkgs."symantic-parser".components.sublibs.parsers or (errorHandler.buildDepError "symantic-parser:parsers"))
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."deepseq" or (errorHandler.buildDepError "deepseq"))
+            (hsPkgs."directory" or (errorHandler.buildDepError "directory"))
+            (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
+            (hsPkgs."hashable" or (errorHandler.buildDepError "hashable"))
+            (hsPkgs."process" or (errorHandler.buildDepError "process"))
+            (hsPkgs."strict" or (errorHandler.buildDepError "strict"))
+            (hsPkgs."tasty" or (errorHandler.buildDepError "tasty"))
+            (hsPkgs."tasty-golden" or (errorHandler.buildDepError "tasty-golden"))
+            (hsPkgs."template-haskell" or (errorHandler.buildDepError "template-haskell"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+            (hsPkgs."unix" or (errorHandler.buildDepError "unix"))
+            (hsPkgs."unordered-containers" or (errorHandler.buildDepError "unordered-containers"))
+            ] ++ (pkgs.lib).optional (flags.dump-core) (hsPkgs."dump-core" or (errorHandler.buildDepError "dump-core"));
+          build-tools = (pkgs.lib).optional (!flags.disable-ormolu-check) (hsPkgs.buildPackages.ormolu.components.exes.ormolu or (pkgs.buildPackages.ormolu or (errorHandler.buildToolDepError "ormolu:ormolu")));
+          buildable = true;
+          };
+        };
+      benchmarks = {
+        "symantic-parser-benchmark" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."symantic-parser" or (errorHandler.buildDepError "symantic-parser"))
+            (hsPkgs."symantic-parser".components.sublibs.parsers or (errorHandler.buildDepError "symantic-parser:parsers"))
+            (hsPkgs."attoparsec" or (errorHandler.buildDepError "attoparsec"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."criterion" or (errorHandler.buildDepError "criterion"))
+            (hsPkgs."deepseq" or (errorHandler.buildDepError "deepseq"))
+            (hsPkgs."megaparsec" or (errorHandler.buildDepError "megaparsec"))
+            (hsPkgs."random" or (errorHandler.buildDepError "random"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."template-haskell" or (errorHandler.buildDepError "template-haskell"))
+            (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
