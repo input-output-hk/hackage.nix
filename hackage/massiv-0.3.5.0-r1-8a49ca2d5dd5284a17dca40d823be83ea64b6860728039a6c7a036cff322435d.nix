aa205ca2808d6a1cef723b937b43627384489389
IOHK <devops+nix-tools@iohk.io>
2021-07-21 01:12:12 +0000

    Automatic update for Wed Jul 21 01:12:11 UTC 2021

---
+++ hackage/massiv-0.3.5.0-r1-8a49ca2d5dd5284a17dca40d823be83ea64b6860728039a6c7a036cff322435d.nix
@@ -0,0 +1,78 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = { unsafe-checks = false; };
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "massiv"; version = "0.3.5.0"; };
+      license = "BSD-3-Clause";
+      copyright = "2018-2019 Alexey Kuleshevich";
+      maintainer = "alexey@kuleshevi.ch";
+      author = "Alexey Kuleshevich";
+      homepage = "https://github.com/lehins/massiv";
+      url = "";
+      synopsis = "Massiv (Массив) is an Array Library.";
+      description = "Multi-dimensional Arrays with fusion, stencils and parallel computation.";
+      buildType = "Custom";
+      setup-depends = [
+        (hsPkgs.buildPackages.base or (pkgs.buildPackages.base or (errorHandler.setupDepError "base")))
+        (hsPkgs.buildPackages.Cabal or (pkgs.buildPackages.Cabal or (errorHandler.setupDepError "Cabal")))
+        (hsPkgs.buildPackages.cabal-doctest or (pkgs.buildPackages.cabal-doctest or (errorHandler.setupDepError "cabal-doctest")))
+        ];
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."data-default-class" or (errorHandler.buildDepError "data-default-class"))
+          (hsPkgs."deepseq" or (errorHandler.buildDepError "deepseq"))
+          (hsPkgs."exceptions" or (errorHandler.buildDepError "exceptions"))
+          (hsPkgs."scheduler" or (errorHandler.buildDepError "scheduler"))
+          (hsPkgs."primitive" or (errorHandler.buildDepError "primitive"))
+          (hsPkgs."unliftio-core" or (errorHandler.buildDepError "unliftio-core"))
+          (hsPkgs."vector" or (errorHandler.buildDepError "vector"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "tests" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."async" or (errorHandler.buildDepError "async"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."data-default" or (errorHandler.buildDepError "data-default"))
+            (hsPkgs."deepseq" or (errorHandler.buildDepError "deepseq"))
+            (hsPkgs."massiv" or (errorHandler.buildDepError "massiv"))
+            (hsPkgs."hspec" or (errorHandler.buildDepError "hspec"))
+            (hsPkgs."scheduler" or (errorHandler.buildDepError "scheduler"))
+            (hsPkgs."QuickCheck" or (errorHandler.buildDepError "QuickCheck"))
+            (hsPkgs."unliftio" or (errorHandler.buildDepError "unliftio"))
+            (hsPkgs."vector" or (errorHandler.buildDepError "vector"))
+            ];
+          buildable = true;
+          };
+        "doctests" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."doctest" or (errorHandler.buildDepError "doctest"))
+            (hsPkgs."QuickCheck" or (errorHandler.buildDepError "QuickCheck"))
+            (hsPkgs."massiv" or (errorHandler.buildDepError "massiv"))
+            (hsPkgs."mersenne-random-pure64" or (errorHandler.buildDepError "mersenne-random-pure64"))
+            (hsPkgs."random" or (errorHandler.buildDepError "random"))
+            (hsPkgs."splitmix" or (errorHandler.buildDepError "splitmix"))
+            (hsPkgs."template-haskell" or (errorHandler.buildDepError "template-haskell"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
---
+++ hackage/massiv-test-0.1.3.1-r1-d5bc592f18c8fe181b708bf7e625cb0cea67ab7b634db192c0dbfd3d9eb05813.nix
@@ -0,0 +1,65 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "massiv-test"; version = "0.1.3.1"; };
+      license = "BSD-3-Clause";
+      copyright = "2018-2019 Alexey Kuleshevich";
+      maintainer = "alexey@kuleshevi.ch";
+      author = "Alexey Kuleshevich";
+      homepage = "https://github.com/lehins/massiv";
+      url = "";
+      synopsis = "Library that contains generators, properties and tests for Massiv Array Library.";
+      description = "This library is designed for users of massiv library that need random generators for writing custom property tests and reusing some of the predefined ones.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."data-default-class" or (errorHandler.buildDepError "data-default-class"))
+          (hsPkgs."deepseq" or (errorHandler.buildDepError "deepseq"))
+          (hsPkgs."exceptions" or (errorHandler.buildDepError "exceptions"))
+          (hsPkgs."QuickCheck" or (errorHandler.buildDepError "QuickCheck"))
+          (hsPkgs."hspec" or (errorHandler.buildDepError "hspec"))
+          (hsPkgs."massiv" or (errorHandler.buildDepError "massiv"))
+          (hsPkgs."scheduler" or (errorHandler.buildDepError "scheduler"))
+          (hsPkgs."primitive" or (errorHandler.buildDepError "primitive"))
+          (hsPkgs."unliftio" or (errorHandler.buildDepError "unliftio"))
+          (hsPkgs."vector" or (errorHandler.buildDepError "vector"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "tests" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."data-default" or (errorHandler.buildDepError "data-default"))
+            (hsPkgs."deepseq" or (errorHandler.buildDepError "deepseq"))
+            (hsPkgs."genvalidity-hspec" or (errorHandler.buildDepError "genvalidity-hspec"))
+            (hsPkgs."massiv" or (errorHandler.buildDepError "massiv"))
+            (hsPkgs."massiv-test" or (errorHandler.buildDepError "massiv-test"))
+            (hsPkgs."mwc-random" or (errorHandler.buildDepError "mwc-random"))
+            (hsPkgs."hspec" or (errorHandler.buildDepError "hspec"))
+            (hsPkgs."primitive" or (errorHandler.buildDepError "primitive"))
+            (hsPkgs."scheduler" or (errorHandler.buildDepError "scheduler"))
+            (hsPkgs."QuickCheck" or (errorHandler.buildDepError "QuickCheck"))
+            (hsPkgs."vector" or (errorHandler.buildDepError "vector"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
