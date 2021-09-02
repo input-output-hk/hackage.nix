5d4fb23a5b2606dfaf394df0b41b870b3bcefce7
IOHK <devops+nix-tools@iohk.io>
2021-09-02 01:12:33 +0000

    Automatic update for Thu Sep  2 01:12:32 UTC 2021

---
+++ hackage/composite-cassava-0.0.3.1-r0-4d3f0823dbe44517b83837b8c5df59a761edda6b08ab8b8900d9b5950519667e.nix
@@ -0,0 +1,55 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.12";
+      identifier = { name = "composite-cassava"; version = "0.0.3.1"; };
+      license = "MIT";
+      copyright = "Daniel Firth";
+      maintainer = "dan.firth@homotopic.tech";
+      author = "Daniel Firth";
+      homepage = "";
+      url = "";
+      synopsis = "Csv parsing functionality for composite.";
+      description = "Csv parsing functionality for composite.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."cassava" or (errorHandler.buildDepError "cassava"))
+          (hsPkgs."composite-base" or (errorHandler.buildDepError "composite-base"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."unordered-containers" or (errorHandler.buildDepError "unordered-containers"))
+          (hsPkgs."vector" or (errorHandler.buildDepError "vector"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "composite-casssava-test" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."cassava" or (errorHandler.buildDepError "cassava"))
+            (hsPkgs."composite-base" or (errorHandler.buildDepError "composite-base"))
+            (hsPkgs."composite-cassava" or (errorHandler.buildDepError "composite-cassava"))
+            (hsPkgs."tasty" or (errorHandler.buildDepError "tasty"))
+            (hsPkgs."tasty-hunit" or (errorHandler.buildDepError "tasty-hunit"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."unordered-containers" or (errorHandler.buildDepError "unordered-containers"))
+            (hsPkgs."vector" or (errorHandler.buildDepError "vector"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
---
+++ hackage/thread-utils-finalizers-0.1.0.0-r0-a8435240bfc0ae96c94704d2986699a11a395c496f9a7f2e5f5d729a0b967549.nix
@@ -0,0 +1,44 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.12";
+      identifier = { name = "thread-utils-finalizers"; version = "0.1.0.0"; };
+      license = "BSD-3-Clause";
+      copyright = "2021 Ian Duncan";
+      maintainer = "ian@iankduncan.com";
+      author = "Ian Duncan";
+      homepage = "https://github.com/iand675/thread-utils#readme";
+      url = "";
+      synopsis = "Perform finalization for threads.";
+      description = "Please see the README on GitHub at <https://github.com/iand675/thread-finalizers#readme>";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."ghc-prim" or (errorHandler.buildDepError "ghc-prim"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "thread-finalizers-test" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."ghc-prim" or (errorHandler.buildDepError "ghc-prim"))
+            (hsPkgs."thread-utils-finalizers" or (errorHandler.buildDepError "thread-utils-finalizers"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
