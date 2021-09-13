5b80e7e2d4509ca78a05307202f0bcf9cfc5a55c
IOHK <devops+nix-tools@iohk.io>
2021-09-13 01:11:54 +0000

    Automatic update for Mon Sep 13 01:11:53 UTC 2021

---
+++ hackage/evm-opcodes-0.1.0-r0-a3a987b4e90497bad01fc2c73567e0866c03074943cfe1f2095bb191d3e4dd22.nix
@@ -0,0 +1,58 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.12";
+      identifier = { name = "evm-opcodes"; version = "0.1.0"; };
+      license = "MIT";
+      copyright = "2020 Simon Shine";
+      maintainer = "shreddedglory@gmail.com";
+      author = "Simon Shine";
+      homepage = "https://github.com/sshine/evm-opcodes";
+      url = "";
+      synopsis = "Opcode types for Ethereum Virtual Machine (EVM)";
+      description = "This library provides opcode types for the Ethereum Virtual Machine.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."cereal" or (errorHandler.buildDepError "cereal"))
+          (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+          (hsPkgs."data-dword" or (errorHandler.buildDepError "data-dword"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "test" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."cereal" or (errorHandler.buildDepError "cereal"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."data-dword" or (errorHandler.buildDepError "data-dword"))
+            (hsPkgs."evm-opcodes" or (errorHandler.buildDepError "evm-opcodes"))
+            (hsPkgs."hedgehog" or (errorHandler.buildDepError "hedgehog"))
+            (hsPkgs."hspec" or (errorHandler.buildDepError "hspec"))
+            (hsPkgs."tasty" or (errorHandler.buildDepError "tasty"))
+            (hsPkgs."tasty-discover" or (errorHandler.buildDepError "tasty-discover"))
+            (hsPkgs."tasty-hedgehog" or (errorHandler.buildDepError "tasty-hedgehog"))
+            (hsPkgs."tasty-hspec" or (errorHandler.buildDepError "tasty-hspec"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
---
+++ hackage/libmdbx-0.2.1.0-r0-6c80e8272900ebad96a952b97d158bd0f710c3b63405b69a2e157ae26868c603.nix
@@ -0,0 +1,81 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.12";
+      identifier = { name = "libmdbx"; version = "0.2.1.0"; };
+      license = "BSD-3-Clause";
+      copyright = "2021 Francisco Vallarino";
+      maintainer = "fjvallarino@gmail.com";
+      author = "Francisco Vallarino";
+      homepage = "https://github.com/fjvallarino/libmdbx-hs#readme";
+      url = "";
+      synopsis = "Bindings for libmdbx, an embedded key/value store";
+      description = "Haskell bindings for [libmdbx](https://github.com/erthink/libmdbx).\n\nSee documentation in the main module or check the\n<https://github.com/fjvallarino/libmdbx-hs#readme README>\non GitHub.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."binary" or (errorHandler.buildDepError "binary"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."data-default" or (errorHandler.buildDepError "data-default"))
+          (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+          (hsPkgs."store" or (errorHandler.buildDepError "store"))
+          (hsPkgs."store-core" or (errorHandler.buildDepError "store-core"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+          ];
+        build-tools = [
+          (hsPkgs.buildPackages.c2hs.components.exes.c2hs or (pkgs.buildPackages.c2hs or (errorHandler.buildToolDepError "c2hs:c2hs")))
+          ];
+        buildable = true;
+        };
+      exes = {
+        "libmdbx-exe" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."binary" or (errorHandler.buildDepError "binary"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."data-default" or (errorHandler.buildDepError "data-default"))
+            (hsPkgs."libmdbx" or (errorHandler.buildDepError "libmdbx"))
+            (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+            (hsPkgs."store" or (errorHandler.buildDepError "store"))
+            (hsPkgs."store-core" or (errorHandler.buildDepError "store-core"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+            ];
+          buildable = true;
+          };
+        };
+      tests = {
+        "libmdbx-test" = {
+          depends = [
+            (hsPkgs."HUnit" or (errorHandler.buildDepError "HUnit"))
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."binary" or (errorHandler.buildDepError "binary"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."data-default" or (errorHandler.buildDepError "data-default"))
+            (hsPkgs."directory" or (errorHandler.buildDepError "directory"))
+            (hsPkgs."hspec" or (errorHandler.buildDepError "hspec"))
+            (hsPkgs."libmdbx" or (errorHandler.buildDepError "libmdbx"))
+            (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+            (hsPkgs."store" or (errorHandler.buildDepError "store"))
+            (hsPkgs."store-core" or (errorHandler.buildDepError "store-core"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
