cb3ae8958aa6f4a7a132c52a444d1c9cd7c338c2
IOHK <devops+nix-tools@iohk.io>
2021-09-20 01:12:30 +0000

    Automatic update for Mon Sep 20 01:12:29 UTC 2021

---
+++ hackage/githash-0.1.6.2-r0-9a136fa54b3e71e7a90d9dcb62259ffe6d55d083556ab2f15027dd4a4109d41d.nix
@@ -0,0 +1,57 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.12";
+      identifier = { name = "githash"; version = "0.1.6.2"; };
+      license = "BSD-3-Clause";
+      copyright = "";
+      maintainer = "michael@snoyman.com";
+      author = "Michael Snoyman, Adam C. Foltzer";
+      homepage = "https://github.com/snoyberg/githash#readme";
+      url = "";
+      synopsis = "Compile git revision info into Haskell projects";
+      description = "Please see the README and documentation at <https://www.stackage.org/package/githash>";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."directory" or (errorHandler.buildDepError "directory"))
+          (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
+          (hsPkgs."process" or (errorHandler.buildDepError "process"))
+          (hsPkgs."template-haskell" or (errorHandler.buildDepError "template-haskell"))
+          (hsPkgs."th-compat" or (errorHandler.buildDepError "th-compat"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "githash-spec" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."directory" or (errorHandler.buildDepError "directory"))
+            (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
+            (hsPkgs."githash" or (errorHandler.buildDepError "githash"))
+            (hsPkgs."hspec" or (errorHandler.buildDepError "hspec"))
+            (hsPkgs."process" or (errorHandler.buildDepError "process"))
+            (hsPkgs."template-haskell" or (errorHandler.buildDepError "template-haskell"))
+            (hsPkgs."temporary" or (errorHandler.buildDepError "temporary"))
+            (hsPkgs."th-compat" or (errorHandler.buildDepError "th-compat"))
+            (hsPkgs."unliftio" or (errorHandler.buildDepError "unliftio"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
---
+++ hackage/trust-chain-0.1.2.0-r0-05174242b6617f61047129b9ab2976efcf86a83ae22792d5e7e98768686aead3.nix
@@ -0,0 +1,54 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "2.4";
+      identifier = { name = "trust-chain"; version = "0.1.2.0"; };
+      license = "MIT";
+      copyright = "";
+      maintainer = "sgschlesinger@gmail.com";
+      author = "Samuel Schlesinger";
+      homepage = "";
+      url = "";
+      synopsis = "An implementation of a trust chain";
+      description = "An implementation of a trust chain.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."network" or (errorHandler.buildDepError "network"))
+          (hsPkgs."binary" or (errorHandler.buildDepError "binary"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+          (hsPkgs."cropty" or (errorHandler.buildDepError "cropty"))
+          (hsPkgs."merge" or (errorHandler.buildDepError "merge"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "test" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."trust-chain" or (errorHandler.buildDepError "trust-chain"))
+            (hsPkgs."cropty" or (errorHandler.buildDepError "cropty"))
+            (hsPkgs."merge" or (errorHandler.buildDepError "merge"))
+            (hsPkgs."binary" or (errorHandler.buildDepError "binary"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
