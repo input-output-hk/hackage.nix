0f74b171a22842063e58de24be042480fe82d1af
IOHK <devops+nix-tools@iohk.io>
2021-09-21 01:16:37 +0000

    Automatic update for Tue Sep 21 01:16:36 UTC 2021

---
+++ hackage/fuzzy-0.1.0.1-r0-7e1ba6e868034e7872bc42aaab64f39e0d942e33c68d007bbb7895eb2d28a4b6.nix
@@ -0,0 +1,44 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "fuzzy"; version = "0.1.0.1"; };
+      license = "MIT";
+      copyright = "";
+      maintainer = "joomy@cattheory.com";
+      author = "Joomy Korkut";
+      homepage = "http://github.com/joom/fuzzy";
+      url = "";
+      synopsis = "Filters a list based on a fuzzy string search.";
+      description = "";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."monoid-subclasses" or (errorHandler.buildDepError "monoid-subclasses"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "tests" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."fuzzy" or (errorHandler.buildDepError "fuzzy"))
+            (hsPkgs."HUnit" or (errorHandler.buildDepError "HUnit"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
