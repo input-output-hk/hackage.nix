3190695d3e697f1394c8f1d7621a5d7d613537bd
IOHK <devops+nix-tools@iohk.io>
2021-09-23 01:12:12 +0000

    Automatic update for Thu Sep 23 01:12:11 UTC 2021

---
+++ hackage/polysemy-RandomFu-0.4.3.0-r0-9e7f6ca7dd4bb8ab7711aa15cfbbfc77d39ce92eac258673b972574fd1b74672.nix
@@ -0,0 +1,58 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.12";
+      identifier = { name = "polysemy-RandomFu"; version = "0.4.3.0"; };
+      license = "BSD-3-Clause";
+      copyright = "2019 Adam Conner-Sax";
+      maintainer = "adam_conner_sax@yahoo.com";
+      author = "Adam Conner-Sax";
+      homepage = "https://github.com/adamConnerSax/polysemy-Extra#readme";
+      url = "";
+      synopsis = "Experimental, RandomFu effect and interpreters for polysemy";
+      description = "Please see the README on GitHub at <https://github.com/adamConnerSax/polysemy-Extra/tree/master/polysemy-RandomFu#polysemy-randomfu>";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."polysemy" or (errorHandler.buildDepError "polysemy"))
+          (hsPkgs."polysemy-plugin" or (errorHandler.buildDepError "polysemy-plugin"))
+          (hsPkgs."polysemy-zoo" or (errorHandler.buildDepError "polysemy-zoo"))
+          (hsPkgs."random-fu" or (errorHandler.buildDepError "random-fu"))
+          (hsPkgs."random-source" or (errorHandler.buildDepError "random-source"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "polysemy-RandomFu-test" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."hspec" or (errorHandler.buildDepError "hspec"))
+            (hsPkgs."polysemy" or (errorHandler.buildDepError "polysemy"))
+            (hsPkgs."polysemy-RandomFu" or (errorHandler.buildDepError "polysemy-RandomFu"))
+            (hsPkgs."polysemy-plugin" or (errorHandler.buildDepError "polysemy-plugin"))
+            (hsPkgs."polysemy-zoo" or (errorHandler.buildDepError "polysemy-zoo"))
+            (hsPkgs."random-fu" or (errorHandler.buildDepError "random-fu"))
+            (hsPkgs."random-source" or (errorHandler.buildDepError "random-source"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."vector" or (errorHandler.buildDepError "vector"))
+            ];
+          build-tools = [
+            (hsPkgs.buildPackages.hspec-discover.components.exes.hspec-discover or (pkgs.buildPackages.hspec-discover or (errorHandler.buildToolDepError "hspec-discover:hspec-discover")))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
