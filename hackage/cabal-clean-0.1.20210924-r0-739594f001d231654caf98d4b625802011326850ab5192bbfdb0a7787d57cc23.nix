ac7a98d37b5ed0a2a6519eed16837ac495012158
IOHK <devops+nix-tools@iohk.io>
2021-09-25 01:11:35 +0000

    Automatic update for Sat Sep 25 01:11:34 UTC 2021

---
+++ hackage/cabal-clean-0.1.20210924-r0-739594f001d231654caf98d4b625802011326850ab5192bbfdb0a7787d57cc23.nix
@@ -0,0 +1,44 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "cabal-clean"; version = "0.1.20210924"; };
+      license = "BSD-3-Clause";
+      copyright = "Andreas Abel, 2021";
+      maintainer = "Andreas Abel <andreas.abel@cse.gu.se>";
+      author = "Andreas Abel";
+      homepage = "https://github.com/andreasabel/cabal-clean";
+      url = "";
+      synopsis = "Remove outdated cabal build artefacts from `dist-newstyle`.";
+      description = "Simple command line tool to remove cabal build artefacts\nfrom `dist-newstyle/build` that are superseded by newer\nversions of the built package or the Haskell compiler.";
+      buildType = "Simple";
+      };
+    components = {
+      exes = {
+        "cabal-clean" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."directory" or (errorHandler.buildDepError "directory"))
+            (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
+            (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+            (hsPkgs."optparse-applicative" or (errorHandler.buildDepError "optparse-applicative"))
+            (hsPkgs."pretty-terminal" or (errorHandler.buildDepError "pretty-terminal"))
+            (hsPkgs."process" or (errorHandler.buildDepError "process"))
+            (hsPkgs."split" or (errorHandler.buildDepError "split"))
+            (hsPkgs."string-qq" or (errorHandler.buildDepError "string-qq"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
---
+++ hackage/docopt-0.7.0.7-r0-a3d2eac54cd77d8c0b306ff96fb57be55542f143d81766aa1ae51458ad790dbe.nix
@@ -0,0 +1,53 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = { template-haskell = true; };
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "docopt"; version = "0.7.0.7"; };
+      license = "MIT";
+      copyright = "(c) 2013-2015 Ryan Artecona";
+      maintainer = "ryanartecona@gmail.com";
+      author = "Ryan Artecona";
+      homepage = "https://github.com/docopt/docopt.hs";
+      url = "";
+      synopsis = "A command-line interface parser that will make you smile";
+      description = "Docopt parses command-line interface usage text that adheres to a familiar syntax, and from it builds a command-line argument parser that will ensure your program is invoked correctly with the available options specified in the usage text. This allows the developer to write a usage text and get an argument parser for free.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."parsec" or (errorHandler.buildDepError "parsec"))
+          (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+          ] ++ (pkgs.lib).optional (compiler.isGhc && (compiler.version).ge "6.10" && flags.template-haskell) (hsPkgs."template-haskell" or (errorHandler.buildDepError "template-haskell"));
+        buildable = true;
+        };
+      tests = {
+        "tests" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."parsec" or (errorHandler.buildDepError "parsec"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."docopt" or (errorHandler.buildDepError "docopt"))
+            (hsPkgs."HUnit" or (errorHandler.buildDepError "HUnit"))
+            (hsPkgs."split" or (errorHandler.buildDepError "split"))
+            (hsPkgs."ansi-terminal" or (errorHandler.buildDepError "ansi-terminal"))
+            (hsPkgs."aeson" or (errorHandler.buildDepError "aeson"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."template-haskell" or (errorHandler.buildDepError "template-haskell"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
