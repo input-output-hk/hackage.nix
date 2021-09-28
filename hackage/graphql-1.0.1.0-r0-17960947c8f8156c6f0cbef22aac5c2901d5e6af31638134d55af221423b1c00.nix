21a7727ca1dac5c3e96a9c21c7e0e7a29c1501b5
IOHK <devops+nix-tools@iohk.io>
2021-09-28 01:12:12 +0000

    Automatic update for Tue Sep 28 01:12:11 UTC 2021

---
+++ hackage/graphql-1.0.1.0-r0-17960947c8f8156c6f0cbef22aac5c2901d5e6af31638134d55af221423b1c00.nix
@@ -0,0 +1,65 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "2.2";
+      identifier = { name = "graphql"; version = "1.0.1.0"; };
+      license = "MPL-2.0 AND BSD-3-Clause";
+      copyright = "(c) 2019-2021 Eugen Wissner,\n(c) 2015-2017 J. Daniel Navarro";
+      maintainer = "belka@caraus.de";
+      author = "Danny Navarro <j@dannynavarro.net>,\nMatthías Páll Gissurarson <mpg@mpg.is>,\nSólrún Halla Einarsdóttir <she@mpg.is>";
+      homepage = "https://www.caraus.tech/projects/pub-graphql";
+      url = "";
+      synopsis = "Haskell GraphQL implementation";
+      description = "Haskell <https://spec.graphql.org/June2018/ GraphQL> implementation.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."aeson" or (errorHandler.buildDepError "aeson"))
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."conduit" or (errorHandler.buildDepError "conduit"))
+          (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+          (hsPkgs."exceptions" or (errorHandler.buildDepError "exceptions"))
+          (hsPkgs."hspec-expectations" or (errorHandler.buildDepError "hspec-expectations"))
+          (hsPkgs."megaparsec" or (errorHandler.buildDepError "megaparsec"))
+          (hsPkgs."parser-combinators" or (errorHandler.buildDepError "parser-combinators"))
+          (hsPkgs."scientific" or (errorHandler.buildDepError "scientific"))
+          (hsPkgs."template-haskell" or (errorHandler.buildDepError "template-haskell"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+          (hsPkgs."unordered-containers" or (errorHandler.buildDepError "unordered-containers"))
+          (hsPkgs."vector" or (errorHandler.buildDepError "vector"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "graphql-test" = {
+          depends = [
+            (hsPkgs."QuickCheck" or (errorHandler.buildDepError "QuickCheck"))
+            (hsPkgs."aeson" or (errorHandler.buildDepError "aeson"))
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."conduit" or (errorHandler.buildDepError "conduit"))
+            (hsPkgs."exceptions" or (errorHandler.buildDepError "exceptions"))
+            (hsPkgs."graphql" or (errorHandler.buildDepError "graphql"))
+            (hsPkgs."hspec" or (errorHandler.buildDepError "hspec"))
+            (hsPkgs."hspec-megaparsec" or (errorHandler.buildDepError "hspec-megaparsec"))
+            (hsPkgs."megaparsec" or (errorHandler.buildDepError "megaparsec"))
+            (hsPkgs."scientific" or (errorHandler.buildDepError "scientific"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."unordered-containers" or (errorHandler.buildDepError "unordered-containers"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
---
+++ hackage/push-notify-apn-0.3.0.1-r0-a4fbba893275d2dbe436aa1eee939d1763526bb9685f435f7a0251c95510f71b.nix
@@ -0,0 +1,76 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "push-notify-apn"; version = "0.3.0.1"; };
+      license = "BSD-3-Clause";
+      copyright = "2017-2020 Memrange UG";
+      maintainer = "hc@hcesperer.org";
+      author = "Hans-Christian Esperer";
+      homepage = "https://github.com/memrange/apn#readme";
+      url = "";
+      synopsis = "Send push notifications to mobile iOS devices";
+      description = "push-notify-apn is a library and command line utility that can be used to send\npush notifications to mobile devices running iOS. Push notifications are small\nmessages that can be sent to apps on smart phones and tablets\nwithout the need to keep open a long lived TCP connection per app, dramatically\nreducing the power consumption in standby mode.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."aeson" or (errorHandler.buildDepError "aeson"))
+          (hsPkgs."base16-bytestring" or (errorHandler.buildDepError "base16-bytestring"))
+          (hsPkgs."binary" or (errorHandler.buildDepError "binary"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+          (hsPkgs."data-default" or (errorHandler.buildDepError "data-default"))
+          (hsPkgs."http2" or (errorHandler.buildDepError "http2"))
+          (hsPkgs."http2-client" or (errorHandler.buildDepError "http2-client"))
+          (hsPkgs."lifted-base" or (errorHandler.buildDepError "lifted-base"))
+          (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+          (hsPkgs."random" or (errorHandler.buildDepError "random"))
+          (hsPkgs."resource-pool" or (errorHandler.buildDepError "resource-pool"))
+          (hsPkgs."semigroups" or (errorHandler.buildDepError "semigroups"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."time" or (errorHandler.buildDepError "time"))
+          (hsPkgs."tls" or (errorHandler.buildDepError "tls"))
+          (hsPkgs."x509" or (errorHandler.buildDepError "x509"))
+          (hsPkgs."x509-store" or (errorHandler.buildDepError "x509-store"))
+          (hsPkgs."x509-system" or (errorHandler.buildDepError "x509-system"))
+          ];
+        buildable = true;
+        };
+      exes = {
+        "sendapn" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."push-notify-apn" or (errorHandler.buildDepError "push-notify-apn"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."optparse-applicative" or (errorHandler.buildDepError "optparse-applicative"))
+            (hsPkgs."semigroups" or (errorHandler.buildDepError "semigroups"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            ];
+          buildable = true;
+          };
+        };
+      tests = {
+        "apn-test" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."push-notify-apn" or (errorHandler.buildDepError "push-notify-apn"))
+            (hsPkgs."aeson" or (errorHandler.buildDepError "aeson"))
+            (hsPkgs."hspec" or (errorHandler.buildDepError "hspec"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
