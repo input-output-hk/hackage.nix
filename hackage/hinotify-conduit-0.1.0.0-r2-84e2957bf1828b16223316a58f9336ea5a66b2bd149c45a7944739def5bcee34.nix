e10364d79e018332b243f233de7f11554c567203
IOHK <devops+nix-tools@iohk.io>
2021-10-04 01:11:18 +0000

    Automatic update for Mon Oct  4 01:11:17 UTC 2021

---
+++ hackage/hinotify-conduit-0.1.0.0-r2-84e2957bf1828b16223316a58f9336ea5a66b2bd149c45a7944739def5bcee34.nix
@@ -0,0 +1,60 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.12";
+      identifier = { name = "hinotify-conduit"; version = "0.1.0.0"; };
+      license = "BSD-3-Clause";
+      copyright = "2021 Jiri Marsicek";
+      maintainer = "jiri.marsicek@gmail.com";
+      author = "Jiri Marsicek";
+      homepage = "https://github.com/j1r1k/hinotify-conduit#readme";
+      url = "";
+      synopsis = "inotify conduit sources";
+      description = "Please see the README on GitHub at <https://github.com/j1r1k/hinotify-conduit#readme>";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."conduit" or (errorHandler.buildDepError "conduit"))
+          (hsPkgs."filepath-bytestring" or (errorHandler.buildDepError "filepath-bytestring"))
+          (hsPkgs."hinotify" or (errorHandler.buildDepError "hinotify"))
+          (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+          (hsPkgs."resourcet" or (errorHandler.buildDepError "resourcet"))
+          (hsPkgs."stm" or (errorHandler.buildDepError "stm"))
+          (hsPkgs."stm-chans" or (errorHandler.buildDepError "stm-chans"))
+          (hsPkgs."stm-conduit" or (errorHandler.buildDepError "stm-conduit"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "hinotify-conduit-test" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."conduit" or (errorHandler.buildDepError "conduit"))
+            (hsPkgs."filepath-bytestring" or (errorHandler.buildDepError "filepath-bytestring"))
+            (hsPkgs."hinotify" or (errorHandler.buildDepError "hinotify"))
+            (hsPkgs."hinotify-conduit" or (errorHandler.buildDepError "hinotify-conduit"))
+            (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+            (hsPkgs."resourcet" or (errorHandler.buildDepError "resourcet"))
+            (hsPkgs."stm" or (errorHandler.buildDepError "stm"))
+            (hsPkgs."stm-chans" or (errorHandler.buildDepError "stm-chans"))
+            (hsPkgs."stm-conduit" or (errorHandler.buildDepError "stm-conduit"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
