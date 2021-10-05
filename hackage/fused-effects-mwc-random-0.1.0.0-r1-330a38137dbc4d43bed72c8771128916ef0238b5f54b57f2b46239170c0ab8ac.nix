0b33cf7ca5f152a6b3acda375433a6bc86f8d3e7
IOHK <devops+nix-tools@iohk.io>
2021-10-05 01:12:13 +0000

    Automatic update for Tue Oct  5 01:12:11 UTC 2021

---
+++ hackage/fused-effects-mwc-random-0.1.0.0-r1-330a38137dbc4d43bed72c8771128916ef0238b5f54b57f2b46239170c0ab8ac.nix
@@ -0,0 +1,62 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "2.4";
+      identifier = { name = "fused-effects-mwc-random"; version = "0.1.0.0"; };
+      license = "BSD-3-Clause";
+      copyright = "2020 Patrick Thomson";
+      maintainer = "patrickt@github.com";
+      author = "Patrick Thomson";
+      homepage = "https://github.com/fused-effects/fused-effects-mwc-random";
+      url = "";
+      synopsis = "High-quality random number generation as an effect.";
+      description = "This package wraps the mwc-random package and provides a convenient interface to invoke its functions from an effect stack.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."fused-effects" or (errorHandler.buildDepError "fused-effects"))
+          (hsPkgs."mwc-random" or (errorHandler.buildDepError "mwc-random"))
+          (hsPkgs."vector" or (errorHandler.buildDepError "vector"))
+          (hsPkgs."primitive" or (errorHandler.buildDepError "primitive"))
+          (hsPkgs."template-haskell" or (errorHandler.buildDepError "template-haskell"))
+          (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "test" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."fused-effects" or (errorHandler.buildDepError "fused-effects"))
+            (hsPkgs."fused-effects-mwc-random" or (errorHandler.buildDepError "fused-effects-mwc-random"))
+            (hsPkgs."mwc-random" or (errorHandler.buildDepError "mwc-random"))
+            (hsPkgs."vector" or (errorHandler.buildDepError "vector"))
+            ];
+          buildable = true;
+          };
+        };
+      benchmarks = {
+        "bench" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."gauge" or (errorHandler.buildDepError "gauge"))
+            (hsPkgs."fused-effects-random" or (errorHandler.buildDepError "fused-effects-random"))
+            (hsPkgs."fused-effects-mwc-random" or (errorHandler.buildDepError "fused-effects-mwc-random"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
