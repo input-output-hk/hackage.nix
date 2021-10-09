3a1868182ac8bdd024843fba6ce72cfcbf429ebc
IOHK <devops+nix-tools@iohk.io>
2021-10-09 01:13:23 +0000

    Automatic update for Sat Oct  9 01:13:21 UTC 2021

---
+++ hackage/SpaceInvaders-0.13.3-r0-60384cbf3d3475e82f3f8d6f38f4c915a65084907f61268744063304e4b225ee.nix
@@ -0,0 +1,41 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "SpaceInvaders"; version = "0.13.3"; };
+      license = "BSD-3-Clause";
+      copyright = "";
+      maintainer = "Ivan Perez <ivan.perez@keera.co.uk>";
+      author = "Henrik Nilsson, Antony Courtney";
+      homepage = "http://www.haskell.org/yampa/";
+      url = "";
+      synopsis = "Video game";
+      description = "Video game implemented in Yampa.\n\n<<https://github.com/ivanperez-keera/SpaceInvaders/blob/develop/screenshots/gameplay.gif?raw=true>>";
+      buildType = "Simple";
+      };
+    components = {
+      exes = {
+        "spaceInvaders" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."array" or (errorHandler.buildDepError "array"))
+            (hsPkgs."random" or (errorHandler.buildDepError "random"))
+            (hsPkgs."HGL" or (errorHandler.buildDepError "HGL"))
+            (hsPkgs."Yampa" or (errorHandler.buildDepError "Yampa"))
+            (hsPkgs."simple-affine-space" or (errorHandler.buildDepError "simple-affine-space"))
+            (hsPkgs."deepseq" or (errorHandler.buildDepError "deepseq"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
---
+++ hackage/emojis-0.1.2-r0-3932d99972b998464beb4d67eddebeb00a21dab3e3b2d68bdb996e5cb0ccbc26.nix
@@ -0,0 +1,46 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "emojis"; version = "0.1.2"; };
+      license = "BSD-3-Clause";
+      copyright = "2019 John MacFarlane";
+      maintainer = "jgm@berkeley.edu";
+      author = "John MacFarlane";
+      homepage = "https://github.com/jgm/emojis#readme";
+      url = "";
+      synopsis = "Conversion between emoji characters and their names.";
+      description = "This package provides functions for converting\nemoji names to emoji characters and vice versa.\n\nHow does it differ from the @emoji@ package?\n\n-   It supports a fuller range of emojis, including all those\nsupported by GitHub\n-   It supports lookup of emoji aliases from emoji\n-   It uses Text rather than String\n-   It has a lighter dependency footprint: in particular, it\ndoes not require aeson\n-   It does not require TemplateHaskell";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "emojis-test" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."emojis" or (errorHandler.buildDepError "emojis"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."HUnit" or (errorHandler.buildDepError "HUnit"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
