0ee6f33d656369408cfd691db459cd6b865f9d0a
IOHK <devops+nix-tools@iohk.io>
2021-10-14 01:12:27 +0000

    Automatic update for Thu Oct 14 01:12:26 UTC 2021

---
+++ hackage/tokyocabinet-haskell-0.0.2-r1-9a982fa455096deec467adb34b41c286909cd09a1a4b0b7803ef617212acba53.nix
@@ -0,0 +1,38 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.2";
+      identifier = { name = "tokyocabinet-haskell"; version = "0.0.2"; };
+      license = "BSD-3-Clause";
+      copyright = "";
+      maintainer = "tom.lpsd@gmail.com";
+      author = "Tom Tsuruhara";
+      homepage = "";
+      url = "";
+      synopsis = "Haskell binding of Tokyo Cabinet";
+      description = "Bindings to Tokyo Cabinet library.\nTokyo Cabinet is a modern implementation of DBM.\nFor more about Tokyo Cabinet, see <http://tokyocabinet.sourceforge.net/index.html>.\nThis package provides `tokyocabinet.idl compliant' naive interfaces.\nSee, <http://tokyocabinet.sourceforge.net/tokyocabinet.idl>.\nMap interface and TDB have not supported yet.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+          ];
+        libs = [
+          (pkgs."tokyocabinet" or (errorHandler.sysDepError "tokyocabinet"))
+          ];
+        buildable = true;
+        };
+      };
+    }
---
+++ hackage/tokyocabinet-haskell-0.0.4-r1-8add5af282543ff198f708bc4748d955dc4547dfd6fbff979f92a69191557765.nix
@@ -0,0 +1,38 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = { buildtest = false; };
+    package = {
+      specVersion = "1.2";
+      identifier = { name = "tokyocabinet-haskell"; version = "0.0.4"; };
+      license = "BSD-3-Clause";
+      copyright = "";
+      maintainer = "tom.lpsd@gmail.com";
+      author = "Tom Tsuruhara";
+      homepage = "http://tom-lpsd.github.com/tokyocabinet-haskell/";
+      url = "";
+      synopsis = "Haskell binding of Tokyo Cabinet";
+      description = "Bindings to Tokyo Cabinet library.\nTokyo Cabinet is a modern implementation of DBM.\nFor more about Tokyo Cabinet, see <http://tokyocabinet.sourceforge.net/index.html>.\nThis package provides `tokyocabinet.idl compliant' naive interfaces.\nSee, <http://tokyocabinet.sourceforge.net/tokyocabinet.idl>.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+          ];
+        libs = [
+          (pkgs."tokyocabinet" or (errorHandler.sysDepError "tokyocabinet"))
+          ];
+        buildable = true;
+        };
+      };
+    }
