9890fbf353b9e26962eb5d37c728879e58075321
IOHK <devops+nix-tools@iohk.io>
2021-10-17 01:12:15 +0000

    Automatic update for Sun Oct 17 01:12:14 UTC 2021

---
+++ hackage/ghc-datasize-0.2.5-r0-bc24a756ecd6162e9ac32fcd9c1cd35ee284d327f30fb02f0c812e15837e97c9.nix
@@ -0,0 +1,35 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "ghc-datasize"; version = "0.2.5"; };
+      license = "BSD-3-Clause";
+      copyright = "Dennis Felsing 2012-2019";
+      maintainer = "Dennis Felsing <dennis@felsin9.de>";
+      author = "Dennis Felsing <dennis@felsin9.de>";
+      homepage = "http://felsin9.de/nnis/ghc-datasize";
+      url = "";
+      synopsis = "Determine the size of data structures in GHC's memory";
+      description = "ghc-datasize is a tool to determine the size of data\nstructures in GHC's memory. Determining the size of\nrecursive data structures is supported. All sizes are in\nBytes.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."deepseq" or (errorHandler.buildDepError "deepseq"))
+          (hsPkgs."ghc-prim" or (errorHandler.buildDepError "ghc-prim"))
+          ];
+        buildable = true;
+        };
+      };
+    }
---
+++ hackage/tldr-0.9.2-r0-d9f6f7b0ee25c14afd40be7a1f883b6684a97a269701d7087c9ba7e733e571ed.nix
@@ -0,0 +1,66 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = { static = false; };
+    package = {
+      specVersion = "1.12";
+      identifier = { name = "tldr"; version = "0.9.2"; };
+      license = "BSD-3-Clause";
+      copyright = "2017 Sibi Prabakaran";
+      maintainer = "sibi@psibi.in";
+      author = "Sibi Prabakaran";
+      homepage = "https://github.com/psibi/tldr-hs#readme";
+      url = "";
+      synopsis = "Haskell tldr client";
+      description = "Haskell tldr client with support for viewing tldr pages. Has offline\ncache for accessing pages. Visit https://tldr.sh for more details.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."ansi-terminal" or (errorHandler.buildDepError "ansi-terminal"))
+          (hsPkgs."attoparsec" or (errorHandler.buildDepError "attoparsec"))
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."cmark" or (errorHandler.buildDepError "cmark"))
+          (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+          (hsPkgs."directory" or (errorHandler.buildDepError "directory"))
+          (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
+          (hsPkgs."http-conduit" or (errorHandler.buildDepError "http-conduit"))
+          (hsPkgs."optparse-applicative" or (errorHandler.buildDepError "optparse-applicative"))
+          (hsPkgs."semigroups" or (errorHandler.buildDepError "semigroups"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."time" or (errorHandler.buildDepError "time"))
+          (hsPkgs."zip-archive" or (errorHandler.buildDepError "zip-archive"))
+          ];
+        buildable = true;
+        };
+      exes = {
+        "tldr" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."tldr" or (errorHandler.buildDepError "tldr"))
+            ];
+          buildable = true;
+          };
+        };
+      tests = {
+        "tldr-test" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."tasty" or (errorHandler.buildDepError "tasty"))
+            (hsPkgs."tasty-golden" or (errorHandler.buildDepError "tasty-golden"))
+            (hsPkgs."tldr" or (errorHandler.buildDepError "tldr"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
