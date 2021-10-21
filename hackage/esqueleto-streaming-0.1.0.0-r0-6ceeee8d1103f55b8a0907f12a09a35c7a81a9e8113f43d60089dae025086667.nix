8dc4c2cd845c32242379b0c8e9c266f6f274328e
IOHK <devops+nix-tools@iohk.io>
2021-10-21 01:12:41 +0000

    Automatic update for Thu Oct 21 01:12:40 UTC 2021

---
+++ hackage/esqueleto-streaming-0.1.0.0-r0-6ceeee8d1103f55b8a0907f12a09a35c7a81a9e8113f43d60089dae025086667.nix
@@ -0,0 +1,39 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "esqueleto-streaming"; version = "0.1.0.0"; };
+      license = "BSD-3-Clause";
+      copyright = "(c) 2021 Supercede Ltd.";
+      maintainer = "support@supercede.com";
+      author = "Isaac van Bakel";
+      homepage = "https://github.com/SupercedeTech/persistent-postgresql-streaming#readme";
+      url = "";
+      synopsis = "Memory-constant streaming of Esqueleto results from PostgreSQL";
+      description = "This library provides a set of APIs for performing queries in Esqueleto\nin constant memory, streaming the results using @conduit@.\n\nThe library relies on PostgreSQL-specific features to avoid loading all the\nresults of a query into memory at once. This allows for accessing results of\nmillions of rows from Haskell without a memory blow-up.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."conduit" or (errorHandler.buildDepError "conduit"))
+          (hsPkgs."esqueleto" or (errorHandler.buildDepError "esqueleto"))
+          (hsPkgs."persistent-postgresql-streaming" or (errorHandler.buildDepError "persistent-postgresql-streaming"))
+          (hsPkgs."persistent-postgresql" or (errorHandler.buildDepError "persistent-postgresql"))
+          (hsPkgs."resourcet" or (errorHandler.buildDepError "resourcet"))
+          (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+          ];
+        buildable = true;
+        };
+      };
+    }
---
+++ hackage/persistent-postgresql-streaming-0.1.0.0-r0-6a6615e79ca0de43a2160bc3a420717b3320f3dde4ba0765586655995fa7650a.nix
@@ -0,0 +1,45 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.10";
+      identifier = {
+        name = "persistent-postgresql-streaming";
+        version = "0.1.0.0";
+        };
+      license = "BSD-3-Clause";
+      copyright = "(c) 2021 Supercede Ltd.";
+      maintainer = "support@supercede.com";
+      author = "Isaac van Bakel";
+      homepage = "https://github.com/SupercedeTech/persistent-postgresql-streaming#readme";
+      url = "";
+      synopsis = "Memory-constant streaming of Persistent entities from PostgreSQL";
+      description = "This library provides a set of APIs for performing queries on Persistent\nentities in constant memory, streaming the results using @conduit@.\n\nThe library relies on PostgreSQL-specific features to avoid loading all the\nresults of a query into memory at once. This allows for accessing tables of\nmillions of entities from Haskell without a memory blow-up.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."conduit" or (errorHandler.buildDepError "conduit"))
+          (hsPkgs."monad-logger" or (errorHandler.buildDepError "monad-logger"))
+          (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+          (hsPkgs."persistent" or (errorHandler.buildDepError "persistent"))
+          (hsPkgs."persistent-postgresql" or (errorHandler.buildDepError "persistent-postgresql"))
+          (hsPkgs."postgresql-simple" or (errorHandler.buildDepError "postgresql-simple"))
+          (hsPkgs."resourcet" or (errorHandler.buildDepError "resourcet"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+          ];
+        buildable = true;
+        };
+      };
+    }
