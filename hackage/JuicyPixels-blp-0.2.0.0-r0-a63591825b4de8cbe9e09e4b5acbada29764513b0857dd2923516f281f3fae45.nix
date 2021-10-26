18b5139f31c35bb58f1a9e197ddcfffce8083412
IOHK <devops+nix-tools@iohk.io>
2021-10-26 01:11:38 +0000

    Automatic update for Tue Oct 26 01:11:37 UTC 2021

---
+++ hackage/JuicyPixels-blp-0.2.0.0-r0-a63591825b4de8cbe9e09e4b5acbada29764513b0857dd2923516f281f3fae45.nix
@@ -0,0 +1,56 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "JuicyPixels-blp"; version = "0.2.0.0"; };
+      license = "BSD-3-Clause";
+      copyright = "2015-2018 Anton Gushcha";
+      maintainer = "ncrashed@gmail.com";
+      author = "Anton Gushcha";
+      homepage = "http://github.com/NCrashed/JuicyPixels-blp#readme";
+      url = "";
+      synopsis = "BLP format decoder/encoder over JuicyPixels library";
+      description = "The library provides decoding/encoding functions for BLP1 texture format of Warcraft III game.\nThe result is represented in types of [JuicyPixels](http://hackage.haskell.org/package/JuicyPixels) library.\nPlease see README.md for detailed description.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."attoparsec" or (errorHandler.buildDepError "attoparsec"))
+          (hsPkgs."binary" or (errorHandler.buildDepError "binary"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."hashable" or (errorHandler.buildDepError "hashable"))
+          (hsPkgs."JuicyPixels" or (errorHandler.buildDepError "JuicyPixels"))
+          (hsPkgs."text-show" or (errorHandler.buildDepError "text-show"))
+          (hsPkgs."vector" or (errorHandler.buildDepError "vector"))
+          ];
+        buildable = true;
+        };
+      exes = {
+        "blp2any" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."directory" or (errorHandler.buildDepError "directory"))
+            (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
+            (hsPkgs."JuicyPixels" or (errorHandler.buildDepError "JuicyPixels"))
+            (hsPkgs."JuicyPixels-blp" or (errorHandler.buildDepError "JuicyPixels-blp"))
+            (hsPkgs."optparse-simple" or (errorHandler.buildDepError "optparse-simple"))
+            (hsPkgs."text-show" or (errorHandler.buildDepError "text-show"))
+            (hsPkgs."unordered-containers" or (errorHandler.buildDepError "unordered-containers"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
---
+++ hackage/gopher-proxy-0.1.1.3-r0-6028d960b777e548346e598d06d4f4af4e5b8a8f8bb84dc5622b85a5784f790d.nix
@@ -0,0 +1,47 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "gopher-proxy"; version = "0.1.1.3"; };
+      license = "GPL-3.0-only";
+      copyright = "";
+      maintainer = "sternenseemann@systemli.org";
+      author = "sternenseemann";
+      homepage = "https://github.com/sternenseemann/gopher-proxy";
+      url = "";
+      synopsis = "proxy gopher over http";
+      description = "@gopher-proxy@ allows to proxy gopher over HTTP, which is mainly useful for HTTP-ifying a specific gopher space.\n\nA simple invocation looks like this:\n\n@\ngopher-proxy --host example.org --http-port 8080\n@\n\nIn this particular example @gopher-proxy@ does the following things:\n\n* Takes HTTP requests on @127.0.0.1:8080@ (to be used by a proxying web server, like nginx), converts those to gopher requests to @example.org@ and returns the gopher responses as HTTP responses\n\n* Links menu items pointing to external servers to @gopher:\\/\\/@ URLs (this ensures that only one gopher server is proxied) and recognizes <https://en.wikipedia.org/wiki/Gopher_(protocol)#URL_links URL links> which are converted to normal HTML links\n\n* Tries to guess the correct mime-type for every gopher-served file (as it is not included in the response) and wraps text files in a HTML container.\n\n* And generally works hard to offer the best HTTP equivalent of a given gopher space :)\n\nTo learn about the other parameters for tweaking the behavior of @gopher-proxy@ <#readme see the readme>.";
+      buildType = "Simple";
+      };
+    components = {
+      exes = {
+        "gopher-proxy" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."wai" or (errorHandler.buildDepError "wai"))
+            (hsPkgs."warp" or (errorHandler.buildDepError "warp"))
+            (hsPkgs."http-types" or (errorHandler.buildDepError "http-types"))
+            (hsPkgs."lucid" or (errorHandler.buildDepError "lucid"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."network" or (errorHandler.buildDepError "network"))
+            (hsPkgs."attoparsec" or (errorHandler.buildDepError "attoparsec"))
+            (hsPkgs."errors" or (errorHandler.buildDepError "errors"))
+            (hsPkgs."mime-types" or (errorHandler.buildDepError "mime-types"))
+            (hsPkgs."optparse-applicative" or (errorHandler.buildDepError "optparse-applicative"))
+            (hsPkgs."directory" or (errorHandler.buildDepError "directory"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
