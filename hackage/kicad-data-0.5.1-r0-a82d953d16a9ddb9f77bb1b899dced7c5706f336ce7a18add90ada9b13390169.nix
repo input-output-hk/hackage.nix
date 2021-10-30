e288423ea3b5e63106f5edef0193b233ba97ad97
IOHK <devops+nix-tools@iohk.io>
2021-10-30 01:11:32 +0000

    Automatic update for Sat Oct 30 01:11:31 UTC 2021

---
+++ hackage/kicad-data-0.5.1-r0-a82d953d16a9ddb9f77bb1b899dced7c5706f336ce7a18add90ada9b13390169.nix
@@ -0,0 +1,58 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "kicad-data"; version = "0.5.1"; };
+      license = "MIT";
+      copyright = "2018";
+      maintainer = "kaspar@monostable.co.uk";
+      author = "Kaspar Emanuel";
+      homepage = "http://github.com/monostable/haskell-kicad-data";
+      url = "";
+      synopsis = "Parser and writer for KiCad files.";
+      description = "Parse and write <https://kicad.org/ KiCad> data\n(currently @.kicad_mod@ files only).";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."parsec" or (errorHandler.buildDepError "parsec"))
+          (hsPkgs."lens-family" or (errorHandler.buildDepError "lens-family"))
+          (hsPkgs."ieee754" or (errorHandler.buildDepError "ieee754"))
+          (hsPkgs."pretty-compact" or (errorHandler.buildDepError "pretty-compact"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "kicad-data-quickcheck" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."parsec" or (errorHandler.buildDepError "parsec"))
+            (hsPkgs."lens-family" or (errorHandler.buildDepError "lens-family"))
+            (hsPkgs."ieee754" or (errorHandler.buildDepError "ieee754"))
+            (hsPkgs."QuickCheck" or (errorHandler.buildDepError "QuickCheck"))
+            (hsPkgs."HUnit" or (errorHandler.buildDepError "HUnit"))
+            (hsPkgs."test-framework" or (errorHandler.buildDepError "test-framework"))
+            (hsPkgs."test-framework-quickcheck2" or (errorHandler.buildDepError "test-framework-quickcheck2"))
+            (hsPkgs."test-framework-hunit" or (errorHandler.buildDepError "test-framework-hunit"))
+            (hsPkgs."test-framework-quickcheck2" or (errorHandler.buildDepError "test-framework-quickcheck2"))
+            (hsPkgs."pretty-compact" or (errorHandler.buildDepError "pretty-compact"))
+            (hsPkgs."charsetdetect" or (errorHandler.buildDepError "charsetdetect"))
+            (hsPkgs."encoding" or (errorHandler.buildDepError "encoding"))
+            (hsPkgs."kicad-data" or (errorHandler.buildDepError "kicad-data"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
---
+++ hackage/purenix-1.0-r0-e3729a516deeaef5aa953fdc5544fa64327159fa9597843fb144d1eaf29e8fc6.nix
@@ -0,0 +1,53 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "2.4";
+      identifier = { name = "purenix"; version = "1.0"; };
+      license = "BSD-3-Clause";
+      copyright = "2021 Dennis Gosnell, Jonas Carpay";
+      maintainer = "Dennis Gosnell <cdep.illabout@gmail.com>";
+      author = "Dennis Gosnell, Jonas Carpay";
+      homepage = "https://github.com/purenix-org/purenix";
+      url = "";
+      synopsis = "Nix backend for PureScript.  Transpile PureScript code to Nix.";
+      description = "";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."aeson" or (errorHandler.buildDepError "aeson"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+          (hsPkgs."directory" or (errorHandler.buildDepError "directory"))
+          (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
+          (hsPkgs."microlens-platform" or (errorHandler.buildDepError "microlens-platform"))
+          (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+          (hsPkgs."pretty-simple" or (errorHandler.buildDepError "pretty-simple"))
+          (hsPkgs."purescript" or (errorHandler.buildDepError "purescript"))
+          (hsPkgs."purescript-cst" or (errorHandler.buildDepError "purescript-cst"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          ];
+        buildable = true;
+        };
+      exes = {
+        "purenix" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."purenix" or (errorHandler.buildDepError "purenix"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
