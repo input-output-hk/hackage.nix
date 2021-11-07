fac98ac53231b9a965315362356c64582000c02c
IOHK <devops+nix-tools@iohk.io>
2021-11-07 01:12:06 +0000

    Automatic update for Sun Nov  7 01:12:05 UTC 2021

---
+++ hackage/bishbosh-0.1.2.0-r0-b1f6674fb47d8beb6ad8abad6bd8db52a28d66daefd9fbbed20a9aeb33a512fb.nix
@@ -0,0 +1,142 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {
+      llvm = false;
+      hxtrelaxng = true;
+      narrownumbers = false;
+      newtypewrappers = false;
+      polyparse = true;
+      precision = false;
+      threaded = true;
+      unboxedarrays = false;
+      unix = true;
+      };
+    package = {
+      specVersion = "2.4";
+      identifier = { name = "bishbosh"; version = "0.1.2.0"; };
+      license = "GPL-3.0-or-later";
+      copyright = "(C) 2018 Dr. Alistair Ward";
+      maintainer = "mailto:bishbosh@functionalley.com";
+      author = "Dr. Alistair Ward";
+      homepage = "https://functionalley.com/BishBosh/bishbosh.html";
+      url = "";
+      synopsis = "Plays chess.";
+      description = "A chess-game which can be rendered in a terminal (emulator) in raw ASCII, or used as an engine by @xboard@.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = ([
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+          (hsPkgs."data-default" or (errorHandler.buildDepError "data-default"))
+          (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
+          (hsPkgs."array" or (errorHandler.buildDepError "array"))
+          (hsPkgs."deepseq" or (errorHandler.buildDepError "deepseq"))
+          (hsPkgs."extra" or (errorHandler.buildDepError "extra"))
+          (hsPkgs."factory" or (errorHandler.buildDepError "factory"))
+          (hsPkgs."hxt" or (errorHandler.buildDepError "hxt"))
+          (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+          (hsPkgs."process" or (errorHandler.buildDepError "process"))
+          (hsPkgs."random" or (errorHandler.buildDepError "random"))
+          (hsPkgs."time" or (errorHandler.buildDepError "time"))
+          (hsPkgs."toolshed" or (errorHandler.buildDepError "toolshed"))
+          ] ++ (if flags.polyparse
+          then [
+            (hsPkgs."polyparse" or (errorHandler.buildDepError "polyparse"))
+            ]
+          else [
+            (hsPkgs."parsec" or (errorHandler.buildDepError "parsec"))
+            ])) ++ (pkgs.lib).optional (flags.threaded) (hsPkgs."parallel" or (errorHandler.buildDepError "parallel"));
+        buildable = true;
+        };
+      exes = {
+        "bishbosh" = {
+          depends = ([
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."data-default" or (errorHandler.buildDepError "data-default"))
+            (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
+            (hsPkgs."bishbosh" or (errorHandler.buildDepError "bishbosh"))
+            (hsPkgs."hxt" or (errorHandler.buildDepError "hxt"))
+            (hsPkgs."array" or (errorHandler.buildDepError "array"))
+            (hsPkgs."deepseq" or (errorHandler.buildDepError "deepseq"))
+            (hsPkgs."directory" or (errorHandler.buildDepError "directory"))
+            (hsPkgs."extra" or (errorHandler.buildDepError "extra"))
+            (hsPkgs."factory" or (errorHandler.buildDepError "factory"))
+            (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+            (hsPkgs."random" or (errorHandler.buildDepError "random"))
+            (hsPkgs."toolshed" or (errorHandler.buildDepError "toolshed"))
+            ] ++ (pkgs.lib).optional (flags.hxtrelaxng) (hsPkgs."hxt-relaxng" or (errorHandler.buildDepError "hxt-relaxng"))) ++ (pkgs.lib).optional (flags.unix) (hsPkgs."unix" or (errorHandler.buildDepError "unix"));
+          buildable = true;
+          };
+        "duel" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."data-default" or (errorHandler.buildDepError "data-default"))
+            (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
+            (hsPkgs."bishbosh" or (errorHandler.buildDepError "bishbosh"))
+            (hsPkgs."hxt" or (errorHandler.buildDepError "hxt"))
+            (hsPkgs."process" or (errorHandler.buildDepError "process"))
+            ];
+          buildable = true;
+          };
+        };
+      tests = {
+        "hunit-tests" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."data-default" or (errorHandler.buildDepError "data-default"))
+            (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
+            (hsPkgs."array" or (errorHandler.buildDepError "array"))
+            (hsPkgs."bishbosh" or (errorHandler.buildDepError "bishbosh"))
+            (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+            (hsPkgs."random" or (errorHandler.buildDepError "random"))
+            (hsPkgs."toolshed" or (errorHandler.buildDepError "toolshed"))
+            (hsPkgs."HUnit" or (errorHandler.buildDepError "HUnit"))
+            (hsPkgs."hxt" or (errorHandler.buildDepError "hxt"))
+            ] ++ (if flags.polyparse
+            then [
+              (hsPkgs."polyparse" or (errorHandler.buildDepError "polyparse"))
+              ]
+            else [
+              (hsPkgs."parsec" or (errorHandler.buildDepError "parsec"))
+              ]);
+          buildable = true;
+          };
+        "quickcheck-tests" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."data-default" or (errorHandler.buildDepError "data-default"))
+            (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
+            (hsPkgs."array" or (errorHandler.buildDepError "array"))
+            (hsPkgs."bishbosh" or (errorHandler.buildDepError "bishbosh"))
+            (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+            (hsPkgs."random" or (errorHandler.buildDepError "random"))
+            (hsPkgs."toolshed" or (errorHandler.buildDepError "toolshed"))
+            (hsPkgs."deepseq" or (errorHandler.buildDepError "deepseq"))
+            (hsPkgs."extra" or (errorHandler.buildDepError "extra"))
+            (hsPkgs."QuickCheck" or (errorHandler.buildDepError "QuickCheck"))
+            ] ++ (if flags.polyparse
+            then [
+              (hsPkgs."polyparse" or (errorHandler.buildDepError "polyparse"))
+              ]
+            else [
+              (hsPkgs."parsec" or (errorHandler.buildDepError "parsec"))
+              ]);
+          buildable = true;
+          };
+        };
+      };
+    }
