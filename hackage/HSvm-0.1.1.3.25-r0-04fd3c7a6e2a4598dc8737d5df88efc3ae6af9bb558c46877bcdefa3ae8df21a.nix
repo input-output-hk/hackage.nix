cebdce72a115476f3ea681812b449fba35cfe40f
IOHK <devops+nix-tools@iohk.io>
2021-11-13 01:14:25 +0000

    Automatic update for Sat Nov 13 01:14:24 UTC 2021

---
+++ hackage/HSvm-0.1.1.3.25-r0-04fd3c7a6e2a4598dc8737d5df88efc3ae6af9bb558c46877bcdefa3ae8df21a.nix
@@ -0,0 +1,35 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "HSvm"; version = "0.1.1.3.25"; };
+      license = "BSD-3-Clause";
+      copyright = "(c) 2009 Paolo Losi, 2017 Pavel Ryzhov";
+      maintainer = "Pavel Ryzhov <paul@paulrz.cz>";
+      author = "Paolo Losi <paolo.losi@gmail.com>";
+      homepage = "";
+      url = "";
+      synopsis = "Haskell Bindings for libsvm";
+      description = "The library embeds libsvm and provides a type-safe interface into SVM models.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+          ];
+        libs = [ (pkgs."stdc++" or (errorHandler.sysDepError "stdc++")) ];
+        buildable = true;
+        };
+      };
+    }
---
+++ hackage/argo-0.2021.11.11-r0-4439367a01566065d16bbe3be59cec77938abb82b730f162c84711239724822a.nix
@@ -0,0 +1,88 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = { pedantic = false; };
+    package = {
+      specVersion = "2.2";
+      identifier = { name = "argo"; version = "0.2021.11.11"; };
+      license = "MIT";
+      copyright = "";
+      maintainer = "Taylor Fausak";
+      author = "";
+      homepage = "";
+      url = "";
+      synopsis = "Parse and render JSON.";
+      description = "Argo parses and renders JSON.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+          (hsPkgs."deepseq" or (errorHandler.buildDepError "deepseq"))
+          (hsPkgs."template-haskell" or (errorHandler.buildDepError "template-haskell"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+          ];
+        buildable = true;
+        };
+      exes = {
+        "argo" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."deepseq" or (errorHandler.buildDepError "deepseq"))
+            (hsPkgs."template-haskell" or (errorHandler.buildDepError "template-haskell"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+            (hsPkgs."argo" or (errorHandler.buildDepError "argo"))
+            ];
+          buildable = true;
+          };
+        };
+      tests = {
+        "argo-test-suite" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."deepseq" or (errorHandler.buildDepError "deepseq"))
+            (hsPkgs."template-haskell" or (errorHandler.buildDepError "template-haskell"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+            (hsPkgs."argo" or (errorHandler.buildDepError "argo"))
+            (hsPkgs."tasty" or (errorHandler.buildDepError "tasty"))
+            (hsPkgs."tasty-hunit" or (errorHandler.buildDepError "tasty-hunit"))
+            (hsPkgs."tasty-quickcheck" or (errorHandler.buildDepError "tasty-quickcheck"))
+            ];
+          buildable = true;
+          };
+        };
+      benchmarks = {
+        "argo-benchmark" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."deepseq" or (errorHandler.buildDepError "deepseq"))
+            (hsPkgs."template-haskell" or (errorHandler.buildDepError "template-haskell"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+            (hsPkgs."argo" or (errorHandler.buildDepError "argo"))
+            (hsPkgs."tasty-bench" or (errorHandler.buildDepError "tasty-bench"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
