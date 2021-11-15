7cf44386965dcef6865d56b387651ddf3d6549eb
IOHK <devops+nix-tools@iohk.io>
2021-11-15 01:13:54 +0000

    Automatic update for Mon Nov 15 01:13:53 UTC 2021

---
+++ hackage/ascii-group-1.0.0.8-r0-48a4ba94f4997812ce437d99674ee2a0fab49d6b68d0a82227b32f2ff7a3d7e9.nix
@@ -0,0 +1,45 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "2.0";
+      identifier = { name = "ascii-group"; version = "1.0.0.8"; };
+      license = "Apache-2.0";
+      copyright = "";
+      maintainer = "Chris Martin, Julie Moronuki";
+      author = "Chris Martin";
+      homepage = "https://github.com/typeclasses/ascii";
+      url = "";
+      synopsis = "ASCII character groups";
+      description = "This package defines a @Group@ type that describes the two varieties of ASCII character, and provides some functions for classifying characters by group.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."ascii-char" or (errorHandler.buildDepError "ascii-char"))
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."hashable" or (errorHandler.buildDepError "hashable"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "test" = {
+          depends = [
+            (hsPkgs."ascii-char" or (errorHandler.buildDepError "ascii-char"))
+            (hsPkgs."ascii-group" or (errorHandler.buildDepError "ascii-group"))
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
---
+++ hackage/store-0.7.10-r1-30d051a47ac66b6f360192830087cefd4ebe7d336596257308a71b9d3d1f1d9c.nix
@@ -0,0 +1,270 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {
+      comparison-bench = false;
+      integer-simple = false;
+      small-bench = false;
+      };
+    package = {
+      specVersion = "1.12";
+      identifier = { name = "store"; version = "0.7.10"; };
+      license = "MIT";
+      copyright = "2016 FP Complete";
+      maintainer = "Michael Sloan <mgsloan@gmail.com>";
+      author = "";
+      homepage = "https://github.com/mgsloan/store#readme";
+      url = "";
+      synopsis = "Fast binary serialization";
+      description = "";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = ([
+          (hsPkgs."array" or (errorHandler.buildDepError "array"))
+          (hsPkgs."async" or (errorHandler.buildDepError "async"))
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."base-orphans" or (errorHandler.buildDepError "base-orphans"))
+          (hsPkgs."base64-bytestring" or (errorHandler.buildDepError "base64-bytestring"))
+          (hsPkgs."bifunctors" or (errorHandler.buildDepError "bifunctors"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+          (hsPkgs."contravariant" or (errorHandler.buildDepError "contravariant"))
+          (hsPkgs."cryptohash" or (errorHandler.buildDepError "cryptohash"))
+          (hsPkgs."deepseq" or (errorHandler.buildDepError "deepseq"))
+          (hsPkgs."directory" or (errorHandler.buildDepError "directory"))
+          (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
+          (hsPkgs."free" or (errorHandler.buildDepError "free"))
+          (hsPkgs."ghc-prim" or (errorHandler.buildDepError "ghc-prim"))
+          (hsPkgs."hashable" or (errorHandler.buildDepError "hashable"))
+          (hsPkgs."hspec" or (errorHandler.buildDepError "hspec"))
+          (hsPkgs."hspec-smallcheck" or (errorHandler.buildDepError "hspec-smallcheck"))
+          (hsPkgs."lifted-base" or (errorHandler.buildDepError "lifted-base"))
+          (hsPkgs."monad-control" or (errorHandler.buildDepError "monad-control"))
+          (hsPkgs."mono-traversable" or (errorHandler.buildDepError "mono-traversable"))
+          (hsPkgs."nats" or (errorHandler.buildDepError "nats"))
+          (hsPkgs."network" or (errorHandler.buildDepError "network"))
+          (hsPkgs."primitive" or (errorHandler.buildDepError "primitive"))
+          (hsPkgs."resourcet" or (errorHandler.buildDepError "resourcet"))
+          (hsPkgs."safe" or (errorHandler.buildDepError "safe"))
+          (hsPkgs."smallcheck" or (errorHandler.buildDepError "smallcheck"))
+          (hsPkgs."store-core" or (errorHandler.buildDepError "store-core"))
+          (hsPkgs."syb" or (errorHandler.buildDepError "syb"))
+          (hsPkgs."template-haskell" or (errorHandler.buildDepError "template-haskell"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."th-lift" or (errorHandler.buildDepError "th-lift"))
+          (hsPkgs."th-lift-instances" or (errorHandler.buildDepError "th-lift-instances"))
+          (hsPkgs."th-orphans" or (errorHandler.buildDepError "th-orphans"))
+          (hsPkgs."th-reify-many" or (errorHandler.buildDepError "th-reify-many"))
+          (hsPkgs."th-utilities" or (errorHandler.buildDepError "th-utilities"))
+          (hsPkgs."time" or (errorHandler.buildDepError "time"))
+          (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+          (hsPkgs."unordered-containers" or (errorHandler.buildDepError "unordered-containers"))
+          (hsPkgs."vector" or (errorHandler.buildDepError "vector"))
+          (hsPkgs."void" or (errorHandler.buildDepError "void"))
+          ] ++ (if flags.integer-simple
+          then [
+            (hsPkgs."integer-simple" or (errorHandler.buildDepError "integer-simple"))
+            ]
+          else [
+            (hsPkgs."integer-gmp" or (errorHandler.buildDepError "integer-gmp"))
+            ])) ++ (pkgs.lib).optionals (compiler.isGhc && (compiler.version).lt "8.0") [
+          (hsPkgs."fail" or (errorHandler.buildDepError "fail"))
+          (hsPkgs."semigroups" or (errorHandler.buildDepError "semigroups"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "store-test" = {
+          depends = ([
+            (hsPkgs."array" or (errorHandler.buildDepError "array"))
+            (hsPkgs."async" or (errorHandler.buildDepError "async"))
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."base-orphans" or (errorHandler.buildDepError "base-orphans"))
+            (hsPkgs."base64-bytestring" or (errorHandler.buildDepError "base64-bytestring"))
+            (hsPkgs."bifunctors" or (errorHandler.buildDepError "bifunctors"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."clock" or (errorHandler.buildDepError "clock"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."contravariant" or (errorHandler.buildDepError "contravariant"))
+            (hsPkgs."cryptohash" or (errorHandler.buildDepError "cryptohash"))
+            (hsPkgs."deepseq" or (errorHandler.buildDepError "deepseq"))
+            (hsPkgs."directory" or (errorHandler.buildDepError "directory"))
+            (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
+            (hsPkgs."free" or (errorHandler.buildDepError "free"))
+            (hsPkgs."ghc-prim" or (errorHandler.buildDepError "ghc-prim"))
+            (hsPkgs."hashable" or (errorHandler.buildDepError "hashable"))
+            (hsPkgs."hspec" or (errorHandler.buildDepError "hspec"))
+            (hsPkgs."hspec-smallcheck" or (errorHandler.buildDepError "hspec-smallcheck"))
+            (hsPkgs."lifted-base" or (errorHandler.buildDepError "lifted-base"))
+            (hsPkgs."monad-control" or (errorHandler.buildDepError "monad-control"))
+            (hsPkgs."mono-traversable" or (errorHandler.buildDepError "mono-traversable"))
+            (hsPkgs."nats" or (errorHandler.buildDepError "nats"))
+            (hsPkgs."network" or (errorHandler.buildDepError "network"))
+            (hsPkgs."primitive" or (errorHandler.buildDepError "primitive"))
+            (hsPkgs."resourcet" or (errorHandler.buildDepError "resourcet"))
+            (hsPkgs."safe" or (errorHandler.buildDepError "safe"))
+            (hsPkgs."smallcheck" or (errorHandler.buildDepError "smallcheck"))
+            (hsPkgs."store" or (errorHandler.buildDepError "store"))
+            (hsPkgs."store-core" or (errorHandler.buildDepError "store-core"))
+            (hsPkgs."syb" or (errorHandler.buildDepError "syb"))
+            (hsPkgs."template-haskell" or (errorHandler.buildDepError "template-haskell"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."th-lift" or (errorHandler.buildDepError "th-lift"))
+            (hsPkgs."th-lift-instances" or (errorHandler.buildDepError "th-lift-instances"))
+            (hsPkgs."th-orphans" or (errorHandler.buildDepError "th-orphans"))
+            (hsPkgs."th-reify-many" or (errorHandler.buildDepError "th-reify-many"))
+            (hsPkgs."th-utilities" or (errorHandler.buildDepError "th-utilities"))
+            (hsPkgs."time" or (errorHandler.buildDepError "time"))
+            (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+            (hsPkgs."unordered-containers" or (errorHandler.buildDepError "unordered-containers"))
+            (hsPkgs."vector" or (errorHandler.buildDepError "vector"))
+            (hsPkgs."void" or (errorHandler.buildDepError "void"))
+            ] ++ (if flags.integer-simple
+            then [
+              (hsPkgs."integer-simple" or (errorHandler.buildDepError "integer-simple"))
+              ]
+            else [
+              (hsPkgs."integer-gmp" or (errorHandler.buildDepError "integer-gmp"))
+              ])) ++ (pkgs.lib).optionals (compiler.isGhc && (compiler.version).lt "8.0") [
+            (hsPkgs."fail" or (errorHandler.buildDepError "fail"))
+            (hsPkgs."semigroups" or (errorHandler.buildDepError "semigroups"))
+            ];
+          buildable = true;
+          };
+        };
+      benchmarks = {
+        "store-bench" = {
+          depends = (([
+            (hsPkgs."array" or (errorHandler.buildDepError "array"))
+            (hsPkgs."async" or (errorHandler.buildDepError "async"))
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."base-orphans" or (errorHandler.buildDepError "base-orphans"))
+            (hsPkgs."base64-bytestring" or (errorHandler.buildDepError "base64-bytestring"))
+            (hsPkgs."bifunctors" or (errorHandler.buildDepError "bifunctors"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."contravariant" or (errorHandler.buildDepError "contravariant"))
+            (hsPkgs."criterion" or (errorHandler.buildDepError "criterion"))
+            (hsPkgs."cryptohash" or (errorHandler.buildDepError "cryptohash"))
+            (hsPkgs."deepseq" or (errorHandler.buildDepError "deepseq"))
+            (hsPkgs."directory" or (errorHandler.buildDepError "directory"))
+            (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
+            (hsPkgs."free" or (errorHandler.buildDepError "free"))
+            (hsPkgs."ghc-prim" or (errorHandler.buildDepError "ghc-prim"))
+            (hsPkgs."hashable" or (errorHandler.buildDepError "hashable"))
+            (hsPkgs."hspec" or (errorHandler.buildDepError "hspec"))
+            (hsPkgs."hspec-smallcheck" or (errorHandler.buildDepError "hspec-smallcheck"))
+            (hsPkgs."lifted-base" or (errorHandler.buildDepError "lifted-base"))
+            (hsPkgs."monad-control" or (errorHandler.buildDepError "monad-control"))
+            (hsPkgs."mono-traversable" or (errorHandler.buildDepError "mono-traversable"))
+            (hsPkgs."nats" or (errorHandler.buildDepError "nats"))
+            (hsPkgs."network" or (errorHandler.buildDepError "network"))
+            (hsPkgs."primitive" or (errorHandler.buildDepError "primitive"))
+            (hsPkgs."resourcet" or (errorHandler.buildDepError "resourcet"))
+            (hsPkgs."safe" or (errorHandler.buildDepError "safe"))
+            (hsPkgs."smallcheck" or (errorHandler.buildDepError "smallcheck"))
+            (hsPkgs."store" or (errorHandler.buildDepError "store"))
+            (hsPkgs."store-core" or (errorHandler.buildDepError "store-core"))
+            (hsPkgs."syb" or (errorHandler.buildDepError "syb"))
+            (hsPkgs."template-haskell" or (errorHandler.buildDepError "template-haskell"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."th-lift" or (errorHandler.buildDepError "th-lift"))
+            (hsPkgs."th-lift-instances" or (errorHandler.buildDepError "th-lift-instances"))
+            (hsPkgs."th-orphans" or (errorHandler.buildDepError "th-orphans"))
+            (hsPkgs."th-reify-many" or (errorHandler.buildDepError "th-reify-many"))
+            (hsPkgs."th-utilities" or (errorHandler.buildDepError "th-utilities"))
+            (hsPkgs."time" or (errorHandler.buildDepError "time"))
+            (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+            (hsPkgs."unordered-containers" or (errorHandler.buildDepError "unordered-containers"))
+            (hsPkgs."vector" or (errorHandler.buildDepError "vector"))
+            (hsPkgs."void" or (errorHandler.buildDepError "void"))
+            ] ++ (if flags.integer-simple
+            then [
+              (hsPkgs."integer-simple" or (errorHandler.buildDepError "integer-simple"))
+              ]
+            else [
+              (hsPkgs."integer-gmp" or (errorHandler.buildDepError "integer-gmp"))
+              ])) ++ (pkgs.lib).optionals (compiler.isGhc && (compiler.version).lt "8.0") [
+            (hsPkgs."fail" or (errorHandler.buildDepError "fail"))
+            (hsPkgs."semigroups" or (errorHandler.buildDepError "semigroups"))
+            ]) ++ (pkgs.lib).optionals (flags.comparison-bench) [
+            (hsPkgs."binary" or (errorHandler.buildDepError "binary"))
+            (hsPkgs."cereal" or (errorHandler.buildDepError "cereal"))
+            (hsPkgs."cereal-vector" or (errorHandler.buildDepError "cereal-vector"))
+            (hsPkgs."vector-binary-instances" or (errorHandler.buildDepError "vector-binary-instances"))
+            ];
+          buildable = true;
+          };
+        "store-weigh" = {
+          depends = ([
+            (hsPkgs."array" or (errorHandler.buildDepError "array"))
+            (hsPkgs."async" or (errorHandler.buildDepError "async"))
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."base-orphans" or (errorHandler.buildDepError "base-orphans"))
+            (hsPkgs."base64-bytestring" or (errorHandler.buildDepError "base64-bytestring"))
+            (hsPkgs."bifunctors" or (errorHandler.buildDepError "bifunctors"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."cereal" or (errorHandler.buildDepError "cereal"))
+            (hsPkgs."cereal-vector" or (errorHandler.buildDepError "cereal-vector"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."contravariant" or (errorHandler.buildDepError "contravariant"))
+            (hsPkgs."criterion" or (errorHandler.buildDepError "criterion"))
+            (hsPkgs."cryptohash" or (errorHandler.buildDepError "cryptohash"))
+            (hsPkgs."deepseq" or (errorHandler.buildDepError "deepseq"))
+            (hsPkgs."directory" or (errorHandler.buildDepError "directory"))
+            (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
+            (hsPkgs."free" or (errorHandler.buildDepError "free"))
+            (hsPkgs."ghc-prim" or (errorHandler.buildDepError "ghc-prim"))
+            (hsPkgs."hashable" or (errorHandler.buildDepError "hashable"))
+            (hsPkgs."hspec" or (errorHandler.buildDepError "hspec"))
+            (hsPkgs."hspec-smallcheck" or (errorHandler.buildDepError "hspec-smallcheck"))
+            (hsPkgs."lifted-base" or (errorHandler.buildDepError "lifted-base"))
+            (hsPkgs."monad-control" or (errorHandler.buildDepError "monad-control"))
+            (hsPkgs."mono-traversable" or (errorHandler.buildDepError "mono-traversable"))
+            (hsPkgs."nats" or (errorHandler.buildDepError "nats"))
+            (hsPkgs."network" or (errorHandler.buildDepError "network"))
+            (hsPkgs."primitive" or (errorHandler.buildDepError "primitive"))
+            (hsPkgs."resourcet" or (errorHandler.buildDepError "resourcet"))
+            (hsPkgs."safe" or (errorHandler.buildDepError "safe"))
+            (hsPkgs."smallcheck" or (errorHandler.buildDepError "smallcheck"))
+            (hsPkgs."store" or (errorHandler.buildDepError "store"))
+            (hsPkgs."store-core" or (errorHandler.buildDepError "store-core"))
+            (hsPkgs."syb" or (errorHandler.buildDepError "syb"))
+            (hsPkgs."template-haskell" or (errorHandler.buildDepError "template-haskell"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."th-lift" or (errorHandler.buildDepError "th-lift"))
+            (hsPkgs."th-lift-instances" or (errorHandler.buildDepError "th-lift-instances"))
+            (hsPkgs."th-orphans" or (errorHandler.buildDepError "th-orphans"))
+            (hsPkgs."th-reify-many" or (errorHandler.buildDepError "th-reify-many"))
+            (hsPkgs."th-utilities" or (errorHandler.buildDepError "th-utilities"))
+            (hsPkgs."time" or (errorHandler.buildDepError "time"))
+            (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+            (hsPkgs."unordered-containers" or (errorHandler.buildDepError "unordered-containers"))
+            (hsPkgs."vector" or (errorHandler.buildDepError "vector"))
+            (hsPkgs."vector-binary-instances" or (errorHandler.buildDepError "vector-binary-instances"))
+            (hsPkgs."void" or (errorHandler.buildDepError "void"))
+            (hsPkgs."weigh" or (errorHandler.buildDepError "weigh"))
+            ] ++ (if flags.integer-simple
+            then [
+              (hsPkgs."integer-simple" or (errorHandler.buildDepError "integer-simple"))
+              ]
+            else [
+              (hsPkgs."integer-gmp" or (errorHandler.buildDepError "integer-gmp"))
+              ])) ++ (pkgs.lib).optionals (compiler.isGhc && (compiler.version).lt "8.0") [
+            (hsPkgs."fail" or (errorHandler.buildDepError "fail"))
+            (hsPkgs."semigroups" or (errorHandler.buildDepError "semigroups"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
