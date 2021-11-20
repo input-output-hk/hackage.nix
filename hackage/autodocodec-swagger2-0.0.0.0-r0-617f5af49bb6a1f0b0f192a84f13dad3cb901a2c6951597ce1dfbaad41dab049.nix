b5d378fff3bed69e22de9d374cc85f19340254ec
IOHK <devops+nix-tools@iohk.io>
2021-11-20 01:14:04 +0000

    Automatic update for Sat Nov 20 01:14:03 UTC 2021

---
+++ hackage/autodocodec-swagger2-0.0.0.0-r0-617f5af49bb6a1f0b0f192a84f13dad3cb901a2c6951597ce1dfbaad41dab049.nix
@@ -0,0 +1,39 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.12";
+      identifier = { name = "autodocodec-swagger2"; version = "0.0.0.0"; };
+      license = "MIT";
+      copyright = "2021 Tom Sydney Kerckhove";
+      maintainer = "syd@cs-syd.eu";
+      author = "Tom Sydney Kerckhove";
+      homepage = "https://github.com/NorfairKing/autodocodec#readme";
+      url = "";
+      synopsis = "Autodocodec interpreters for swagger2";
+      description = "";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."aeson" or (errorHandler.buildDepError "aeson"))
+          (hsPkgs."autodocodec" or (errorHandler.buildDepError "autodocodec"))
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."insert-ordered-containers" or (errorHandler.buildDepError "insert-ordered-containers"))
+          (hsPkgs."scientific" or (errorHandler.buildDepError "scientific"))
+          (hsPkgs."swagger2" or (errorHandler.buildDepError "swagger2"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          ];
+        buildable = true;
+        };
+      };
+    }
