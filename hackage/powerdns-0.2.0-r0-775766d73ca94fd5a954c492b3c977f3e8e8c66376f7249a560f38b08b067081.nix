570213fc60942cc03907cc46dd369bde228327dc
IOHK <devops+nix-tools@iohk.io>
2021-11-26 01:12:09 +0000

    Automatic update for Fri Nov 26 01:12:08 AM UTC 2021

---
+++ hackage/powerdns-0.2.0-r0-775766d73ca94fd5a954c492b3c977f3e8e8c66376f7249a560f38b08b067081.nix
@@ -0,0 +1,58 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "powerdns"; version = "0.2.0"; };
+      license = "BSD-3-Clause";
+      copyright = "(c) 2021 Victor Nawothnig";
+      maintainer = "Victor Nawothnig (dminuoso@icloud.com)";
+      author = "Victor Nawothnig, Julian Jacobi";
+      homepage = "";
+      url = "";
+      synopsis = "PowerDNS API bindings for api/v1";
+      description = "PowerDNS API allows programmatic manipulation of zones and other metadata. This\nlibrary is a thin wrapper, exposing most of the idiosyncracies directly.\n\nUsers are advised to refer to the PowerDNS documentation and its source code for\nthe exact semantics.\n\nAll exposed API endpoints are provided.\n\n> import qualified PowerDNS.Client as P\n> import           Network.HTTP.Client (newManager, defaultManagerSettings)\n> import           Servant.Client (runClientM, mkClientEnv, parseBarseUrl)\n>\n> main :: IO\n> main = do\n>   uri <- parseBaseUrl \"http://localhost:8081\"\n>   mgr <- newManager defaultManagerSettings\n>   env <- P.applyXApiKey \"secret\" <\$> mkClientEnv\n>\n>   Right r <- runClientM (P.statistics \"localhost\" Nothing Nothing) env)\n>   traverse_ print r";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."aeson" or (errorHandler.buildDepError "aeson"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."deepseq" or (errorHandler.buildDepError "deepseq"))
+          (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."time" or (errorHandler.buildDepError "time"))
+          (hsPkgs."base64-bytestring" or (errorHandler.buildDepError "base64-bytestring"))
+          (hsPkgs."case-insensitive" or (errorHandler.buildDepError "case-insensitive"))
+          (hsPkgs."servant" or (errorHandler.buildDepError "servant"))
+          (hsPkgs."servant-client" or (errorHandler.buildDepError "servant-client"))
+          (hsPkgs."servant-client-core" or (errorHandler.buildDepError "servant-client-core"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "powerdns-test" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."powerdns" or (errorHandler.buildDepError "powerdns"))
+            (hsPkgs."tasty" or (errorHandler.buildDepError "tasty"))
+            (hsPkgs."tasty-hunit" or (errorHandler.buildDepError "tasty-hunit"))
+            (hsPkgs."servant-client" or (errorHandler.buildDepError "servant-client"))
+            (hsPkgs."servant-client-core" or (errorHandler.buildDepError "servant-client-core"))
+            (hsPkgs."http-client" or (errorHandler.buildDepError "http-client"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
