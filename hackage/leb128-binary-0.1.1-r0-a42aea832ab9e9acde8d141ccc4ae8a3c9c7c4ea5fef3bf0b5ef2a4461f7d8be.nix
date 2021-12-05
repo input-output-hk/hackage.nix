93bd38138d8b4dcc846b6cb62dbfed9d67e548f9
IOHK <devops+nix-tools@iohk.io>
2021-12-05 01:11:52 +0000

    Automatic update for Sun Dec  5 01:11:51 AM UTC 2021

---
+++ hackage/leb128-binary-0.1.1-r0-a42aea832ab9e9acde8d141ccc4ae8a3c9c7c4ea5fef3bf0b5ef2a4461f7d8be.nix
@@ -0,0 +1,50 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "2.4";
+      identifier = { name = "leb128-binary"; version = "0.1.1"; };
+      license = "Apache-2.0";
+      copyright = "Renzo Carbonara, 2021";
+      maintainer = "renλren.zone";
+      author = "Renzo Carbonara";
+      homepage = "https://gitlab.com/k0001/leb128-binary";
+      url = "";
+      synopsis = "Signed and unsigned LEB128 codec for binary library";
+      description = "Signed and unsigned LEB128 codec for @binary@ library.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."binary" or (errorHandler.buildDepError "binary"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "test" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."binary" or (errorHandler.buildDepError "binary"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."hedgehog" or (errorHandler.buildDepError "hedgehog"))
+            (hsPkgs."leb128-binary" or (errorHandler.buildDepError "leb128-binary"))
+            (hsPkgs."tasty" or (errorHandler.buildDepError "tasty"))
+            (hsPkgs."tasty-hedgehog" or (errorHandler.buildDepError "tasty-hedgehog"))
+            (hsPkgs."tasty-hunit" or (errorHandler.buildDepError "tasty-hunit"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
---
+++ hackage/pqueue-1.4.1.4-r0-6c8ca63f67241a3b57ed063c9f72d70dbbb6a1115d735e49481bfaecd241b661.nix
@@ -0,0 +1,45 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "pqueue"; version = "1.4.1.4"; };
+      license = "BSD-3-Clause";
+      copyright = "";
+      maintainer = "Lennart Spitzner <hexagoxel@hexagoxel.de>\nLouis Wasserman <wasserman.louis@gmail.com>";
+      author = "Louis Wasserman";
+      homepage = "";
+      url = "";
+      synopsis = "Reliable, persistent, fast priority queues.";
+      description = "A fast, reliable priority queue implementation based on a binomial heap.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."deepseq" or (errorHandler.buildDepError "deepseq"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "test" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."deepseq" or (errorHandler.buildDepError "deepseq"))
+            (hsPkgs."QuickCheck" or (errorHandler.buildDepError "QuickCheck"))
+            (hsPkgs."pqueue" or (errorHandler.buildDepError "pqueue"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
