96684f1b365fe0e7265ec9212ce7ef30a0e28117
IOHK <devops+nix-tools@iohk.io>
2021-12-08 01:21:56 +0000

    Automatic update for Wed Dec  8 01:21:55 AM UTC 2021

---
+++ hackage/haskellish-0.3.1-r0-7a765a13e473b7147a9f0bff82e41b9a0f98c1ac13878c941481080392eacb2d.nix
@@ -0,0 +1,38 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "haskellish"; version = "0.3.1"; };
+      license = "BSD-3-Clause";
+      copyright = "(c) 2019- David Ogborn";
+      maintainer = "David Ogborn <ogbornd@mcmaster.ca>";
+      author = "David Ogborn";
+      homepage = "http://github.com/dktr0/Haskellish";
+      url = "";
+      synopsis = "For parsing Haskell-ish languages";
+      description = "A library for parsing miniature and esoteric languages that are similar to Haskell";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."haskell-src-exts" or (errorHandler.buildDepError "haskell-src-exts"))
+          (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+          (hsPkgs."template-haskell" or (errorHandler.buildDepError "template-haskell"))
+          (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          ];
+        buildable = true;
+        };
+      };
+    }
