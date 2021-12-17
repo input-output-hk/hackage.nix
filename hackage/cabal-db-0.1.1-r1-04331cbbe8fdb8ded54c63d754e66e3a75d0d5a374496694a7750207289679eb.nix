d02c3fd790dbba4370df4d363dcfa61bd6456d5d
IOHK <devops+nix-tools@iohk.io>
2021-12-17 01:13:05 +0000

    Automatic update for Fri Dec 17 01:13:04 AM UTC 2021

---
+++ hackage/cabal-db-0.1.1-r1-04331cbbe8fdb8ded54c63d754e66e3a75d0d5a374496694a7750207289679eb.nix
@@ -0,0 +1,45 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.8";
+      identifier = { name = "cabal-db"; version = "0.1.1"; };
+      license = "BSD-3-Clause";
+      copyright = "Vincent Hanquez <vincent@snarc.org>";
+      maintainer = "Vincent Hanquez <vincent@snarc.org>";
+      author = "Vincent Hanquez <vincent@snarc.org>";
+      homepage = "http://github.com/vincenthz/cabal-db";
+      url = "";
+      synopsis = "cabal-db";
+      description = "cabal-db";
+      buildType = "Simple";
+      };
+    components = {
+      exes = {
+        "cabal-db" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+            (hsPkgs."Cabal" or (errorHandler.buildDepError "Cabal"))
+            (hsPkgs."tar" or (errorHandler.buildDepError "tar"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
+            (hsPkgs."directory" or (errorHandler.buildDepError "directory"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."utf8-string" or (errorHandler.buildDepError "utf8-string"))
+            (hsPkgs."pretty" or (errorHandler.buildDepError "pretty"))
+            (hsPkgs."process" or (errorHandler.buildDepError "process"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
---
+++ hackage/cabal-db-0.1.8-r1-b9c9387805fe109d3d750ea6df134c3266ea63886170b0b658022faeac95222c.nix
@@ -0,0 +1,47 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.8";
+      identifier = { name = "cabal-db"; version = "0.1.8"; };
+      license = "BSD-3-Clause";
+      copyright = "Vincent Hanquez <vincent@snarc.org>";
+      maintainer = "Vincent Hanquez <vincent@snarc.org>";
+      author = "Vincent Hanquez <vincent@snarc.org>";
+      homepage = "http://github.com/vincenthz/cabal-db";
+      url = "";
+      synopsis = "query tools for the local cabal database (revdeps, graph, info, search-by, license, bounds)";
+      description = "Query tool for the local cabal database\n\n* Diff between package versions\n\n* List reverse dependencies (revdeps)\n\n* Search by author or maintainer\n\n* Generate graphs of dependencies in dot format\n\n* List licenses of package and their dependencies\n\n* List all the upper version bounds that could be bumped for a list of packages";
+      buildType = "Simple";
+      };
+    components = {
+      exes = {
+        "cabal-db" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+            (hsPkgs."Cabal" or (errorHandler.buildDepError "Cabal"))
+            (hsPkgs."tar" or (errorHandler.buildDepError "tar"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
+            (hsPkgs."directory" or (errorHandler.buildDepError "directory"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."utf8-string" or (errorHandler.buildDepError "utf8-string"))
+            (hsPkgs."pretty" or (errorHandler.buildDepError "pretty"))
+            (hsPkgs."process" or (errorHandler.buildDepError "process"))
+            (hsPkgs."optparse-applicative" or (errorHandler.buildDepError "optparse-applicative"))
+            (hsPkgs."ansi-wl-pprint" or (errorHandler.buildDepError "ansi-wl-pprint"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
