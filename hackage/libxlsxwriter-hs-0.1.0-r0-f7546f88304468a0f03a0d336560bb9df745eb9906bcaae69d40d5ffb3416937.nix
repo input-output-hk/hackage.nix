0f6872c52bf346973c2eceb8d071382e88fac7ca
IOHK <devops+nix-tools@iohk.io>
2021-12-23 01:12:15 +0000

    Automatic update for Thu Dec 23 01:12:14 AM UTC 2021

---
+++ hackage/libxlsxwriter-hs-0.1.0-r0-f7546f88304468a0f03a0d336560bb9df745eb9906bcaae69d40d5ffb3416937.nix
@@ -0,0 +1,37 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "libxlsxwriter-hs"; version = "0.1.0"; };
+      license = "BSD-3-Clause";
+      copyright = "";
+      maintainer = "Supercede Technology Ltd <tech@supercede.com>";
+      author = "Supercede Technology Ltd";
+      homepage = "";
+      url = "";
+      synopsis = "Bindings to libxlsxwriter";
+      description = "Bindings to the libxlsxwriter C library. Please refer to the upstream documentation.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          ];
+        libs = [
+          (pkgs."xlsxwriter" or (errorHandler.sysDepError "xlsxwriter"))
+          ];
+        buildable = true;
+        };
+      };
+    }
---
+++ hackage/openal-ffi-0.0.1-r0-b8af0f6baf4c1fddec6ce84d0dd90f011a6a0999697a0890ad3412a3eb986658.nix
@@ -0,0 +1,42 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.12";
+      identifier = { name = "openal-ffi"; version = "0.0.1"; };
+      license = "BSD-3-Clause";
+      copyright = "2021 IC Rainbow";
+      maintainer = "aenor.realm@gmail.com";
+      author = "IC Rainbow";
+      homepage = "";
+      url = "";
+      synopsis = "Low-level bindings to OpenAL.";
+      description = "";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [ (hsPkgs."base" or (errorHandler.buildDepError "base")) ];
+        libs = [ (pkgs."openal" or (errorHandler.sysDepError "openal")) ];
+        frameworks = (pkgs.lib).optional (system.isOsx || system.isIos) (pkgs."OpenAL" or (errorHandler.sysDepError "OpenAL"));
+        buildable = true;
+        };
+      tests = {
+        "openal-ffi-test" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."openal-ffi" or (errorHandler.buildDepError "openal-ffi"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
