d8bc1f49d43ef9f3e15c5ee1abbc566b5d863f96
IOHK <devops+nix-tools@iohk.io>
2021-12-25 01:11:29 +0000

    Automatic update for Sat Dec 25 01:11:27 AM UTC 2021

---
+++ hackage/splaytree-0.1.3-r1-b9aaf4b7db32198b446ae60f7c41c0e6abcaae9878afcda0e3f6e19f9f75497c.nix
@@ -0,0 +1,34 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.6";
+      identifier = { name = "splaytree"; version = "0.1.3"; };
+      license = "BSD-3-Clause";
+      copyright = "";
+      maintainer = "jwlato@gmail.com";
+      author = "John W. Lato";
+      homepage = "http://www.tiresiaspress.us/~jwlato/haskell/splaytree";
+      url = "";
+      synopsis = "Provides an annotated splay tree";
+      description = "Annotated splay trees (compare to 2-3 finger trees)";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."deepseq" or (errorHandler.buildDepError "deepseq"))
+          ];
+        buildable = true;
+        };
+      };
+    }
---
+++ hackage/splaytree-0.1.5-r1-9ca6ec7da6bde418806b937f8f24c61e2134b278ad41f7c3c1520ee419a74215.nix
@@ -0,0 +1,47 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.12";
+      identifier = { name = "splaytree"; version = "0.1.5"; };
+      license = "BSD-3-Clause";
+      copyright = "";
+      maintainer = "jwlato@gmail.com";
+      author = "John W. Lato";
+      homepage = "http://github.com/JohnLato/splaytree";
+      url = "";
+      synopsis = "Provides an annotated splay tree";
+      description = "Annotated splay trees (compare to 2-3 finger trees)";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."deepseq" or (errorHandler.buildDepError "deepseq"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "testSplay" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."splaytree" or (errorHandler.buildDepError "splaytree"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."QuickCheck" or (errorHandler.buildDepError "QuickCheck"))
+            (hsPkgs."test-framework" or (errorHandler.buildDepError "test-framework"))
+            (hsPkgs."test-framework-quickcheck2" or (errorHandler.buildDepError "test-framework-quickcheck2"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
