dc401407123742b52232378cfee334009d1eeefa
IOHK <devops+nix-tools@iohk.io>
2021-12-30 01:11:58 +0000

    Automatic update for Thu Dec 30 01:11:57 AM UTC 2021

---
+++ hackage/copr-api-0.1.0-r0-8f8ce3dcb5929d3d01de239069bf6a7073a608fdffc39779703b6f9d09afa05c.nix
@@ -0,0 +1,39 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.22";
+      identifier = { name = "copr-api"; version = "0.1.0"; };
+      license = "GPL-3.0-only";
+      copyright = "2020-2021 Jens Petersen";
+      maintainer = "Jens Petersen <petersen@redhat.com>";
+      author = "Jens Petersen <petersen@redhat.com>";
+      homepage = "https://github.com/juhp/copr-api-hs";
+      url = "";
+      synopsis = "Copr API client libary";
+      description = "Fedora Copr buildsystem client api library";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."aeson" or (errorHandler.buildDepError "aeson"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."directory" or (errorHandler.buildDepError "directory"))
+          (hsPkgs."http-query" or (errorHandler.buildDepError "http-query"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."unordered-containers" or (errorHandler.buildDepError "unordered-containers"))
+          ];
+        buildable = true;
+        };
+      };
+    }
