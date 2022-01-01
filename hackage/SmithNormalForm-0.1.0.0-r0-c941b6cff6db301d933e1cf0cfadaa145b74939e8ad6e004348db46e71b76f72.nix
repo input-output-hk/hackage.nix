c3df473256cecdcc12355efe04a993713c4a520f
IOHK <devops+nix-tools@iohk.io>
2022-01-01 01:11:36 +0000

    Automatic update for Sat Jan  1 01:11:35 AM UTC 2022

---
+++ hackage/SmithNormalForm-0.1.0.0-r0-c941b6cff6db301d933e1cf0cfadaa145b74939e8ad6e004348db46e71b76f72.nix
@@ -0,0 +1,35 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.12";
+      identifier = { name = "SmithNormalForm"; version = "0.1.0.0"; };
+      license = "MIT";
+      copyright = "2021 Brian Hwang";
+      maintainer = "bhwang@math.cornell.edu";
+      author = "Brian Hwang";
+      homepage = "https://notabug.org/bwh/SmithNormalForm";
+      url = "";
+      synopsis = "A lightweight Haskell implementation of Smith normal form over the integers.";
+      description = "This package contains an implementation of Smith normal form for arbitrary matrices over the integers. In particular, it does not assume that the matrix is invertible or square.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."matrix" or (errorHandler.buildDepError "matrix"))
+          (hsPkgs."vector" or (errorHandler.buildDepError "vector"))
+          ];
+        buildable = true;
+        };
+      };
+    }
---
+++ hackage/hashable-1.2.4.0-r1-e09e08b7a5a0697ac2ab9048212b29eefda36049406400b639d4056277936f22.nix
@@ -0,0 +1,73 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = { integer-gmp = true; sse2 = true; sse41 = false; };
+    package = {
+      specVersion = "1.8";
+      identifier = { name = "hashable"; version = "1.2.4.0"; };
+      license = "BSD-3-Clause";
+      copyright = "";
+      maintainer = "johan.tibell@gmail.com";
+      author = "Milan Straka <fox@ucw.cz>\nJohan Tibell <johan.tibell@gmail.com>";
+      homepage = "http://github.com/tibbe/hashable";
+      url = "";
+      synopsis = "A class for types that can be converted to a hash value";
+      description = "This package defines a class, 'Hashable', for types that\ncan be converted to a hash value.  This class\nexists for the benefit of hashing-based data\nstructures.  The package provides instances for\nbasic types and a way to combine hash values.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = ([
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          ] ++ (pkgs.lib).optionals (compiler.isGhc && true) [
+          (hsPkgs."ghc-prim" or (errorHandler.buildDepError "ghc-prim"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          ]) ++ (pkgs.lib).optional (compiler.isGhc && true && flags.integer-gmp) (hsPkgs."integer-gmp" or (errorHandler.buildDepError "integer-gmp"));
+        libs = (pkgs.lib).optionals (!(compiler.isGhc && (compiler.version).ge "6.8")) ((pkgs.lib).optional (system.isWindows) (pkgs."advapi32" or (errorHandler.sysDepError "advapi32")));
+        buildable = true;
+        };
+      tests = {
+        "tests" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."ghc-prim" or (errorHandler.buildDepError "ghc-prim"))
+            (hsPkgs."hashable" or (errorHandler.buildDepError "hashable"))
+            (hsPkgs."test-framework" or (errorHandler.buildDepError "test-framework"))
+            (hsPkgs."test-framework-hunit" or (errorHandler.buildDepError "test-framework-hunit"))
+            (hsPkgs."test-framework-quickcheck2" or (errorHandler.buildDepError "test-framework-quickcheck2"))
+            (hsPkgs."HUnit" or (errorHandler.buildDepError "HUnit"))
+            (hsPkgs."QuickCheck" or (errorHandler.buildDepError "QuickCheck"))
+            (hsPkgs."random" or (errorHandler.buildDepError "random"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            ] ++ (pkgs.lib).optional (!system.isWindows) (hsPkgs."unix" or (errorHandler.buildDepError "unix"));
+          buildable = true;
+          };
+        };
+      benchmarks = {
+        "benchmarks" = {
+          depends = ([
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."criterion" or (errorHandler.buildDepError "criterion"))
+            (hsPkgs."ghc-prim" or (errorHandler.buildDepError "ghc-prim"))
+            (hsPkgs."siphash" or (errorHandler.buildDepError "siphash"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            ] ++ (pkgs.lib).optionals (compiler.isGhc && true) [
+            (hsPkgs."ghc-prim" or (errorHandler.buildDepError "ghc-prim"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            ]) ++ (pkgs.lib).optional (compiler.isGhc && true && flags.integer-gmp) (hsPkgs."integer-gmp" or (errorHandler.buildDepError "integer-gmp"));
+          libs = (pkgs.lib).optionals (!(compiler.isGhc && (compiler.version).ge "6.8")) ((pkgs.lib).optional (system.isWindows) (pkgs."advapi32" or (errorHandler.sysDepError "advapi32")));
+          buildable = true;
+          };
+        };
+      };
+    }
