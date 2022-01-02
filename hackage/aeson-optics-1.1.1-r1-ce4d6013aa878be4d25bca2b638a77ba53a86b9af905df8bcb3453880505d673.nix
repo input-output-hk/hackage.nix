1f1b7a75b3c69bd36c6781e95430afca28f00aad
IOHK <devops+nix-tools@iohk.io>
2022-01-02 01:12:33 +0000

    Automatic update for Sun Jan  2 01:12:32 AM UTC 2022

---
+++ hackage/aeson-optics-1.1.1-r1-ce4d6013aa878be4d25bca2b638a77ba53a86b9af905df8bcb3453880505d673.nix
@@ -0,0 +1,43 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.24";
+      identifier = { name = "aeson-optics"; version = "1.1.1"; };
+      license = "MIT";
+      copyright = "Copyright (C) 2012 Paul Wilson\nCopyright (C) 2013 Edward A. Kmett\nCopyright (C) 2019 Oleg Grenrus";
+      maintainer = "Oleg Grenrus <oleg.grenrus@iki.fi>";
+      author = "Edward A. Kmett, Oleg Grenrus";
+      homepage = "http://github.com/phadej/aeson-optics";
+      url = "";
+      synopsis = "Law-abiding optics for aeson";
+      description = "Law-abiding optics for aeson.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."aeson" or (errorHandler.buildDepError "aeson"))
+          (hsPkgs."attoparsec" or (errorHandler.buildDepError "attoparsec"))
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."base-compat" or (errorHandler.buildDepError "base-compat"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."optics-core" or (errorHandler.buildDepError "optics-core"))
+          (hsPkgs."optics-extra" or (errorHandler.buildDepError "optics-extra"))
+          (hsPkgs."scientific" or (errorHandler.buildDepError "scientific"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."unordered-containers" or (errorHandler.buildDepError "unordered-containers"))
+          (hsPkgs."vector" or (errorHandler.buildDepError "vector"))
+          ];
+        buildable = true;
+        };
+      };
+    }
