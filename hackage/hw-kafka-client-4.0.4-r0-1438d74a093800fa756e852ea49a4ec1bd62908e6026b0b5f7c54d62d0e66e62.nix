7869ffa37c57a793f30fc3a3d7a969b2eb59f4f4
IOHK <devops+nix-tools@iohk.io>
2022-01-07 01:13:11 +0000

    Automatic update for Fri Jan  7 01:13:10 AM UTC 2022

---
+++ hackage/hw-kafka-client-4.0.4-r0-1438d74a093800fa756e852ea49a4ec1bd62908e6026b0b5f7c54d62d0e66e62.nix
@@ -0,0 +1,92 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = { examples = false; it = false; };
+    package = {
+      specVersion = "2.2";
+      identifier = { name = "hw-kafka-client"; version = "4.0.4"; };
+      license = "MIT";
+      copyright = "";
+      maintainer = "Alexey Raga <alexey.raga@gmail.com>";
+      author = "Alexey Raga <alexey.raga@gmail.com>";
+      homepage = "https://github.com/haskell-works/hw-kafka-client";
+      url = "";
+      synopsis = "Kafka bindings for Haskell";
+      description = "Apache Kafka bindings backed by the librdkafka C library.\n\nFeatures include:\n\n* Consumer groups: auto-rebalancing consumers\n\n* Keyed and keyless messages producing/consuming\n\n* Batch producing messages";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."bifunctors" or (errorHandler.buildDepError "bifunctors"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+          (hsPkgs."unix" or (errorHandler.buildDepError "unix"))
+          ] ++ (pkgs.lib).optional (compiler.isGhc && (compiler.version).lt "8.0") (hsPkgs."semigroups" or (errorHandler.buildDepError "semigroups"));
+        libs = [ (pkgs."rdkafka" or (errorHandler.sysDepError "rdkafka")) ];
+        build-tools = [
+          (hsPkgs.buildPackages.c2hs.components.exes.c2hs or (pkgs.buildPackages.c2hs or (errorHandler.buildToolDepError "c2hs:c2hs")))
+          ];
+        buildable = true;
+        };
+      exes = {
+        "kafka-client-example" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."hw-kafka-client" or (errorHandler.buildDepError "hw-kafka-client"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            ];
+          buildable = if !flags.examples then false else true;
+          };
+        };
+      tests = {
+        "integration-tests" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."bifunctors" or (errorHandler.buildDepError "bifunctors"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."either" or (errorHandler.buildDepError "either"))
+            (hsPkgs."hspec" or (errorHandler.buildDepError "hspec"))
+            (hsPkgs."hw-kafka-client" or (errorHandler.buildDepError "hw-kafka-client"))
+            (hsPkgs."monad-loops" or (errorHandler.buildDepError "monad-loops"))
+            (hsPkgs."random" or (errorHandler.buildDepError "random"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+            ];
+          build-tools = [
+            (hsPkgs.buildPackages.hspec-discover.components.exes.hspec-discover or (pkgs.buildPackages.hspec-discover or (errorHandler.buildToolDepError "hspec-discover:hspec-discover")))
+            ];
+          buildable = if !flags.it then false else true;
+          };
+        "tests" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."bifunctors" or (errorHandler.buildDepError "bifunctors"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."either" or (errorHandler.buildDepError "either"))
+            (hsPkgs."hspec" or (errorHandler.buildDepError "hspec"))
+            (hsPkgs."hw-kafka-client" or (errorHandler.buildDepError "hw-kafka-client"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."monad-loops" or (errorHandler.buildDepError "monad-loops"))
+            ];
+          build-tools = [
+            (hsPkgs.buildPackages.hspec-discover.components.exes.hspec-discover or (pkgs.buildPackages.hspec-discover or (errorHandler.buildToolDepError "hspec-discover:hspec-discover")))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
---
+++ hackage/units-2.4.1.5-r0-6d422a47af998f1886165722f6569303e764983a60e461da759c13f9d86c716a.nix
@@ -0,0 +1,63 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "units"; version = "2.4.1.5"; };
+      license = "BSD-3-Clause";
+      copyright = "";
+      maintainer = "Richard Eisenberg <rae@richarde.dev>";
+      author = "Richard Eisenberg <rae@richarde.dev>, Takayuki Muranushi <muranushi@gmail.com>";
+      homepage = "https://github.com/goldfirere/units";
+      url = "";
+      synopsis = "A domain-specific type system for dimensional analysis";
+      description = "The units package provides a mechanism for compile-time\ndimensional analysis in Haskell programs. It defines an embedded\ntype system based on units-of-measure. The units defined are fully\nextensible, and need not relate to physical properties.\nThe package supports defining multiple inter-convertible units,\nsuch as Meter and Foot. When extracting a number from a\ndimensioned quantity, the desired unit must be specified, and the\nvalue is converted into that unit.\nIf you are looking for specific systems of units (such as SI),\nplease see the @units-defs@ package.\nTests for this package are in a companion package @units-test@,\navailable from this package's source repository.\nThe Haddock documentation is insufficient for using the units\npackage. Please see the README file, available from the package\nhome page.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = if compiler.isGhc && (compiler.version).ge "9.0"
+          then [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."th-desugar" or (errorHandler.buildDepError "th-desugar"))
+            (hsPkgs."singletons" or (errorHandler.buildDepError "singletons"))
+            (hsPkgs."singletons-th" or (errorHandler.buildDepError "singletons-th"))
+            (hsPkgs."singletons-base" or (errorHandler.buildDepError "singletons-base"))
+            (hsPkgs."vector-space" or (errorHandler.buildDepError "vector-space"))
+            (hsPkgs."linear" or (errorHandler.buildDepError "linear"))
+            (hsPkgs."template-haskell" or (errorHandler.buildDepError "template-haskell"))
+            (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+            (hsPkgs."multimap" or (errorHandler.buildDepError "multimap"))
+            (hsPkgs."syb" or (errorHandler.buildDepError "syb"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."units-parser" or (errorHandler.buildDepError "units-parser"))
+            (hsPkgs."lens" or (errorHandler.buildDepError "lens"))
+            (hsPkgs."deepseq" or (errorHandler.buildDepError "deepseq"))
+            ]
+          else [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."th-desugar" or (errorHandler.buildDepError "th-desugar"))
+            (hsPkgs."singletons" or (errorHandler.buildDepError "singletons"))
+            (hsPkgs."vector-space" or (errorHandler.buildDepError "vector-space"))
+            (hsPkgs."linear" or (errorHandler.buildDepError "linear"))
+            (hsPkgs."template-haskell" or (errorHandler.buildDepError "template-haskell"))
+            (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+            (hsPkgs."multimap" or (errorHandler.buildDepError "multimap"))
+            (hsPkgs."syb" or (errorHandler.buildDepError "syb"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."units-parser" or (errorHandler.buildDepError "units-parser"))
+            (hsPkgs."lens" or (errorHandler.buildDepError "lens"))
+            (hsPkgs."deepseq" or (errorHandler.buildDepError "deepseq"))
+            ];
+        buildable = true;
+        };
+      };
+    }
