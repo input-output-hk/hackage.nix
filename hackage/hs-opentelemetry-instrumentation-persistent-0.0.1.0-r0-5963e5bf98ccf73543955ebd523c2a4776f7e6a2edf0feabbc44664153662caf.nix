75f75372897bbc65a940f87c2d21f8625d92cb1d
IOHK <devops+nix-tools@iohk.io>
2022-02-01 01:19:22 +0000

    Automatic update for Tue Feb  1 01:19:21 AM UTC 2022

---
+++ hackage/hs-opentelemetry-instrumentation-persistent-0.0.1.0-r0-5963e5bf98ccf73543955ebd523c2a4776f7e6a2edf0feabbc44664153662caf.nix
@@ -0,0 +1,59 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.12";
+      identifier = {
+        name = "hs-opentelemetry-instrumentation-persistent";
+        version = "0.0.1.0";
+        };
+      license = "BSD-3-Clause";
+      copyright = "2021 Ian Duncan";
+      maintainer = "ian@iankduncan.com";
+      author = "Ian Duncan";
+      homepage = "https://github.com/iand675/hs-opentelemetry#readme";
+      url = "";
+      synopsis = "";
+      description = "Please see the README on GitHub at <https://github.com/iand675/hs-opentelemetry/tree/main/instrumentation/persistent#readme>";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."hs-opentelemetry-api" or (errorHandler.buildDepError "hs-opentelemetry-api"))
+          (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+          (hsPkgs."persistent" or (errorHandler.buildDepError "persistent"))
+          (hsPkgs."resourcet" or (errorHandler.buildDepError "resourcet"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."unliftio" or (errorHandler.buildDepError "unliftio"))
+          (hsPkgs."vault" or (errorHandler.buildDepError "vault"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "hs-opentelemetry-persistent-test" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."hs-opentelemetry-api" or (errorHandler.buildDepError "hs-opentelemetry-api"))
+            (hsPkgs."hs-opentelemetry-instrumentation-persistent" or (errorHandler.buildDepError "hs-opentelemetry-instrumentation-persistent"))
+            (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+            (hsPkgs."persistent" or (errorHandler.buildDepError "persistent"))
+            (hsPkgs."resourcet" or (errorHandler.buildDepError "resourcet"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."unliftio" or (errorHandler.buildDepError "unliftio"))
+            (hsPkgs."vault" or (errorHandler.buildDepError "vault"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
---
+++ hackage/hslua-module-doclayout-1.0.1-r0-bb0ad9b1bbc8cc88bc011f2fcb4d0dd77e19c527b2d28a71b20dc8463ed2b602.nix
@@ -0,0 +1,51 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "2.0";
+      identifier = { name = "hslua-module-doclayout"; version = "1.0.1"; };
+      license = "MIT";
+      copyright = "© 2020-2021 Albert Krewinkel";
+      maintainer = "Albert Krewinkel <albert@zeitkraut.de>";
+      author = "Albert Krewinkel";
+      homepage = "https://github.com/hslua/hslua-module-doclayout";
+      url = "";
+      synopsis = "Lua module wrapping Text.DocLayout.";
+      description = "Lua module wrapping @Text.DocLayout@.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."doclayout" or (errorHandler.buildDepError "doclayout"))
+          (hsPkgs."hslua" or (errorHandler.buildDepError "hslua"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "hslua-module-doclayout-test" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."doclayout" or (errorHandler.buildDepError "doclayout"))
+            (hsPkgs."hslua" or (errorHandler.buildDepError "hslua"))
+            (hsPkgs."hslua-module-doclayout" or (errorHandler.buildDepError "hslua-module-doclayout"))
+            (hsPkgs."tasty" or (errorHandler.buildDepError "tasty"))
+            (hsPkgs."tasty-hunit" or (errorHandler.buildDepError "tasty-hunit"))
+            (hsPkgs."tasty-lua" or (errorHandler.buildDepError "tasty-lua"))
+            (hsPkgs."hslua-module-doclayout" or (errorHandler.buildDepError "hslua-module-doclayout"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
