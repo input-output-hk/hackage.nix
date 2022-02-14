6e3e6bb53c542e792299920455bc511d29c2dd1f
IOHK <devops+nix-tools@iohk.io>
2022-02-14 01:14:31 +0000

    Automatic update for Mon Feb 14 01:14:30 AM UTC 2022

---
+++ hackage/distribution-opensuse-1.1.3-r0-0ebf05cfc1a022bda11a6b7146d908df989217362f4cb4b2f1f5558aaeb58cb7.nix
@@ -0,0 +1,70 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "distribution-opensuse"; version = "1.1.3"; };
+      license = "BSD-3-Clause";
+      copyright = "";
+      maintainer = "simons@cryp.to";
+      author = "Peter Simons";
+      homepage = "https://github.com/peti/distribution-opensuse/";
+      url = "";
+      synopsis = "Types, functions, and tools to manipulate the openSUSE distribution";
+      description = "This library is a loose collection of types, functions, and tools that\nusers and developers of the\n<https://opensuse.org/ openSUSE Linux distribution> might find useful.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."Diff" or (errorHandler.buildDepError "Diff"))
+          (hsPkgs."aeson" or (errorHandler.buildDepError "aeson"))
+          (hsPkgs."binary" or (errorHandler.buildDepError "binary"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+          (hsPkgs."deepseq" or (errorHandler.buildDepError "deepseq"))
+          (hsPkgs."extra" or (errorHandler.buildDepError "extra"))
+          (hsPkgs."foldl" or (errorHandler.buildDepError "foldl"))
+          (hsPkgs."hashable" or (errorHandler.buildDepError "hashable"))
+          (hsPkgs."hsemail" or (errorHandler.buildDepError "hsemail"))
+          (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+          (hsPkgs."parsec-class" or (errorHandler.buildDepError "parsec-class"))
+          (hsPkgs."pretty" or (errorHandler.buildDepError "pretty"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."time" or (errorHandler.buildDepError "time"))
+          (hsPkgs."turtle" or (errorHandler.buildDepError "turtle"))
+          ];
+        buildable = true;
+        };
+      exes = {
+        "guess-changelog" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."distribution-opensuse" or (errorHandler.buildDepError "distribution-opensuse"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."turtle" or (errorHandler.buildDepError "turtle"))
+            ];
+          buildable = true;
+          };
+        };
+      tests = {
+        "test-strip-space" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."distribution-opensuse" or (errorHandler.buildDepError "distribution-opensuse"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
---
+++ hackage/velma-0.2022.2.13-r0-ef29a666af636093e1b6586d316f69485b3da8bbc1213551101915353c1dedde.nix
@@ -0,0 +1,59 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = { pedantic = false; };
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "velma"; version = "0.2022.2.13"; };
+      license = "MIT";
+      copyright = "";
+      maintainer = "Taylor Fausak";
+      author = "";
+      homepage = "";
+      url = "";
+      synopsis = "Automatically add files to exposed-modules and other-modules.";
+      description = "Velma is a Haskell package that makes it easy to automatically add files to\n@exposed-modules@ and @other-modules@ in Cabal package descriptions.\n\n== Related projects\n\n- [autopack](https://github.com/kowainik/autopack)\n- [cabal-auto-expose](https://github.com/deech/cabal-auto-expose)\n- [cabal-fmt](https://github.com/phadej/cabal-fmt)\n- [hpack](https://github.com/sol/hpack)";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."Cabal" or (errorHandler.buildDepError "Cabal"))
+          (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+          (hsPkgs."directory" or (errorHandler.buildDepError "directory"))
+          (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
+          ];
+        buildable = true;
+        };
+      exes = {
+        "velma" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."velma" or (errorHandler.buildDepError "velma"))
+            ];
+          buildable = true;
+          };
+        };
+      tests = {
+        "velma-test-suite" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."Cabal" or (errorHandler.buildDepError "Cabal"))
+            (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
+            (hsPkgs."hspec" or (errorHandler.buildDepError "hspec"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."velma" or (errorHandler.buildDepError "velma"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
