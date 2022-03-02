30843635b4bfdd088e3c31b4383dfdda0405fecd
IOHK <devops+nix-tools@iohk.io>
2022-03-02 01:15:56 +0000

    Automatic update for Wed Mar  2 01:15:55 AM UTC 2022

---
+++ hackage/aws-arn-0.1.0.1-r1-fcb68005b14a601a3c1fccc2a0b54d53a57c197c58143ecfc85d4c8f5d7aa9ca.nix
@@ -0,0 +1,55 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "2.2";
+      identifier = { name = "aws-arn"; version = "0.1.0.1"; };
+      license = "BSD-3-Clause";
+      copyright = "Copyright (C) 2020-2021 Bellroy Pty Ltd";
+      maintainer = "Bellroy Tech Team <haskell@bellroy.com>";
+      author = "Bellroy Tech Team <haskell@bellroy.com>";
+      homepage = "";
+      url = "";
+      synopsis = "Types and optics for manipulating Amazon Resource Names (ARNs)";
+      description = "This library provides a type representing [Amazon Resource Names\n(ARNs)](https://docs.aws.amazon.com/general/latest/gr/aws-arns-and-namespaces.html),\nand parsing/unparsing functions for them. The provided optics make\nit very convenient to rewrite parts of ARNs.\n\nStart reading at the \"Network.AWS.ARN\" module, which defines the\ncore data type and includes some examples.\n\nThe @ARN@ type is not designed to be a 100% correct-by-construction\nrepresentation of only valid ARNs; it is designed to be a\nlightweight way to destructure and reassemble ARNs to be used in\nplace of string munging.\n\nThe library aims to provide additional parsers for destructuring the\n\"resource\" part of an ARN, but many are missing right now. PRs to\nadd this support for more AWS resource types are __especially__\nwelcome.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."deriving-compat" or (errorHandler.buildDepError "deriving-compat"))
+          (hsPkgs."lens" or (errorHandler.buildDepError "lens"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."hashable" or (errorHandler.buildDepError "hashable"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "spec" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."deriving-compat" or (errorHandler.buildDepError "deriving-compat"))
+            (hsPkgs."lens" or (errorHandler.buildDepError "lens"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."aws-arn" or (errorHandler.buildDepError "aws-arn"))
+            (hsPkgs."tasty" or (errorHandler.buildDepError "tasty"))
+            (hsPkgs."tasty-discover" or (errorHandler.buildDepError "tasty-discover"))
+            (hsPkgs."tasty-hunit" or (errorHandler.buildDepError "tasty-hunit"))
+            ];
+          build-tools = [
+            (hsPkgs.buildPackages.tasty-discover.components.exes.tasty-discover or (pkgs.buildPackages.tasty-discover or (errorHandler.buildToolDepError "tasty-discover:tasty-discover")))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
---
+++ hackage/haspara-0.0.0.1-r0-423210f99857170cb76d05fbc127da771bb077e08ca0b75676ab17a004ced8ec.nix
@@ -0,0 +1,67 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.12";
+      identifier = { name = "haspara"; version = "0.0.0.1"; };
+      license = "MIT";
+      copyright = "Copyright (c) 2021-2022 Telostat Pte Ltd";
+      maintainer = "vst@vsthost.com";
+      author = "Vehbi Sinan Tunalioglu";
+      homepage = "https://github.com/telostat/haspara#readme";
+      url = "";
+      synopsis = "A library providing definitions to work with monetary values.";
+      description = "Please see the README on GitHub at <https://github.com/telostat/haspara#readme>";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."aeson" or (errorHandler.buildDepError "aeson"))
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."deriving-aeson" or (errorHandler.buildDepError "deriving-aeson"))
+          (hsPkgs."hashable" or (errorHandler.buildDepError "hashable"))
+          (hsPkgs."megaparsec" or (errorHandler.buildDepError "megaparsec"))
+          (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+          (hsPkgs."refined" or (errorHandler.buildDepError "refined"))
+          (hsPkgs."safe-decimal" or (errorHandler.buildDepError "safe-decimal"))
+          (hsPkgs."scientific" or (errorHandler.buildDepError "scientific"))
+          (hsPkgs."template-haskell" or (errorHandler.buildDepError "template-haskell"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."time" or (errorHandler.buildDepError "time"))
+          (hsPkgs."unordered-containers" or (errorHandler.buildDepError "unordered-containers"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "haspara-doctest" = {
+          depends = [
+            (hsPkgs."aeson" or (errorHandler.buildDepError "aeson"))
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."deriving-aeson" or (errorHandler.buildDepError "deriving-aeson"))
+            (hsPkgs."doctest" or (errorHandler.buildDepError "doctest"))
+            (hsPkgs."hashable" or (errorHandler.buildDepError "hashable"))
+            (hsPkgs."haspara" or (errorHandler.buildDepError "haspara"))
+            (hsPkgs."megaparsec" or (errorHandler.buildDepError "megaparsec"))
+            (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+            (hsPkgs."refined" or (errorHandler.buildDepError "refined"))
+            (hsPkgs."safe-decimal" or (errorHandler.buildDepError "safe-decimal"))
+            (hsPkgs."scientific" or (errorHandler.buildDepError "scientific"))
+            (hsPkgs."template-haskell" or (errorHandler.buildDepError "template-haskell"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."time" or (errorHandler.buildDepError "time"))
+            (hsPkgs."unordered-containers" or (errorHandler.buildDepError "unordered-containers"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
