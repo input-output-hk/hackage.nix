ab615b887a639209f439d49e6b2a67af6edde248
IOHK <devops+nix-tools@iohk.io>
2022-03-19 01:12:16 +0000

    Automatic update for Sat Mar 19 01:12:15 AM UTC 2022

---
+++ hackage/queue-sheet-0.7.0.2-r1-cdc514aa29041d6abb4ea3c20d59da8eebe7da88b143ff37050c1f697a081f98.nix
@@ -0,0 +1,68 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = { write-hie = false; };
+    package = {
+      specVersion = "1.24";
+      identifier = { name = "queue-sheet"; version = "0.7.0.2"; };
+      license = "MIT";
+      copyright = "Copyright (c) 2020-2022 Travis Cardwell";
+      maintainer = "Travis Cardwell <travis.cardwell@extrema.is>";
+      author = "Travis Cardwell <travis.cardwell@extrema.is>";
+      homepage = "https://github.com/ExtremaIS/queue-sheet-haskell#readme";
+      url = "";
+      synopsis = "queue sheet utility";
+      description = "This package provides a utility for creating queue sheets.  Please see the\nREADME on GitHub at\n<https://github.com/ExtremaIS/queue-sheet-haskell#readme>.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."aeson" or (errorHandler.buildDepError "aeson"))
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."directory" or (errorHandler.buildDepError "directory"))
+          (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
+          (hsPkgs."ginger" or (errorHandler.buildDepError "ginger"))
+          (hsPkgs."process" or (errorHandler.buildDepError "process"))
+          (hsPkgs."scientific" or (errorHandler.buildDepError "scientific"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+          (hsPkgs."ttc" or (errorHandler.buildDepError "ttc"))
+          (hsPkgs."vector" or (errorHandler.buildDepError "vector"))
+          (hsPkgs."yaml" or (errorHandler.buildDepError "yaml"))
+          ];
+        buildable = true;
+        };
+      exes = {
+        "queue-sheet" = {
+          depends = [
+            (hsPkgs."ansi-wl-pprint" or (errorHandler.buildDepError "ansi-wl-pprint"))
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."optparse-applicative" or (errorHandler.buildDepError "optparse-applicative"))
+            (hsPkgs."queue-sheet" or (errorHandler.buildDepError "queue-sheet"))
+            ];
+          buildable = true;
+          };
+        };
+      tests = {
+        "queue-sheet-test" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."queue-sheet" or (errorHandler.buildDepError "queue-sheet"))
+            (hsPkgs."tasty" or (errorHandler.buildDepError "tasty"))
+            (hsPkgs."tasty-hunit" or (errorHandler.buildDepError "tasty-hunit"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
---
+++ hackage/servant-typescript-0.1.0.0-r0-2dc94c15b8f532d7676f5b79747ffe7518eb978c97c9ff0111d8589a6041e732.nix
@@ -0,0 +1,84 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.12";
+      identifier = { name = "servant-typescript"; version = "0.1.0.0"; };
+      license = "BSD-3-Clause";
+      copyright = "2022 Tom McLaughlin";
+      maintainer = "tom@codedown.io";
+      author = "Tom McLaughlin";
+      homepage = "https://github.com/github.com/codedownio#readme";
+      url = "";
+      synopsis = "TypeScript client generation for Servant";
+      description = "Please see the README on GitHub at <https://github.com/codedownio/servant-typescript>";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."aeson" or (errorHandler.buildDepError "aeson"))
+          (hsPkgs."aeson-typescript" or (errorHandler.buildDepError "aeson-typescript"))
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+          (hsPkgs."directory" or (errorHandler.buildDepError "directory"))
+          (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
+          (hsPkgs."lens" or (errorHandler.buildDepError "lens"))
+          (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+          (hsPkgs."servant" or (errorHandler.buildDepError "servant"))
+          (hsPkgs."servant-foreign" or (errorHandler.buildDepError "servant-foreign"))
+          (hsPkgs."string-interpolate" or (errorHandler.buildDepError "string-interpolate"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          ];
+        buildable = true;
+        };
+      exes = {
+        "servant-typescript-exe" = {
+          depends = [
+            (hsPkgs."aeson" or (errorHandler.buildDepError "aeson"))
+            (hsPkgs."aeson-typescript" or (errorHandler.buildDepError "aeson-typescript"))
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."directory" or (errorHandler.buildDepError "directory"))
+            (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
+            (hsPkgs."lens" or (errorHandler.buildDepError "lens"))
+            (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+            (hsPkgs."servant" or (errorHandler.buildDepError "servant"))
+            (hsPkgs."servant-foreign" or (errorHandler.buildDepError "servant-foreign"))
+            (hsPkgs."servant-typescript" or (errorHandler.buildDepError "servant-typescript"))
+            (hsPkgs."string-interpolate" or (errorHandler.buildDepError "string-interpolate"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            ];
+          buildable = true;
+          };
+        };
+      tests = {
+        "servant-typescript-test" = {
+          depends = [
+            (hsPkgs."aeson" or (errorHandler.buildDepError "aeson"))
+            (hsPkgs."aeson-typescript" or (errorHandler.buildDepError "aeson-typescript"))
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."directory" or (errorHandler.buildDepError "directory"))
+            (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
+            (hsPkgs."lens" or (errorHandler.buildDepError "lens"))
+            (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+            (hsPkgs."servant" or (errorHandler.buildDepError "servant"))
+            (hsPkgs."servant-foreign" or (errorHandler.buildDepError "servant-foreign"))
+            (hsPkgs."servant-typescript" or (errorHandler.buildDepError "servant-typescript"))
+            (hsPkgs."string-interpolate" or (errorHandler.buildDepError "string-interpolate"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
