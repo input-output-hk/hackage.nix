ad588a20c781b192c01d647bcfbbc94b60b5657c
IOHK <devops+nix-tools@iohk.io>
2022-04-03 01:13:06 +0000

    Automatic update for Sun Apr  3 01:13:05 AM UTC 2022

---
+++ hackage/theatre-1.0.0.2-r0-e10c63281612578e8b3f23619b2a70a4ff3182a55e137ab9d6f51efa58ed2ccb.nix
@@ -0,0 +1,36 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "3.0";
+      identifier = { name = "theatre"; version = "1.0.0.2"; };
+      license = "MIT";
+      copyright = "(c) 2017, Nikita Volkov";
+      maintainer = "Nikita Volkov <nikita.y.volkov@mail.ru>";
+      author = "Nikita Volkov <nikita.y.volkov@mail.ru>";
+      homepage = "https://github.com/nikita-volkov/theatre";
+      url = "";
+      synopsis = "Minimalistic actor library";
+      description = "";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."contravariant" or (errorHandler.buildDepError "contravariant"))
+          (hsPkgs."slave-thread" or (errorHandler.buildDepError "slave-thread"))
+          (hsPkgs."unagi-chan" or (errorHandler.buildDepError "unagi-chan"))
+          ];
+        buildable = true;
+        };
+      };
+    }
