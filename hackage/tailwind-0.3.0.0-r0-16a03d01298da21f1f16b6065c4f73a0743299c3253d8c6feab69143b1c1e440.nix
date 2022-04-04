8161cdcc97e48b44e9d9698c589cb03fd6fce85b
IOHK <devops+nix-tools@iohk.io>
2022-04-04 01:13:47 +0000

    Automatic update for Mon Apr  4 01:13:46 AM UTC 2022

---
+++ hackage/tailwind-0.3.0.0-r0-16a03d01298da21f1f16b6065c4f73a0743299c3253d8c6feab69143b1c1e440.nix
@@ -0,0 +1,74 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "2.4";
+      identifier = { name = "tailwind"; version = "0.3.0.0"; };
+      license = "MIT";
+      copyright = "2022 Sridhar Ratnakumar";
+      maintainer = "srid@srid.ca";
+      author = "Sridhar Ratnakumar";
+      homepage = "";
+      url = "";
+      synopsis = "Tailwind wrapped in Haskell";
+      description = "Run Tailwind from Haskell without touching JavaScript";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."data-default" or (errorHandler.buildDepError "data-default"))
+          (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
+          (hsPkgs."filepattern" or (errorHandler.buildDepError "filepattern"))
+          (hsPkgs."monad-logger" or (errorHandler.buildDepError "monad-logger"))
+          (hsPkgs."optics-th" or (errorHandler.buildDepError "optics-th"))
+          (hsPkgs."relude" or (errorHandler.buildDepError "relude"))
+          (hsPkgs."aeson" or (errorHandler.buildDepError "aeson"))
+          (hsPkgs."async" or (errorHandler.buildDepError "async"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+          (hsPkgs."deriving-aeson" or (errorHandler.buildDepError "deriving-aeson"))
+          (hsPkgs."directory" or (errorHandler.buildDepError "directory"))
+          (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+          (hsPkgs."neat-interpolation" or (errorHandler.buildDepError "neat-interpolation"))
+          (hsPkgs."profunctors" or (errorHandler.buildDepError "profunctors"))
+          (hsPkgs."safe-exceptions" or (errorHandler.buildDepError "safe-exceptions"))
+          (hsPkgs."temporary" or (errorHandler.buildDepError "temporary"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."time" or (errorHandler.buildDepError "time"))
+          (hsPkgs."unliftio" or (errorHandler.buildDepError "unliftio"))
+          (hsPkgs."which" or (errorHandler.buildDepError "which"))
+          (hsPkgs."with-utf8" or (errorHandler.buildDepError "with-utf8"))
+          ];
+        buildable = true;
+        };
+      exes = {
+        "tailwind-run" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."data-default" or (errorHandler.buildDepError "data-default"))
+            (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
+            (hsPkgs."filepattern" or (errorHandler.buildDepError "filepattern"))
+            (hsPkgs."monad-logger" or (errorHandler.buildDepError "monad-logger"))
+            (hsPkgs."optics-th" or (errorHandler.buildDepError "optics-th"))
+            (hsPkgs."relude" or (errorHandler.buildDepError "relude"))
+            (hsPkgs."optics-core" or (errorHandler.buildDepError "optics-core"))
+            (hsPkgs."optparse-applicative" or (errorHandler.buildDepError "optparse-applicative"))
+            (hsPkgs."parser-combinators" or (errorHandler.buildDepError "parser-combinators"))
+            (hsPkgs."tailwind" or (errorHandler.buildDepError "tailwind"))
+            (hsPkgs."with-utf8" or (errorHandler.buildDepError "with-utf8"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
