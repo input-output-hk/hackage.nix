a340ec7e1ab2255702d48a186505e5824c96bf6b
IOHK <devops+nix-tools@iohk.io>
2022-04-11 01:13:30 +0000

    Automatic update for Mon Apr 11 01:13:29 AM UTC 2022

---
+++ hackage/cl3-3.0.0.0-r0-f6c420297acbcb48a616cbca289b30c93b142e214075300f625fe239caa82bb8.nix
@@ -0,0 +1,81 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {
+      do-no-derived-instances = false;
+      do-no-random = false;
+      do-no-storable = false;
+      do-liquid = false;
+      };
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "cl3"; version = "3.0.0.0"; };
+      license = "BSD-3-Clause";
+      copyright = "Copyright (C) 2017-2022 Nathan Waivio";
+      maintainer = "Nathan Waivio <nathan.waivio@gmail.com>";
+      author = "Nathan Waivio";
+      homepage = "https://github.com/waivio/cl3";
+      url = "";
+      synopsis = "Clifford Algebra of three dimensional space.";
+      description = "Haskell Library implementing standard functions for the Algebra of Physical Space Cl(3,0)";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = (([
+          (hsPkgs."deepseq" or (errorHandler.buildDepError "deepseq"))
+          ] ++ (pkgs.lib).optional (!flags.do-no-random) (hsPkgs."random" or (errorHandler.buildDepError "random"))) ++ (pkgs.lib).optional (!flags.do-liquid) (hsPkgs."base" or (errorHandler.buildDepError "base"))) ++ (pkgs.lib).optionals (flags.do-liquid) [
+          (hsPkgs."liquid-base" or (errorHandler.buildDepError "liquid-base"))
+          (hsPkgs."liquidhaskell" or (errorHandler.buildDepError "liquidhaskell"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "test-cl3" = {
+          depends = [
+            (hsPkgs."cl3" or (errorHandler.buildDepError "cl3"))
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."criterion" or (errorHandler.buildDepError "criterion"))
+            (hsPkgs."random" or (errorHandler.buildDepError "random"))
+            ];
+          buildable = true;
+          };
+        };
+      benchmarks = {
+        "bench-cl3-nbody" = {
+          depends = [
+            (hsPkgs."cl3" or (errorHandler.buildDepError "cl3"))
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."criterion" or (errorHandler.buildDepError "criterion"))
+            ];
+          buildable = true;
+          };
+        "bench-cl3-weigh" = {
+          depends = [
+            (hsPkgs."cl3" or (errorHandler.buildDepError "cl3"))
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."vector" or (errorHandler.buildDepError "vector"))
+            (hsPkgs."massiv" or (errorHandler.buildDepError "massiv"))
+            (hsPkgs."weigh" or (errorHandler.buildDepError "weigh"))
+            ];
+          buildable = true;
+          };
+        "bench-cl3-massiv-nbody" = {
+          depends = [
+            (hsPkgs."cl3" or (errorHandler.buildDepError "cl3"))
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."time" or (errorHandler.buildDepError "time"))
+            (hsPkgs."massiv" or (errorHandler.buildDepError "massiv"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
---
+++ hackage/idiomatic-0.1.0.0-r0-27e71ecb7cf5d8cf6939c7758b03b140a2cf1aa203d4369f6a5de4ce160588d5.nix
@@ -0,0 +1,31 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "idiomatic"; version = "0.1.0.0"; };
+      license = "BSD-3-Clause";
+      copyright = "";
+      maintainer = "Baldur Blöndal";
+      author = "Baldur Blöndal";
+      homepage = "https://github.com/Icelandjack/idiomatic";
+      url = "";
+      synopsis = "Deriving Applicative for sum types.. Idiomatically.";
+      description = "'Idiomatically' is used with 'DerivingVia' to derive 'Applicative'\nfor types with multiple constructors.\n\nIt features an extensible domain-specific language of sums with\n`Applicative` instances. `Idiomatically` is then passed a type-level\nlist of applicative sums that specify how deriving should take\nplace.\n\n> {-# Language DataKinds          #-}\n> {-# Language DeriveGeneric      #-}\n> {-# Language DerivingStrategies #-}\n> {-# Language DerivingVia        #-}\n>\n> import Generic.Applicative\n>\n> data Zip a = No | a ::: Zip a\n>   deriving\n>   stock (Show, Generic1)\n>\n>   deriving (Functor, Applicative)\n>   via Idiomatically Zip '[RightBias Terminal]\n\nThis derives the standard behaviour of `ZipList` but this same\n\"RightBias Terminal\" behaviour describes the `Maybe` and\n`Validation` applicative as well.\n\n> pure @Zip a = a ::: a ::: a ::: ...\n>\n> liftA2 (+) No       No        = No\n> liftA2 (+) No       (⊥:::⊥)   = No\n> liftA2 (+) (⊥:::⊥)  No        = No\n> liftA2 (+) (2:::No) (10:::No) = 12:::No\n\n`Idiomatically` shares an intimate relationship with `Generically1`:\nit is defined in terms of `Generically1` and they are\ninterchangeable when there is an empty list of sums:\n\n> type Generically1 :: (k -> Type) -> (k -> Type)\n> type Generically1 f = Idiomatically f '[]\n\nBased on <http://ekmett.github.io/reader/2012/abstracting-with-applicatives/index.html Abstracting with Applicatives>.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [ (hsPkgs."base" or (errorHandler.buildDepError "base")) ];
+        buildable = true;
+        };
+      };
+    }
