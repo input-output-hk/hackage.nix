40ece2086131c72486271f2920fe78b2a9d1973b
IOHK <devops+nix-tools@iohk.io>
2022-04-19 01:13:19 +0000

    Automatic update for Tue Apr 19 01:13:18 AM UTC 2022

---
+++ hackage/ordering-util-0.1.3.0-r0-f8724e5320d46b23c3785aec622b9247cc72eabc93d8a37c0124851801f386b4.nix
@@ -0,0 +1,31 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "3.0";
+      identifier = { name = "ordering-util"; version = "0.1.3.0"; };
+      license = "MIT";
+      copyright = "ⓒ 2022 Anselm Schüler";
+      maintainer = "mail@anselmschueler.com";
+      author = "Anselm Schüler";
+      homepage = "https://github.com/schuelermine/ordering-util";
+      url = "";
+      synopsis = "Utilities for Orderings";
+      description = "Utilities for Ordering values (the type returned by compare)";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [ (hsPkgs."base" or (errorHandler.buildDepError "base")) ];
+        buildable = true;
+        };
+      };
+    }
