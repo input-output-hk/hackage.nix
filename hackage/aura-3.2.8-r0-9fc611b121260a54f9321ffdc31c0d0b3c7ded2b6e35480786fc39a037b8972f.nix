c13c8b760296b9e832ab4c187850f59415e9a22c
IOHK <devops+nix-tools@iohk.io>
2022-04-24 01:13:19 +0000

    Automatic update for Sun Apr 24 01:13:18 AM UTC 2022

---
+++ hackage/aura-3.2.8-r0-9fc611b121260a54f9321ffdc31c0d0b3c7ded2b6e35480786fc39a037b8972f.nix
@@ -0,0 +1,99 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "2.2";
+      identifier = { name = "aura"; version = "3.2.8"; };
+      license = "GPL-3.0-only";
+      copyright = "";
+      maintainer = "colin@fosskers.ca";
+      author = "Colin Woodbury";
+      homepage = "https://github.com/fosskers/aura";
+      url = "";
+      synopsis = "A secure package manager for Arch Linux and the AUR.";
+      description = "Aura is a package manager for Arch Linux. It connects to both the\nofficial Arch repostitories and to the AUR, allowing easy control of all\npackages on an Arch system. It allows /all/ pacman operations and provides\n/new/ custom ones for dealing with AUR packages. This differs from some other\nAUR package managers.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+          (hsPkgs."megaparsec" or (errorHandler.buildDepError "megaparsec"))
+          (hsPkgs."rio" or (errorHandler.buildDepError "rio"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."versions" or (errorHandler.buildDepError "versions"))
+          (hsPkgs."aeson" or (errorHandler.buildDepError "aeson"))
+          (hsPkgs."aur" or (errorHandler.buildDepError "aur"))
+          (hsPkgs."http-client" or (errorHandler.buildDepError "http-client"))
+          (hsPkgs."prettyprinter" or (errorHandler.buildDepError "prettyprinter"))
+          (hsPkgs."prettyprinter-ansi-terminal" or (errorHandler.buildDepError "prettyprinter-ansi-terminal"))
+          (hsPkgs."scheduler" or (errorHandler.buildDepError "scheduler"))
+          (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+          (hsPkgs."typed-process" or (errorHandler.buildDepError "typed-process"))
+          (hsPkgs."unix" or (errorHandler.buildDepError "unix"))
+          (hsPkgs."algebraic-graphs" or (errorHandler.buildDepError "algebraic-graphs"))
+          (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
+          (hsPkgs."hashable" or (errorHandler.buildDepError "hashable"))
+          (hsPkgs."http-types" or (errorHandler.buildDepError "http-types"))
+          (hsPkgs."language-bash" or (errorHandler.buildDepError "language-bash"))
+          (hsPkgs."network-uri" or (errorHandler.buildDepError "network-uri"))
+          (hsPkgs."stm" or (errorHandler.buildDepError "stm"))
+          (hsPkgs."time" or (errorHandler.buildDepError "time"))
+          ];
+        buildable = true;
+        };
+      exes = {
+        "aura" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."megaparsec" or (errorHandler.buildDepError "megaparsec"))
+            (hsPkgs."rio" or (errorHandler.buildDepError "rio"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."versions" or (errorHandler.buildDepError "versions"))
+            (hsPkgs."aeson" or (errorHandler.buildDepError "aeson"))
+            (hsPkgs."aur" or (errorHandler.buildDepError "aur"))
+            (hsPkgs."http-client" or (errorHandler.buildDepError "http-client"))
+            (hsPkgs."prettyprinter" or (errorHandler.buildDepError "prettyprinter"))
+            (hsPkgs."prettyprinter-ansi-terminal" or (errorHandler.buildDepError "prettyprinter-ansi-terminal"))
+            (hsPkgs."scheduler" or (errorHandler.buildDepError "scheduler"))
+            (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+            (hsPkgs."typed-process" or (errorHandler.buildDepError "typed-process"))
+            (hsPkgs."unix" or (errorHandler.buildDepError "unix"))
+            (hsPkgs."aura" or (errorHandler.buildDepError "aura"))
+            (hsPkgs."http-client-tls" or (errorHandler.buildDepError "http-client-tls"))
+            (hsPkgs."optparse-applicative" or (errorHandler.buildDepError "optparse-applicative"))
+            ];
+          buildable = true;
+          };
+        };
+      tests = {
+        "aura-test" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."megaparsec" or (errorHandler.buildDepError "megaparsec"))
+            (hsPkgs."rio" or (errorHandler.buildDepError "rio"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."versions" or (errorHandler.buildDepError "versions"))
+            (hsPkgs."aura" or (errorHandler.buildDepError "aura"))
+            (hsPkgs."tasty" or (errorHandler.buildDepError "tasty"))
+            (hsPkgs."tasty-hunit" or (errorHandler.buildDepError "tasty-hunit"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
---
+++ hackage/polysemy-scoped-fs-0.1.0.0-r0-24722159534a3db7bc4472173f0571bbd6ea147cca0dd3b00235564b50510a0d.nix
@@ -0,0 +1,79 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "2.2";
+      identifier = { name = "polysemy-scoped-fs"; version = "0.1.0.0"; };
+      license = "AGPL-3.0-or-later";
+      copyright = "2022 Hisaket VioletRed";
+      maintainer = "hisaket@outlook.jp";
+      author = "Hisaket VioletRed";
+      homepage = "https://gitlab.com/hisaket/polysemy-scoped-fs";
+      url = "";
+      synopsis = "Well-typed filesystem operation effects.";
+      description = "Please see the README on GitLab at <https://gitlab.com/hisaket/polysemy-scoped-fs/-/blob/master/README.adoc>";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."directory" or (errorHandler.buildDepError "directory"))
+          (hsPkgs."doctest" or (errorHandler.buildDepError "doctest"))
+          (hsPkgs."extra" or (errorHandler.buildDepError "extra"))
+          (hsPkgs."path" or (errorHandler.buildDepError "path"))
+          (hsPkgs."polysemy" or (errorHandler.buildDepError "polysemy"))
+          (hsPkgs."polysemy-fs" or (errorHandler.buildDepError "polysemy-fs"))
+          (hsPkgs."polysemy-path" or (errorHandler.buildDepError "polysemy-path"))
+          (hsPkgs."polysemy-plugin" or (errorHandler.buildDepError "polysemy-plugin"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "polysemy-scoped-fs-doctest" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."directory" or (errorHandler.buildDepError "directory"))
+            (hsPkgs."doctest" or (errorHandler.buildDepError "doctest"))
+            (hsPkgs."extra" or (errorHandler.buildDepError "extra"))
+            (hsPkgs."path" or (errorHandler.buildDepError "path"))
+            (hsPkgs."polysemy" or (errorHandler.buildDepError "polysemy"))
+            (hsPkgs."polysemy-fs" or (errorHandler.buildDepError "polysemy-fs"))
+            (hsPkgs."polysemy-path" or (errorHandler.buildDepError "polysemy-path"))
+            (hsPkgs."polysemy-plugin" or (errorHandler.buildDepError "polysemy-plugin"))
+            (hsPkgs."polysemy-scoped-fs" or (errorHandler.buildDepError "polysemy-scoped-fs"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            ];
+          buildable = true;
+          };
+        "polysemy-scoped-fs-test" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."directory" or (errorHandler.buildDepError "directory"))
+            (hsPkgs."doctest" or (errorHandler.buildDepError "doctest"))
+            (hsPkgs."extra" or (errorHandler.buildDepError "extra"))
+            (hsPkgs."path" or (errorHandler.buildDepError "path"))
+            (hsPkgs."polysemy" or (errorHandler.buildDepError "polysemy"))
+            (hsPkgs."polysemy-fs" or (errorHandler.buildDepError "polysemy-fs"))
+            (hsPkgs."polysemy-path" or (errorHandler.buildDepError "polysemy-path"))
+            (hsPkgs."polysemy-plugin" or (errorHandler.buildDepError "polysemy-plugin"))
+            (hsPkgs."polysemy-scoped-fs" or (errorHandler.buildDepError "polysemy-scoped-fs"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
