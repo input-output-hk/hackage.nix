d1d2ce6bb4bf964e43e015409432748a30960d8d
IOHK <devops+nix-tools@iohk.io>
2022-05-13 01:16:51 +0000

    Automatic update for Fri May 13 01:16:49 AM UTC 2022

---
+++ hackage/mu-rpc-0.5.0.2-r0-470f5bc372de1b212f625f9e7310cdf82b7fa41ddea03e092fccff0cd9e19db1.nix
@@ -0,0 +1,42 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "mu-rpc"; version = "0.5.0.2"; };
+      license = "Apache-2.0";
+      copyright = "Copyright © 2019-2020 <http://47deg.com 47 Degrees>";
+      maintainer = "alejandro.serrano@47deg.com";
+      author = "Alejandro Serrano, Flavio Corpa";
+      homepage = "https://higherkindness.io/mu-haskell/";
+      url = "";
+      synopsis = "Protocol-independent declaration of services and servers.";
+      description = "Protocol-independent declaration of services and servers for mu-haskell.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."aeson" or (errorHandler.buildDepError "aeson"))
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."conduit" or (errorHandler.buildDepError "conduit"))
+          (hsPkgs."http-types" or (errorHandler.buildDepError "http-types"))
+          (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+          (hsPkgs."mu-schema" or (errorHandler.buildDepError "mu-schema"))
+          (hsPkgs."sop-core" or (errorHandler.buildDepError "sop-core"))
+          (hsPkgs."template-haskell" or (errorHandler.buildDepError "template-haskell"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."wai" or (errorHandler.buildDepError "wai"))
+          ];
+        buildable = true;
+        };
+      };
+    }
---
+++ hackage/nix-graph-1.0.2.0-r0-4b646688d7f8f7cb6b2552f506ca8a9d1be805f2ec95fb1d7a495c975691de6f.nix
@@ -0,0 +1,54 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "2.2";
+      identifier = { name = "nix-graph"; version = "1.0.2.0"; };
+      license = "BSD-3-Clause";
+      copyright = "2022 Arista Networks";
+      maintainer = "Arista Networks <opensource@awakesecurity.com>";
+      author = "Arista Networks <opensource@awakesecurity.com>";
+      homepage = "https://github.com/awakesecurity/nix-graph";
+      url = "";
+      synopsis = "Reify the Nix build graph into a Haskell graph data structure";
+      description = "Reify the Nix build graph into a Haskell graph data structure";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."algebraic-graphs" or (errorHandler.buildDepError "algebraic-graphs"))
+          (hsPkgs."attoparsec" or (errorHandler.buildDepError "attoparsec"))
+          (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+          (hsPkgs."hashable" or (errorHandler.buildDepError "hashable"))
+          (hsPkgs."nix-derivation" or (errorHandler.buildDepError "nix-derivation"))
+          (hsPkgs."stm" or (errorHandler.buildDepError "stm"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."ttrie" or (errorHandler.buildDepError "ttrie"))
+          (hsPkgs."unliftio" or (errorHandler.buildDepError "unliftio"))
+          (hsPkgs."unordered-containers" or (errorHandler.buildDepError "unordered-containers"))
+          ];
+        buildable = true;
+        };
+      exes = {
+        "nix-graph" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."algebraic-graphs" or (errorHandler.buildDepError "algebraic-graphs"))
+            (hsPkgs."nix-graph" or (errorHandler.buildDepError "nix-graph"))
+            (hsPkgs."optparse-applicative" or (errorHandler.buildDepError "optparse-applicative"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
