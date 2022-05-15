b0eecedf42be238ab752cf9c2d3ee86d33aa9d4c
IOHK <devops+nix-tools@iohk.io>
2022-05-15 01:13:15 +0000

    Automatic update for Sun May 15 01:13:14 AM UTC 2022

---
+++ hackage/inline-c-0.9.1.6-r0-1b6e4a20b610688a95fdaf54634736ddf7c3f8919cc336e01af22d0723fd6276.nix
@@ -0,0 +1,84 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = { gsl-example = false; };
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "inline-c"; version = "0.9.1.6"; };
+      license = "MIT";
+      copyright = "(c) 2015-2016 FP Complete Corporation, (c) 2017-2019 Francesco Mazzoli";
+      maintainer = "f@mazzo.li";
+      author = "Francesco Mazzoli, Mathieu Boespflug";
+      homepage = "";
+      url = "";
+      synopsis = "Write Haskell source files including C code inline. No FFI required.";
+      description = "See <https://github.com/fpco/inline-c/blob/master/README.md>.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."ansi-wl-pprint" or (errorHandler.buildDepError "ansi-wl-pprint"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+          (hsPkgs."hashable" or (errorHandler.buildDepError "hashable"))
+          (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+          (hsPkgs."parsec" or (errorHandler.buildDepError "parsec"))
+          (hsPkgs."parsers" or (errorHandler.buildDepError "parsers"))
+          (hsPkgs."template-haskell" or (errorHandler.buildDepError "template-haskell"))
+          (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+          (hsPkgs."unordered-containers" or (errorHandler.buildDepError "unordered-containers"))
+          (hsPkgs."vector" or (errorHandler.buildDepError "vector"))
+          ];
+        buildable = true;
+        };
+      exes = {
+        "gsl-ode" = {
+          depends = (pkgs.lib).optionals (flags.gsl-example) [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."inline-c" or (errorHandler.buildDepError "inline-c"))
+            (hsPkgs."vector" or (errorHandler.buildDepError "vector"))
+            (hsPkgs."Chart" or (errorHandler.buildDepError "Chart"))
+            (hsPkgs."Chart-cairo" or (errorHandler.buildDepError "Chart-cairo"))
+            ];
+          libs = [
+            (pkgs."gsl" or (errorHandler.sysDepError "gsl"))
+            (pkgs."gslcblas" or (errorHandler.sysDepError "gslcblas"))
+            (pkgs."m" or (errorHandler.sysDepError "m"))
+            ];
+          buildable = if flags.gsl-example then true else false;
+          };
+        };
+      tests = {
+        "tests" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."QuickCheck" or (errorHandler.buildDepError "QuickCheck"))
+            (hsPkgs."ansi-wl-pprint" or (errorHandler.buildDepError "ansi-wl-pprint"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."hashable" or (errorHandler.buildDepError "hashable"))
+            (hsPkgs."hspec" or (errorHandler.buildDepError "hspec"))
+            (hsPkgs."inline-c" or (errorHandler.buildDepError "inline-c"))
+            (hsPkgs."parsers" or (errorHandler.buildDepError "parsers"))
+            (hsPkgs."QuickCheck" or (errorHandler.buildDepError "QuickCheck"))
+            (hsPkgs."raw-strings-qq" or (errorHandler.buildDepError "raw-strings-qq"))
+            (hsPkgs."regex-posix" or (errorHandler.buildDepError "regex-posix"))
+            (hsPkgs."template-haskell" or (errorHandler.buildDepError "template-haskell"))
+            (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+            (hsPkgs."unordered-containers" or (errorHandler.buildDepError "unordered-containers"))
+            (hsPkgs."vector" or (errorHandler.buildDepError "vector"))
+            (hsPkgs."split" or (errorHandler.buildDepError "split"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
---
+++ hackage/size-based-0.1.3.0-r0-a2e94b4251c718baee5b02732e9e32247842009d49ece3232b5edf65a806f62d.nix
@@ -0,0 +1,36 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "size-based"; version = "0.1.3.0"; };
+      license = "BSD-3-Clause";
+      copyright = "(c) Jonas Duregård";
+      maintainer = "byorgey@gmail.com";
+      author = "Jonas Duregård";
+      homepage = "";
+      url = "";
+      synopsis = "Sized functors, for size-based enumerations";
+      description = "A framework for size-based enumerations. See the module documentation for details.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."dictionary-sharing" or (errorHandler.buildDepError "dictionary-sharing"))
+          (hsPkgs."testing-type-modifiers" or (errorHandler.buildDepError "testing-type-modifiers"))
+          (hsPkgs."template-haskell" or (errorHandler.buildDepError "template-haskell"))
+          ];
+        buildable = true;
+        };
+      };
+    }
