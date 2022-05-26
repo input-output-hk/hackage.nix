5f70e4d7b85eacad77b56356fdb9e072600d4355
IOHK <devops+nix-tools@iohk.io>
2022-05-26 01:15:23 +0000

    Automatic update for Thu May 26 01:15:22 AM UTC 2022

---
+++ hackage/cabal-info-0.2.1-r1-73be1dbed4dd565e383ac1707bafdb25f231902758c4d312fbb3731d4698bcb2.nix
@@ -0,0 +1,49 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "cabal-info"; version = "0.2.1"; };
+      license = "MIT";
+      copyright = "";
+      maintainer = "mike@barrucadu.co.uk";
+      author = "Michael Walker";
+      homepage = "https://github.com/barrucadu/cabal-info";
+      url = "";
+      synopsis = "Read information from cabal files";
+      description = "Have you ever needed to get information from a cabal file in a shell\nscript? Now you can! cabal-info exposes a simple command-line\ninterface to the cabal file format.\n\nThere is also a library interface, to solve tasks based on searching\nfor a .cabal file and then doing something with it.\n\nSee the <https://github.com/barrucadu/cabal-info README> for more\ndetails.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."Cabal" or (errorHandler.buildDepError "Cabal"))
+          (hsPkgs."directory" or (errorHandler.buildDepError "directory"))
+          (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
+          (hsPkgs."optparse-applicative" or (errorHandler.buildDepError "optparse-applicative"))
+          ];
+        buildable = true;
+        };
+      exes = {
+        "cabal-info" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."cabal-info" or (errorHandler.buildDepError "cabal-info"))
+            (hsPkgs."Cabal" or (errorHandler.buildDepError "Cabal"))
+            (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
+            (hsPkgs."optparse-applicative" or (errorHandler.buildDepError "optparse-applicative"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
---
+++ hackage/newtype-0.2.2.0-r3-9d292bac9208a847de8b4bf82948bbcf6fe1207348ccb6197ea241d63c42947a.nix
@@ -0,0 +1,33 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.12";
+      identifier = { name = "newtype"; version = "0.2.2.0"; };
+      license = "BSD-3-Clause";
+      copyright = "";
+      maintainer = "Andreas Abel";
+      author = "Herbert Valerio Riedel, Darius Jahandarie, Conor McBride";
+      homepage = "";
+      url = "";
+      synopsis = "A typeclass and set of functions for working with newtypes.";
+      description = "Per Conor McBride, the 'Newtype' typeclass represents the packing and unpacking of a @newtype@, and allows you to operate under that @newtype@ with functions such as 'ala'. See \"Control.Newtype\" for documentation and examples.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          ] ++ (pkgs.lib).optional (!(compiler.isGhc && (compiler.version).ge "8.0")) (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"));
+        buildable = true;
+        };
+      };
+    }
