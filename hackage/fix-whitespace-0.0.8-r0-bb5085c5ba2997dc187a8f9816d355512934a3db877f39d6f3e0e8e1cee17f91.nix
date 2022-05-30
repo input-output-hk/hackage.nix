846b4f9ccdf47702cfd61fc5ff33b96967a3491f
IOHK <devops+nix-tools@iohk.io>
2022-05-30 01:23:45 +0000

    Automatic update for Mon May 30 01:23:44 AM UTC 2022

---
+++ hackage/fix-whitespace-0.0.8-r0-bb5085c5ba2997dc187a8f9816d355512934a3db877f39d6f3e0e8e1cee17f91.nix
@@ -0,0 +1,41 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.24";
+      identifier = { name = "fix-whitespace"; version = "0.0.8"; };
+      license = "LicenseRef-OtherLicense";
+      copyright = "";
+      maintainer = "Liang-Ting Chen <liang.ting.chen.tw@gmail.com>, Andreas Abel";
+      author = "fix-whitespace was originally written by Nils Anders Danielsson as part of Agda 2 with contributions from Ulf Norell, Andrés Sicard-Ramírez, Andreas Abel, Philipp Hausmann, Jesper Cockx, Vlad Semenov, and Liang-Ting Chen.";
+      homepage = "https://github.com/agda/fix-whitespace";
+      url = "";
+      synopsis = "Fixes whitespace issues.";
+      description = "Removes trailing whitespace, lines containing only whitespace and ensure that every file ends in a newline character.";
+      buildType = "Simple";
+      };
+    components = {
+      exes = {
+        "fix-whitespace" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."directory" or (errorHandler.buildDepError "directory"))
+            (hsPkgs."extra" or (errorHandler.buildDepError "extra"))
+            (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
+            (hsPkgs."filepattern" or (errorHandler.buildDepError "filepattern"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."yaml" or (errorHandler.buildDepError "yaml"))
+            ] ++ (pkgs.lib).optional (compiler.isGhc && (compiler.version).lt "8.4.1" || compiler.isGhc && (compiler.version).gt "8.4.3") (hsPkgs."text" or (errorHandler.buildDepError "text"));
+          buildable = true;
+          };
+        };
+      };
+    }
