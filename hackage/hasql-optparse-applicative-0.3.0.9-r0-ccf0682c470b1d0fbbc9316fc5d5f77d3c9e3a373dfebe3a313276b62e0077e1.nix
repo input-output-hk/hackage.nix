b56ea8605eae78ce3c4a7ac58c532f384381ba28
IOHK <devops+nix-tools@iohk.io>
2022-06-02 01:16:33 +0000

    Automatic update for Thu Jun  2 01:16:31 AM UTC 2022

---
+++ hackage/hasql-optparse-applicative-0.3.0.9-r0-ccf0682c470b1d0fbbc9316fc5d5f77d3c9e3a373dfebe3a313276b62e0077e1.nix
@@ -0,0 +1,39 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.10";
+      identifier = {
+        name = "hasql-optparse-applicative";
+        version = "0.3.0.9";
+        };
+      license = "MIT";
+      copyright = "(c) 2016, Sannsyn AS";
+      maintainer = "Nikita Volkov <nikita.y.volkov@mail.ru>";
+      author = "Nikita Volkov <nikita.y.volkov@mail.ru>";
+      homepage = "https://github.com/sannsyn/hasql-optparse-applicative ";
+      url = "";
+      synopsis = "\"optparse-applicative\" parsers for \"hasql\"";
+      description = "";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."hasql" or (errorHandler.buildDepError "hasql"))
+          (hsPkgs."hasql-pool" or (errorHandler.buildDepError "hasql-pool"))
+          (hsPkgs."optparse-applicative" or (errorHandler.buildDepError "optparse-applicative"))
+          (hsPkgs."base-prelude" or (errorHandler.buildDepError "base-prelude"))
+          ];
+        buildable = true;
+        };
+      };
+    }
