b6543396beed02768ed84cc5d24a14cd92498eec
IOHK <devops+nix-tools@iohk.io>
2022-06-06 01:16:04 +0000

    Automatic update for Mon Jun  6 01:16:03 AM UTC 2022

---
+++ hackage/typelits-witnesses-0.4.0.0-r1-1acae3ffa1c39b0dfd6bed46b9ddde30975ced276230a505c4b4d7ea3fd89786.nix
@@ -0,0 +1,34 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.12";
+      identifier = { name = "typelits-witnesses"; version = "0.4.0.0"; };
+      license = "MIT";
+      copyright = "(c) Justin Le 2018";
+      maintainer = "justin@jle.im";
+      author = "Justin Le";
+      homepage = "https://github.com/mstksg/typelits-witnesses";
+      url = "";
+      synopsis = "Existential witnesses, singletons, and classes for operations on GHC TypeLits";
+      description = "This library contains:\n\n*   A small specialized subset of the *singletons* library as it pertains to\n`Nat` and `Symbol`, for when you need some simple functionality without\nwanting to invoke the entire *singletons* library.\n*   Operations for manipulating these singletons and `KnownNat` and\n`KnownSymbol` instances, such as addition and multiplication of\nsingletons/`KnownNat` instances.\n*   Operations for the comparison of `Nat`s in a way that works well with\n*GHC.TypeLits*'s different comparison systems.  This is helpful for\nbridging together libraries that use different systems; this functionality\nis not yet provided by *singletons*.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."dependent-sum" or (errorHandler.buildDepError "dependent-sum"))
+          ];
+        buildable = true;
+        };
+      };
+    }
