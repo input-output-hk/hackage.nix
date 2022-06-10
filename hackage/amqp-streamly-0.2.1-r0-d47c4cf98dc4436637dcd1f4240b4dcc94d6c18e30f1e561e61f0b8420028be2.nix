f0a8972ca2724c5ac5cd829050b2b3c02a04ba41
IOHK <devops+nix-tools@iohk.io>
2022-06-10 01:18:18 +0000

    Automatic update for Fri Jun 10 01:18:17 AM UTC 2022

---
+++ hackage/amqp-streamly-0.2.1-r0-d47c4cf98dc4436637dcd1f4240b4dcc94d6c18e30f1e561e61f0b8420028be2.nix
@@ -0,0 +1,52 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.12";
+      identifier = { name = "amqp-streamly"; version = "0.2.1"; };
+      license = "BSD-3-Clause";
+      copyright = "Gautier DI FOLCO";
+      maintainer = "gautier.difolco@gmail.com";
+      author = "Gautier DI FOLCO";
+      homepage = "https://github.com/blackheaven/amqp-streamly#readme";
+      url = "";
+      synopsis = "A simple streamly wrapper for amqp";
+      description = "A simple streamly wrapper for amqp. Provides two functions `produce` and `consume`.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."amqp" or (errorHandler.buildDepError "amqp"))
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."streamly" or (errorHandler.buildDepError "streamly"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "amqp-streamly-test" = {
+          depends = [
+            (hsPkgs."amqp" or (errorHandler.buildDepError "amqp"))
+            (hsPkgs."amqp-streamly" or (errorHandler.buildDepError "amqp-streamly"))
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."hspec" or (errorHandler.buildDepError "hspec"))
+            (hsPkgs."process" or (errorHandler.buildDepError "process"))
+            (hsPkgs."streamly" or (errorHandler.buildDepError "streamly"))
+            (hsPkgs."testcontainers" or (errorHandler.buildDepError "testcontainers"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
---
+++ hackage/ghc-corroborate-1.0.0-r0-60e580741703235dc81232c41b6a7b29b5cf5e337c92ef1745509e8abf22d2b5.nix
@@ -0,0 +1,34 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "2.0";
+      identifier = { name = "ghc-corroborate"; version = "1.0.0"; };
+      license = "MPL-2.0";
+      copyright = "© 2020-2022 Phil de Joux, © 2020-2022 Block Scope Limited";
+      maintainer = "phil.dejoux@blockscope.com";
+      author = "Phil de Joux";
+      homepage = "https://github.com/blockscope/ghc-corroborate#readme";
+      url = "";
+      synopsis = "An flatter API for GHC typechecker plugins";
+      description = "The GHC API changes over time. This package re-exports most GHC\nimports needed by typechecker plugins from its GHC.Corroborate module\nwith a stable API over multiple GHC versions.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = ((((((([
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."ghc-tcplugins-extra" or (errorHandler.buildDepError "ghc-tcplugins-extra"))
+          ] ++ (pkgs.lib).optional (compiler.isGhc && (compiler.version).ge "9.2" && (compiler.isGhc && (compiler.version).lt "9.4")) (hsPkgs."ghc" or (errorHandler.buildDepError "ghc"))) ++ (pkgs.lib).optional (compiler.isGhc && (compiler.version).ge "9.0" && (compiler.isGhc && (compiler.version).lt "9.2")) (hsPkgs."ghc" or (errorHandler.buildDepError "ghc"))) ++ (pkgs.lib).optional (compiler.isGhc && (compiler.version).ge "8.10.0" && (compiler.isGhc && (compiler.version).lt "9.0")) (hsPkgs."ghc" or (errorHandler.buildDepError "ghc"))) ++ (pkgs.lib).optional (compiler.isGhc && (compiler.version).ge "8.8.0" && (compiler.isGhc && (compiler.version).lt "8.10.0")) (hsPkgs."ghc" or (errorHandler.buildDepError "ghc"))) ++ (pkgs.lib).optional (compiler.isGhc && (compiler.version).ge "8.6.0" && (compiler.isGhc && (compiler.version).lt "8.8.0")) (hsPkgs."ghc" or (errorHandler.buildDepError "ghc"))) ++ (pkgs.lib).optional (compiler.isGhc && (compiler.version).ge "8.4.0" && (compiler.isGhc && (compiler.version).lt "8.6.0")) (hsPkgs."ghc" or (errorHandler.buildDepError "ghc"))) ++ (pkgs.lib).optional (compiler.isGhc && (compiler.version).ge "8.2.0" && (compiler.isGhc && (compiler.version).lt "8.4.0")) (hsPkgs."ghc" or (errorHandler.buildDepError "ghc"))) ++ (pkgs.lib).optional (compiler.isGhc && (compiler.version).ge "8.0.0" && (compiler.isGhc && (compiler.version).lt "8.2.0")) (hsPkgs."ghc" or (errorHandler.buildDepError "ghc"));
+        buildable = true;
+        };
+      };
+    }
