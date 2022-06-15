8cf566f870b1e11ba34236711006d4656a50029d
IOHK <devops+nix-tools@iohk.io>
2022-06-15 01:13:57 +0000

    Automatic update for Wed Jun 15 01:13:56 AM UTC 2022

---
+++ hackage/posable-1.0.0.0-r1-2d4a8076491fcc7693350672561d5d7528c847bcd7e4d888e9aea91b847101d8.nix
@@ -0,0 +1,51 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "2.2";
+      identifier = { name = "posable"; version = "1.0.0.0"; };
+      license = "BSD-3-Clause";
+      copyright = "";
+      maintainer = "Rick van Hoef <hackage@rickvanhoef.nl>";
+      author = "Rick van Hoef";
+      homepage = "https://github.com/Riscky/posable";
+      url = "";
+      synopsis = "A product-of-sums generics library";
+      description = "A generics library that represents a non-recursive Haskell 98\ndatatype as a product-of-sums. Each type is represented\nwith a single tag, and a product of sums of fields. The tag\nrepresents all constructor choices in the type, the fields\ncontain all the values in the type. This representation\nmaps easily to a struct of unions, which is a\nmemory-efficient way to store sum datatypes.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."finite-typelits" or (errorHandler.buildDepError "finite-typelits"))
+          (hsPkgs."generics-sop" or (errorHandler.buildDepError "generics-sop"))
+          (hsPkgs."template-haskell" or (errorHandler.buildDepError "template-haskell"))
+          (hsPkgs."ghc-typelits-knownnat" or (errorHandler.buildDepError "ghc-typelits-knownnat"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "unit-test" = {
+          depends = [
+            (hsPkgs."posable" or (errorHandler.buildDepError "posable"))
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."ghc-typelits-knownnat" or (errorHandler.buildDepError "ghc-typelits-knownnat"))
+            (hsPkgs."template-haskell" or (errorHandler.buildDepError "template-haskell"))
+            (hsPkgs."tasty" or (errorHandler.buildDepError "tasty"))
+            (hsPkgs."tasty-hunit" or (errorHandler.buildDepError "tasty-hunit"))
+            (hsPkgs."tasty-quickcheck" or (errorHandler.buildDepError "tasty-quickcheck"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
---
+++ hackage/shellmet-0.0.4.1-r0-6d1dd3ce296b00a589e439d20a4466515cdf3e6156a0a147156804ee544ee9d1.nix
@@ -0,0 +1,58 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "2.4";
+      identifier = { name = "shellmet"; version = "0.0.4.1"; };
+      license = "MPL-2.0";
+      copyright = "2019-2022 Kowainik";
+      maintainer = "Kowainik <xrom.xkov@gmail.com>";
+      author = "Dmitrii Kovanikov";
+      homepage = "https://github.com/kowainik/shellmet";
+      url = "";
+      synopsis = "Out of the shell solution for scripting in Haskell";
+      description = "Shellmet provides easy and convenient way to call shell commands from Haskell programs";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."process" or (errorHandler.buildDepError "process"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          ];
+        buildable = true;
+        };
+      exes = {
+        "readme" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."shellmet" or (errorHandler.buildDepError "shellmet"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            ];
+          build-tools = [
+            (hsPkgs.buildPackages.markdown-unlit.components.exes.markdown-unlit or (pkgs.buildPackages.markdown-unlit or (errorHandler.buildToolDepError "markdown-unlit:markdown-unlit")))
+            ];
+          buildable = if system.isWindows then false else true;
+          };
+        };
+      tests = {
+        "shellmet-doctest" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."doctest" or (errorHandler.buildDepError "doctest"))
+            (hsPkgs."Glob" or (errorHandler.buildDepError "Glob"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
