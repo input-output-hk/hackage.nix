0a0c08cfa874869f45b6057396c304ee963abd0a
IOHK <devops+nix-tools@iohk.io>
2022-07-09 01:45:42 +0000

    Automatic update for Sat Jul  9 01:45:41 AM UTC 2022

---
+++ hackage/heystone-0.1.0-r0-eb69b30861b2507bddf7e9a76feb070a7d75b38861d7de7c0f00dd13e34c4412.nix
@@ -0,0 +1,41 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.12";
+      identifier = { name = "heystone"; version = "0.1.0"; };
+      license = "GPL-2.0-only";
+      copyright = "";
+      maintainer = "Ben Orchard <thefirstmuffinman@gmail.com>";
+      author = "Adrian Herrera, Ben Orchard";
+      homepage = "https://github.com/raehik/heystone#readme";
+      url = "";
+      synopsis = "Haskell bindings for the Keystone assembler framework";
+      description = "Please see README.md.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+          ];
+        pkgconfig = [
+          (pkgconfPkgs."keystone" or (errorHandler.pkgConfDepError "keystone"))
+          ];
+        build-tools = [
+          (hsPkgs.buildPackages.c2hs.components.exes.c2hs or (pkgs.buildPackages.c2hs or (errorHandler.buildToolDepError "c2hs:c2hs")))
+          ];
+        buildable = true;
+        };
+      };
+    }
---
+++ hackage/libsecp256k1-0.0.2-r0-ab865b281bd4f6e72c2480d426fdd61394835787c22cbb06bce6cdbe0ce6532e.nix
@@ -0,0 +1,60 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "2.0";
+      identifier = { name = "libsecp256k1"; version = "0.0.2"; };
+      license = "MIT";
+      copyright = "(c) 2017 Jean-Pierre Rupp; (c) 2020 Haskoin Developers; (c) 2022 Keagan McClelland";
+      maintainer = "keagan.mcclelland@gmail.com";
+      author = "Keagan McClelland";
+      homepage = "http://github.com/ProofOfKeags/secp256k1-haskell#readme";
+      url = "";
+      synopsis = "Bindings for secp256k1";
+      description = "Sign and verify signatures using the secp256k1 library.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."cryptonite" or (errorHandler.buildDepError "cryptonite"))
+          (hsPkgs."memory" or (errorHandler.buildDepError "memory"))
+          (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+          ];
+        pkgconfig = [
+          (pkgconfPkgs."libsecp256k1" or (errorHandler.pkgConfDepError "libsecp256k1"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "spec" = {
+          depends = [
+            (hsPkgs."HUnit" or (errorHandler.buildDepError "HUnit"))
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."cryptonite" or (errorHandler.buildDepError "cryptonite"))
+            (hsPkgs."hspec" or (errorHandler.buildDepError "hspec"))
+            (hsPkgs."memory" or (errorHandler.buildDepError "memory"))
+            (hsPkgs."monad-par" or (errorHandler.buildDepError "monad-par"))
+            (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+            (hsPkgs."secp256k1-haskell" or (errorHandler.buildDepError "secp256k1-haskell"))
+            (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+            ];
+          build-tools = [
+            (hsPkgs.buildPackages.hspec-discover.components.exes.hspec-discover or (pkgs.buildPackages.hspec-discover or (errorHandler.buildToolDepError "hspec-discover:hspec-discover")))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
