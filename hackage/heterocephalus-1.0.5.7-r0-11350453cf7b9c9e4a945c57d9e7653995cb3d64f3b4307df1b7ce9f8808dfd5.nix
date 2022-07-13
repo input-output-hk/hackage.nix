3e64c5e5f41e4e038fe6aff3bebf2f50ef4b9657
IOHK <devops+nix-tools@iohk.io>
2022-07-13 01:53:34 +0000

    Automatic update for Wed Jul 13 01:53:33 AM UTC 2022

---
+++ hackage/heterocephalus-1.0.5.7-r0-11350453cf7b9c9e4a945c57d9e7653995cb3d64f3b4307df1b7ce9f8808dfd5.nix
@@ -0,0 +1,62 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "heterocephalus"; version = "1.0.5.7"; };
+      license = "MIT";
+      copyright = "2016 Kadzuya Okamoto";
+      maintainer = "arow.okamoto+github@gmail.com";
+      author = "Kadzuya Okamoto";
+      homepage = "https://github.com/arowM/heterocephalus#readme";
+      url = "";
+      synopsis = "A type-safe template engine for working with front end development tools";
+      description = "Recent front end development tools and languages are growing fast and have\nquite a complicated ecosystem. Few front end developers want to be forced\nuse Shakespeare templates. Instead, they would rather use @node@-friendly\nengines such that @pug@, @slim@, and @haml@. However, in using these\ntemplate engines, we lose the compile-time variable interpolation and type\nchecking from Shakespeare.\n\nHeterocephalus is intended for use with another feature rich template\nengine and provides a way to interpolate server side variables into a\nprecompiled template file with @forall@, @if@, and @case@ statements.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."blaze-html" or (errorHandler.buildDepError "blaze-html"))
+          (hsPkgs."blaze-markup" or (errorHandler.buildDepError "blaze-markup"))
+          (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+          (hsPkgs."dlist" or (errorHandler.buildDepError "dlist"))
+          (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+          (hsPkgs."parsec" or (errorHandler.buildDepError "parsec"))
+          (hsPkgs."shakespeare" or (errorHandler.buildDepError "shakespeare"))
+          (hsPkgs."template-haskell" or (errorHandler.buildDepError "template-haskell"))
+          (hsPkgs."template-haskell-compat-v0208" or (errorHandler.buildDepError "template-haskell-compat-v0208"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "heterocephalus-test" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."heterocephalus" or (errorHandler.buildDepError "heterocephalus"))
+            ];
+          buildable = true;
+          };
+        "doctest" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."Glob" or (errorHandler.buildDepError "Glob"))
+            (hsPkgs."doctest" or (errorHandler.buildDepError "doctest"))
+            (hsPkgs."heterocephalus" or (errorHandler.buildDepError "heterocephalus"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
---
+++ hackage/postgrest-9.0.1-r0-e7d4c9b2d3e5f99d03133d0128b630a077f769b182e53eaadc2594439f0fd5a1.nix
@@ -0,0 +1,162 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = { dev = false; hpc = true; };
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "postgrest"; version = "9.0.1"; };
+      license = "MIT";
+      copyright = "";
+      maintainer = "Steve Chavez <stevechavezast@gmail.com>";
+      author = "Joe Nelson, Adam Baker, Steve Chavez";
+      homepage = "https://postgrest.org";
+      url = "";
+      synopsis = "REST API for any Postgres database";
+      description = "Reads the schema of a PostgreSQL database and creates RESTful routes\nfor tables, views, and functions, supporting all HTTP verbs that security\npermits.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."HTTP" or (errorHandler.buildDepError "HTTP"))
+          (hsPkgs."Ranged-sets" or (errorHandler.buildDepError "Ranged-sets"))
+          (hsPkgs."aeson" or (errorHandler.buildDepError "aeson"))
+          (hsPkgs."auto-update" or (errorHandler.buildDepError "auto-update"))
+          (hsPkgs."base64-bytestring" or (errorHandler.buildDepError "base64-bytestring"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."case-insensitive" or (errorHandler.buildDepError "case-insensitive"))
+          (hsPkgs."cassava" or (errorHandler.buildDepError "cassava"))
+          (hsPkgs."configurator-pg" or (errorHandler.buildDepError "configurator-pg"))
+          (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+          (hsPkgs."contravariant-extras" or (errorHandler.buildDepError "contravariant-extras"))
+          (hsPkgs."cookie" or (errorHandler.buildDepError "cookie"))
+          (hsPkgs."either" or (errorHandler.buildDepError "either"))
+          (hsPkgs."gitrev" or (errorHandler.buildDepError "gitrev"))
+          (hsPkgs."hasql" or (errorHandler.buildDepError "hasql"))
+          (hsPkgs."hasql-dynamic-statements" or (errorHandler.buildDepError "hasql-dynamic-statements"))
+          (hsPkgs."hasql-notifications" or (errorHandler.buildDepError "hasql-notifications"))
+          (hsPkgs."hasql-pool" or (errorHandler.buildDepError "hasql-pool"))
+          (hsPkgs."hasql-transaction" or (errorHandler.buildDepError "hasql-transaction"))
+          (hsPkgs."heredoc" or (errorHandler.buildDepError "heredoc"))
+          (hsPkgs."http-types" or (errorHandler.buildDepError "http-types"))
+          (hsPkgs."insert-ordered-containers" or (errorHandler.buildDepError "insert-ordered-containers"))
+          (hsPkgs."interpolatedstring-perl6" or (errorHandler.buildDepError "interpolatedstring-perl6"))
+          (hsPkgs."jose" or (errorHandler.buildDepError "jose"))
+          (hsPkgs."lens" or (errorHandler.buildDepError "lens"))
+          (hsPkgs."lens-aeson" or (errorHandler.buildDepError "lens-aeson"))
+          (hsPkgs."mtl" or (errorHandler.buildDepError "mtl"))
+          (hsPkgs."network-uri" or (errorHandler.buildDepError "network-uri"))
+          (hsPkgs."optparse-applicative" or (errorHandler.buildDepError "optparse-applicative"))
+          (hsPkgs."parsec" or (errorHandler.buildDepError "parsec"))
+          (hsPkgs."protolude" or (errorHandler.buildDepError "protolude"))
+          (hsPkgs."regex-tdfa" or (errorHandler.buildDepError "regex-tdfa"))
+          (hsPkgs."retry" or (errorHandler.buildDepError "retry"))
+          (hsPkgs."scientific" or (errorHandler.buildDepError "scientific"))
+          (hsPkgs."swagger2" or (errorHandler.buildDepError "swagger2"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."time" or (errorHandler.buildDepError "time"))
+          (hsPkgs."unordered-containers" or (errorHandler.buildDepError "unordered-containers"))
+          (hsPkgs."vector" or (errorHandler.buildDepError "vector"))
+          (hsPkgs."wai" or (errorHandler.buildDepError "wai"))
+          (hsPkgs."wai-cors" or (errorHandler.buildDepError "wai-cors"))
+          (hsPkgs."wai-extra" or (errorHandler.buildDepError "wai-extra"))
+          (hsPkgs."warp" or (errorHandler.buildDepError "warp"))
+          ] ++ (pkgs.lib).optionals (!system.isWindows) [
+          (hsPkgs."unix" or (errorHandler.buildDepError "unix"))
+          (hsPkgs."directory" or (errorHandler.buildDepError "directory"))
+          (hsPkgs."network" or (errorHandler.buildDepError "network"))
+          ];
+        buildable = true;
+        };
+      exes = {
+        "postgrest" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."postgrest" or (errorHandler.buildDepError "postgrest"))
+            (hsPkgs."protolude" or (errorHandler.buildDepError "protolude"))
+            ];
+          buildable = true;
+          };
+        };
+      tests = {
+        "spec" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."aeson" or (errorHandler.buildDepError "aeson"))
+            (hsPkgs."aeson-qq" or (errorHandler.buildDepError "aeson-qq"))
+            (hsPkgs."async" or (errorHandler.buildDepError "async"))
+            (hsPkgs."auto-update" or (errorHandler.buildDepError "auto-update"))
+            (hsPkgs."base64-bytestring" or (errorHandler.buildDepError "base64-bytestring"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."case-insensitive" or (errorHandler.buildDepError "case-insensitive"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."hasql-pool" or (errorHandler.buildDepError "hasql-pool"))
+            (hsPkgs."hasql-transaction" or (errorHandler.buildDepError "hasql-transaction"))
+            (hsPkgs."heredoc" or (errorHandler.buildDepError "heredoc"))
+            (hsPkgs."hspec" or (errorHandler.buildDepError "hspec"))
+            (hsPkgs."hspec-wai" or (errorHandler.buildDepError "hspec-wai"))
+            (hsPkgs."hspec-wai-json" or (errorHandler.buildDepError "hspec-wai-json"))
+            (hsPkgs."http-types" or (errorHandler.buildDepError "http-types"))
+            (hsPkgs."lens" or (errorHandler.buildDepError "lens"))
+            (hsPkgs."lens-aeson" or (errorHandler.buildDepError "lens-aeson"))
+            (hsPkgs."monad-control" or (errorHandler.buildDepError "monad-control"))
+            (hsPkgs."postgrest" or (errorHandler.buildDepError "postgrest"))
+            (hsPkgs."process" or (errorHandler.buildDepError "process"))
+            (hsPkgs."protolude" or (errorHandler.buildDepError "protolude"))
+            (hsPkgs."regex-tdfa" or (errorHandler.buildDepError "regex-tdfa"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."transformers-base" or (errorHandler.buildDepError "transformers-base"))
+            (hsPkgs."wai" or (errorHandler.buildDepError "wai"))
+            (hsPkgs."wai-extra" or (errorHandler.buildDepError "wai-extra"))
+            ];
+          buildable = true;
+          };
+        "querycost" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."aeson" or (errorHandler.buildDepError "aeson"))
+            (hsPkgs."base64-bytestring" or (errorHandler.buildDepError "base64-bytestring"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."case-insensitive" or (errorHandler.buildDepError "case-insensitive"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."contravariant" or (errorHandler.buildDepError "contravariant"))
+            (hsPkgs."hasql" or (errorHandler.buildDepError "hasql"))
+            (hsPkgs."hasql-dynamic-statements" or (errorHandler.buildDepError "hasql-dynamic-statements"))
+            (hsPkgs."hasql-pool" or (errorHandler.buildDepError "hasql-pool"))
+            (hsPkgs."hasql-transaction" or (errorHandler.buildDepError "hasql-transaction"))
+            (hsPkgs."heredoc" or (errorHandler.buildDepError "heredoc"))
+            (hsPkgs."hspec" or (errorHandler.buildDepError "hspec"))
+            (hsPkgs."hspec-wai" or (errorHandler.buildDepError "hspec-wai"))
+            (hsPkgs."http-types" or (errorHandler.buildDepError "http-types"))
+            (hsPkgs."lens" or (errorHandler.buildDepError "lens"))
+            (hsPkgs."lens-aeson" or (errorHandler.buildDepError "lens-aeson"))
+            (hsPkgs."postgrest" or (errorHandler.buildDepError "postgrest"))
+            (hsPkgs."process" or (errorHandler.buildDepError "process"))
+            (hsPkgs."protolude" or (errorHandler.buildDepError "protolude"))
+            (hsPkgs."regex-tdfa" or (errorHandler.buildDepError "regex-tdfa"))
+            (hsPkgs."wai-extra" or (errorHandler.buildDepError "wai-extra"))
+            ];
+          buildable = true;
+          };
+        "doctests" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."doctest" or (errorHandler.buildDepError "doctest"))
+            (hsPkgs."postgrest" or (errorHandler.buildDepError "postgrest"))
+            (hsPkgs."pretty-simple" or (errorHandler.buildDepError "pretty-simple"))
+            (hsPkgs."protolude" or (errorHandler.buildDepError "protolude"))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
