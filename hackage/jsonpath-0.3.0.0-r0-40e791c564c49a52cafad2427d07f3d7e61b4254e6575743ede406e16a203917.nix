6af4d804e6e7c7f57324bbf386a76a5d1e3a8799
IOHK <devops+nix-tools@iohk.io>
2022-07-25 01:17:45 +0000

    Automatic update for Mon Jul 25 01:17:43 AM UTC 2022

---
+++ hackage/jsonpath-0.3.0.0-r0-40e791c564c49a52cafad2427d07f3d7e61b4254e6575743ede406e16a203917.nix
@@ -0,0 +1,61 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.12";
+      identifier = { name = "jsonpath"; version = "0.3.0.0"; };
+      license = "BSD-3-Clause";
+      copyright = "Akshay Mankar";
+      maintainer = "itsakshaymankar@gmail.com";
+      author = "Akshay Mankar";
+      homepage = "https://github.com/akshaymankar/jsonpath-hs#readme";
+      url = "";
+      synopsis = "Library to parse and execute JSONPath";
+      description = "Please see the README on GitHub at <https://github.com/akshaymankar/jsonpath-hs#readme>";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."aeson" or (errorHandler.buildDepError "aeson"))
+          (hsPkgs."megaparsec" or (errorHandler.buildDepError "megaparsec"))
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."scientific" or (errorHandler.buildDepError "scientific"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."unordered-containers" or (errorHandler.buildDepError "unordered-containers"))
+          (hsPkgs."vector" or (errorHandler.buildDepError "vector"))
+          ];
+        buildable = true;
+        };
+      tests = {
+        "jsonpath-test" = {
+          depends = [
+            (hsPkgs."aeson" or (errorHandler.buildDepError "aeson"))
+            (hsPkgs."aeson-casing" or (errorHandler.buildDepError "aeson-casing"))
+            (hsPkgs."megaparsec" or (errorHandler.buildDepError "megaparsec"))
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+            (hsPkgs."file-embed" or (errorHandler.buildDepError "file-embed"))
+            (hsPkgs."hspec" or (errorHandler.buildDepError "hspec"))
+            (hsPkgs."hspec-megaparsec" or (errorHandler.buildDepError "hspec-megaparsec"))
+            (hsPkgs."jsonpath" or (errorHandler.buildDepError "jsonpath"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."unordered-containers" or (errorHandler.buildDepError "unordered-containers"))
+            (hsPkgs."vector" or (errorHandler.buildDepError "vector"))
+            ];
+          build-tools = [
+            (hsPkgs.buildPackages.hspec-discover.components.exes.hspec-discover or (pkgs.buildPackages.hspec-discover or (errorHandler.buildToolDepError "hspec-discover:hspec-discover")))
+            ];
+          buildable = true;
+          };
+        };
+      };
+    }
