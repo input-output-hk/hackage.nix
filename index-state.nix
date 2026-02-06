3f53777e5497658fd6a56fe8e3c2a3d6ea07ac0d
IOHK <devops+nix-tools@iohk.io>
2026-02-06 11:50:33 +0000

    Update Hackage index state to

--- index-state.nix
+++ index-state.nix
@@ -1,3 +1,3 @@
 {
-  "2026-02-06T11:30:40Z" = "ec714fa9dac1bef0c0eeb726039f240afcbf2dd47bd0bad0325e27ebfec22d79";
+  "2026-02-06T11:45:03Z" = "bcca1396671f524add82ff7b5faafd0e98bb8b0e4df6debf9d3082cd3f225ab7";
 }
